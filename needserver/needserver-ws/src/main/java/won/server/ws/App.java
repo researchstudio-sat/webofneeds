45210519471dc9af795fb93c42f3c72236492d46
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2012-10-10 10:26:46 +0200

    removed maven generated classes

---
+++ needserver/needserver-ws/src/main/java/won/server/ws/App.java
@@ -0,0 +1,13 @@
+package won.server.ws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
