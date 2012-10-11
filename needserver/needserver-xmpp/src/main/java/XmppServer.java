b6845ccfc8f7bb3522ec8f2102f8d257b7e8afe3
Gabriel <gabriel.grill@researchstudio.at>
2012-10-11 14:29:09 +0200

    vysper added

---
+++ needserver/needserver-xmpp/src/main/java/XmppServer.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ggrill
+ * Date: 11.10.12
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class XmppServer {
+    public static void main() {
+
+    }
+}
