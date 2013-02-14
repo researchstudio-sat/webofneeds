9838724f083babc5b4ed8a714e06548430fcc44f
gabriel <gab.grill@gmail.com>
2013-02-14 17:05:17 +0100

    pom.xml fixed

---
+++ webofneeds/won-core/src/main/java/won/protocol/ws/MatcherProtocolNeedWebServiceEndpoint.java
@@ -0,0 +1,53 @@
+package won.protocol.ws;
+
+import won.protocol.exception.IllegalMessageForNeedStateException;
+import won.protocol.exception.NoSuchConnectionException;
+import won.protocol.exception.NoSuchNeedException;
+import won.protocol.matcher.MatcherProtocolNeedService;
+import won.protocol.model.Connection;
+import won.protocol.model.Need;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import java.net.URI;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gabriel
+ * Date: 14.02.13
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+@WebService(serviceName = "matcherProtocol", targetNamespace = "http://www.webofneeds.org/protocol/matcher/soap/1.0/")
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface MatcherProtocolNeedWebServiceEndpoint {
+    @WebMethod
+    void hint(
+            @WebParam(name = "needURI") URI needURI,
+            @WebParam(name = "otherNeedURI") URI otherNeedURI,
+            @WebParam(name = "score") double score,
+            @WebParam(name = "originatorURI") URI originatorURI) throws NoSuchNeedException, IllegalMessageForNeedStateException;
+
+    @WebMethod
+    String readConnectionContent(@WebParam(name = "connectionURI") URI connectionURI) throws NoSuchConnectionException;
+
+    @WebMethod
+    Connection readConnection(@WebParam(name = "connectionURI") URI connectionURI) throws NoSuchConnectionException;
+
+    @WebMethod
+    String readNeedContent(@WebParam(name = "needURI") URI needURI) throws NoSuchNeedException;
+
+    @WebMethod
+    Need readNeed(@WebParam(name = "needURI") URI needURI) throws NoSuchNeedException;
+
+    @WebMethod
+    URI[] listConnectionURIs(@WebParam(name = "needURI") URI needURI) throws NoSuchNeedException;
+
+    @WebMethod
+    URI[] listNeedURIs();
+
+    @WebMethod(exclude = true)
+    void setMatcherProtocolNeedService(MatcherProtocolNeedService matcherProtocolNeedService);
+}
