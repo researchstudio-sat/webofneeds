e9bda64e4906c49e8091526112f5f457c268d6bb
Alan Tus <alan.tus@gmail.com>
2013-04-24 13:46:21 +0200

    refactoring DateTimeUtils
    it's not pretty but now it should be thread safe

--- webofneeds/won-core/src/main/java/won/protocol/util/DateTimeUtils.java
+++ webofneeds/won-core/src/main/java/won/protocol/util/DateTimeUtils.java
@@ -11,15 +11,13 @@ public class DateTimeUtils
 {
   private static SimpleDateFormat sdf;
 
-  static {
-    sdf = new SimpleDateFormat("YYYY-MM-DD'T'hh:mm:ss.sss");
-  }
-
   public static String format(Date date) {
+    sdf = new SimpleDateFormat("YYYY-MM-DD'T'hh:mm:ss.sss");
     return sdf.format(date);
   }
 
   public  static String getCurrentDateTimeStamp() {
+    sdf = new SimpleDateFormat("YYYY-MM-DD'T'hh:mm:ss.sss");
     return sdf.format(new Date());
   }
 }
