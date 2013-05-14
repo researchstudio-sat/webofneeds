75e6155ee63d5fb6e4baeab375665e47c4e7b98e
Alan Tus <alan.tus@gmail.com>
2013-05-14 21:46:32 +0200

    code cleanup

--- webofneeds/won-core/src/main/java/won/protocol/ws/MatcherProtocolNeedWebServiceEndpoint.java
+++ webofneeds/won-core/src/main/java/won/protocol/ws/MatcherProtocolNeedWebServiceEndpoint.java
@@ -1,11 +1,8 @@
 package won.protocol.ws;
 
 import won.protocol.exception.IllegalMessageForNeedStateException;
-import won.protocol.exception.NoSuchConnectionException;
 import won.protocol.exception.NoSuchNeedException;
 import won.protocol.matcher.MatcherProtocolNeedService;
-import won.protocol.model.Connection;
-import won.protocol.model.Need;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -14,22 +11,21 @@ import javax.jws.soap.SOAPBinding;
 import java.net.URI;
 
 /**
- * Created with IntelliJ IDEA.
  * User: gabriel
  * Date: 14.02.13
  * Time: 14:19
- * To change this template use File | Settings | File Templates.
  */
 @WebService(serviceName = "matcherProtocol", targetNamespace = "http://www.webofneeds.org/protocol/matcher/soap/1.0/")
 @SOAPBinding(style = SOAPBinding.Style.RPC)
-public interface MatcherProtocolNeedWebServiceEndpoint {
-    @WebMethod
-    void hint(
-            @WebParam(name = "needURI") URI needURI,
-            @WebParam(name = "otherNeedURI") URI otherNeedURI,
-            @WebParam(name = "score") double score,
-            @WebParam(name = "originatorURI") URI originatorURI) throws NoSuchNeedException, IllegalMessageForNeedStateException;
+public interface MatcherProtocolNeedWebServiceEndpoint
+{
+  @WebMethod
+  void hint(
+      @WebParam(name = "needURI") URI needURI,
+      @WebParam(name = "otherNeedURI") URI otherNeedURI,
+      @WebParam(name = "score") double score,
+      @WebParam(name = "originatorURI") URI originatorURI) throws NoSuchNeedException, IllegalMessageForNeedStateException;
 
-    @WebMethod(exclude = true)
-    void setMatcherProtocolNeedService(MatcherProtocolNeedService matcherProtocolNeedService);
+  @WebMethod(exclude = true)
+  void setMatcherProtocolNeedService(MatcherProtocolNeedService matcherProtocolNeedService);
 }
