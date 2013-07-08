21d4095d536701f42762456d6c19fecf3599ad4b
Alan Tus <alan.tus@gmail.com>
2013-07-08 19:24:52 +0200

    prepared hint code

--- webofneeds/won-matcher-solr/src/main/java/won/matcher/solr/MatcherRequestProcessor.java
+++ webofneeds/won-matcher-solr/src/main/java/won/matcher/solr/MatcherRequestProcessor.java
@@ -1,5 +1,8 @@
 package won.matcher.solr;
 
+import com.sun.corba.se.spi.servicecontext.SendingContextServiceContext;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.*;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.core.CoreContainer;
@@ -11,9 +14,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import won.matcher.protocol.impl.MatcherProtocolNeedServiceClient;
 import won.matcher.query.*;
+import won.protocol.exception.IllegalMessageForNeedStateException;
+import won.protocol.exception.NoSuchNeedException;
 import won.protocol.solr.SolrFields;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -112,7 +118,27 @@ public class MatcherRequestProcessor extends UpdateRequestProcessor
   {
     super.finish();
     logger.info("Matcher finish called.");
-
     //cleanup or send prepared matches
+
+//    try {
+    URI originator = URI.create("http://LDSpiderMatcher.webofneeds");
+
+    IndexReader indexReader = solrCore.getSearcher().get().getIndexReader();
+
+    for (String match : knownMatches.keySet()) {
+      URI fromDoc = URI.create(match);
+      for (ScoreDoc score : knownMatches.get(match)) {
+        Document document = indexReader.document(score.doc);
+        URI toDoc = URI.create(document.get(SolrFields.URL));
+        log.info(String.format("Sending hint %s -> %s :: %3.2f",fromDoc,toDoc,score.score));
+        //client.hint(fromDoc, toDoc, score.score, originator, null);
+      }
+    }
+//    } catch (NoSuchNeedException e) {
+//      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//    } catch (IllegalMessageForNeedStateException e) {
+//      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//    }
+
   }
 }
