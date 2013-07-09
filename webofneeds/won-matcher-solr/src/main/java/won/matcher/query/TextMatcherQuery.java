2cad28d2c2996c3a643015571294b398be413b32
Alan Tus <alan.tus@gmail.com>
2013-07-09 14:26:53 +0200

    playing with the text matcher.. still not working

--- webofneeds/won-matcher-solr/src/main/java/won/matcher/query/TextMatcherQuery.java
+++ webofneeds/won-matcher-solr/src/main/java/won/matcher/query/TextMatcherQuery.java
@@ -6,14 +6,9 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.similar.MoreLikeThis;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.search.SolrIndexSearcher;
-import org.hibernate.type.ListType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * User: gabriel
@@ -38,13 +33,14 @@ public class TextMatcherQuery extends AbstractQuery
 
     mlt.setMinDocFreq(1);
     mlt.setMinTermFreq(1);
-    mlt.setFieldNames(fields);
-
+    mlt.setMaxDocFreqPct(100);
+    mlt.setMinTermFreq(1);
+    //mlt.setFieldNames(fields);
 
     BooleanQuery booleanQuery = new BooleanQuery();
     for (String field : fields)
       if (inputDocument.getField(field) != null) {
-        Query query = mlt.like(new StringReader(inputDocument.getFieldValue(field).toString()),field);
+        Query query = mlt.like(new StringReader(inputDocument.getFieldValue(field).toString()), field);
         BooleanClause clause = new BooleanClause(query, BooleanClause.Occur.SHOULD);
         booleanQuery.add(clause);
       }
