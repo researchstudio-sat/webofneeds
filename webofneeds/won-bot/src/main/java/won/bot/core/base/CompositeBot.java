21609dde2b1247dcc45989259d7f77cbc560c1cc
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2014-01-21 17:17:42 +0100

    added need creator bot

---
+++ webofneeds/won-bot/src/main/java/won/bot/core/base/CompositeBot.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2012  Research Studios Austria Forschungsges.m.b.H.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package won.bot.core.base;
+
+import won.bot.core.Bot;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Bot that takes other (delegate) bots when it is created and provides the logic to decide when to delegate to which.
+ * Always delegates initialize and shutdown.
+ */
+public abstract class CompositeBot extends BasicServiceBot
+{
+  Map<String, BasicServiceBot> delegates = null;
+
+
+  @Override
+  public void onConnectFromOtherNeed()
+  {
+    super.onConnectFromOtherNeed();
+  }
+
+  @Override
+  public void onOpenFromOtherNeed()
+  {
+    super.onOpenFromOtherNeed();
+  }
+
+  @Override
+  public void onCloseFromOtherNeed()
+  {
+    super.onCloseFromOtherNeed();
+  }
+
+  @Override
+  public void onHintFromMatcher()
+  {
+    super.onHintFromMatcher();
+  }
+
+  @Override
+  public void onMessageFromOtherNeed()
+  {
+    super.onMessageFromOtherNeed();
+  }
+
+  @Override
+  public void act() throws Exception
+  {
+    super.act();
+  }
+
+  @Override
+  protected final void doInitialize()
+  {
+    if (this.delegates != null){
+      for (Bot delegate : this.delegates.values()){
+        if (delegate.getLifecyclePhase().isDown()) try {
+          if (delegate instanceof BaseBot){
+            ((BaseBot)delegate).setBotContext(getBotContext());
+          }
+          delegate.initialize();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    doInitializeCustom();
+  }
+
+  /**
+   * Override this method to do free resources during shutdown. Can't be overriden. If you
+   * have custom initialization work to do, override doInitializeCustom.
+   */
+  protected void doShutdownCustom() {}
+
+
+  /**
+   * Delegates shutdown to all delegate bots. Can't be overriden. If you
+   * have custom shutdown work to do, override doShutdownCustom.
+   */
+  @Override
+  protected final void doShutdown()
+  {
+    if (this.delegates != null){
+      for (Bot delegate : this.delegates.values()){
+        if (delegate.getLifecyclePhase().isActive()) try {
+          delegate.shutdown();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    doShutdownCustom();
+  }
+
+  /**
+   * Override this method to do initialization work.
+   */
+  protected void doInitializeCustom() {};
+
+  protected Map<String, BasicServiceBot> getDelegates()
+  {
+    return delegates;
+  }
+
+  public void setDelegates(final Map<String, BasicServiceBot> delegates)
+  {
+    this.delegates = Collections.unmodifiableMap(delegates);
+    if (this.delegates != null){
+      for (BasicServiceBot delegate : this.delegates.values()){
+        //let the bots share the same context
+        delegate.setBotContext(this.getBotContext());
+      }
+    }
+  }
+}
