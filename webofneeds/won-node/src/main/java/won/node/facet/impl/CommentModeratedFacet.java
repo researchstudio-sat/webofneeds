64ffecd956619a3da9faa909f9f3e532aa73efc0
Gabriel Grill <gab.grill@gmail.com>
2014-01-30 17:30:08 +0100

    merge and comment facet without linked data change

---
+++ webofneeds/won-node/src/main/java/won/node/facet/impl/CommentModeratedFacet.java
@@ -0,0 +1,25 @@
+package won.node.facet.impl;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import won.protocol.exception.ConnectionAlreadyExistsException;
+import won.protocol.exception.IllegalMessageForNeedStateException;
+import won.protocol.exception.NoSuchNeedException;
+import won.protocol.model.Connection;
+import won.protocol.model.FacetType;
+
+/**
+ * User: gabriel
+ * Date: 17/01/14
+ */
+public class CommentModeratedFacet extends Facet {
+  @Override
+  public FacetType getFacetType() {
+    return null;
+  }
+
+  @Override
+  public void connectFromOwner(Connection con, Model content) throws NoSuchNeedException, IllegalMessageForNeedStateException, ConnectionAlreadyExistsException {
+    super.connectFromOwner(con, content);
+    /* when connected change linked data*/
+  }
+}
---
+++ webofneeds/won-node/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gabriel
+  Date: 28/10/13
+  Time: 12:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
