53d9b113f21f893f43fc740fe3a6dee1208aae1d
syim <liam21c@gmail.com>
2014-07-21 20:08:33 +0200

    REST API Refactoring
    - RestNeedController, RestUserController

--- webofneeds/won-owner-webapp/src/main/webapp/app/app.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/app.js
@@ -108,21 +108,22 @@ app.directive('header', function(){
 });
 app.run(function($httpBackend){
 
-        $httpBackend.whenGET('/owner/rest/need/\d+').respond('test');
+        //$httpBackend.whenGET('/owner/rest/need/\d+').respond('test');
 
-        $httpBackend.whenPOST('/owner/rest/need/create').respond(function(method, url, data){
+       /* $httpBackend.whenPOST('/owner/rest/needs').respond(function(method, url, data){
 
             return [200, 'text',{}];
-        });
+        });   */
        /* $httpBackend.whenPOST('/owner/rest/need/create/saveDraft').respond(function(method, url, data){
 
             return [200, 'text',{}];
         });*/
-
-        $httpBackend.whenPOST('/owner/rest/need/create/saveDraft').passThrough();
+        $httpBackend.whenPOST('/owner/rest/needs/').passThrough();
+        $httpBackend.whenPOST('/owner/rest/needs/drafts').passThrough();
+       // $httpBackend.whenPOST('/owner/rest/need/create/saveDraft').passThrough();
         $httpBackend.whenGET(/.*/).passThrough();
         $httpBackend.whenPOST(/.*/).passThrough();
-        $httpBackend.whenPOST(/\/owner\/rest\/user\/.*/).passThrough();
+       // $httpBackend.whenPOST(/\/owner\/rest\/user\/.*/).passThrough();
         $httpBackend.whenPOST('/owner').passThrough();
         $httpBackend.whenGET('/').passThrough();
         $httpBackend.whenGET('/signin').passThrough();
