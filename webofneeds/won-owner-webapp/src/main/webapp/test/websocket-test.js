17b1ac26de17bb35447bffb55c431ab2ad42fbe7
Fabian Salcher <f.salcher@gmx.net>
2014-08-06 18:17:14 +0200

    added mega-minimalistic example of client side webSocket in JS

---
+++ webofneeds/won-owner-webapp/src/main/webapp/test/websocket-test.js
@@ -0,0 +1,44 @@
+/**
+ * Created by fsalcher on 06.08.2014.
+ */
+
+
+
+
+var socket;
+
+
+var connect = function () {
+
+    var url = document.getElementById("targetAddress").value;
+
+    socket = new WebSocket(url);
+
+    socket.onopen = function () {
+        writeOutput("connection has been established!");
+    }
+
+    socket.onmessage = function (event) {
+        writeOutput('Received data: ' + event.data);
+    };
+
+    socket.onclose = function () {
+        writeOutput('Lost connection!');
+    };
+};
+
+var sendMessage = function () {
+
+    var message = document.getElementById("messageText").value;
+
+    socket.send(message);
+}
+
+var closeConnection = function () {
+    socket.close;
+}
+
+var writeOutput = function (output) {
+    var outputDIV = document.getElementById("output");
+    outputDIV.value = outputDIV.innerHTML + output + "<br />";
+}
