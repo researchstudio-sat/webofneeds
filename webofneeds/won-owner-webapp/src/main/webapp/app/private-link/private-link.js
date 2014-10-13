32507d6a559850690a386645d52095f72df85ed7
yanapanchenko <yana_panchenko@yahoo.com>
2014-10-13 18:04:34 +0200

    display date of the last event for the messages, order by that date and add sorting by date and title
    use last event's timestamp as the date to display
    order by the last event's timestamp by default - latest on top
    enable possibility to sort by title and by date - smart table implementation is replaced by pure angularjs as was done in postbox page

--- webofneeds/won-owner-webapp/src/main/webapp/app/private-link/private-link.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/private-link/private-link.js
@@ -29,11 +29,17 @@ angular.module('won.owner')
         console.log("events changed! now have " + newValue.length + " events!" + " had: " + oldValue.length +"...");
     });
 
-    // copy the references from the real data holder, used in message table for displaying
-    // and is required by smart table (st-) for sorting the items that can change
-    // see http://lorenzofox3.github.io/smart-table-website/
-    $scope.displayedMessages = [].concat($scope.lastEventOfEachConnectionOfCurrentNeed);
+    $scope.sortedField = 'event.hasTimestamp';
+    $scope.reversedSort = true;
 
+    $scope.setSortParams = function(fieldName) {
+        if ($scope.sortedField == fieldName) {
+            $scope.reversedSort = !$scope.reversedSort;
+        } else {
+            $scope.reversedSort = false;
+            $scope.sortedField = fieldName;
+        }
+    }
 
     $scope.currentEventType = [
      won.WONMSG.connectionMessage,
