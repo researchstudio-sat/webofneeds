32c79802a9b19827ec430ec7be5ea3218f8c89e5
moru <kevin.singer@student.tuwien.ac.at>
2015-02-05 18:13:01 +0100

    Moved location form to a cstm directive.

--- webofneeds/won-owner-webapp/src/main/webapp/app/create-need/create-need.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/create-need/create-need.js
@@ -23,13 +23,9 @@ angular.module('won.owner').controller('CreateNeedCtrlNew', function
     , $routeParams
     , needService
     , applicationStateService
-    , mapService
     , userService
     , utilService
     , wonService
-    , osmService
-    //, uiKeyup
-    //, uiEvent
     ) {
     $scope.currentStep = $routeParams.step;
     $scope.selectedType = $routeParams.selectedType;
@@ -181,115 +177,6 @@ angular.module('won.owner').controller('CreateNeedCtrlNew', function
             return won.WON.BasicNeedTypeCritique;
         }
     }
-
-    // <leaflet-map> -----------------------------------
-    var map;
-    $scope.mapInit = function () {
-        // -------- snippet from leafletjs.com ----------
-        // create a map in the "map" div, set the view to a given place and zoom
-        map = L.map('leaflet-canvas');
-        map.fitWorld().zoomIn(); // zoom=0 isn't rectangular (-> gray letterboxing) -> zoomIn to fix this
-
-        // add an OpenStreetMap tile layer with attributions
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-    };
-    $scope.mapInit();
-
-    map.removeCstmMarker = function() {
-        if(map.cstmMarker != undefined) {
-            map.removeLayer(map.cstmMarker); // remove the previous marker //TODO does this delete the popup as well?
-        }
-    };
-
-    $scope.submitAddressQuery = function (address) {
-         //clear old results
-        $scope.addressSearchResults = undefined;
-        $scope.selectedAddress = {}
-        map.removeCstmMarker();
-        //map.fitWorld().zoomIn(); // TODO a good idea ux-wise? or just stay at the previous location?
-
-        if(address && address !== "" && address !== {}) {
-            $("#locationForm").addClass('open'); //show drop-down //TODO hackish
-            osmService.matchingLocations(address).then(function(resp){
-                $scope.addressSearchResults = resp;
-            }, function failed(){
-                $log.error("Address resolution failed.");
-            });
-        }
-    }
-
-    $scope.selectedAddress = {};
-    $scope.selectAddress = function (address) {
-        $("#locationForm").removeClass('open'); //hide drop-down //TODO hackish
-        $scope.selectedAddress = address;
-        $scope.setMapLocation(address.lat, address.lon, address.display_name);
-        $scope.addressText = address.display_name;
-    };
-    $scope.setMapLocation = function (lat, lon, adr) { //TODO not in $scope but only usable here in link?
-        map.removeCstmMarker();
-
-        map.cstmMarker = L.marker([lat, lon]);
-        map.cstmMarker.addTo(map).bindPopup(adr);
-
-        //TODO base zoomlevel (L.latLng(lat, lon, alt(!))) on size of the selected area
-        map.setView([lat, lon], 13);
-
-        map.cstmMarker.openPopup();
-    };
-    //TODO start searching as soon as the user pauses/presses down, (followed by: select choice, press enter)
-    //TODO enter selects the first entry? shows an error popup and asks to select a correct location (same on focus loss)? enter jumps to first line of dropdown?
-    $scope.onArrowDownInSearchField = function (event) { //TODO DELETEME
-        // TODO only go into list if there are search results
-        console.log("In onArrowDownInSearchField. Event: " + JSON.stringify(event));
-        $scope.locationResultsVisible = true; //doesn't work(?)
-        //$('#locationDropDownToggle').dropdown();
-        console.log($('#locationDropDownToggle'));
-        console.log($('.dropDownToggle'));
-        $("#locationForm").addClass('open'); //TODO not very stable (e.g. if class name changes)
-    };
-    $scope.onArrowUpInSearchField = function (event) { //TODO DELETEME
-        console.log("In onArrowUpInSearchField. Event: " + JSON.stringify(event));
-        $("#locationForm").removeClass('open'); //TODO not very stable (e.g. if class name changes)
-    };
-    $scope.isopen = true;
-    $scope.toggleDropdown = function($event) {
-        console.log("in toggleDropdown. isopen = " + $scope.isopen);
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.isopen = !$scope.isopen;
-    };
-    $scope.onLocationDropdownToggle = function() {
-        console.log("Toggled dropdown.");
-    };
-
-    (function() { // <anonymous wrapper>
-        //setup before functions
-        var typingTimer;                //timer identifier
-        var doneTypingInterval = 1300;  //time in ms
-
-        //on keyup, start the countdown
-        $('#addressTextField').keyup(function(){
-            clearTimeout(typingTimer);
-            typingTimer = setTimeout(doneTyping, doneTypingInterval);
-        });
-
-        //on keydown, clear the countdown
-        $('#addressTextField').keydown(function(){
-            clearTimeout(typingTimer);
-        });
-
-        //user is "finished typing," do something
-        function doneTyping () {
-            var address = $('#addressTextField').val();
-            //alert(encodeURIComponent(address));
-            $scope.submitAddressQuery(address);
-        }
-    })(); // </anonymous wrapper>
-    // </leaflet-map> --------------------------
-
-
 
     $scope.showPublic = function (num) {
         if (num == $scope.currentStep) {
@@ -779,7 +666,7 @@ angular.module('won.owner').directive('wonGallery', function factory() {
     };
 });
 angular.module('won.owner').controller('AdditionalInfoCtrl',
-    function ($scope, $location, $http, $log, needService, mapService, userService) {
+    function ($scope, $location, $http, $log, needService, userService) {
         $scope.imageInputFieldCollapsed = true;
         $scope.locationInputFieldCollapsed = true;
         $scope.timeInputFieldCollapsed = true;
