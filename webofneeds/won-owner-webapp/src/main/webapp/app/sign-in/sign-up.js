f2b595015e66b82ca941cf600b911528cf13f4a8
moru <kevin.singer@student.tuwien.ac.at>
2015-03-04 19:57:05 +0100

    Sign-up spinner.

--- webofneeds/won-owner-webapp/src/main/webapp/app/sign-in/sign-up.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/sign-in/sign-up.js
@@ -15,9 +15,9 @@ angular.module('won.owner').controller('SignUpModalInstanceCtrl',
         $scope.password = '';
         $scope.passwordAgain = '';
         $scope.error = '';
+        $scope.processing = false;
 
         $scope.signUp = function (name, pw, pwRepeated) {
-
             if ($scope.signUpForm.$valid &&
                 pw.length > 0 &&
                 pw === pwRepeated) {
@@ -29,6 +29,7 @@ angular.module('won.owner').controller('SignUpModalInstanceCtrl',
                 }
 
                 //TODO spinning wheel
+                $scope.processing = true;
                 userService.registerUser(credentials).then(function onRegisterResponse(response) {
                         if (response.status == "OK") {
                             $scope.error = '';
@@ -36,14 +37,18 @@ angular.module('won.owner').controller('SignUpModalInstanceCtrl',
                             //angular.resetForm($scope, "registerForm"); //<-- would force retyping == bad ux
                             userService.logInAndSetUpApplicationState(credentials).then(
                                 function success(){
+                                    $scope.processing = false;
                                     $modalInstance.close(); //close() could also take params that would be returned
                                 },
                                 function failure(){
+                                    $scope.processing = false;
                                     $scope.error = response.message;
                                 });
                         } else if (response.status == "ERROR") {
+                            $scope.processing = false;
                             $scope.error = response.message;
                         } else {
+                            $scope.processing = false;
                             $log.debug(response.message);
                         }
                     }
