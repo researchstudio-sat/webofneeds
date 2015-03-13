28a68bdac45b2132ce9fd453c63598a36e7b7920
syim <liam21c@gmail.com>
2015-03-13 18:31:13 +0100

    Component scan autowiring test package

---
+++ webofneeds/component-scan/src/main/java/sat/SAT.java
@@ -0,0 +1,9 @@
+package sat;
+
+/**
+ * User: syim
+ * Date: 13.03.2015
+ */
+public class SAT
+{
+}
---
+++ webofneeds/component-scan/src/main/java/scan/World.java
@@ -0,0 +1,9 @@
+package scan;
+
+/**
+ * User: syim
+ * Date: 13.03.2015
+ */
+public class World
+{
+}
