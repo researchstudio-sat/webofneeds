6c39c34647f7f619637bc2fdfd9a240ccf5850f2
yanapanchenko <yana_panchenko@yahoo.com>
2015-04-14 12:02:14 +0200

    spring beans config bugfixes:
    - fix class name
    - add autowired annotation

--- webofneeds/won-owner/src/main/java/won/owner/protocol/message/base/MessageExtractingOwnerCallbackAdapter.java
+++ webofneeds/won-owner/src/main/java/won/owner/protocol/message/base/MessageExtractingOwnerCallbackAdapter.java
@@ -16,6 +16,8 @@
 
 package won.owner.protocol.message.base;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import won.owner.protocol.message.OwnerCallback;
 import won.protocol.message.WonMessage;
 import won.protocol.model.Connection;
@@ -66,7 +68,8 @@ public class MessageExtractingOwnerCallbackAdapter extends OwnerCallbackAdapter
     return con;
   }
 
-
+  @Autowired(required = false)
+  @Qualifier("default")
   public void setAdaptee(OwnerCallback adaptee) {
     super.setAdaptee(adaptee);
   }
