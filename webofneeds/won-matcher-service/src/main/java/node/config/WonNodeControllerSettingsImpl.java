14746aa30d7b1ffe0c7a5af8fa26376f59bf3d8f
hfriedrich <heiko.friedrich@researchstudio.at>
2015-05-26 18:02:39 +0200

    add basic error handling to won node controller

--- webofneeds/won-matcher-service/src/main/java/node/config/WonNodeControllerSettingsImpl.java
+++ webofneeds/won-matcher-service/src/main/java/node/config/WonNodeControllerSettingsImpl.java
@@ -2,8 +2,11 @@ package node.config;
 
 import akka.actor.Extension;
 import com.typesafe.config.Config;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Settings configuration class of the won node controller
@@ -17,11 +20,14 @@ public class WonNodeControllerSettingsImpl implements Extension
   public final String SPARQL_ENDPOINT;
   public final List<String> WON_NODES_CRAWL;
   public final List<String> WON_NODES_SKIP;
+  public final FiniteDuration WON_NODE_LIFE_CHECK_DURATION;
 
   public WonNodeControllerSettingsImpl(Config config) {
 
     SPARQL_ENDPOINT = config.getString("wonNodeController.sparqlEndpoint");
     WON_NODES_CRAWL = config.getStringList("wonNodeController.wonNode.crawl");
     WON_NODES_SKIP = config.getStringList("wonNodeController.wonNode.skip");
+    WON_NODE_LIFE_CHECK_DURATION = Duration.create(config.getDuration(
+      "wonNodeController.wonNode.lifeCheckDuration", TimeUnit.MILLISECONDS), TimeUnit.MILLISECONDS);
   }
 }
