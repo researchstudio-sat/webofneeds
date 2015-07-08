ff0e3e010baa15240354f43c71dcfc009d0a6737
moru <kevin.singer@student.tuwien.ac.at>
2015-07-08 19:50:34 +0200

    A bit hacky solution to spreading the items.

--- webofneeds/won-owner-webapp/src/main/webapp/app/wonAppTag.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/wonAppTag.js
@@ -2,26 +2,33 @@
  *
  * Created by ksinger on 07.07.2015.
  */
-
-
+;
 export default function wonAppTag() {
     let template = '\
         <header>\
             <nav class="topnav">\
-                <ul class="topnav__list">\
-                    <li class="topnav__list__title">[Web of Needs]</li>\
-                    <li>New Need</li>\
-                    <li>Groups</li>\
-                    <li>Username</li>\
+                <ul class="topnav__list spring-row">\
+                    <li class="topnav__list__title">\
+                        <a href="#">[Logo] Web of Needs</a>\
+                    </li>\
+                    <li class="spring"></li>\
+                    <li class="">\
+                        <a href="#">New Need</a>\
+                    </li>\
+                    <li class="spring"></li>\
+                    <li class="">\
+                        <a href="#">Groups</a>\
+                    </li>\
+                    <li><a href="#">Username</a></li>\
                 </ul>\
             </nav>\
             <nav ng-cloak ng-hide="{{true}}">\
                 <ul>\
-                    <li>Feed</li>\
-                    <li>Posts</li>\
-                    <li>Incoming Requests</li>\
-                    <li>Matches</li>\
-                    <li>[Search]</li>\
+                    <li><a href="#">Feed</a></li>\
+                    <li><a href="#">Posts</a></li>\
+                    <li><a href="#">Incoming Requests</a></li>\
+                    <li><a href="#">Matches</a></li>\
+                    <li><a href="#">[Search]</a></li>\
                 </ul>\
             </nav>\
         </header>\
