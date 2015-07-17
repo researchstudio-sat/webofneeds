8212f7904804a79ff6e825971712cfb88d78d5a9
moru <kevin.singer@student.tuwien.ac.at>
2015-07-17 16:13:20 +0200

    Build tasks for svg-sprite.

--- webofneeds/won-owner-webapp/src/main/webapp/gulpfile.js
+++ webofneeds/won-owner-webapp/src/main/webapp/gulpfile.js
@@ -5,6 +5,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
+var svgSprite = require('gulp-svg-sprite');
 
 //gulp.task('sass', function () {
 //    gulp.src('./sass/**/*.scss')
@@ -16,7 +17,13 @@ var autoprefixer = require('gulp-autoprefixer');
 //    gulp.watch('./sass/**/*.scss', ['sass']);
 //});
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'iconsprite']);
+gulp.task('watch', function() {
+    gulp.watch('./style/**/*.scss', ['sass']);
+    gulp.watch('./images/won-icons/**/*.svg', ['iconsprite']);
+});
+
+
 
 gulp.task('sass', function(done) {
     var generatedStyleFolder =  './generated/';
@@ -37,6 +44,34 @@ gulp.task('sass', function(done) {
         .on('end', done);
 });
 
-gulp.task('watch', function() {
-    gulp.watch('./style/**/*.scss', ['sass']);
+
+
+var svgSpriteConf = {
+    shape : {
+        dimension : {
+            //default would be just 2
+            precision: 4
+        }
+    },
+    mode : {
+        view : {
+            //example: true //generates html with usage-demo
+
+            //default would be './view'
+            dest: '.',
+
+            //instead of default 'svg/sprite.view.svg'
+            sprite: 'icon-sprite.svg',
+
+            // don't add a hash to the name (it's intended to
+            // circumvent cache staleness, but makes referencing the sprite hard)
+            bust: false
+        }
+    }
+};
+gulp.task('iconsprite', function(done) {
+    gulp.src('**/*.svg', {cwd: 'images/won-icons'})
+        .pipe(svgSprite(svgSpriteConf))
+        .pipe(gulp.dest('generated'))
+        .on('end', done);
 });
