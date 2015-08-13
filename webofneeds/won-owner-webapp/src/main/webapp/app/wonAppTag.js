686d3dab21088dd776a90b2ea625cef2ae4ad9f1
moru <kevin.singer@student.tuwien.ac.at>
2015-08-13 16:57:38 +0200

    Working on the speech-bubble-triangle.

--- webofneeds/won-owner-webapp/src/main/webapp/app/wonAppTag.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/wonAppTag.js
@@ -38,34 +38,62 @@ export default function wonAppTag() {
                     </ul>
                 </div>
             </nav>
-            <nav ng-cloak ng-hide="{{false}}" class="mainTabs">
+            <nav ng-cloak ng-show="{{true}}" class="mainTabs">
                 <div class="mainTabs__left"></div>
                 <div class="mainTabs__center">
-                    <ul class="mainTabs__list">
-                        <li class="selectedTab"><a href="#">Feed</a></li>
+                    <ul class="tabs">
+                        <li class=""><a href="#">Feed</a></li>
                         <li><a href="#">Posts
-                            <span class="mainTabs__list__unread">5</span>
+                            <span class="tabs__unread">5</span>
                         </a></li>
-                        <li><a href="#">Incoming Requests
-                            <span class="mainTabs__list__unread">5</span>
+                        <li class="selectedTab"><a href="#">Incoming Requests
+                            <span class="tabs__unread">5</span>
                         </a></li>
                         <li><a href="#">Matches
-                            <span class="mainTabs__list__unread">18</span>
+                            <span class="tabs__unread">18</span>
                         </a></li>
                     </ul>
                 </div>
                 <div class="mainTabs__right">
                     <a href="#">
-                        <img src="generated/icon-sprite.svg#ico36_search" class="mainTabs__right__searchicon">
+                        <img src="generated/icon-sprite.svg#ico36_search_nomargin" class="mainTabs__right__searchicon">
                     </a>
+                </div>
+            </nav>
+            <nav ng-cloak ng-show="{{true}}" class="createNeedTitle">
+                <div class="cNT_left">x</div>
+                <div class="cNT__center">What is your need?</div>
+                <div class="cNT__right">
                 </div>
             </nav>
         </header>
         <section id="main" class="contentArea">
             <h1 ng-click="ctrl.fooFun()">Hello, from your lovely app-directive! Foo{{ctrl.foo}}! </h1>
+
+            <div class="speechbubbletest">Hello!</div>
+
+
+            <svg id="speech-bubble-triangle"
+                 class="speech-bubble-triangle"
+                 xmlns="http://www.w3.org/2000/svg"
+                 version="1.1"
+                 viewBox="0 0 16.063818 8.2639771"
+                 height="8.2639771"
+                 width="16.063818">
+              <g transform="translate(-0.08281408,-1044.1785)"
+                 id="layer1">
+                <path id="speech-bubble-triangle-path" class="speech-bubble-colors"
+                      d="m 0.00174102,1052.5466 8.11893778,-8.1188 8.1189362,8.1188"
+                      style="fill-opacity:1;fill-rule:evenodd;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
+              </g>
+            </svg>
+
         </section>
         <footer></footer>
         `;
+
+
+    // style="fill:#00a9ca;fill-opacity:1;fill-rule:evenodd;stroke:#dc0000;stroke-width:0.33678553px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
 
     console.log('updated this file at last');
 
