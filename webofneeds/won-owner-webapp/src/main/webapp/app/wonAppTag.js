8bd811af7516b78f8d65028cb8c2ba1efc8c1459
moru <kevin.singer@student.tuwien.ac.at>
2015-08-20 14:26:41 +0200

    MaxContentWidth.

--- webofneeds/won-owner-webapp/src/main/webapp/app/wonAppTag.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/wonAppTag.js
@@ -10,57 +10,61 @@ export default function wonAppTag() {
 
 
             <nav class="topnav">
-                <div class="topnav__left">
-                    <a href="#" class="topnav__button">
-                        <img src="generated/icon-sprite.svg#WON_ico_header" class="topnav__button__icon">
-                        <span class="topnav__page-title">Web of Needs</span>
-                    </a>
-                </div>
-                <div class="topnav__center">
-                    <a href="#" class="topnav__button">
-                        <img src="generated/icon-sprite.svg#ico36_plus" class="topnav__button__icon">
-                        <span class="topnav__button__caption">New Need</span>
-                    </a>
-                </div>
-                <div class="topnav__right">
-                    <ul class="topnav__list">
-                        <li class="">
-                            <a href="#" class="topnav__button">
-                                <span class="topnav__button__caption">Groups</span>
-                                <img src="generated/icon-sprite.svg#ico16_arrow_down" class="topnav__carret">
-                                <img src="generated/icon-sprite.svg#ico36_group" class="topnav__button__icon">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="topnav__button">
-                                <span class="topnav__button__caption">Username</span>
-                                <img src="generated/icon-sprite.svg#ico16_arrow_down" class="topnav__carret">
-                                <img src="generated/icon-sprite.svg#ico36_person" class="topnav__button__icon">
-                            </a>
-                        </li>
-                    </ul>
+                <div class="topnav__inner">
+                    <div class="topnav__inner__left">
+                        <a href="#" class="topnav__button">
+                            <img src="generated/icon-sprite.svg#WON_ico_header" class="topnav__button__icon">
+                            <span class="topnav__page-title">Web of Needs</span>
+                        </a>
+                    </div>
+                    <div class="topnav__inner__center">
+                        <a href="#" class="topnav__button">
+                            <img src="generated/icon-sprite.svg#ico36_plus" class="topnav__button__icon">
+                            <span class="topnav__button__caption">New Need</span>
+                        </a>
+                    </div>
+                    <div class="topnav__inner__right">
+                        <ul class="topnav__list">
+                            <li class="">
+                                <a href="#" class="topnav__button">
+                                    <span class="topnav__button__caption">Groups</span>
+                                    <img src="generated/icon-sprite.svg#ico16_arrow_down" class="topnav__carret">
+                                    <img src="generated/icon-sprite.svg#ico36_group" class="topnav__button__icon">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="topnav__button">
+                                    <span class="topnav__button__caption">Username</span>
+                                    <img src="generated/icon-sprite.svg#ico16_arrow_down" class="topnav__carret">
+                                    <img src="generated/icon-sprite.svg#ico36_person" class="topnav__button__icon">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
 
 
             <nav ng-cloak ng-show="{{true}}" class="main-tab-bar">
-                <div class="mtb__left"></div>
-                <ul class="mtb__center mtb__tabs">
-                    <li class=""><a href="#">Feed</a></li>
-                    <li><a href="#">Posts
-                        <span class="mtb__tabs__unread">5</span>
-                    </a></li>
-                    <li class="mtb__tabs__selected"><a href="#">Incoming Requests
-                        <span class="mtb__tabs__unread">5</span>
-                    </a></li>
-                    <li><a href="#">Matches
-                        <span class="mtb__tabs__unread">18</span>
-                    </a></li>
-                </ul>
-                <div class="mtb__right">
-                    <a href="#" class="mtb__searchbtn">
-                        <img src="generated/icon-sprite.svg#ico36_search_nomargin" class="mtb__icon">
-                    </a>
+                <div class="mtb__inner">
+                    <div class="mtb__inner__left"></div>
+                    <ul class="mtb__inner__center mtb__tabs">
+                        <li class=""><a href="#">Feed</a></li>
+                        <li><a href="#">Posts
+                            <span class="mtb__tabs__unread">5</span>
+                        </a></li>
+                        <li class="mtb__tabs__selected"><a href="#">Incoming Requests
+                            <span class="mtb__tabs__unread">5</span>
+                        </a></li>
+                        <li><a href="#">Matches
+                            <span class="mtb__tabs__unread">18</span>
+                        </a></li>
+                    </ul>
+                    <div class="mtb__inner__right">
+                        <a href="#" class="mtb__searchbtn">
+                            <img src="generated/icon-sprite.svg#ico36_search_nomargin" class="mtb__icon">
+                        </a>
+                    </div>
                 </div>
             </nav>
 
@@ -69,11 +73,13 @@ export default function wonAppTag() {
 
 
             <nav class="create-need-title" ng-cloak ng-show="{{true}}">
-                <div class="cnt__left">
-                    <img src="generated/icon-sprite.svg#ico27_close" class="cnt__icon">
+                <div class="cnt__inner">
+                    <div class="cnt__inner__left">
+                        <img src="generated/icon-sprite.svg#ico27_close" class="cnt__icon">
+                    </div>
+                    <h1 class="cnt__inner__center cnt__title">What is your need?</div>
+                    <div class="cnt__inner__right"></div>
                 </div>
-                <h1 class="cnt__center cnt__title">What is your need?</div>
-                <div class="cnt__right"></div>
             </nav>
 
 
@@ -82,37 +88,39 @@ export default function wonAppTag() {
 
 
             <nav class="need-tab-bar" ng-cloak ng-show="{{true}}">
-                <div class="ntb__left">
-                    <img src="generated/icon-sprite.svg#ico36_backarrow" class="ntb__icon">
-                    <img class="ntb__left__image" src="images/someNeedTitlePic.png"></img>
-                    <div class="ntb__left__titles">
-                        <h1 class="ntb__title">New flat, need furniture [TITLE]</h1>
-                        <div class="ntb__left__titles__type">I want to have something [TYPE]</div>
+                <div class="ntb__inner">
+                    <div class="ntb__inner__left">
+                        <img src="generated/icon-sprite.svg#ico36_backarrow" class="ntb__icon">
+                        <img class="ntb__inner__left__image" src="images/someNeedTitlePic.png"></img>
+                        <div class="ntb__inner__left__titles">
+                            <h1 class="ntb__title">New flat, need furniture [TITLE]</h1>
+                            <div class="ntb__inner__left__titles__type">I want to have something [TYPE]</div>
+                        </div>
                     </div>
-                </div>
-                <div class="ntb__center"></div>
-                <div class="ntb__right">
-                    <!--<div class="ntb__right__settingscontainer">-->
-                        <img class=" ntb__right__settings ntb__icon" src="generated/icon-sprite.svg#ico_settings">
-                    <!--</div>-->
-                    <ul class="ntb__tabs">
-                        <li><a href="#">
-                            Messages
-                            <span class="ntb__tabs__unread">5</span>
-                        </a></li>
-                        <li class="ntb__tabs__selected"><a href="#">
-                            Matches
-                            <span class="ntb__tabs__unread">5</span>
-                        </a></li>
-                        <li><a href="#">
-                             Requests
-                            <span class="ntb__tabs__unread">18</span>
-                        </a></li>
-                        <li><a href="#">
-                             Sent Requests
-                            <span class="ntb__tabs__unread">18</span>
-                        </a></li>
-                    </ul>
+                    <div class="ntb__inner__center"></div>
+                    <div class="ntb__inner__right">
+                        <!--<div class="ntb__inner__right__settingscontainer">-->
+                            <img class=" ntb__inner__right__settings ntb__icon" src="generated/icon-sprite.svg#ico_settings">
+                        <!--</div>-->
+                        <ul class="ntb__tabs">
+                            <li><a href="#">
+                                Messages
+                                <span class="ntb__tabs__unread">5</span>
+                            </a></li>
+                            <li class="ntb__tabs__selected"><a href="#">
+                                Matches
+                                <span class="ntb__tabs__unread">5</span>
+                            </a></li>
+                            <li><a href="#">
+                                 Requests
+                                <span class="ntb__tabs__unread">18</span>
+                            </a></li>
+                            <li><a href="#">
+                                 Sent Requests
+                                <span class="ntb__tabs__unread">18</span>
+                            </a></li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
 
@@ -123,23 +131,25 @@ export default function wonAppTag() {
 
 
             <nav class="settings-tab-bar" ng-cloak ng-show="{{true}}">
-                <div class="astb__left">
-                    <img src="generated/icon-sprite.svg#ico36_backarrow" class="astb__icon">
-                </div>
-                <div class="astb__center">
-                    <h1 class="astb__title">Account Settings</h1>
-                    <ul class="astb__tabs">
-                        <li class="astb__tabs__selected"><a href="#">
-                            General Settings
-                            <span class="astb__tabs__unread">5</span>
-                        </a></li>
-                        <li><a href="#">
-                            Manage Avatars
-                            <span class="astb__tabs__unread">5</span>
-                        </a></li>
-                    </ul>
-                </div>
-                <div class="astb__right">
+                <div class="astb__inner">
+                    <div class="astb__inner__left">
+                        <img src="generated/icon-sprite.svg#ico36_backarrow" class="astb__icon">
+                    </div>
+                    <div class="astb__inner__center">
+                        <h1 class="astb__title">Account Settings</h1>
+                        <ul class="astb__tabs">
+                            <li class="astb__tabs__selected"><a href="#">
+                                General Settings
+                                <span class="astb__tabs__unread">5</span>
+                            </a></li>
+                            <li><a href="#">
+                                Manage Avatars
+                                <span class="astb__tabs__unread">5</span>
+                            </a></li>
+                        </ul>
+                    </div>
+                    <div class="astb__inner__right">
+                    </div>
                 </div>
             </nav>
 
