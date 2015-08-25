393e0bd54825574095a6d103517a119798192098
yanapanchenko <yana_panchenko@yahoo.com>
2015-08-25 18:53:38 +0200

    path to APR library on docker specified

--- webofneeds/won-docker/wonnode/setenv.sh
+++ webofneeds/won-docker/wonnode/setenv.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export CATALINA_OPTS="${CATALINA_OPTS} -DWON_CONFIG_DIR=${WON_CONFIG_DIR} -Dlogback.configurationFile=${WON_CONFIG_DIR}/${LOGBACK_CONFIG}"
 echo "added WON params to CATALINA_OPTS"
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/apr/lib
+export LD_LIBRARY_PATH
