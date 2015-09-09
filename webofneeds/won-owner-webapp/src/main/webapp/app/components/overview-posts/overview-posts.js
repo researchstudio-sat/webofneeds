2e6e63f3e2f56cf963515d002a50815f1d311d6b
quasarchimaere <sudafabian@gmail.com>
2015-09-09 13:22:09 +0200

    finished overview posts draft implementation (with static json data in the overview-posts.js)

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-posts/overview-posts.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-posts/overview-posts.js
@@ -8,10 +8,20 @@ class OverviewPostsController {
     constructor() {
         this.selection = 1;
 
-        this.activePosts = [{name: "blabla"}, {name: "tutu"}, {name: "blabla"}, {name: "tutu"}, {name: "blabla"}, {name: "tutu"}];
+        this.activePosts = [{id: "121337345", title: "New flat, need furniture", creationDate: "20.11.2015", type: 1, group: "ux barcamp stuff", requests: [{},{},{}], matches: [{},{},{}], titleImgSrc: "images/someNeedTitlePic.png"},
+            {id: "121337345", title: "Clean park 1020 Vienna", creationDate: "20.11.1998", type: 4, group: "gaming", messages: [{},{},{}]},
+            {id: "121337345", title: "Car sharing 1020 Vienna", creationDate: "2.3.2001", type: 2, requests: [{},{},{}],matches: [{},{},{}],messages: [{},{},{}], titleImgSrc: "images/someNeedTitlePic.png"},
+            {id: "121337345", title: "tutu", creationDate: "7.9.2015", type: 3, group: "sat lunch group", requests: [{},{},{}]},
+            {id: "121337345", title: "Local Artistry", creationDate: "20.11.2005", type: 2, matches: [{},{},{}], titleImgSrc: "images/someNeedTitlePic.png"},
+            {id: "121337345", title: "Cycling Tour de France", creationDate: "1.1.2000", type: 3}];
+
 
         this.closedPosts = this.activePosts;
         this.drafts = this.activePosts;
+
+        this.activePostsOpen = true;
+        this.draftsOpen = false;
+        this.closedPostsOpen = false;
     }
 }
 
--- webofneeds/won-owner-webapp/src/main/webapp/app/components/post-item-line.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/post-item-line.js
@@ -4,11 +4,68 @@ import angular from 'angular';
 
 function genComponentConf() {
     let template = `
-            <div>{{self.item.name}}</div>
+            <a ng-href="post/{{self.item.id}}">
+                <img class="pil__image clickable" ng-show="self.item.titleImgSrc" ng-src="{{self.item.titleImgSrc}}"/>
+                <!--<img class="pil__image clickable" ng-show="!self.item.titleImgSrc" src="generated/icon-sprite.svg#illu_drag_here"/>-->
+                <div class="pil__image clickable" style="background-color: {{self.generateHexColor(self.item.title)}}" ng-show="!self.item.titleImgSrc"><!-- figure out some better way to color -->
+                    <span class="pil__image__noimage">{{self.item.title.charAt(0)}}</span>
+                </div>
+            </a>
+            <a class="pil__description clickable" ng-href="post/{{self.item.id}}">
+                <div class="pil__description__topline">
+                    <div class="pil__description__topline__title">{{self.item.title}}</div>
+                    <div class="pil__description__topline__creationdate">{{self.item.creationDate}}</div>
+                </div>
+                <div class="pil__description__subtitle">
+                    <span class="pil__description__subtitle__group" ng-show="self.item.group">
+                        <img src="generated/icon-sprite.svg#ico36_group" class="pil__description__subtitle__group__icon">{{self.item.group}}<span class="pil__description__subtitle__group__dash"> &ndash; </span>
+                    </span>
+                    <span class="pil__description__subtitle__type">{{self.getType(self.item.type)}}</span>
+                </div>
+            </a>
+            <div class="pil__indicators">
+                <a class="pil__indicators__item clickable" ng-href="post/{{self.item.id}}/owner/messages">
+                    <img src="generated/icon-sprite.svg#ico36_message" ng-show="self.item.messages" class="pil__indicators__item__icon">
+                    <img src="generated/icon-sprite.svg#ico36_message_grey" ng-show="!self.item.messages" class="pil__indicators__item__icon">
+                    <span class="pil__indicators__item__caption">{{self.item.messages.length}}</span>
+                </a>
+                <a class="pil__indicators__item clickable" ng-href="post/{{self.item.id}}/owner/requests">
+                    <img src="generated/icon-sprite.svg#ico36_incoming" ng-show="self.item.requests"  class="pil__indicators__item__icon">
+                    <img src="generated/icon-sprite.svg#ico36_incoming_grey"  ng-show="!self.item.requests" class="pil__indicators__item__icon">
+                    <span class="pil__indicators__item__caption">{{self.item.requests.length}}</span>
+                </a>
+                <a class="pil__indicators__item clickable" ng-href="post/{{self.item.id}}/owner/matches">
+                    <img src="generated/icon-sprite.svg#ico36_match" ng-show="self.item.matches" class="pil__indicators__item__icon">
+                    <img src="generated/icon-sprite.svg#ico36_match_grey" ng-show="!self.item.matches" class="pil__indicators__item__icon">
+                    <span class="pil__indicators__item__caption">{{self.item.matches.length}}</span>
+                </a>
+            </div>
     `;
 
     class Controller {
         constructor() { }
+
+
+        getType(type) {
+            switch(type){
+                case 1: return 'I want to have something';
+                case 2: return 'I offer something';
+                case 3: return 'I want to do something together';
+                case 4: return 'I want to change something';
+            }
+        }
+
+        generateHexColor(title) {
+            var hash = 0;
+            for (var i = 0; i < title.length; i++) {
+                hash = title.charCodeAt(i) + ((hash << 5) - hash);
+            }
+
+            var c = (hash & 0x00FFFFFF)
+                .toString(16);
+
+            return "#"+("00000".substring(0, 6 - c.length) + c);
+        }
     }
 
     return {
