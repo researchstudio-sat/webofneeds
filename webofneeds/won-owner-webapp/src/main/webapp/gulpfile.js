b0f869dad08e854cca0412adc5cdede0619304de
moru <kevin.singer@student.tuwien.ac.at>
2015-09-14 13:10:59 +0200

    Making sure tasks run before the watch starts.

--- webofneeds/won-owner-webapp/src/main/webapp/gulpfile.js
+++ webofneeds/won-owner-webapp/src/main/webapp/gulpfile.js
@@ -9,7 +9,7 @@ var svgSprite = require('gulp-svg-sprite');
 var sassImportOnce = require('node-sass-import-once');
 
 gulp.task('default', ['sass', 'iconsprite']);
-gulp.task('watch', function() {
+gulp.task('watch', ['sass', 'iconsprite'], function() {
     gulp.watch('./style/**/*.scss', ['sass']);
     gulp.watch('./style/**/_*.scss', ['sass']);
     gulp.watch('./images/won-icons/**/*.svg', ['iconsprite']);
