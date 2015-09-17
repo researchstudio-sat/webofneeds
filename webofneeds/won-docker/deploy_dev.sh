0d004eea6bbe7af1f120f59936e64ef27c1ad58c
hfriedrich <heiko.friedrich@researchstudio.at>
2015-09-17 12:48:36 +0200

    add deploy script for dev

---
+++ webofneeds/won-docker/deploy_dev.sh
@@ -0,0 +1,52 @@
+# build won docker images and deploy to sat cluster
+echo Start Docker build and deployment:
+
+# wonnode
+docker -H satsrv04:2375 build -t webofneeds/wonnode:dev $WORKSPACE/webofneeds/won-docker/wonnode/
+docker -H satsrv04:2375 stop wonnode_dev || echo 'No docker container found to stop with name: wonnode_dev'
+docker -H satsrv04:2375 rm wonnode_dev || echo 'No docker container found to remove with name: wonnode_dev'
+docker -H satsrv04:2375 run --name=wonnode_dev -d -e "uri.host=satsrv04.researchstudio.at" \
+-e "http.port=8888" -e "activemq.broker.port=61616" -p 8888:8080 -p 61617:61617 webofneeds/wonnode:dev
+
+# owner
+docker -H satsrv04:2375 build -t webofneeds/owner:dev $WORKSPACE/webofneeds/won-docker/owner/
+docker -H satsrv04:2375 stop owner_dev || echo 'No docker container found to stop with name: owner_dev'
+docker -H satsrv04:2375 rm owner_dev || echo 'No docker container found to remove with name: owner_dev'
+sleep 20
+docker -H satsrv04:2375 run --name=owner_dev -d -e "node.default.host=satsrv04.researchstudio.at" \
+-e "node.default.http.port=8888" -p 8081:8080 webofneeds/owner:dev
+
+# bigdata
+docker -H satsrv06:2375 stop bigdata_dev || echo 'No docker container found to stop with name: bigdata_dev'
+docker -H satsrv06:2375 rm bigdata_dev || echo 'No docker container found to remove with name: bigdata_dev'
+docker -H satsrv06:2375 run --name=bigdata_dev -d -p 9999:9999 webofneeds/bigdata
+
+# matcher service
+docker -H satsrv06:2375 build -t webofneeds/matcher_service:dev $WORKSPACE/webofneeds/won-docker/matcher-service/
+docker -H satsrv06:2375 stop matcher_service_dev || echo 'No docker container found to stop with name: matcher_service_dev'
+docker -H satsrv06:2375 rm matcher_service_dev || echo 'No docker container found to remove with name: matcher_service_dev'
+docker -H satsrv06:2375 run --name=matcher_service_dev -d -e "node.host=satsrv06.researchstudio.at" \
+-e "cluster.seed.host=satsrv06.researchstudio.at" \
+-e "uri.sparql.endpoint=http://satsrv06.researchstudio.at:9999/bigdata/namespace/kb/sparql" \
+-e "wonNodeController.wonNode.crawl=http://satsrv04.researchstudio.at:8888/won/resource" \
+-e "cluster.local.port=2551" -e "cluster.seed.port=2551" -p 2551:2551 webofneeds/matcher_service:dev
+
+# siren matcher
+docker -H satsrv05:2375 build -t webofneeds/matcher_siren:dev $WORKSPACE/webofneeds/won-docker/matcher-siren/
+docker -H satsrv05:2375 stop matcher_siren_dev || echo 'No docker container found to stop with name: matcher_siren_dev'
+docker -H satsrv05:2375 rm matcher_siren_dev || echo 'No docker container found to remove with name: matcher_siren_dev'
+docker -H satsrv05:2375 run --name=matcher_siren_dev -d -e "node.host=satsrv05.researchstudio.at" \
+-e "cluster.seed.host=satsrv06.researchstudio.at" -e "cluster.seed.port=2551" -e "cluster.local.port=2552" \
+-p 2552:2552 webofneeds/matcher_siren:dev
+
+# siren solr server
+docker -H satsrv05:2375 stop sirensolr_dev || echo 'No docker container found to stop with name: sirensolr_dev'
+docker -H satsrv05:2375 rm sirensolr_dev || echo 'No docker container found to remove with name: sirensolr_dev'
+docker -H satsrv05:2375 run --name=sirensolr_dev -d -p 8983:8983 webofneeds/sirensolr
+
+# push the newly build images to the dockerhub
+docker -H localhost:2375 login -u heikofriedrich
+docker -H localhost:2375 push webofneeds/wonnode:dev
+docker -H localhost:2375 push webofneeds/owner:dev
+docker -H localhost:2375 push webofneeds/matcher_service:dev
+docker -H localhost:2375 push webofneeds/matcher_siren:dev
--- webofneeds/won-docker/deploy_int.sh
+++ webofneeds/won-docker/deploy_int.sh
@@ -5,33 +5,41 @@ echo Start Docker build and deployment:
 docker -H satsrv04:2375 build -t webofneeds/wonnode:int $WORKSPACE/webofneeds/won-docker/wonnode/
 docker -H satsrv04:2375 stop wonnode_int || echo 'No docker container found to stop with name: wonnode_int'
 docker -H satsrv04:2375 rm wonnode_int || echo 'No docker container found to remove with name: wonnode_int'
-docker -H satsrv04:2375 run --name=wonnode_int -d -e "uri.host=satsrv04.researchstudio.at" -e "http.port=8889" -e "activemq.broker.port=61617" -p 8889:8080 -p 61617:61617 webofneeds/wonnode:int
+docker -H satsrv04:2375 run --name=wonnode_int -d -e "uri.host=satsrv04.researchstudio.at" \
+-e "http.port=8889" -e "activemq.broker.port=61617" -p 8889:8080 -p 61617:61617 webofneeds/wonnode:int
 
 # owner
 docker -H satsrv04:2375 build -t webofneeds/owner:int $WORKSPACE/webofneeds/won-docker/owner/
 docker -H satsrv04:2375 stop owner_int || echo 'No docker container found to stop with name: owner_int'
 docker -H satsrv04:2375 rm owner_int || echo 'No docker container found to remove with name: owner_int'
 sleep 20
-docker -H satsrv04:2375 run --name=owner_int -d -e "node.default.host=satsrv04.researchstudio.at" -e "node.default.http.port=8889" -p 8082:8080 webofneeds/owner:int
+docker -H satsrv04:2375 run --name=owner_int -d -e "node.default.host=satsrv04.researchstudio.at" \
+-e "node.default.http.port=8889" -p 8082:8080 webofneeds/owner:int
 
 # bigdata
 docker -H satsrv06:2375 stop bigdata_int || echo 'No docker container found to stop with name: bigdata_int'
 docker -H satsrv06:2375 rm bigdata_int || echo 'No docker container found to remove with name: bigdata_int'
-docker -H satsrv06:2375 run --name=bigdata_int -d -p 9999:9999 webofneeds/bigdata
+docker -H satsrv06:2375 run --name=bigdata_int -d -p 10000:9999 webofneeds/bigdata
 
 # matcher service
 docker -H satsrv06:2375 build -t webofneeds/matcher_service:int $WORKSPACE/webofneeds/won-docker/matcher-service/
 docker -H satsrv06:2375 stop matcher_service_int || echo 'No docker container found to stop with name: matcher_service_int'
 docker -H satsrv06:2375 rm matcher_service_int || echo 'No docker container found to remove with name: matcher_service_int'
-docker -H satsrv06:2375 run --name=matcher_service_int -d -e "node.host=satsrv06.researchstudio.at" -e "cluster.seed.host=satsrv06.researchstudio.at" -e "uri.sparql.endpoint=http://satsrv06.researchstudio.at:9999/bigdata/namespace/kb/sparql" -e "wonNodeController.wonNode.crawl=http://satsrv04.researchstudio.at:8889/won/resource" -p 2551:2551 webofneeds/matcher_service:int
+docker -H satsrv06:2375 run --name=matcher_service_int -d -e "node.host=satsrv06.researchstudio.at" \
+-e "cluster.seed.host=satsrv06.researchstudio.at" \
+-e "uri.sparql.endpoint=http://satsrv06.researchstudio.at:10000/bigdata/namespace/kb/sparql" \
+-e "wonNodeController.wonNode.crawl=http://satsrv04.researchstudio.at:8889/won/resource" \
+-e "cluster.local.port=2561" -e "cluster.seed.port=2561" -p 2561:2561 webofneeds/matcher_service:int
 
 # siren matcher
 docker -H satsrv05:2375 build -t webofneeds/matcher_siren:int $WORKSPACE/webofneeds/won-docker/matcher-siren/
 docker -H satsrv05:2375 stop matcher_siren_int || echo 'No docker container found to stop with name: matcher_siren_int'
 docker -H satsrv05:2375 rm matcher_siren_int || echo 'No docker container found to remove with name: matcher_siren_int'
-docker -H satsrv05:2375 run --name=matcher_siren_int -d -e "node.host=satsrv05.researchstudio.at" -e "cluster.seed.host=satsrv06.researchstudio.at" -p 2552:2552 webofneeds/matcher_siren:int
+docker -H satsrv05:2375 run --name=matcher_siren_int -d -e "node.host=satsrv05.researchstudio.at" \
+-e "cluster.seed.host=satsrv06.researchstudio.at" -e "cluster.seed.port=2561" -e "cluster.local.port=2562" \
+-p 2562:2562 webofneeds/matcher_siren:int
 
 # siren solr server
 docker -H satsrv05:2375 stop sirensolr_int || echo 'No docker container found to stop with name: sirensolr_int'
 docker -H satsrv05:2375 rm sirensolr_int || echo 'No docker container found to remove with name: sirensolr_int'
-docker -H satsrv05:2375 run --name=sirensolr_int -d -p 8983:8983 webofneeds/sirensolr
+docker -H satsrv05:2375 run --name=sirensolr_int -d -p 8984:8983 webofneeds/sirensolr
