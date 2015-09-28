8a3c348e91996c2f57bc89f6f1992d54bba213bc
moru <kevin.singer@student.tuwien.ac.at>
2015-09-28 18:21:19 +0200

    More docu.

--- webofneeds/won-owner-webapp/src/main/webapp/app/actions.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/actions.js
@@ -41,6 +41,10 @@ export const actionTypes = tree2constants({
 /**
  * actionCreators are functions that take the payload and output
  * an action object, thus prebinding the action-type.
+ * This object follows the structure of the actionTypes-object,
+ * but is flattened for use with ng-redux. Thus calling
+ * `$ngRedux.dispatch(actionCreators.drafts__new(myDraft))` will trigger an action
+ * `{type: actionTypes.drafts.new, payload: myDraft}`
  *
  * e.g.:
  *
