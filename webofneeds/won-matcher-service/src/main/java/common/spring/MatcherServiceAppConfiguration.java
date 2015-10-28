098c0566dc664e24b86b1b56df2e90a453407b91
yanapanchenko <yana_panchenko@yahoo.com>
2015-10-28 13:39:51 +0100

    matcher-service fixes:    fix spring configs for matcher-service;    create LinkedDataSource base implementation without caching;    move there the methods relevant for both caching and non-caching sources;    make caching implementation extend base implementation;    use non-caching implementation in matcher-service.

--- webofneeds/won-matcher-service/src/main/java/common/spring/MatcherServiceAppConfiguration.java
+++ webofneeds/won-matcher-service/src/main/java/common/spring/MatcherServiceAppConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  */
 @Configuration
 @ImportResource({"classpath:/spring/component/matcher-service/ehcache/spring-node-ehcache.xml",
-                 "classpath:/spring/component/matcher-service/linkeddatasource/linkeddatasource.xml",
                  "classpath:/spring/component/matcher-service/transmission/matcher-security.xml",
                  "classpath:/spring/component/cryptographyServices.xml",
                  "classpath:/spring/component/wonNodeInformationService.xml"})
