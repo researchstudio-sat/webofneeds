1bc37f400dfe008af55236f1f188383eb8d6b515
moru <kevin.singer@student.tuwien.ac.at>
2015-11-05 18:15:51 +0100

    Refactored action creation so all actions are visible in the json-object.

--- webofneeds/won-owner-webapp/src/main/webapp/app/actions.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/actions.js
@@ -6,36 +6,57 @@
 import { tree2constants, deepFreeze, reduceAndMapTreeKeys, flattenTree } from './utils';
 import './service/won';
 
-export const actionTypes = tree2constants({
+import { stateGo, stateReload, stateTransitionTo } from 'redux-ui-router';
 
+const injDefault = 'INJECT_DEFAULT_ACTION_CREATOR';
+const actionHierarchy = {
     /* actions received as responses or push notifications */
     received: {
         /* contains all user-bound data, e.g. ownedPosts,
          * drafts, messages,...
          * This action will likely be caused as a consequence of signing in.
          */
-        userData : null
+        userData : injDefault
     },
     drafts: {
         /*
          * A new draft was created (either through the view in this client or on another browser)
          */
-        new: null,
+        new: injDefault,
         /*
          * A draft has changed. Pass along the draftURI and the respective data.
          */
         change: {
-            type: null,
-            title: null,
-            thumbnail: null,
+            type: injDefault,
+            title: injDefault,
+            thumbnail: injDefault,
         },
-        delete: null,
-        select: null,
-        publish: null,
-        notifyOfSuccessfulPublish: null //triggered by server
+        delete: injDefault,
+        select: injDefault,
+        publish: injDefault,
+        notifyOfSuccessfulPublish: injDefault //triggered by server
     },
-    moreWub: null
-});
+    router: {
+        stateGo,
+        stateReload,
+        stateTransitionTo
+    },
+    moreWub: injDefault,
+    /*
+     * This action creator uses thunk (https://github.com/gaearon/redux-thunk) which
+     * allows using it with a normal dispatch(actionCreator(payload)) even though
+     *  it does asynchronous calls. This is a requirement for using it with
+     *  $ngRedux.connect(..., actionCreators, ...)
+     */
+    delayedWub : (nrOfWubs, milliseconds = 1000) => (dispatch) =>
+        delay(milliseconds).then(
+                args => dispatch(actionCreators.moreWub(nrOfWubs)),
+                error => console.err('actions.js: Error while delaying for delayed Wub.')
+        )
+}
+
+//as string constans, e.g. actionTypes.drafts.change.type === "drafts.change.type"
+export const actionTypes = tree2constants(actionHierarchy);
 
 /**
  * actionCreators are functions that take the payload and output
@@ -53,37 +74,23 @@ export const actionTypes = tree2constants({
  * }
  * ```
  */
-export const actionCreators = flattenTree(
+export const actionCreators = Object.freeze(flattenTree(
         reduceAndMapTreeKeys(
-            (acc, k) => acc.concat(k), //construct paths, e.g. ['draft', 'new']
-            (acc) => createActionCreator(won.lookup(actionTypes, acc)), //lookup constant at path
-            [], actionTypes
+            (path, k) => path.concat(k), //construct paths, e.g. ['draft', 'new']
+            (path) => {
+                /* leaf can either be a defined creator or a
+                 * placeholder asking to generate one.
+                 */
+                const potentialCreator = won.lookup(actionHierarchy, path);
+                if(typeof potentialCreator === 'function') {
+                    return potentialCreator; //already a defined creator. hopefully.
+                } else {
+                    const type = won.lookup(actionTypes, path);
+                    return createActionCreator(type);
+                }
+            }, [], actionHierarchy
         ),
-    '__');
-
-/*
- * This action creator uses thunk (https://github.com/gaearon/redux-thunk) which
- * allows using it with a normal dispatch(actionCreator(payload)) even though
- *  it does asynchronous calls. This is a requirement for using it with
- *  $ngRedux.connect(..., actionCreators, ...)
- */
-actionCreators.delayedWub = (nrOfWubs, milliseconds = 1000) => (dispatch) =>
-    delay(milliseconds).then(
-        args => dispatch(actionCreators.moreWub(nrOfWubs)),
-        error => console.err('actions.js: Error while delaying for delayed Wub.')
-    );
-
-import { stateGo, stateReload, stateTransitionTo } from 'redux-ui-router';
-actionCreators.router__stateGo = stateGo;
-actionCreators.router__stateReload = stateReload;
-actionCreators.router__stateTransitionTo = stateTransitionTo;
-
-window.delay = delay;
-function delay(milliseconds) {
-    return new Promise((resolve, reject) =>
-        window.setTimeout(() => resolve(), milliseconds)
-    );
-}
+    '__'));
 
 function createActionCreator(type) {
     return (payload) => {
@@ -92,12 +99,11 @@ function createActionCreator(type) {
     };
 }
 
-/*
- * NOTE: Add any custom action-creators here!
- */
-
-Object.freeze(actionCreators); //to make sure it's not modified elsewhere
-
+function delay(milliseconds) {
+    return new Promise((resolve, reject) =>
+        window.setTimeout(() => resolve(), milliseconds)
+    );
+}
 
 /*
  * TODO deletme; for debugging
