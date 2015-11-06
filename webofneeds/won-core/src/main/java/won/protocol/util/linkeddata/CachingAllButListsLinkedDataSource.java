e3d788e59099f7f2d5fd9fc2153dfd62e3cb3b75
yanapanchenko <yana_panchenko@yahoo.com>
2015-11-06 16:45:40 +0100

    use no cache in linked data source in case of list of connection

---
+++ webofneeds/won-core/src/main/java/won/protocol/util/linkeddata/CachingAllButListsLinkedDataSource.java
@@ -0,0 +1,56 @@
+package won.protocol.util.linkeddata;
+
+import com.hp.hpl.jena.query.Dataset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * User: ypanchenko
+ * Date: 06.11.2015
+ */
+public class CachingAllButListsLinkedDataSource extends CachingLinkedDataSource
+{
+  //TODO instead of predefined resources should look into RDF and decide whether it is a list or not
+
+  //TODO actually the connection itself should also not be cached - i.e. status can be updated
+
+  private Pattern pattern_connections_list_uri = Pattern.compile("(.+)/resource/connection(/)+");
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Override
+  public Dataset getDataForResource(URI resource) {
+    if (isCachingAllowed(resource)) {
+      return super.getDataForResource(resource);
+    } else {
+      Dataset dataset = linkedDataRestClient.readResourceData(resource);
+      //TODO debug log level
+      logger.info("connections list uri request performed");
+      return dataset;
+    }
+  }
+
+  @Override
+  public Dataset getDataForResource(URI resource, URI requesterWebID) {
+    if (isCachingAllowed(resource)) {
+      return super.getDataForResource(resource, requesterWebID);
+    } else {
+      Dataset dataset = linkedDataRestClient.readResourceData(resource, requesterWebID);
+      logger.info("connections list uri request performed");
+      return dataset;
+    }
+  }
+
+  private boolean isCachingAllowed(final URI resource) {
+    Matcher matcher = pattern_connections_list_uri.matcher(resource.toString());
+    if (matcher.matches()) {
+      return false;
+    }
+    return true;
+  }
+
+}
