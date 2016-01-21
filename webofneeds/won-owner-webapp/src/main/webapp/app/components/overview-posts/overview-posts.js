4e5212772bba7187530214bbbb729919cade7994
moru <kevin.singer@student.tuwien.ac.at>
2016-01-21 19:48:41 +0100

    Working on overview posts.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-posts/overview-posts.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-posts/overview-posts.js
@@ -7,6 +7,12 @@ import { actionCreators }  from '../../actions/actions';
 import { attach } from '../../utils';
 import won from '../../won-es6';
 
+const ZERO_UNSEEN = Object.freeze({
+    matches: 0,
+    incomingRequests: 0,
+    conversations: 0,
+});
+
 const serviceDependencies = ['$q', '$ngRedux', '$scope', /*'$routeParams' /*injections as strings here*/];
 class OverviewPostsController {
 
@@ -25,20 +31,77 @@ class OverviewPostsController {
                     unseenMatchesCounts = unseenMatchesCounts.set(receiverNeed, 1);
                 }
                 else{
-                    unseenMatchesCounts =unseenMatchesCounts.set(receiverNeed, count + 1);
+                    unseenMatchesCounts = unseenMatchesCounts.set(receiverNeed, count + 1);
                 }
 
             });
+
+
+
+
+            //won.EVENT.HINT_RECEIVED -> matches
+            //won.EVENT.WON_MESSAGE_RECEIVED -> convoMessages (!= convos with new messages <- we want this)
+            //won.EVENT.CONNECT_RECEIVED -> incomingRequests
+
+            /*
+
+            ///-------------
+            function groupByNeed(unreadEvents) {
+                let need2events = Immutable.Map();
+                unreadEvents.forEach(e => {
+                    const receiverNeed = e.get('hasReceiverNeed');
+                    need2events
+                    //....
+                });
+            }
+
+            //-----------
+            let unseenCounts = {};
+            unreadEvents.forEach(e => {
+                const receiverNeed = e.get('hasReceiverNeed');
+                const prevCounts = unseenCounts[receiverNeed];
+                {}
+
+                unseenCounts[receiverNeed] = unseenCounts[receiverNeed]? unseenCounts[receiverNeed] : {};
+t
+                switch(e.get('eventType')) {
+                    case won.EVENT.HINT_RECEIVED:
+                        const count = unseenCounts[receiverNeed].matches? unseenCounts[receiverNeed].matchesObje
+                        this._unseenCounts[receiverNeed].matches += 1;
+                    break;
+                    case won.EVENT.WON_MESSAGE_RECEIVED:
+                        //TODO count unique convos
+                        this._unseenCounts[receiverNeed].conversations += 1;
+                    break;
+                    case won.EVENT.CONNECTION_MESSAGE_RECEIVED:
+                        //TODO v--^ which of these two?
+                    case won.EVENT.CONNECT_RECEIVED:
+                        this._unseenCounts[receiverNeed].incomingRequests += 1;
+                    break;
+
+                        if(!this._unseenCounts[receiverNeed]) this._unseenCounts[receiverNeed] = {};
+                }
+
+            });
+            */
+
+            //goal: unseenCounts = { <uri> : { matches: 11, conversations: 0, incomingRequests: 2 }, <uri>:...}
+            //TODO use memoized selector to avoid running this calculation on every tick
+
+
+
             return {
                 posts: state.getIn(["needs", "needs"]).toJS(),
                 unreadEvents,
                 unreadMatchEventsOfNeed: unseenMatchesCounts,
+                //nrOfPostsWithNotifications: unseenMatchesCounts.length
                 drafts: null,
                 activePostsOpen: state.getIn(["postOverview", "activePostsView"]),
                 draftsOpen: false,
                 closedPostsOpen: state.getIn(["postOverview", "closedPostsView"])
             }
         }
+
         window.opc = this;
 /*        this.$scope.getMatches = function(uri){
             this.$filter('filterEventByType')(this.$scope.unreadEvents,uri,won.EVENT.HINT_RECEIVED)
