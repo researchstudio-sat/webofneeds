219ad4f235ab275def2591af5d2c3f206290df2e
moru <kevin.singer@student.tuwien.ac.at>
2016-01-26 13:12:49 +0100

    User friendly relative timestamps.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/post-item-line.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/post-item-line.js
@@ -3,7 +3,8 @@
 import angular from 'angular';
 import squareImageModule from '../components/square-image';
 import won from '../won-es6';
-import { labels } from '../won-label-utils';
+import { labels, relativeTime  } from '../won-label-utils';
+import { createSelector } from 'reselect';
 
 function genComponentConf() {
     let template = `
@@ -13,7 +14,7 @@ function genComponentConf() {
             <a class="pil__description clickable" ui-sref="postInfo({myUri: self.item.uri})">
                 <div class="pil__description__topline">
                     <div class="pil__description__topline__title">{{self.item.title}}</div>
-                    <div class="pil__description__topline__creationdate">{{self.item.creationDate}}</div>
+                    <div class="pil__description__topline__creationdate">{{self.creationDate()}}</div>
                 </div>
                 <div class="pil__description__subtitle">
                     <span class="pil__description__subtitle__group" ng-show="self.item.group">
@@ -71,12 +72,24 @@ function genComponentConf() {
             //this.EVENT = won.EVENT;
         }
 
-        unreadXCount(type) {return !this.unreadCounts? undefined : //ensure existence of count object
-            this.unreadCounts.get(type)
+        creationDate() {
+            return relativeTime(Date.now(), this.item.creationDate);
         }
-        unreadMatchesCount() { return this.unreadXCount(won.EVENT.HINT_RECEIVED) }
-        unreadRequestsCount() { return this.unreadXCount(won.EVENT.CONNECT_RECEIVED) }
-        unreadConversationsCount() { return this.unreadXCount(won.EVENT.WON_MESSAGE_RECEIVED) }
+
+        unreadXCount(type) {
+            return !this.unreadCounts?
+                undefined : //ensure existence of count object
+                this.unreadCounts.get(type)
+        }
+        unreadMatchesCount() {
+            return this.unreadXCount(won.EVENT.HINT_RECEIVED)
+        }
+        unreadRequestsCount() {
+            return this.unreadXCount(won.EVENT.CONNECT_RECEIVED)
+        }
+        unreadConversationsCount() {
+            return this.unreadXCount(won.EVENT.WON_MESSAGE_RECEIVED)
+        }
 
     }
 
--- webofneeds/won-owner-webapp/src/main/webapp/app/won-label-utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/won-label-utils.js
@@ -9,3 +9,43 @@ export const labels = Object.freeze({
         [won.WON.BasicNeedTypeCritique]: 'I want to change something',
     }
 });
+
+/**
+ * Both input parameters can be anything that `Date(...)` can
+ * parse (incl. other `Date`s, xsd-strings,...)
+ *
+ * Adapted from ["Javascript timestamp to relative time" at Stackoverflow](http://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time-eg-2-seconds-ago-one-week-ago-etc-best)
+ *
+ * @param now
+ * @param previous
+ */
+export function relativeTime(now, previous) {
+    now = new Date(now);
+    previous = new Date(previous);
+    const elapsed = now - previous; // in ms
+
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const msPerYear = msPerDay * 365;
+
+    const labelGen = (msPerUnit, unitName) => {
+        const rounded = Math.round(elapsed/msPerUnit);
+        return rounded + ' ' + unitName + (rounded !== 1 ? 's' : '') + ' ago';
+    }
+
+    if (elapsed < msPerMinute) {
+        return '< 1 minute ago';
+    } else if (elapsed < msPerHour) {
+        return labelGen(msPerMinute, 'minute');
+    } else if (elapsed < msPerDay ) {
+        return labelGen(msPerHour, 'hour');
+    } else if (elapsed < msPerMonth) {
+        return labelGen(msPerDay, 'day');
+    } else if (elapsed < msPerYear) {
+        return 'approximately ' + labelGen(msPerMonth, 'month');
+    } else {
+        return 'approximately ' + labelGen(msPerYear, 'year');
+    }
+}
