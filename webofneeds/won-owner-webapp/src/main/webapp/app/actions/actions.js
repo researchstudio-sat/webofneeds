b0ff9c7ec849b981a77711072c49528c07743677
moru <kevin.singer@student.tuwien.ac.at>
2016-02-17 18:58:21 +0100

    added a TODO.

--- webofneeds/won-owner-webapp/src/main/webapp/app/actions/actions.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/actions/actions.js
@@ -81,13 +81,13 @@ const actionHierarchy = {
         load:(data) => (dispatch, getState) => {
             const state = getState();
             for(let needUri in data){
-                won.getConnectionInStateForNeedWithRemoteNeed(needUri,"won:Suggested").then(function(results){
+                won.getConnectionInStateForNeedWithRemoteNeed(needUri, "won:Suggested").then(function(results){
                     let needData = state.getIn(['needs', 'ownNeeds', needUri]).toJS();
                     let data = { ownNeed: needData, connections: results };
+                    //TODO only one action should be dispatched for every interaction! (reducers should be able to handle arrays)
                     results.forEach(function(entry){
                         dispatch(actionCreators.matches__add(entry))
                     })
-
                 })
             }
         },
