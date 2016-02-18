b38fa1d3e206e4992ac32ae42a2547961fbd9225
moru <kevin.singer@student.tuwien.ac.at>
2016-02-18 17:35:57 +0100

    Moved the load action skeleton to a seperate file.

--- webofneeds/won-owner-webapp/src/main/webapp/app/actions/actions.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/actions/actions.js
@@ -95,45 +95,6 @@ const actionHierarchy = {
     },
     connections:{
         load : (needUris) => dispatch =>{
-            window.needUris4dbg = needUris;
-
-            /**
-             const allAccessibleAndRelevantData = {
-                ownNeeds: {
-                    <needUri> : {
-                        *:*,
-                        connections: [<connectionUri>, <connectionUri>]
-                    }
-                    <needUri> : {
-                        *:*,
-                        connections: [<connectionUri>, <connectionUri>]
-                    }
-                },
-                theirNeeds: {
-                    <needUri>: {
-                        *:*,
-                        connections: [<connectionUri>, <connectionUri>] <--?
-                    }
-                },
-                connections: {
-                    <connectionUri> : {
-                        *:*,
-                        events: [<eventUri>, <eventUri>]
-                    }
-                    <connectionUri> : {
-                        *:*,
-                        events: [<eventUri>, <eventUri>]
-                    }
-                }
-                events: {
-                    <eventUri> : { *:* },
-                    <eventUri> : { *:* }
-                }
-             }
-
-
-            */
-
             needUris.forEach(needUri =>
                 won.executeCrawlableQuery(won.queries["getAllConnectionUrisOfNeed"], needUri)
                     .then(function(connectionsOfNeed){
---
+++ webofneeds/won-owner-webapp/src/main/webapp/app/actions/load-action.js
@@ -0,0 +1,45 @@
+/**
+ * Created by ksinger on 18.02.2016.
+ */
+
+
+
+export const load = (needUris) => dispatch => {
+    window.needUris4dbg = needUris;
+    /**
+     const allAccessibleAndRelevantData = {
+                ownNeeds: {
+                    <needUri> : {
+                        *:*,
+                        connections: [<connectionUri>, <connectionUri>]
+                    }
+                    <needUri> : {
+                        *:*,
+                        connections: [<connectionUri>, <connectionUri>]
+                    }
+                },
+                theirNeeds: {
+                    <needUri>: {
+                        *:*,
+                        connections: [<connectionUri>, <connectionUri>] <--?
+                    }
+                },
+                connections: {
+                    <connectionUri> : {
+                        *:*,
+                        events: [<eventUri>, <eventUri>]
+                    }
+                    <connectionUri> : {
+                        *:*,
+                        events: [<eventUri>, <eventUri>]
+                    }
+                }
+                events: {
+                    <eventUri> : { *:* },
+                    <eventUri> : { *:* }
+                }
+             }
+
+
+     */
+}
