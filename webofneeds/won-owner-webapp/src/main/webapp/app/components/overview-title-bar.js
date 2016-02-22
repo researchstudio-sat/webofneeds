25cb993a24059eca58198785a40191ca08173686
quasarchimaere <sudafabian@gmail.com>
2016-02-22 16:11:24 +0100

    updated title bars to disable tabs when there is nothing in the respective views

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-title-bar.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-title-bar.js
@@ -6,7 +6,8 @@
 import angular from 'angular';
 import { attach } from '../utils';
 import { actionCreators }  from '../actions/actions';
-import { selectUnreadCountsByType, selectUnreadEventsByNeed } from '../selectors';
+import {    selectUnreadCountsByType,
+            selectUnreadEventsByNeed } from '../selectors';
 import won from '../won-es6';
 
 
@@ -16,16 +17,33 @@ function genComponentConf() {
         <nav ng-cloak ng-show="{{true}}" class="main-tab-bar">
             <div class="mtb__inner">
                 <ul class="mtb__inner__center mtb__tabs">
-                    <li ng-class="{'mtb__tabs__selected' : self.selection == 0}"><a ui-sref="feed">Feed</a></li>
-                    <li ng-class="{'mtb__tabs__selected' : self.selection == 1}"><a ui-sref="overviewPosts">Posts
-                        <span class="mtb__tabs__unread">{{ self.nrOfNeedsWithUnreadEvents }}</span>
-                    </a></li>
-                    <li ng-class="{'mtb__tabs__selected' : self.selection == 2}"><a ui-sref="overviewIncomingRequests">Incoming Requests
-                        <span class="mtb__tabs__unread">{{ self.unreadRequests }}</span>
-                    </a></li>
-                    <li ng-class="{'mtb__tabs__selected' : self.selection == 3}"><a ui-sref="overviewMatches">Matches
-                        <span class="mtb__tabs__unread">{{ self.unreadMatches }}</span>
-                    </a></li>
+                    <li ng-class="{'mtb__tabs__selected' : self.selection == 0}">
+                        <a ui-sref="feed"
+                            ng-class="{'disabled' : !self.hasPosts}">
+                            Feed
+                        </a>
+                    </li>
+                    <li ng-class="{'mtb__tabs__selected' : self.selection == 1}">
+                        <a ui-sref="overviewPosts"
+                            ng-class="{'disabled' : !self.hasPosts}">
+                            Posts
+                            <span class="mtb__tabs__unread">{{ self.nrOfNeedsWithUnreadEvents }}</span>
+                        </a>
+                    </li>
+                    <li ng-class="{'mtb__tabs__selected' : self.selection == 2}">
+                        <a ui-sref="overviewIncomingRequests"
+                            ng-class="{'disabled' : !self.hasRequests}">
+                            Incoming Requests
+                            <span class="mtb__tabs__unread">{{ self.unreadRequests }}</span>
+                        </a>
+                    </li>
+                    <li ng-class="{'mtb__tabs__selected' : self.selection == 3}">
+                        <a ui-sref="overviewMatches"
+                            ng-class="{'disabled' : !self.hasMatches}">
+                            Matches
+                            <span class="mtb__tabs__unread">{{ self.unreadMatches }}</span>
+                        </a>
+                    </li>
                 </ul>
                 <div class="mtb__inner__right">
                     <a href="#" class="mtb__searchbtn">
@@ -40,13 +58,26 @@ function genComponentConf() {
         constructor() {
             attach(this, serviceDependencies, arguments);
 
-            window.otb = this;
-
             const selectFromState = (state) => {
                 const unreadCounts = selectUnreadCountsByType(state);
                 const nrOfNeedsWithUnread = selectUnreadEventsByNeed(state).size;
 
                 return {
+                    hasPosts: state.getIn(["needs", "ownNeeds"]).length > 0,
+                    hasRequests: Object.keys(state.getIn(['connections','connections']).toJS())
+                        .map(key=>state.getIn(['connections','connections']).toJS()[key])
+                        .filter(conn=>{
+                            if(conn.connection.hasConnectionState===won.WON.RequestReceived){
+                                return true
+                            }
+                        }).length > 0,
+                    hasMatches: Object.keys(state.getIn(['connections','connections']).toJS())
+                        .map(key=>state.getIn(['connections','connections']).toJS()[key])
+                        .filter(conn=>{
+                            if(conn.connection.hasConnectionState===won.WON.Suggested){
+                                return true
+                            }
+                        }).length > 0,
                     unreadRequests: unreadCounts.get(won.EVENT.CONNECT_RECEIVED),
                     unreadMatches: unreadCounts.get(won.EVENT.HINT_RECEIVED),
                     nrOfNeedsWithUnreadEvents: nrOfNeedsWithUnread > 0? nrOfNeedsWithUnread : undefined,
@@ -54,7 +85,6 @@ function genComponentConf() {
             };
 
             const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
-            //  this.loadMatches();
             this.$scope.$on('$destroy', disconnect);
         }
     }
--- webofneeds/won-owner-webapp/src/main/webapp/app/components/owner-title-bar.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/owner-title-bar.js
@@ -1,11 +1,11 @@
-/**
- * Created by ksinger on 20.08.2015.
- */
 ;
 
 import angular from 'angular';
+import squareImageModule from '../components/square-image';
 import { attach,mapToMatches } from '../utils';
 import won from '../won-es6';
+import { labels } from '../won-label-utils';
+import { selectUnreadEventsByNeedAndType } from '../selectors';
 import { actionCreators }  from '../actions/actions';
 
 const serviceDependencies = ['$q', '$ngRedux', '$scope'];
@@ -14,34 +14,51 @@ function genComponentConf() {
         <nav class="need-tab-bar" ng-cloak ng-show="{{true}}">
             <div class="ntb__inner">
                 <div class="ntb__inner__left">
-                    <a ng-click="self.back()">
+                    <a ui-sref="overviewPosts">
                         <img src="generated/icon-sprite.svg#ico36_backarrow" class="ntb__icon">
                     </a>
-                    <won-square-image title="blabla" src="images/need.jpg"></won-square-image>
+                    <won-square-image src="self.item.titleImgSrc" title="self.item.title"></won-square-image>
                     <div class="ntb__inner__left__titles">
-                        <h1 class="ntb__title">New flat, need furniture</h1>
-                        <div class="ntb__inner__left__titles__type">I want to have something</div>
+                        <h1 class="ntb__title">{{self.item.title}}</h1>
+                        <div class="ntb__inner__left__titles__type">{{self.labels.type[self.item.basicNeedType]}}</div>
                     </div>
                 </div>
                 <div class="ntb__inner__right">
                     <img class="ntb__icon" src="generated/icon-sprite.svg#ico_settings">
                     <ul class="ntb__tabs">
-                        <li ng-class="{'mtb__tabs__selected' : self.selection == 0}"><a ui-sref="postConversations({myUri: 'http://example.org/121337345'})">
-                            Messages
-                            <span class="ntb__tabs__unread">5</span>
-                        </a></li>
-                        <li ng-class="{'mtb__tabs__selected' : self.selection == 1}"><a href="#">
-                            Matches
-                            <span class="ntb__tabs__unread">5</span>
-                        </a></li>
-                        <li ng-class="{'mtb__tabs__selected' : self.selection == 2}"><a href="#">
-                             Requests
-                            <span class="ntb__tabs__unread">18</span>
-                        </a></li>
-                        <li ng-class="{'mtb__tabs__selected' : self.selection == 3}"><a href="#">
-                             Sent Requests
-                            <span class="ntb__tabs__unread">18</span>
-                        </a></li>
+                        <li ng-class="{'ntb__tabs__selected' : self.selection == 4}">
+                            <a ui-sref="postInfo({myUri: self.item.uri})">
+                                Post Info
+                            </a>
+                        </li>
+                        <li ng-class="{'ntb__tabs__selected' : self.selection == 0}">
+                            <a ui-sref="postConversations({myUri: self.item.uri})"
+                                ng-class="{'disabled' : !self.hasMessages}">
+                                Messages
+                                <span class="ntb__tabs__unread">{{ self.unreadMessages }}</span>
+                            </a>
+                        </li>
+                        <li ng-class="{'ntb__tabs__selected' : self.selection == 1}">
+                            <a ui-sref="overviewMatches({myUri: self.item.uri})"
+                                ng-class="{'disabled' : !self.hasMatches}">
+                                Matches
+                                <span class="ntb__tabs__unread">{{ self.unreadMatches }}</span>
+                            </a>
+                        </li>
+                        <li ng-class="{'ntb__tabs__selected' : self.selection == 2}">
+                            <a ui-sref="overviewIncomingRequests({myUri: self.item.uri})"
+                                ng-class="{'disabled' : !self.hasIncomingRequests}">
+                                Requests
+                                <span class="ntb__tabs__unread">{{ self.unreadIncomingRequests }}</span>
+                            </a>
+                        </li>
+                        <li ng-class="{'ntb__tabs__selected' : self.selection == 3}">
+                            <a href="#"
+                                ng-class="{'disabled' : !self.hasSentRequests}">
+                                Sent Requests
+                                <span class="ntb__tabs__unread">{{ self.unreadSentRequests }}</span>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -53,31 +70,61 @@ function genComponentConf() {
         constructor() {
             attach(this, serviceDependencies, arguments);
 
+            window.otb = this;
+            this.labels = labels;
+
             const selectFromState = (state)=>{
+                const unreadCounts = selectUnreadEventsByNeedAndType(state);
 
                 return {
-                    matchesCount: Object.keys(state.getIn(['events','unreadEventUris']).toJS()).filter(event =>{
-                        if(event.eventType===won.EVENT.HINT_RECEIVED){
-                            return true
-                        }
-                    }),
-                    matchesOfNeed:mapToMatches(state.getIn(['matches','matches']).toJS())
+                    hasIncomingRequests: Object.keys(state.getIn(['connections','connections']).toJS())
+                        .map(key=>state.getIn(['connections','connections']).toJS()[key])
+                        .filter(conn=>{
+                            if(conn.connection.hasConnectionState===won.WON.RequestReceived && conn.ownNeed.uri === this.item.uri){
+                                return true
+                            }
+                        }).length > 0,
+                    hasSentRequests: Object.keys(state.getIn(['connections','connections']).toJS())
+                        .map(key=>state.getIn(['connections','connections']).toJS()[key])
+                        .filter(conn=>{
+                            if(conn.connection.hasConnectionState===won.WON.RequestReceived && conn.ownNeed.uri === this.item.uri){  //TODO: CHANGE TO REQUESTS SENT
+                                return true
+                            }
+                        }).length > 0,
+                    hasMatches: Object.keys(state.getIn(['connections','connections']).toJS())
+                        .map(key=>state.getIn(['connections','connections']).toJS()[key])
+                        .filter(conn=>{
+                            if(conn.connection.hasConnectionState===won.WON.Suggested && conn.ownNeed.uri === this.item.uri){
+                                return true
+                            }
+                        }).length > 0,
+                    hasMessages: Object.keys(state.getIn(['connections','connections']).toJS())
+                        .map(key=>state.getIn(['connections','connections']).toJS()[key])
+                        .filter(conn=>{
+                            if(conn.connection.hasConnectionState===won.WON.Connected && conn.ownNeed.uri === this.item.uri){
+                                return true
+                            }
+                        }).length > 0,
+                    unreadMessages: unreadCounts.getIn([this.item.uri, won.WON.Connected]), //TODO: NOT REALLY THE MESSAGE COUNT ONLY THE CONVERSATION COUNT
+                    unreadIncomingRequests: unreadCounts.getIn([this.item.uri, won.WON.RequestReceived]),
+                    unreadSentRequests: unreadCounts.getIn([this.item.uri, won.WON.RequestReceived]), //TODO: CHANGE TO REQUESTS SENT
+                    unreadMatches: unreadCounts.getIn([this.item.uri, won.WON.Suggested]),
                 };
-            }
+            };
 
             const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
-            //  this.loadMatches();
             this.$scope.$on('$destroy', disconnect);
         }
-        back() { window.history.back() }
     }
     Controller.$inject = serviceDependencies;
     return {
         restrict: 'E',
         controller: Controller,
         controllerAs: 'self',
+        bindToController: true, //scope-bindings -> ctrl
         template: template,
-        scope: {selection: "="}
+        scope: {selection: "=",
+                item: "="}
     }
 }
 
