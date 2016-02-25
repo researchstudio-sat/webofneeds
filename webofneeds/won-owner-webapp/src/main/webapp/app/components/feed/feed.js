dd645397d9663f5642f0150fb714a0d0852cd6e5
moru <kevin.singer@student.tuwien.ac.at>
2016-02-25 19:51:13 +0100

    working on unread events

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/feed/feed.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/feed/feed.js
@@ -7,6 +7,7 @@ import { attach } from '../../utils';
 import {
     selectConnectionsByNeed,
     selectUnreadCountsByNeedAndType,
+    selectUnreadEvents
 } from '../../selectors';
 
 const serviceDependencies = ['$q', '$ngRedux', '$scope', /*'$routeParams' /*injections as strings here*/];
@@ -15,13 +16,43 @@ class FeedController {
         attach(this, serviceDependencies, arguments);
         this.selection = 0;
 
-        const selectFromState = (state) =>({
-            posts: state.getIn(["needs", "ownNeeds"]).toJS(),
-            connectionsByNeed: selectConnectionsByNeed(state),
-            unreadCountsByNeedAndType: selectUnreadCountsByNeedAndType(state),
 
-            state4dgb: state,
-        })
+        const selectFromState = (state) => {
+
+
+            /*
+            const unreadEventUris = state
+                .getIn(['events', 'unreadEventUris'])
+                .map(event => event.get('uri'))
+                .toSet();
+                */
+            //const events = state.getIn(['events', 'events']);
+            //unreadEventUris.map(eventUri => events.get(eventUri));
+
+            const unreadEvents = selectUnreadEvents(state);
+
+            window.selectUnread4dbg = selectUnreadEvents;
+
+            const eventsByConnection = state.getIn(['events', 'events'])
+
+            const connectionsByNeed = state.getIn(['needs', 'ownNeeds'])
+                .map(ownNeed => ownNeed.get('hasConnections'));
+            //TODO attach events
+
+            // sort by newest event (excluding matches)
+
+            // wenn sich die sortierung aufgrund neuer events verändern wuerde, wird ein button/link angezeigt ("new messages/requests. click to update")
+            // always show latest message in a line
+
+            return {
+                unreadEvents4dbg: unreadEvents,
+                state4dbg: state,
+
+                posts: state.getIn(["needs", "ownNeeds"]),
+                connectionsByNeed: selectConnectionsByNeed(state),
+                unreadCountsByNeedAndType: selectUnreadCountsByNeedAndType(state),
+            }
+        }
         const disconnect = this.$ngRedux.connect(selectFromState,actionCreators)(this)
         this.$scope.$on('$destroy', disconnect);
 
