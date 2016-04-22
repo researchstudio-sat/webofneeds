5f35fb7f567290148089c7c11b86f064a708a284
soheilk <soheil.khosravipour@researchstudio.at>
2016-04-22 11:30:01 +0200

    the very top-level folder of the project in github cleaned up

--- ldspider-scripts/ldspider-loop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SLEEPTIME=30
-while(true) 
-	do 
-		./ldspider-run.sh 
-		echo finished ldspider run, sleeping for $SLEEPTIME seconds
-		sleep $SLEEPTIME
-		echo waking up and re-running ldspider 
-	done
