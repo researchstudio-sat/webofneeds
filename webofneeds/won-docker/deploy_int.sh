ff71be46c6f1be166cd33dfd6d72d1c0842d8cec
hfriedrich <heiko.friedrich@researchstudio.at>
2016-05-02 14:50:35 +0200

    deploy nginx for devel test env

--- webofneeds/won-docker/deploy_int.sh
+++ webofneeds/won-docker/deploy_int.sh
@@ -1,3 +1,22 @@
+#############################################################################################################
+# Deployment descriptions:
+# This script deploys several docker containers of the won application of the following servers:
+#
+# - create server certificate for satsrv04
+# - create server certificate for satsrv05 and satsrv06
+# - start nginx proxy server on satsrv06 for owner and wonnode on host satsrv05
+# - start portsgres need databases for owner and wonnode on satsrv04 and satsrv05
+# - start wonnode on satsrv04 (without proxy) => https://satsrv04:8889/won
+# - start wonnode on satsrv05 (with proxy on satsrv06) => https://satsrv06/won
+# - start owner on satsrv04 (without proxy) => https://satsrv04:8082/owner
+# - start owner on satsrv05 (with proxy on satsrv06) => https://satsrv06/
+# - start bigdata rdf store on satsrv06 for matcher service
+# - start matcher service on satsrv06 and connect with wonnodes on satsrv04 and proxied wonnode on satsrv05
+# - start siren solr server on satsrv06 as a need index
+# - start siren matcher on satsrv06 as a matcher and connect to matcher service
+# - start debug bot on satsrv06
+##############################################################################################################
+
 # fail the whole script if one command fails
 set -e
 
@@ -10,53 +29,23 @@ base_folder=/home/install/won/int
 # - emptying the postgres database (all need data is lost!) => is done later in the script anyway
 if [ "$GENERATE_NEW_CERTIFICATES" = true ] ; then
   echo generating new certificates! Old files and postgres need database will be deleted!
-  ssh root@satsrv04 rm -rf $base_folder/won-server-certs/*
-  ssh root@satsrv05 rm -rf $base_folder/won-server-certs/*
-  ssh root@satsrv06 rm -rf $base_folder/won-server-certs/*
-  ssh root@satsrv04 rm -rf $base_folder/won-client-certs/*
-  ssh root@satsrv05 rm -rf $base_folder/won-client-certs/*
-  ssh root@satsrv06 rm -rf $base_folder/won-client-certs/*
+  ssh root@satsrv04 rm -rf $base_folder/won-server-certs
+  ssh root@satsrv05 rm -rf $base_folder/won-server-certs
+  ssh root@satsrv06 rm -rf $base_folder/won-server-certs
+  ssh root@satsrv04 rm -rf $base_folder/won-client-certs
+  ssh root@satsrv05 rm -rf $base_folder/won-client-certs
+  ssh root@satsrv06 rm -rf $base_folder/won-client-certs
 fi
 
 # build the won docker images on every server of the cluster so that everywhere is the latest version available
 echo start docker build of images:
 
-# wonnode 1
-docker -H satsrv04:2375 build -t webofneeds/wonnode:int $WORKSPACE/webofneeds/won-docker/wonnode/
-docker -H satsrv05:2375 build -t webofneeds/wonnode:int $WORKSPACE/webofneeds/won-docker/wonnode/
-docker -H satsrv06:2375 build -t webofneeds/wonnode:int $WORKSPACE/webofneeds/won-docker/wonnode/
-docker -H satsrv07:2375 build -t webofneeds/wonnode:int $WORKSPACE/webofneeds/won-docker/wonnode/
-
-# owner 1
-docker -H satsrv04:2375 build -t webofneeds/owner:int $WORKSPACE/webofneeds/won-docker/owner/
-docker -H satsrv05:2375 build -t webofneeds/owner:int $WORKSPACE/webofneeds/won-docker/owner/
-docker -H satsrv06:2375 build -t webofneeds/owner:int $WORKSPACE/webofneeds/won-docker/owner/
-docker -H satsrv07:2375 build -t webofneeds/owner:int $WORKSPACE/webofneeds/won-docker/owner/
-
-# matcher service
-docker -H satsrv04:2375 build -t webofneeds/matcher_service:int $WORKSPACE/webofneeds/won-docker/matcher-service/
-docker -H satsrv05:2375 build -t webofneeds/matcher_service:int $WORKSPACE/webofneeds/won-docker/matcher-service/
-docker -H satsrv06:2375 build -t webofneeds/matcher_service:int $WORKSPACE/webofneeds/won-docker/matcher-service/
-docker -H satsrv07:2375 build -t webofneeds/matcher_service:int $WORKSPACE/webofneeds/won-docker/matcher-service/
-
-# siren matcher
-docker -H satsrv04:2375 build -t webofneeds/matcher_siren:int $WORKSPACE/webofneeds/won-docker/matcher-siren/
-docker -H satsrv05:2375 build -t webofneeds/matcher_siren:int $WORKSPACE/webofneeds/won-docker/matcher-siren/
-docker -H satsrv06:2375 build -t webofneeds/matcher_siren:int $WORKSPACE/webofneeds/won-docker/matcher-siren/
-docker -H satsrv07:2375 build -t webofneeds/matcher_siren:int $WORKSPACE/webofneeds/won-docker/matcher-siren/
-
-# wonnode/owner server certificate generator
-docker -H satsrv04:2375 build -t webofneeds/gencert:int $WORKSPACE/webofneeds/won-docker/gencert/
-docker -H satsrv05:2375 build -t webofneeds/gencert:int $WORKSPACE/webofneeds/won-docker/gencert/
-docker -H satsrv06:2375 build -t webofneeds/gencert:int $WORKSPACE/webofneeds/won-docker/gencert/
-docker -H satsrv07:2375 build -t webofneeds/gencert:int $WORKSPACE/webofneeds/won-docker/gencert/
-
-
-# bots
-docker -H satsrv04:2375 build -t webofneeds/bots:int $WORKSPACE/webofneeds/won-docker/bots/
-docker -H satsrv05:2375 build -t webofneeds/bots:int $WORKSPACE/webofneeds/won-docker/bots/
-docker -H satsrv06:2375 build -t webofneeds/bots:int $WORKSPACE/webofneeds/won-docker/bots/
-docker -H satsrv07:2375 build -t webofneeds/bots:int $WORKSPACE/webofneeds/won-docker/bots/
+ssh root@satsrv04 mkdir -p $base_folder/won-server-certs
+ssh root@satsrv05 mkdir -p $base_folder/won-server-certs
+ssh root@satsrv06 mkdir -p $base_folder/won-server-certs
+ssh root@satsrv04 mkdir -p $base_folder/won-client-certs
+ssh root@satsrv05 mkdir -p $base_folder/won-client-certs
+ssh root@satsrv06 mkdir -p $base_folder/won-client-certs
 
 
 # start the won containers on dedicated servers of the cluster
@@ -64,7 +53,7 @@ echo run docker containers:
 
 # copy the openssl.conf file to the server where the certificates are generated
 ssh root@satsrv05 mkdir -p $base_folder/won-server-certs
-scp $WORKSPACE/webofneeds/won-docker/gencert/openssl-dev-int.conf root@satsrv05:$base_folder/openssl-dev-int.conf
+scp $WORKSPACE/webofneeds/won-docker/gencert/openssl-int.conf root@satsrv05:$base_folder/openssl-int.conf
 
 # wonnode/owner server certificate generator
 # Please note that value of PASS (if you set a non-default password) should be the same used in your server.xml for
@@ -73,15 +62,17 @@ scp $WORKSPACE/webofneeds/won-docker/gencert/openssl-dev-int.conf root@satsrv05:
 # PASS=file:<your_file_with_password>"
 
 # satsrv04 => standard certificate creation for this host only
+docker -H satsrv04:2375 build -t webofneeds/gencert:int $WORKSPACE/webofneeds/won-docker/gencert/
 docker -H satsrv04:2375 rm gencert_int || echo 'No docker container found to remove with name: gencert_int'
 docker -H satsrv04:2375 run --name=gencert_int -e CN="satsrv04.researchstudio.at" \
 -e "PASS=pass:${won_certificate_passwd}" -v $base_folder/won-server-certs:/usr/local/certs/out/ webofneeds/gencert:int
 
 # satsrv05 => certificate creation for multiple hosts
+docker -H satsrv05:2375 build -t webofneeds/gencert:int $WORKSPACE/webofneeds/won-docker/gencert/
 docker -H satsrv05:2375 rm gencert_int || echo 'No docker container found to remove with name: gencert_int'
 docker -H satsrv05:2375 run --name=gencert_int -e CN="satsrv06.researchstudio.at" \
 -e "OPENSSL_CONFIG_FILE=/usr/local/openssl.conf"  -e "PASS=pass:${won_certificate_passwd}" \
--v $base_folder/won-server-certs:/usr/local/certs/out/ -v $base_folder/openssl-dev-int.conf:/usr/local/openssl.conf \
+-v $base_folder/won-server-certs:/usr/local/certs/out/ -v $base_folder/openssl-int.conf:/usr/local/openssl.conf \
 webofneeds/gencert:int
 
 # get the certificates and create a password file (for the nginx) to read the certificate
@@ -120,6 +111,7 @@ sleep 10
 
 
 # wonnode 1
+docker -H satsrv04:2375 build -t webofneeds/wonnode:int $WORKSPACE/webofneeds/won-docker/wonnode/
 docker -H satsrv04:2375 stop wonnode_int || echo 'No docker container found to stop with name: wonnode_int'
 docker -H satsrv04:2375 rm wonnode_int || echo 'No docker container found to remove with name: wonnode_int'
 docker -H satsrv04:2375 run --name=wonnode_int -d -e "uri.host=satsrv04.researchstudio.at" -e "http.port=8889" -e \
@@ -137,9 +129,10 @@ docker -H satsrv04:2375 run --name=wonnode_int -d -e "uri.host=satsrv04.research
 webofneeds/wonnode:int
 
 # wonnode 2 (used with the nginx proxy that runs on satsrv06)
+docker -H satsrv05:2375 build -t webofneeds/wonnode:int $WORKSPACE/webofneeds/won-docker/wonnode/
 docker -H satsrv05:2375 stop wonnode_int || echo 'No docker container found to stop with name: wonnode_int'
 docker -H satsrv05:2375 rm wonnode_int || echo 'No docker container found to remove with name: wonnode_int'
-docker -H satsrv05:2375 run --name=wonnode_int -d -e "uri.host=satsrv05.researchstudio.at" -e "http.port=8889" \
+docker -H satsrv05:2375 run --name=wonnode_int -d -e "uri.host=satsrv06.researchstudio.at" -e "http.port=8889" \
 -e "uri.prefix=https://satsrv06.researchstudio.at/won" \
 -e "client.authentication.behind.proxy=true" \
 -e "activemq.broker.port=61617" -p 8889:8443 -p 61617:61617 \
@@ -155,50 +148,7 @@ docker -H satsrv05:2375 run --name=wonnode_int -d -e "uri.host=satsrv05.research
 -m 350m \
 webofneeds/wonnode:int
 
-
 sleep 20
-
-# expect OWNER won-mail-sender host, user and password (i.e. configuration for no-replay won-owner-app-email-account) be
-# set as environment variables, e.g. MAIL_USER=changeuser MAIL_PASS=changepass MAIL_HOST=smtp.changehost.com
-echo ${MAIL_USER} at ${MAIL_HOST} is used as owner no-replay won-owner-app-email-account
-
-# owner 1
-docker -H satsrv04:2375 stop owner_int || echo 'No docker container found to stop with name: owner_int'
-docker -H satsrv04:2375 rm owner_int || echo 'No docker container found to remove with name: owner_int'
-docker -H satsrv04:2375 run --name=owner_int -d -e "node.default.host=satsrv04.researchstudio.at" \
--e "uri.host=satsrv04.researchstudio.at" -e "http.port=8082" \
--e "email.from.won.user=${MAIL_USER}" -e "email.from.won.password=${MAIL_PASS}" -e "email.from.won.smtp.host=${MAIL_HOST}" \
--e "node.default.http.port=8889" -p 8082:8443 \
--v $base_folder/won-server-certs:/usr/local/tomcat/conf/ssl/ \
--v $base_folder/won-client-certs/owner_int:/usr/local/tomcat/won/client-certs/ \
--e "db.sql.jdbcDriverClass=org.postgresql.Driver" \
--e "db.sql.jdbcUrl=jdbc:postgresql://satsrv04:5433/won_owner" \
--e "db.sql.user=won" -e "db.sql.password=won" \
--e "CERTIFICATE_PASSWORD=${won_certificate_passwd}" \
--p 9011:9011 \
--e "JMX_OPTS=-Dcom.sun.management.jmxremote.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=9011 -Djava.rmi.server.hostname=satsrv04.researchstudio.at" \
--e "JMEM_OPTS=-Xmx1000m -XX:MaxMetaspaceSize=200m -XX:+HeapDumpOnOutOfMemoryError" \
-webofneeds/owner:int
-
-# owner 2
-docker -H satsrv05:2375 stop owner_int || echo 'No docker container found to stop with name: owner_int'
-docker -H satsrv05:2375 rm owner_int || echo 'No docker container found to remove with name: owner_int'
-docker -H satsrv05:2375 run --name=owner_int -d -e "node.default.host=satsrv06.researchstudio.at" \
--e "uri.host=satsrv05.researchstudio.at" -e "http.port=8082" \
--e "email.from.won.user=${MAIL_USER}" -e "email.from.won.password=${MAIL_PASS}" -e "email.from.won.smtp.host=${MAIL_HOST}" \
--e "node.default.http.port=443" -p 8082:8443 \
--e "uri.prefix.node.default=https://satsrv06.researchstudio.at/won" \
--v $base_folder/won-server-certs:/usr/local/tomcat/conf/ssl/ \
--v $base_folder/won-client-certs/owner_int:/usr/local/tomcat/won/client-certs/ \
--e "db.sql.jdbcDriverClass=org.postgresql.Driver" \
--e "db.sql.jdbcUrl=jdbc:postgresql://satsrv05:5433/won_owner" \
--e "db.sql.user=won" -e "db.sql.password=won" \
--e "CERTIFICATE_PASSWORD=${won_certificate_passwd}" \
--p 9011:9011 \
--e "JMX_OPTS=-Dcom.sun.management.jmxremote.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=9011 -Djava.rmi.server.hostname=satsrv05.researchstudio.at" \
--e "JMEM_OPTS=-Xmx170m -XX:MaxMetaspaceSize=160m -XX:+HeapDumpOnOutOfMemoryError" \
--m 350m \
-webofneeds/owner:int
 
 # bigdata
 docker -H satsrv06:2375 pull webofneeds/bigdata
@@ -208,6 +158,7 @@ docker -H satsrv06:2375 run --name=bigdata_int -d -p 10000:9999 \
 -m 256m webofneeds/bigdata
 
 # matcher service
+docker -H satsrv06:2375 build -t webofneeds/matcher_service:int $WORKSPACE/webofneeds/won-docker/matcher-service/
 docker -H satsrv06:2375 stop matcher_service_int || echo 'No docker container found to stop with name: matcher_service_int'
 docker -H satsrv06:2375 rm matcher_service_int || echo 'No docker container found to remove with name: matcher_service_int'
 docker -H satsrv06:2375 run --name=matcher_service_int -d -e "node.host=satsrv06.researchstudio.at" \
@@ -231,9 +182,54 @@ docker -H satsrv06:2375 run --name=sirensolr_int -d -p 7071:8080 -p 8984:8983 \
 -e CATALINA_OPTS="-Xmx200m  -XX:MaxPermSize=150m -XX:+HeapDumpOnOutOfMemoryError -Dcom.sun.management.jmxremote.port=9012 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=9012 -Djava.rmi.server.hostname=satsrv06.researchstudio.at" \
 -m 350m webofneeds/sirensolr
 
+# expect OWNER won-mail-sender host, user and password (i.e. configuration for no-replay won-owner-app-email-account) be
+# set as environment variables, e.g. MAIL_USER=changeuser MAIL_PASS=changepass MAIL_HOST=smtp.changehost.com
+echo ${MAIL_USER} at ${MAIL_HOST} is used as owner no-replay won-owner-app-email-account
+
+# owner 1
+docker -H satsrv04:2375 build -t webofneeds/owner:int $WORKSPACE/webofneeds/won-docker/owner/
+docker -H satsrv04:2375 stop owner_int || echo 'No docker container found to stop with name: owner_int'
+docker -H satsrv04:2375 rm owner_int || echo 'No docker container found to remove with name: owner_int'
+docker -H satsrv04:2375 run --name=owner_int -d -e "node.default.host=satsrv04.researchstudio.at" \
+-e "uri.host=satsrv04.researchstudio.at" -e "http.port=8082" \
+-e "email.from.won.user=${MAIL_USER}" -e "email.from.won.password=${MAIL_PASS}" -e "email.from.won.smtp.host=${MAIL_HOST}" \
+-e "node.default.http.port=8889" -p 8082:8443 \
+-v $base_folder/won-server-certs:/usr/local/tomcat/conf/ssl/ \
+-v $base_folder/won-client-certs/owner_int:/usr/local/tomcat/won/client-certs/ \
+-e "db.sql.jdbcDriverClass=org.postgresql.Driver" \
+-e "db.sql.jdbcUrl=jdbc:postgresql://satsrv04:5433/won_owner" \
+-e "db.sql.user=won" -e "db.sql.password=won" \
+-e "CERTIFICATE_PASSWORD=${won_certificate_passwd}" \
+-p 9011:9011 \
+-e "JMX_OPTS=-Dcom.sun.management.jmxremote.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=9011 -Djava.rmi.server.hostname=satsrv04.researchstudio.at" \
+-e "JMEM_OPTS=-Xmx1000m -XX:MaxMetaspaceSize=200m -XX:+HeapDumpOnOutOfMemoryError" \
+webofneeds/owner:int
+
+# owner 2
+docker -H satsrv05:2375 build -t webofneeds/owner:int $WORKSPACE/webofneeds/won-docker/owner/
+docker -H satsrv05:2375 stop owner_int || echo 'No docker container found to stop with name: owner_int'
+docker -H satsrv05:2375 rm owner_int || echo 'No docker container found to remove with name: owner_int'
+docker -H satsrv05:2375 run --name=owner_int -d -e "node.default.host=satsrv06.researchstudio.at" \
+-e "uri.host=satsrv06.researchstudio.at" -e "http.port=8082" \
+-e "email.from.won.user=${MAIL_USER}" -e "email.from.won.password=${MAIL_PASS}" -e "email.from.won.smtp.host=${MAIL_HOST}" \
+-e "node.default.http.port=443" -p 8082:8443 \
+-e "uri.prefix.node.default=https://satsrv06.researchstudio.at/won" \
+-v $base_folder/won-server-certs:/usr/local/tomcat/conf/ssl/ \
+-v $base_folder/won-client-certs/owner_int:/usr/local/tomcat/won/client-certs/ \
+-e "db.sql.jdbcDriverClass=org.postgresql.Driver" \
+-e "db.sql.jdbcUrl=jdbc:postgresql://satsrv05:5433/won_owner" \
+-e "db.sql.user=won" -e "db.sql.password=won" \
+-e "CERTIFICATE_PASSWORD=${won_certificate_passwd}" \
+-p 9011:9011 \
+-e "JMX_OPTS=-Dcom.sun.management.jmxremote.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port=9011 -Djava.rmi.server.hostname=satsrv05.researchstudio.at" \
+-e "JMEM_OPTS=-Xmx170m -XX:MaxMetaspaceSize=160m -XX:+HeapDumpOnOutOfMemoryError" \
+-m 350m \
+webofneeds/owner:int
+
 sleep 10
 
 # siren matcher
+docker -H satsrv06:2375 build -t webofneeds/matcher_siren:int $WORKSPACE/webofneeds/won-docker/matcher-siren/
 docker -H satsrv06:2375 stop matcher_siren_int || echo 'No docker container found to stop with name: matcher_siren_int'
 docker -H satsrv06:2375 rm matcher_siren_int || echo 'No docker container found to remove with name: matcher_siren_int'
 docker -H satsrv06:2375 run --name=matcher_siren_int -d -e "node.host=satsrv06.researchstudio.at" \
@@ -248,6 +244,7 @@ docker -H satsrv06:2375 run --name=matcher_siren_int -d -e "node.host=satsrv06.r
 
 
 sleep 20
+docker -H satsrv06:2375 build -t webofneeds/bots:int $WORKSPACE/webofneeds/won-docker/bots/
 docker -H satsrv06:2375 stop need_creator_bot_int || echo 'No docker container found to stop with name: need_creator_bot_int'
 docker -H satsrv06:2375 rm need_creator_bot_int || echo 'No docker container found to remove with name: need_creator_bot_int'
 docker -H satsrv06:2375 stop echo_bot_int || echo 'No docker container found to stop with name: echo_bot_int'
