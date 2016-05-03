18b41128e44c2f13207875af1d02eb26b2bb2971
moru <kevin.singer@student.tuwien.ac.at>
2016-05-03 16:25:17 +0200

    Componentalized overview-matches

---
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/matches.js
@@ -0,0 +1,138 @@
+;
+
+import angular from 'angular';
+import overviewTitleBarModule from './overview-title-bar';
+import matchesFlowItemModule from './matches-flow-item';
+import matchesGridItemModule from './matches-grid-item';
+import matchesListItemModule from './matches-list-item';
+import sendRequestModule from './send-request';
+
+import { attach,mapToMatches} from '../utils';
+import { labels } from '../won-label-utils';
+import { actionCreators }  from '../actions/actions';
+import { selectAllByConnections } from '../selectors';
+
+const serviceDependencies = ['$ngRedux', '$scope'];
+let template = `
+    <div class="overviewmatchescontent">
+        <a class="curtain" ng-if="self.connection"></a>
+        <div class="omc__inner">
+            <div class="omc__header">
+                <div class="dummy"></div>
+                <div class="title" ng-if="!self.post">Matches to your needs</div>
+                <div class="omc__header__viewtype">
+                    <a ui-sref="overviewMatches({layout: 'tiles'})">
+                        <img ng-src="{{self.layout === 'tiles' ? 'generated/icon-sprite.svg#ico-filter_tile_selected' : 'generated/icon-sprite.svg#ico-filter_tile'}}"
+                         class="omc__header__viewtype__icon clickable"/>
+                    </a>
+                    <a ui-sref="overviewMatches({layout: 'grid'})">
+                        <img ng-src="{{self.layout === 'grid' ? 'generated/icon-sprite.svg#ico-filter_compact_selected' : 'generated/icon-sprite.svg#ico-filter_compact'}}"
+                         class="omc__header__viewtype__icon clickable"/>
+                    </a>
+                    <a ui-sref="overviewMatches({layout: 'list'})">
+                        <img ng-src="{{self.layout === 'list' ? 'generated/icon-sprite.svg#ico-filter_list_selected' : 'generated/icon-sprite.svg#ico-filter_list'}}"
+                         class="omc__header__viewtype__icon clickable"/>
+                    </a>
+                </div>
+            </div>
+            <div ng-if="self.layout === 'tiles'" class="omc__content__flow">
+                <won-matches-flow-item
+                        connection-uri="m.getIn(['connection','uri'])"
+                        ng-repeat="m in self.matches">
+                </won-matches-flow-item>
+            </div>
+            <div ng-if="self.layout === 'grid'" class="omc__content__grid">
+                <won-matches-grid-item
+                        connection-uri="m.getIn(['connection','uri'])"
+                        ng-repeat="m in self.matches">
+                </won-matches-grid-item>
+            </div>
+            <div ng-if="self.layout === 'list'" class="omc__content__list">
+                <won-matches-list-item
+                        item="item"
+                        ng-repeat="(key,item) in self.matchesOfNeed">
+                </won-matches-list-item>
+            </div>
+        </div>
+        <div class="omc__sendrequest" ng-if="self.connection">
+            <won-send-request></won-send-request>
+        </div>
+    </div>
+`
+class Controller {
+    constructor() {
+        attach(this, serviceDependencies, arguments);
+
+        window.omc=this;
+
+        this.labels = labels;
+
+        const selectFromState = (state) => {
+            const allMatchesByConnections = selectAllByConnections(state)
+                    .filter(conn => conn.getIn(['connection', 'hasConnectionState']) === won.WON.Suggested);
+
+            const connectionUri = decodeURIComponent(state.getIn(['router', 'currentParams', 'connectionUri']));
+            const encodedPostUri =
+                state.getIn(['router', 'currentParams', 'postUri']) ||
+                state.getIn(['router', 'currentParams', 'myUri']); //deprecated parameter
+            const postUri = decodeURIComponent(encodedPostUri)
+
+            // either of 'tiles', 'grid', 'list'
+            let layout = state.getIn(['router','currentParams','layout']);
+            if(!layout) {
+                layout = 'tiles';
+            }
+
+            let matchesByConnectionUri;
+            if(state.getIn(['router', 'currentParams', 'myUri']) === undefined) { //overview
+                matchesByConnectionUri = allMatchesByConnections.toList();
+            } else { // post-owner view
+                matchesByConnectionUri = allMatchesByConnections
+                    .filter(conn => conn.getIn(['ownNeed', 'uri']) === postUri)
+                    .toList();
+            }
+
+            return {
+                layout,
+                connection: state.getIn(['connections', connectionUri]),
+                matches: matchesByConnectionUri.toArray(),
+                matchesOfNeed: mapToMatches(matchesByConnectionUri.toJS()),//TODO plz don't do `.toJS()`. every time an ng-binding somewhere cries.
+                post: state.getIn(['needs','ownNeeds', postUri]),
+            };
+        };
+        const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
+      //  this.loadMatches();
+        this.$scope.$on('$destroy', disconnect);
+    }
+
+    loadMatches(){
+        this.matches__load(
+            this.$ngRedux.getState().getIn(['needs','ownNeeds']).toJS()
+        )
+    }
+
+}
+Controller.$inject = serviceDependencies;
+
+function genComponentConf() {
+    return {
+        restrict: 'E',
+        controller: Controller,
+        controllerAs: 'self',
+        bindToController: true, //scope-bindings -> ctrl
+        scope: {},
+        template: template
+    }
+}
+
+export default angular.module('won.owner.components.matches', [
+        overviewTitleBarModule,
+        matchesFlowItemModule,
+        matchesGridItemModule,
+        matchesListItemModule,
+        sendRequestModule
+    ])
+    .directive('wonMatches', genComponentConf)
+    //.controller('OverviewMatchesController', [...serviceDependencies,OverviewMatchesController])
+    .name;
+
--- webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-matches/overview-matches.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/overview-matches/overview-matches.js
@@ -1,80 +1,33 @@
 ;
 
 import angular from 'angular';
+
 import overviewTitleBarModule from '../overview-title-bar';
-import matchesFlowItemModule from '../matches-flow-item';
-import matchesGridItemModule from '../matches-grid-item';
-import matchesListItemModule from '../matches-list-item';
-import sendRequestModule from '../send-request';
+import matchesModule from  '../matches';
+import topnavModule from '../topnav';
 
-import { attach,mapToMatches} from '../../utils';
-import { labels } from '../../won-label-utils';
+import { attach } from '../../utils';
 import { actionCreators }  from '../../actions/actions';
-import { selectAllByConnections } from '../../selectors';
 
-const serviceDependencies = ['$q', '$ngRedux', '$scope'];
+const serviceDependencies = ['$ngRedux', '$scope'];
 class OverviewMatchesController {
     constructor() {
         attach(this, serviceDependencies, arguments);
 
         window.omc=this;
 
-        this.labels = labels;
-
-        const selectFromState = (state) => {
-            const allMatchesByConnections = selectAllByConnections(state)
-                    .filter(conn => conn.getIn(['connection', 'hasConnectionState']) === won.WON.Suggested);
-
-            const connectionUri = decodeURIComponent(state.getIn(['router', 'currentParams', 'connectionUri']));
-            const encodedPostUri =
-                state.getIn(['router', 'currentParams', 'postUri']) ||
-                state.getIn(['router', 'currentParams', 'myUri']); //deprecated parameter
-            const postUri = decodeURIComponent(encodedPostUri)
-
-            // either of 'tiles', 'grid', 'list'
-            let layout = state.getIn(['router','currentParams','layout']);
-            if(!layout) {
-                layout = 'tiles';
-            }
-
-            let matchesByConnectionUri;
-            if(state.getIn(['router', 'currentParams', 'myUri']) === undefined) { //overview
-                matchesByConnectionUri = allMatchesByConnections.toList();
-            } else { // post-owner view
-                matchesByConnectionUri = allMatchesByConnections
-                    .filter(conn => conn.getIn(['ownNeed', 'uri']) === postUri)
-                    .toList();
-            }
-
-            return {
-                layout,
-                connection: state.getIn(['connections', connectionUri]),
-                matches: matchesByConnectionUri.toArray(),
-                matchesOfNeed: mapToMatches(matchesByConnectionUri.toJS()),//TODO plz don't do `.toJS()`. every time an ng-binding somewhere cries.
-                post: state.getIn(['needs','ownNeeds', postUri]),
-            };
-        };
+        const selectFromState = (state) => ({ });
         const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
-      //  this.loadMatches();
         this.$scope.$on('$destroy', disconnect);
     }
-
-    loadMatches(){
-        this.matches__load(
-            this.$ngRedux.getState().getIn(['needs','ownNeeds']).toJS()
-        )
-    }
-
 }
 
 
 export default angular.module('won.owner.components.overviewMatches', [
+    topnavModule,
     overviewTitleBarModule,
-    matchesFlowItemModule,
-    matchesGridItemModule,
-    matchesListItemModule,
-    sendRequestModule
+    matchesModule
 ])
-    .controller('OverviewMatchesController', [...serviceDependencies,OverviewMatchesController])
+    .controller('OverviewMatchesController', [...serviceDependencies, OverviewMatchesController])
     .name;
 
