19afa20695fbbd10ee2a64a8810bbe4bc567b5e7
moru <kevin.singer@student.tuwien.ac.at>
2016-05-10 18:36:03 +0200

    Moved userreducer to a seperate file.

---
+++ webofneeds/won-owner-webapp/src/main/webapp/app/reducers/user-reducer.js
@@ -0,0 +1,37 @@
+/**
+ * Created by ksinger on 10.05.2016.
+ */
+import { actionTypes } from '../actions/actions';
+import { repeatVar } from '../utils';
+import Immutable from 'immutable';
+import { createReducer } from 'redux-immutablejs'
+import { combineReducersStable } from '../redux-utils';
+import { buildCreateMessage } from '../won-message-utils';
+import won from '../won-es6';
+
+const initialState = Immutable.fromJS({});
+
+export default function(userData = initialState, action = {}) {
+    switch(action.type) {
+        case actionTypes.user.loggedIn:
+            var {loggedIn, email} = action.payload
+            if(loggedIn == true){
+                console.log('reducers.js: received successful-login action from app-server');
+                return Immutable.fromJS({loggedIn: true, email: email});
+            }else{
+                console.log('reducers.js: received notlogged in action from app-server');
+                return Immutable.fromJS({loggedIn: false});
+            }
+
+        case actionTypes.user.loginFailed:
+            console.log('reducers.js: received UNsuccessful-login action from app-server');
+            return Immutable.fromJS({loginError: action.payload.loginError});
+
+        case actionTypes.user.registerFailed:
+            console.log('reducers.js: received UNsuccessful-login action from app-server');
+            return Immutable.fromJS({registerError: action.payload.registerError});
+
+        default:
+            return userData;
+    }
+}
