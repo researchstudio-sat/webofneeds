a322d3a32bcbe55910988b4cf7b5a258570cd16a
moru <kevin.singer@student.tuwien.ac.at>
2016-05-10 17:18:58 +0200

    Changed visitor titlebar to take post from state.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/visitor-title-bar.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/visitor-title-bar.js
@@ -4,8 +4,13 @@
 ;
 
 import angular from 'angular';
+import { attach, mapToMatches, decodeUriComponentProperly } from '../utils';
+import won from '../won-es6';
 import { labels } from '../won-label-utils';
+import { selectOpenPost } from '../selectors';
+import { actionCreators }  from '../actions/actions';
 
+const serviceDependencies = ['$ngRedux', '$scope'];
 function genComponentConf() {
     let template = `
         <nav class="visitor-title-bar">
@@ -14,10 +19,10 @@ function genComponentConf() {
                     <a ng-click="self.back()">
                         <img src="generated/icon-sprite.svg#ico36_backarrow" class="vtb__icon">
                     </a>
-                    <won-square-image title="self.item.title" src="self.item.titleImgSrc"></won-square-image>
+                    <won-square-image title="self.post.get('title')" src="self.post.get('titleImgSrc')"></won-square-image>
                     <div class="vtb__inner__left__titles">
-                        <h1 class="vtb__title">{{self.item.title}}</h1>
-                        <div class="vtb__inner__left__titles__type">{{self.labels.type[self.item.type]}}, {{self.item.creationDate}} </div>
+                        <h1 class="vtb__title">{{self.post.get('title')}}</h1>
+                        <div class="vtb__inner__left__titles__type">{{self.labels.type[self.post.get('type')]}}, {{self.post.get('creationDate')}} </div>
                     </div>
                 </div>
                 <div class="vtb__inner__right">
@@ -26,7 +31,7 @@ function genComponentConf() {
                         <li ng-class="self.selection == 0? 'vtb__tabs__selected' : ''" ng-click="self.selection = 0">
                         <a ui-sref="postVisitorMsgs({myUri: 'http://example.org/121337345'})">
                             Messages
-                            <span class="vtb__tabs__unread">{{self.item.messages.length}}</span>
+                            <span class="vtb__tabs__unread">{{self.post.get('messages').length}}</span>
                         </a></li>
                         <li ng-class="self.selection == 1? 'vtb__tabs__selected' : ''" ng-click="self.selection = 1">
                         <a ui-sref="postVisitor({myUri: 'http://example.org/121337345'})">
@@ -40,10 +45,18 @@ function genComponentConf() {
 
     class Controller {
         constructor() {
+            attach(this, serviceDependencies, arguments);
             this.labels = labels;
+            window.vtb4dbg = this;
+            const selectFromState = state => ({
+                post: selectOpenPost(state)
+            });
+            const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
+            this.$scope.$on('$destroy', disconnect);
         }
         back() { window.history.back() }
     }
+    Controller.$inject = serviceDependencies;
 
     return {
         restrict: 'E',
