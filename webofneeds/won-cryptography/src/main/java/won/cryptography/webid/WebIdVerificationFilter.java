12e84947feb138be64fae131601cdc7b0b786b00
Florian Kleedorfer <florian.kleedorfer@researchstudio.at>
2016-05-25 14:32:25 +0200

    fixed typo

--- webofneeds/won-cryptography/src/main/java/won/cryptography/webid/WebIdVerificationFilter.java
+++ webofneeds/won-cryptography/src/main/java/won/cryptography/webid/WebIdVerificationFilter.java
@@ -74,7 +74,7 @@ public class WebIdVerificationFilter implements Filter
       webIDs = CertificateService.getWebIdFromSubjectAlternativeNames(cert);
     } catch (CertificateParsingException e) {
       logger.error("error extracting subject alternative names", e);
-      response.sendError(HttpServletResponse.SC_FORBIDDEN, "No cerificate provided for WebID protected resource");
+      response.sendError(HttpServletResponse.SC_FORBIDDEN, "No certificate provided for WebID protected resource");
       return false;
     }
     if (webIDs == null || webIDs.isEmpty()) {
