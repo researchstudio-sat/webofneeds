516422ee67649997b29ce94f32f3af4effc44f17
Florian Kleedorfer <florian.kleedorfer@researchstudio.at>
2016-05-25 17:48:47 +0200

    adapted message validation so it generates useful error messages

--- webofneeds/won-core/src/main/java/won/protocol/message/WonSparqlValidator.java
+++ webofneeds/won-core/src/main/java/won/protocol/message/WonSparqlValidator.java
@@ -33,46 +33,73 @@ public class WonSparqlValidator
     this.name = name;
   }
 
-  public boolean validate(Dataset input) {
+  public ValidationResult validate(Dataset input) {
     if (constraint.isAskType()) {
       return validateAsk(input);
     } else if (constraint.isSelectType()) {
       return validateSelect(input);
     }
-    return false;
+    return new ValidationResult(false,"Invalid constraint: " + constraint.toString());
   }
 
-  private boolean validateSelect(final Dataset input) {
-    QueryExecution qe = QueryExecutionFactory.create(constraint, input);
-    ResultSet result = qe.execSelect();
-    // for debugging only, uncomment when writing new validation queries
-    //printResult((QueryExecutionFactory.create(constraint, input)).execSelect());
-
-    while (result.hasNext()) {
-      Binding binding = result.nextBinding();
-      Node node = binding.get(SELECT_VALIDATION_VARIABLE);
-      if (node != null) {
-        if (node.isLiteral()) {
-          String resultString = node.getLiteralValue().toString();
-          if (SELECT_VALIDATION_PASSED_VALUE.equals(resultString)) {
-            return true;
+  private ValidationResult validateSelect(final Dataset input) {
+    try (QueryExecution qe = QueryExecutionFactory.create(constraint, input))
+    {
+      ResultSet result = qe.execSelect();
+      while (result.hasNext()) {
+        Binding binding = result.nextBinding();
+        Node node = binding.get(SELECT_VALIDATION_VARIABLE);
+        if (node != null) {
+          if (node.isLiteral()) {
+            String resultString = node.getLiteralValue().toString();
+            if (SELECT_VALIDATION_PASSED_VALUE.equals(resultString)) {
+              return new ValidationResult();
+            } else {
+              return new ValidationResult(false, "SELECT query produced this binding: " + binding
+                .toString());
+            }
           }
         }
       }
+      return new ValidationResult(false, "SELECT query did not produce a binding for required variable 'check'");
     }
-    return false;
   }
 
   private void printResult(final ResultSet result) {
     System.out.println(ResultSetFormatter.asText(result));
   }
 
-  private boolean validateAsk(final Dataset input) {
-    QueryExecution qe = QueryExecutionFactory.create(constraint, input);
-    return !qe.execAsk();
+  private ValidationResult validateAsk(final Dataset input) {
+    try (QueryExecution qe = QueryExecutionFactory.create(constraint, input)) {
+      if (qe.execAsk()){
+        return new ValidationResult(false, "ASK query returned true");
+      }
+      return new ValidationResult();
+    }
   }
 
   public String getName() {
     return name;
+  }
+
+  public static class ValidationResult{
+    private boolean valid = true;
+    private String errorMessage = null;
+
+    public ValidationResult() {
+    }
+
+    public ValidationResult(final boolean valid, final String errorMessage) {
+      this.valid = valid;
+      this.errorMessage = errorMessage;
+    }
+
+    public boolean isValid() {
+      return valid;
+    }
+
+    public String getErrorMessage() {
+      return errorMessage;
+    }
   }
 }
