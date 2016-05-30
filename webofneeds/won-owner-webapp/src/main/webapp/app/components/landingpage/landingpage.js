5ad48a2bfacab786e1417052efadac61906ddaa5
Florian Kleedorfer <florian.kleedorfer@researchstudio.at>
2016-05-30 19:18:38 +0200

    improved text

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/landingpage/landingpage.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/landingpage/landingpage.js
@@ -12,12 +12,11 @@ import { actionCreators }  from '../../actions/actions';
 const serviceDependencies = ['$q', '$ngRedux', '$scope', /*'$routeParams' /*injections as strings here*/];
 
 const workGrid = [{imageSrc: 'generated/icon-sprite.svg#ico36_description', text: 'Post your need anonymously', detail: 'Needs can be very personal, so privacy is important. You don\'t have to reveal your identity here.'},
-    {imageSrc: 'generated/icon-sprite.svg#ico36_match', text: 'Your need is automatically matched with other users\'' +
-    ' needs', detail: 'Based on the information you provide, we will try to find the best matches for you'},
-    {imageSrc: 'generated/icon-sprite.svg#ico36_incoming', text: 'Request Contact', detail: 'If you\'re interested,' +
-    ' make a contact request - or get one if your counterpart is faster than you', separatorText: 'or', text2: 'get' +
-    ' other users\' contact' +
-    ' requests'},
+    {imageSrc: 'generated/icon-sprite.svg#ico36_match', text: 'Get matches', detail: 'Based on the' +
+    ' information you provide, we will try to connect you with others'},
+    {imageSrc: 'generated/icon-sprite.svg#ico36_incoming', text: 'Request contact', detail: 'If you\'re interested,' +
+    ' make a contact request - or get one if your counterpart is faster than you', separatorText: 'or', text2: 'Be' +
+    ' contacted'},
     {imageSrc: 'generated/icon-sprite.svg#ico36_message', text: 'Interact and exchange', detail: 'You found someone' +
     ' who has what you need, wants to meet or change something in your common environment? Go chat with them! '}];
 
