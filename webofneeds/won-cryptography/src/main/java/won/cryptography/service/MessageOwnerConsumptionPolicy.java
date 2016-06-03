dd929d749998e477fa0140407eee91901ca16875
Florian Kleedorfer <florian.kleedorfer@researchstudio.at>
2016-06-03 09:14:40 +0200

    improved log output, externalized alias generation

--- webofneeds/won-cryptography/src/main/java/won/cryptography/service/MessageOwnerConsumptionPolicy.java
+++ webofneeds/won-cryptography/src/main/java/won/cryptography/service/MessageOwnerConsumptionPolicy.java
@@ -3,9 +3,11 @@ package won.cryptography.service;
 import org.apache.activemq.broker.ConnectionContext;
 import org.apache.activemq.command.Message;
 import org.apache.activemq.security.MessageAuthorizationPolicy;
-import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import won.cryptography.ssl.AliasFromFingerprintGenerator;
+import won.cryptography.ssl.AliasGenerator;
 
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 
 /**
@@ -16,6 +18,8 @@ import java.security.cert.X509Certificate;
 public class MessageOwnerConsumptionPolicy implements MessageAuthorizationPolicy
 {
   private static final String OWNER_OUT_DESTINATION_NAME_START = "OwnerProtocol.Out.";
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  private AliasGenerator aliasGenerator = new AliasFromFingerprintGenerator();
 
   @Override
   public boolean isAllowedToConsume(final ConnectionContext context, final Message message) {
@@ -26,7 +30,7 @@ public class MessageOwnerConsumptionPolicy implements MessageAuthorizationPolicy
   }
 
   /**
-   * Owner id is defined as a sha-1 fingerprint of owner certificate, based on the results of
+   * Owner id is defined as a sha-1 fingerprint of owner public key , based on the results of
    * comparing the owner id in queue name and the provided certificate fingerprint, the access
    * to read from that queue can be granted or denied.
    * @param context
@@ -38,8 +42,8 @@ public class MessageOwnerConsumptionPolicy implements MessageAuthorizationPolicy
       X509Certificate ownerCert = ((X509Certificate[]) context.getConnectionState().getInfo().getTransportContext())[0];
       String ownerSha1Fingerprint = null;
       try {
-        ownerSha1Fingerprint = DigestUtils.shaHex(ownerCert.getEncoded());
-      } catch (CertificateEncodingException e) {
+        ownerSha1Fingerprint = aliasGenerator.generateAlias(ownerCert);
+      } catch (Exception  e) {
         new IllegalArgumentException("Could not calculate sha-1 of owner certificate", e);
       }
       String forOwnerId = message.getDestination().getPhysicalName().substring(OWNER_OUT_DESTINATION_NAME_START
@@ -47,8 +51,12 @@ public class MessageOwnerConsumptionPolicy implements MessageAuthorizationPolicy
       if (ownerSha1Fingerprint.equals(forOwnerId)) {
         return true;
       }
+      logger.info("denying message consumption to owner as public key hash does not equal owner id");
+      return false;
+    } else {
+      logger.info("denying message consumption to owner transportContext is not an X.509 certificate");
+      return false;
     }
-    return false;
   }
 
   private boolean isForSpecificOwner(final Message message) {
