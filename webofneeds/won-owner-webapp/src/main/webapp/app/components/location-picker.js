3dd18719d005ab9b8e36d8b0cdae6244bdf2294b
moru <kevin.singer@student.tuwien.ac.at>
2016-07-21 18:42:21 +0200

    Listening for doneTyping.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/location-picker.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/location-picker.js
@@ -6,11 +6,12 @@ import won from '../won-es6';
 import angular from 'angular';
 import 'ng-redux';
 //import { labels } from '../won-label-utils';
-import { attach } from '../utils.js';
+import { attach, searchNominatim } from '../utils.js';
 import { actionCreators }  from '../actions/actions';
 import { } from '../selectors';
+import { doneTypingBufferNg } from '../cstm-ng-utils'
 
-const serviceDependencies = ['$scope', '$ngRedux'];
+const serviceDependencies = ['$scope', '$ngRedux', '$element'];
 function genComponentConf() {
     //TODO input as text-input or contenteditable? need to overl
     let template = `
@@ -30,12 +31,34 @@ function genComponentConf() {
                 };
             }
 
+            doneTypingBufferNg(
+                e => this.doneTyping(e),
+                this.textfieldNg(), 1000
+            )
+
             const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
             this.$scope.$on('$destroy', disconnect);
 
         }
+        doneTyping() {
+            console.log('TODO starting type-ahead search for: ' + this.textfield().value);
+            //buffer for 1s before starting the search
+
+        }
+
+        textfieldNg() {
+            if(!this._textfield) {
+                this._textfield = this.$element.find('.lp__searchbox');
+            }
+            return this._textfield;
+        }
+
+        textfield() {
+            return this.textfieldNg()[0];
+        }
     }
     Controller.$inject = serviceDependencies;
+
 
     return {
         restrict: 'E',
@@ -52,3 +75,6 @@ export default angular.module('won.owner.components.locationPicker', [
     ])
     .directive('wonLocationPicker', genComponentConf)
     .name;
+
+
+window.searchNominatim4dbg = searchNominatim;
