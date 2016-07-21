93ec95d4bd35dafa62f243a892a1357b3ef564a7
moru <kevin.singer@student.tuwien.ac.at>
2016-07-21 18:41:16 +0200

    Util function for delaying/buffering an input handler.

--- webofneeds/won-owner-webapp/src/main/webapp/app/cstm-ng-utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/cstm-ng-utils.js
@@ -18,3 +18,22 @@ export function broadcastEvent(scopeAndElem, eventName, eventData) {
         console.log('broadcasting');
     }
 }
+
+
+/**
+ * Registers an input listener on the ngElement. The
+ * callback will be invoked if the user has stopped
+ * typing for `doneTypingInterval` milliseconds.
+ * @param listenerCallback
+ * @param ngElement
+ * @param doneTypingInterval
+ */
+export function doneTypingBufferNg(listenerCallback, ngElement, doneTypingInterval) {
+    let typingTimer;
+    ngElement.bind('input', e => {
+        if(typingTimer) {
+            clearTimeout(typingTimer);
+        }
+        typingTimer = setTimeout(() => listenerCallback(e), doneTypingInterval)
+    })
+}
