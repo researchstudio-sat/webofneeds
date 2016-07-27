58737ef3b43cca5abd711431a679cf5274a951a3
moru <kevin.singer@student.tuwien.ac.at>
2016-07-27 21:22:05 +0200

    Clone utility function.

--- webofneeds/won-owner-webapp/src/main/webapp/app/utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/utils.js
@@ -620,3 +620,43 @@ function fetchJSON(url) {
             }
         });
 }
+
+/**
+ * Deep clone. Don't feed it recurrent structures!
+ * Thanks to A. Levy at <http://stackoverflow.com/questions/728360/how-do-i-correctly-clone-a-javascript-object>
+ * @param obj
+ * @return {*}
+ */
+export function clone(obj) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
