10a468f5386307b156721a24c2fddd2e5ae940a0
moru <kevin.singer@student.tuwien.ac.at>
2016-08-10 20:49:49 +0200

    Started adapting feed to new need-structure.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/feed-item.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/feed-item.js
@@ -1,51 +1,73 @@
 import angular from 'angular';
 import squareImageModule from '../components/square-image';
 import won from '../won-es6';
+import { actionCreators }  from '../actions/actions';
 import { attach } from '../utils';
-import { labels, updateRelativeTimestamps } from '../won-label-utils';
+import {
+    labels,
+    relativeTime,
+} from '../won-label-utils';
+import {
+    selectOwnNeeds,
+    selectLastUpdateTime,
+    selectConnectionsByNeed,
+    selectUnreadCountsByNeedAndType,
+} from '../selectors';
 
-const serviceDependencies = ['$scope', '$interval'];
+const serviceDependencies = ['$scope', '$interval', '$ngRedux'];
 function genComponentConf() {
     let template = `
-            <div class="fi clickable" ui-sref="post({postUri: self.item.get('uri')})">
-                <won-square-image 
-                    src="self.item.get('titleImg')" 
-                    title="self.item.get('title')"
-                    uri="self.item.get('uri')">
+            <div class="fi clickable" ui-sref="post({postUri: self.ownNeed.get('@id')})">
+                <won-square-image
+                    src="self.ownNeed.get('titleImg')"
+                    title="self.ownNeed.getIn(['won:hasContent','dc:title'])"
+                    uri="self.ownNeed.get('@id')">
                 </won-square-image>
                 <div class="fi__description">
                     <div class="fi__description__topline">
-                        <div class="fi__description__topline__title">{{self.item.get('title')}}</div>
-                        <div class="fi__description__topline__date">{{ self.creationDateLabel }}</div>
+                        <div class="fi__description__topline__title">
+                            {{ self.ownNeed.getIn(['won:hasContent','dc:title']) }}
+                        </div>
+                        <div class="fi__description__topline__date">
+                            {{ self.createdOn }}
+                        </div>
                     </div>
                     <div class="fi__description__subtitle">
                     <!--
-                        <span class="fi__description__subtitle__group" ng-show="self.item.group">
-                            <img src="generated/icon-sprite.svg#ico36_group" class="fi__description__subtitle__group__icon">{{self.item.group}}<span class="fi__description__subtitle__group__dash"> &ndash; </span>
+                        <span class="fi__description__subtitle__group" ng-show="self.ownNeed.group">
+                            <img src="generated/icon-sprite.svg#ico36_group" class="fi__description__subtitle__group__icon">
+                            {{self.ownNeed.group}}
+                            <span class="fi__description__subtitle__group__dash"> &ndash; </span>
                         </span>
                         -->
-                        <span class="fi__description__subtitle__type">{{self.labels.type[self.item.getIn(['won:hasBasicNeedType','@id'])]}}</span>
+                        <span class="fi__description__subtitle__type">
+                            {{
+                                self.labels.type[
+                                    self.ownNeed.getIn(['won:hasBasicNeedType','@id'])
+                                ]
+                            }}
+                        </span>
                     </div>
                 </div>
             </div>
             <div class="fmil">
-                <div class="fmil__item clickable" 
-                ng-repeat="cnct in self.connections.toArray() track by $index" 
-                ng-show="$index < self.maxNrOfItemsShown" 
+                <div class="fmil__item clickable"
+                ng-repeat="cnct in self.connections.toArray() track by $index"
+                ng-show="$index < self.maxNrOfItemsShown"
                 ui-sref="post({
-                            postUri: self.item.get('uri'), 
-                            connectionUri: cnct.getIn(['connection', 'uri']), 
-                            connectionType:  cnct.getIn(['connection', 'hasConnectionState'])
+                    postUri: self.ownNeed.get('@id'),
+                    connectionUri: cnct.getIn(['connection', 'uri']),
+                    connectionType:  cnct.getIn(['connection', 'hasConnectionState'])
                 })">
                     <won-square-image
                         src="cnct.get('titleImg')"
-                        title="cnct.getIn(['remoteNeed','title'])"
-                        uri="cnct.getIn(['remoteNeed','uri'])">
+                        title="cnct.getIn(['remoteNeed','won:hasContent','dc:title'])"
+                        uri="cnct.getIn(['remoteNeed','@id'])">
                     </won-square-image>
                     <div class="fmil__item__description">
                         <div class="fmil__item__description__topline">
                             <div class="fmil__item__description__topline__title">
-                                {{cnct.getIn(['remoteNeed','title'])}}
+                                {{cnct.getIn(['remoteNeed','won:hasContent','dc:title'])}}
                             </div>
                             <div class="fmil__item__description__topline__date">
                                 <!-- TODO only show this when this is a group's thread -->
@@ -71,13 +93,13 @@ function genComponentConf() {
             <div class="fi__footer" ng-show="self.unreadMatchesCount() || self.unreadRequestsCount()">
                 <div class="fi__footer__indicators">
                     <a class="fi__footer__indicators__item clickable"
-                       ui-sref="postMatches({myUri: self.item.get('uri')})"
+                       ui-sref="postMatches({myUri: self.ownNeed.get('@id')})"
                        ng-show="self.unreadMatchesCount()">
                         <img src="generated/icon-sprite.svg#ico36_match" class="fi__footer__indicators__item__icon"/>
                         <span class="fi__footer__indicators__item__caption">{{ self.unreadMatchesCount() }} Matches</span>
                     </a>
                     <a class="fi__footer__indicators__item clickable"
-                       ui-sref="postRequests({myUri: self.item.get('uri')})"
+                       ui-sref="postRequests({myUri: self.ownNeed.get('@id')})"
                        ng-show="self.unreadRequestsCount()">
                         <img src="generated/icon-sprite.svg#ico36_incoming" class="fi__footer__indicators__item__icon"/>
                         <span class="fi__footer__indicators__item__caption">{{self.unreadRequestsCount()}} Incoming Requests</span>
@@ -93,17 +115,26 @@ function genComponentConf() {
             window.fi4dbg = this;
 
             this.labels = labels;
-            /*
-            * TODO there's tick events now. use state.get('lastUpdateTime')
-            * to calculate relative timestamps
-            */
-            updateRelativeTimestamps(
-                this.$scope,
-                this.$interval,
-                this.item.get('creationDate'),
-                    t => this.creationDateLabel = t);
 
             this.maxNrOfItemsShown = 3;
+            const selectFromState = (state) => {
+                const ownNeeds = selectOwnNeeds(state);
+                const lastUpdated = selectLastUpdateTime(state);
+                const connectionsByNeed = selectConnectionsByNeed(state);
+                const unreadCountsByNeedAndType = selectUnreadCountsByNeedAndType(state);
+
+                return {
+                    ownNeed: ownNeeds && ownNeeds.get(this.needUri),
+                    createdOn: ownNeeds && relativeTime(lastUpdated, ownNeeds.get('dct:created')),
+                    connections: connectionsByNeed && connectionsByNeed.get(this.needUri),
+                    unreadCounts: unreadCountsByNeedAndType && unreadCountsByNeedAndType.get(this.needUri),
+
+
+
+                }
+            }
+            const disconnect = this.$ngRedux.connect(selectFromState,actionCreators)(this);
+            this.$scope.$on('$destroy', disconnect);
         }
         showMore() {
             this.maxNrOfItemsShown += 6;
@@ -128,15 +159,15 @@ function genComponentConf() {
         controllerAs: 'self',
         bindToController: true, //scope-bindings -> ctrl
         scope: {
-            item: '=',
+            needUri: '=',
             /*
              ownPost { title, type, creationdate, pic }
              theirPosts/counterparts [
                 { } //need
              ]
             */
-            unreadCounts: '=',
-            connections: '=',
+            //unreadCounts: '=',
+            //connections: '=',
         },
         template: template
     }
