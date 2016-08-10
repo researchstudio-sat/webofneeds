c890b8558f8e41d707dd32fd2c65b8c9cebefc11
moru <kevin.singer@student.tuwien.ac.at>
2016-08-10 20:48:44 +0200

    Adapted matches-views to new need-structure.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/matches-flow-item.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/matches-flow-item.js
@@ -6,54 +6,108 @@ import extendedGalleryModule from './extended-gallery';
 import feedbackGridModule from './feedback-grid';
 import { attach } from '../utils';
 import { actionCreators }  from '../actions/actions';
-import { labels, relativeTime, updateRelativeTimestamps } from '../won-label-utils';
-import { selectAllByConnections } from '../selectors';
+import {
+    labels,
+    relativeTime,
+} from '../won-label-utils';
+import {
+    selectAllByConnections,
+    selectLastUpdateTime,
+} from '../selectors';
 
 const serviceDependencies = ['$q', '$ngRedux', '$scope', '$interval'];
 function genComponentConf() {
     let template = `
         <div ng-show="self.images" class="mfi__gallery">
-            <won-extended-gallery max-thumbnails="self.maxThumbnails" items="self.images" class="horizontal" ng-show="self.images.length > 0"></won-extended-gallery>
+            <won-extended-gallery
+                max-thumbnails="self.maxThumbnails"
+                items="self.images"
+                class="horizontal"
+                ng-show="self.images.length > 0">
+            </won-extended-gallery>
             <won-square-image 
-                title="self.connectionData.getIn(['remoteNeed','title'])"
-                uri="self.connectionData.getIn(['remoteNeed','uri'])"
+                title="self.connectionData.getIn(['remoteNeed','won:hasContent','dc:title'])"
+                uri="self.connectionData.getIn(['remoteNeed','@id'])"
                 ng-show="self.images.length == 0">
             </won-square-image>
         </div>
         <div class="mfi__description clickable">
             <div class="mfi__description__topline">
-                <div class="mfi__description__topline__title clickable"><b>{{self.connectionData.getIn(['remoteNeed','title'])}}</b></div>
-                <div class="mfi__description__topline__date">{{self.creationDate}}</div>
+                <div class="mfi__description__topline__title clickable">
+                    {{self.connectionData.getIn(['remoteNeed','won:hasContent','dc:title'])}}
+                </div>
+                <div class="mfi__description__topline__date">
+                    {{ self.remoteCreatedOn }}
+                </div>
             </div>
             <div class="mfi__description__subtitle">
-                <span class="mfi__description__subtitle__group" ng-show="self.connection.get('group')">
-                    <img src="generated/icon-sprite.svg#ico36_group" class="mfi__description__subtitle__group__icon">{{self.connectionData.get('group')}}<span class="mfi__description__subtitle__group__dash"> &ndash; </span>
+                <span
+                    class="mfi__description__subtitle__group"
+                    ng-show="self.connection.get('group')">
+                        <img
+                            src="generated/icon-sprite.svg#ico36_group"
+                            class="mfi__description__subtitle__group__icon">
+                        <span>
+                            {{ self.connectionData.get('group') }}
+                        </span>
+                        <span class="mfi__description__subtitle__group__dash">
+                            &ndash;
+                        </span>
                 </span>
-                <span class="mfi__description__subtitle__type">{{self.labels.type[self.connectionData.getIn(['remoteNeed','won:hasBasicNeedType', '@id'])]}}</span>
+                <span class="mfi__description__subtitle__type">
+                    {{
+                        self.labels.type[
+                            self.connectionData.getIn(['remoteNeed','won:hasBasicNeedType', '@id'])
+                        ]
+                    }}
+                </span>
             </div>
-            <div class="mfi__description__content" ng-show="false"><!-- include once you have content in your needs that needs to be displayed here -->
+            <!-- include once you have content in your needs that needs to be displayed here -->
+            <div class="mfi__description__content" ng-show="false">
                 <div class="mfi__description__content__location">
-                    <img class="mfi__description__content__indicator" src="generated/icon-sprite.svg#ico16_indicator_location"/>
-                    <span>Vienna area</span>
+                    <img
+                        class="mfi__description__content__indicator"
+                        src="generated/icon-sprite.svg#ico16_indicator_location"/>
+                    <span>
+                        Vienna area
+                    </span>
                 </div>
                 <div class="mfi__description__content__datetime">
-                    <img class="mfi__description__content__indicator" src="generated/icon-sprite.svg#ico16_indicator_time"/>o
+                    <img
+                        class="mfi__description__content__indicator"
+                        src="generated/icon-sprite.svg#ico16_indicator_time"/>
+                    <span>o</span>
                     <span>Available until 5th May</span>
                 </div>
             </div>
         </div>
-        <div class="mfi__match clickable" ng-if="!self.feedbackVisible" ng-click="self.showFeedback()" ng-mouseenter="self.showFeedback()" >
-            <div class="mfi__match__description">
-                <div class="mfi__match__description__title">{{self.connectionData.getIn(['ownNeed','title'])}}</div>
-                <div class="mfi__match__description__type">{{self.labels.type[self.connectionData.getIn(['ownNeed','won:hasBasicNeedType','@id'])]}}</div>
-            </div>
-            <won-square-image 
-                src="self.connectionData.getIn(['ownNeed','titleImgSrc'])" 
-                title="self.connectionData.getIn(['ownNeed','title'])"
-                uri="self.connectionData.getIn(['ownNeed','uri'])">
-            </won-square-image>
+        <div
+            class="mfi__match clickable"
+            ng-if="!self.feedbackVisible"
+            ng-click="self.showFeedback()"
+            ng-mouseenter="self.showFeedback()" >
+                <div class="mfi__match__description">
+                    <div class="mfi__match__description__title">
+                        {{ self.connectionData.getIn(['ownNeed','won:hasContent','dc:title']) }}
+                    </div>
+                    <div class="mfi__match__description__type">
+                        {{
+                            self.labels.type[
+                                self.connectionData.getIn(['ownNeed','won:hasBasicNeedType','@id'])
+                            ]
+                        }}
+                    </div>
+                </div>
+                <won-square-image
+                    src="self.connectionData.getIn(['ownNeed','titleImgSrc'])"
+                    title="self.connectionData.getIn(['ownNeed','won:hasContent','dc:title'])"
+                    uri="self.connectionData.getIn(['ownNeed','@id'])">
+                </won-square-image>
         </div>
-        <won-feedback-grid connection-uri="self.connectionUri" ng-mouseleave="self.hideFeedback()" ng-if="self.feedbackVisible"/>
+        <won-feedback-grid
+            connection-uri="self.connectionUri"
+            ng-mouseleave="self.hideFeedback()"
+            ng-if="self.feedbackVisible"/>
     `;
 
     class Controller {
@@ -65,20 +119,19 @@ function genComponentConf() {
             this.images=[];
 
             const selectFromState = (state) => {
+
+                const connectionData = selectAllByConnections(state).get(this.connectionUri);
                 return {
-                    connectionData: selectAllByConnections(state).get(this.connectionUri)
+                    remoteCreatedOn: relativeTime(
+                        selectLastUpdateTime(state),
+                        connectionData.getIn(['remoteNeed', 'dct:created'])
+                    ),
+                    connectionData: connectionData,
                 };
             };
 
             const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
             this.$scope.$on('$destroy', disconnect);
-
-            updateRelativeTimestamps(
-                this.$scope,
-                this.$interval,
-                this.connectionData.getIn(["remoteNeed","creationDate"]),
-                    t => this.creationDate = t);
-
         }
 
         showFeedback() {
--- webofneeds/won-owner-webapp/src/main/webapp/app/components/matches-grid-item.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/matches-grid-item.js
@@ -13,45 +13,80 @@ function genComponentConf() {
     let template = `
         <div class="mgi__description">
             <div class="mgi__description__post clickable">
-                <!--won-square-image src="self.getRandomImage()" title="self.connectionData.get(.ownNeed.title"></won-square-image-->
                 <div class="mgi__description__post__text">
                     <div class="mgi__description__post__text__topline">
-                        <div class="mgi__description__post__text__topline__title">{{self.connectionData.getIn(['remoteNeed','title'])}}</div>
-                        <div class="mgi__description__post__text__topline__date">{{self.creationDate}}</div>
+                        <div class="mgi__description__post__text__topline__title">
+                            {{ self.connectionData.getIn(['remoteNeed','won:hasContent','dc:title']) }}
+                        </div>
+                        <div class="mgi__description__post__text__topline__date">
+                            {{ self.creationDate }}
+                        </div>
                     </div>
                     <div class="mgi__description__post__text__subtitle">
-                        <span class="mgi__description__post__text__subtitle__group" ng-show="self.connectionData.get('group')">
-                            <img src="generated/icon-sprite.svg#ico36_group" class="mgi__description__post__text__subtitle__group__icon">{{self.connectionData.get('group')}}<span class="mgi__description__post__text__subtitle__group__dash"> &ndash; </span>
+                        <span
+                            class="mgi__description__post__text__subtitle__group"
+                            ng-show="self.connectionData.get('group')">
+                                <img src="generated/icon-sprite.svg#ico36_group"
+                                    class="mgi__description__post__text__subtitle__group__icon">
+                                {{ self.connectionData.get('group') }}
+                                <span class="mgi__description__post__text__subtitle__group__dash"> &ndash; </span>
                         </span>
-                        <span class="mgi__description__post__text__subtitle__type">{{self.labels.type[self.connectionData.getIn(['remoteNeed','won:hasBasicNeedType','@id'])]}}</span>
+                        <span class="mgi__description__post__text__subtitle__type">
+                            {{
+                                self.labels.type[
+                                    self.connectionData.getIn(['remoteNeed','won:hasBasicNeedType','@id'])
+                                ]
+                            }}
+                        </span>
                     </div>
                 </div>
             </div>
-            <div class="mgi__description__content" ng-show="self.theirNeed.get('location') || self.theirNeed.get('deadline')">
-                <div class="mgi__description__content__location"
-                    ng-show="self.theirNeed.get('location')">
-                        <img class="mgi__description__content__indicator" src="generated/icon-sprite.svg#ico16_indicator_location"/>
-                        <span>{{ self.theirNeed.get('location') }}</span>
-                </div>
-                <div class="mgi__description__content__datetime"
-                    ng-show="self.theirNeed.get('deadline')">
-                        <img class="mgi__description__content__indicator" src="generated/icon-sprite.svg#ico16_indicator_time"/>
-                        <span>{{ self.theirNeed.get('deadline') }}</span>
-                </div>
+            <div
+                class="mgi__description__content"
+                ng-show="self.theirNeed.get('location') || self.theirNeed.get('deadline')">
+                    <div class="mgi__description__content__location"
+                        ng-show="self.theirNeed.get('location')">
+                            <img
+                                class="mgi__description__content__indicator"
+                                src="generated/icon-sprite.svg#ico16_indicator_location"/>
+                            <span>{{ self.theirNeed.get('location') }}</span>
+                    </div>
+                    <div class="mgi__description__content__datetime"
+                        ng-show="self.theirNeed.get('deadline')">
+                            <img
+                                class="mgi__description__content__indicator"
+                                src="generated/icon-sprite.svg#ico16_indicator_time"/>
+                            <span>{{ self.theirNeed.get('deadline') }}</span>
+                    </div>
             </div>
         </div>
-        <div class="mgi__match clickable" ng-if="!self.feedbackVisible" ng-click="self.showFeedback()" ng-mouseenter="self.showFeedback()">
-            <div class="mgi__match__description">
-                <div class="mgi__match__description__title">{{self.ownNeed.get('title')}}</div>
-                <div class="mgi__match__description__type">{{self.labels.type[self.ownNeed.getIn(['won:hasBasicNeedType', '@id'])]}}</div>
-            </div>
-            <won-square-image 
-                src="self.connectionData.getIn(['ownNeed','titleImgSrc'])" 
-                title="self.connectionData.getIn(['ownNeed','title'])"
-                uri="self.connectionData.getIn(['ownNeed','uri'])">
-            </won-square-image>
+        <div
+            class="mgi__match clickable"
+            ng-if="!self.feedbackVisible"
+            ng-click="self.showFeedback()"
+            ng-mouseenter="self.showFeedback()">
+                <div class="mgi__match__description">
+                    <div class="mgi__match__description__title">
+                        {{ self.ownNeed.getIn(['won:hasContent','dc:title']) }}
+                    </div>
+                    <div class="mgi__match__description__type">
+                        {{
+                            self.labels.type[
+                                self.ownNeed.getIn(['won:hasBasicNeedType', '@id'])
+                            ]
+                        }}
+                    </div>
+                </div>
+                <won-square-image
+                    src="self.ownNeed.getIn(['won:hasContent','titleImgSrc'])"
+                    title="self.ownNeed.getIn(['won:hasContent','dc:title'])"
+                    uri="self.ownNeed.getIn(['@id'])">
+                </won-square-image>
         </div>
-        <won-feedback-grid connection-uri="self.connectionUri" ng-mouseleave="self.hideFeedback()" ng-if="self.feedbackVisible"/>
+        <won-feedback-grid
+            connection-uri="self.connectionUri"
+            ng-mouseleave="self.hideFeedback()"
+            ng-if="self.feedbackVisible"/>
     `;
 
     class Controller {
@@ -81,7 +116,7 @@ function genComponentConf() {
             updateRelativeTimestamps(
                 this.$scope,
                 this.$interval,
-                this.connectionData.getIn(["remoteNeed","creationDate"]),
+                this.connectionData.getIn(["remoteNeed","dct:created"]),
                     t => this.creationDate = t);
 
         }
