2bc91eebe10b2af766964b46e5acbef757bd3807
moru <kevin.singer@student.tuwien.ac.at>
2016-08-11 16:29:41 +0200

    Location displayed in post-info.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/post-info.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/post-info.js
@@ -16,8 +16,17 @@ import {
     selectLastUpdateTime,
 } from '../selectors';
 import { actionCreators }  from '../actions/actions';
+import L from '../leaflet-bundleable';
+import {
+    initLeaflet,
+    initLeafletBaseMaps,
+} from '../won-utils';
 
-const serviceDependencies = ['$ngRedux', '$scope'];
+import {
+    DomCache,
+} from '../cstm-ng-utils';
+
+const serviceDependencies = ['$ngRedux', '$scope', '$element'];
 function genComponentConf() {
     let template = `
         <div class="post-info__inner">
@@ -46,13 +55,18 @@ function genComponentConf() {
                 <!-- TODO tags -->
 
                 <h2 class="post-info__heading"
-                    ng-show="self.post.get('location')">
+                    ng-show="self.location">
                     Location
                 </h2>
                 <p class="post-info__details"
-                    ng-show="self.post.get('location')">
-                    {{ self.post.get('location') }}
+                    ng-show="self.address">
+                    {{ self.address }}
+                    <a href="" ng-click="self.updateMap(self.location)">
+                        (show)
+                    </a>
                 </p>
+
+                <div class="post-info__mapmount" id="post-info__mapmount" style="height:500px"></div>
             </div>
         </div>
     `;
@@ -61,21 +75,80 @@ function genComponentConf() {
     class Controller {
         constructor() {
             attach(this, serviceDependencies, arguments);
+            this.domCache = new DomCache(this.$element);
+
+            window.pi4dbg = this;
+
+            this.map = initLeaflet(this.mapMount());
+            // this.determineCurrentLocation(); show in reference
+
+
+            //TODO custom icons
+            //TODO for the bounding box: make sure location-bb as well as own location-bb fit into the view.
+            //TODO use different marker for own location
+            //TODO need to resize page for map to render correctly
+
+            this.$scope.$watch(
+                'self.location',
+                (location, mapAlreadyInitialized) =>
+                    !mapAlreadyInitialized && this.updateMap(location)
+            );
+
 
             const selectFromState = (state) => {
                 const post = selectOpenPost(state);
+                const location = post && post.getIn(['won:hasContent', 'won:hasContentDescription', 'won:hasLocation']);
                 return {
                     post: post,
+                    location: location,
+                    address: location && location.get('s:name'),
                     friendlyTimestamp: relativeTime(
                         selectLastUpdateTime(state),
                         post && post.get('dct:created')
                     ),
                 }
             };
-
             const disconnect = this.$ngRedux.connect(selectFromState, actionCreators)(this);
             this.$scope.$on('$destroy', disconnect);
         }
+
+        updateMap(location) {
+            if(!location) {
+                return;
+            }
+
+            const nw = location.getIn(['won:hasBoundingBox', 'won:hasNorthWestCorner']);
+            const se = location.getIn(['won:hasBoundingBox', 'won:hasSouthEastCorner']);
+            const lat = Number.parseFloat(location.getIn(['s:geo', 's:latitude']));
+            const lon = Number.parseFloat(location.getIn(['s:geo', 's:longitude']));
+            const name = location.get('s:name');
+
+            if(!nw || !se || !lat || !lon || !name ) {
+                return;
+            }
+
+            this.map.fitBounds([
+                [
+                    Number.parseFloat(nw.get('s:latitude')),
+                    Number.parseFloat(nw.get('s:longitude')),
+                ],
+                [
+                    Number.parseFloat(se.get('s:latitude')),
+                    Number.parseFloat(se.get('s:longitude')),
+                ]
+            ]);
+
+            if(this.marker) {
+               this.map.removeLayer(this.marker);
+            }
+            this.marker = L.marker([lat, lon]).bindPopup(name);
+            this.map.addLayer(this.marker);
+
+            this.mapAlreadyInitialized = true;
+        }
+
+        mapMountNg() { return this.domCache.ng('.post-info__mapmount'); }
+        mapMount() { return this.domCache.dom('.post-info__mapmount'); }
 }
 Controller.$inject = serviceDependencies;
 return {
