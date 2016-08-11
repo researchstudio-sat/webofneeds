04fbaa77b2ad4a835570955f6321d1262e674329
moru <kevin.singer@student.tuwien.ac.at>
2016-08-11 14:25:46 +0200

    Pulled out base-map intialisation into a seperate function.

--- webofneeds/won-owner-webapp/src/main/webapp/app/won-utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/won-utils.js
@@ -13,30 +13,17 @@ export function initLeaflet(mapMount) {
     L.Icon.Default.imagePath = 'images/map-images/';
     //TODO replace with own icons
 
-    const secureOsmSource = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' // secure osm.org
-    const secureOsm = L.tileLayer(secureOsmSource, {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    });
-
-    const transportSource = 'http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png';
-    const transport = L.tileLayer(transportSource, {
-        attribution: 'Maps &copy; <a href="http://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-    });
+    const baseMaps = initLeafletBaseMaps();
 
     const map = L.map(mapMount,{
         center: [37.44, -42.89], //centered on north-west africa
         zoom: 1, //world-map
-        layers: [secureOsm], //initially visible layers
+        layers: [baseMaps['Detailed default map']], //initially visible layers
 
     }); //.setView([51.505, -0.09], 13);
 
     //map.fitWorld() // shows every continent twice :|
     map.fitBounds([[-80, -190],[80, 190]]); // fitWorld without repetition
-
-    const baseMaps = {
-        "Detailed default map": secureOsm,
-        "Transport (Insecurely loaded!)": transport,
-    };
 
     L.control.layers(baseMaps).addTo(map);
 
@@ -48,6 +35,29 @@ export function initLeaflet(mapMount) {
 
     return map;
 }
+
+export function initLeafletBaseMaps() {
+    if(!L) {
+        throw new Exception("Tried to initialize leaflet map-sources while leaflet wasn't loaded.");
+    }
+    const secureOsmSource = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' // secure osm.org
+    const secureOsm = L.tileLayer(secureOsmSource, {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    const transportSource = 'http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png';
+    const transport = L.tileLayer(transportSource, {
+        attribution: 'Maps &copy; <a href="http://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+    });
+
+    const baseMaps = {
+        "Detailed default map": secureOsm,
+        "Transport (Insecurely loaded!)": transport,
+    };
+
+    return baseMaps;
+}
+
 export function selectTimestamp(event, connectionUri) {
     if(event.get('hasReceiver') === connectionUri) {
         return event.get('hasReceivedTimestamp');
