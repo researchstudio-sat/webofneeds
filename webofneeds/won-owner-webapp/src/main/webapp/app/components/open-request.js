49b96e6f71fd52c286af9ee6775d21f999f3fef7
quasarchimaere <sudafabian@gmail.com>
2016-08-12 10:34:36 +0200

    fixed show description in open-request dialog

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/open-request.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/open-request.js
@@ -69,13 +69,21 @@ function genComponentConf() {
                 </div>
                 -->
                 <div class="or__content__description__text"
-                    ng-show="!!self.theirNeed.getIn(['won:hasContent','won:hasTextDescription']) || !!self.textMsg">
+                    ng-show="!!self.textMsg">
+                    <img
+                        class="or__content__description__indicator"
+                        src="generated/icon-sprite.svg#ico16_indicator_description"/>
+                    <span>
+                        <p>{{ self.textMsg }}</p>
+                    </span>
+                </div>
+                <div class="or__content__description__text"
+                    ng-show="!!self.theirNeed.getIn(['won:hasContent','won:hasTextDescription'])">
                     <img
                         class="or__content__description__indicator"
                         src="generated/icon-sprite.svg#ico16_indicator_description"/>
                     <span>
                         <p>{{ self.theirNeed.getIn(['won:hasContent','won:hasTextDescription']) }}</p>
-                        <p>{{ self.textMsg }}</p>
                     </span>
                 </div>
             </div>
