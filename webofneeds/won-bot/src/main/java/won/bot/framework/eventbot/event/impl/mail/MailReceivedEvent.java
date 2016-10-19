832395dc2acd22d2fdf072c6ee5f8ca08c1fc343
quasarchimaere <sudafabian@gmail.com>
2016-10-19 12:09:35 +0200

    removed unnecessary javadoc from code

--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/mail/MailReceivedEvent.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/mail/MailReceivedEvent.java
@@ -4,9 +4,6 @@ import won.bot.framework.eventbot.event.BaseEvent;
 
 import javax.mail.internet.MimeMessage;
 
-/**
- * Created by fsuda on 30.09.2016.
- */
 public class MailReceivedEvent extends BaseEvent {
     private final MimeMessage message;
 
