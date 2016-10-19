12838d07094b5757bf4ffdc153074d6922063e0b
quasarchimaere <sudafabian@gmail.com>
2016-10-19 12:08:19 +0200

    added URI abstraction to encapsulate the specific UriType(currently connection or need) with the uri

---
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/impl/mail/model/WonURI.java
@@ -0,0 +1,33 @@
+package won.bot.framework.eventbot.action.impl.mail.model;
+
+import won.bot.framework.eventbot.action.impl.mail.receive.util.UriType;
+
+import java.net.URI;
+
+/* Helper Class to encapsulate an uri and the type
+ */
+public class WonURI {
+    private URI uri;
+    private UriType type;
+
+    public WonURI(URI uri, UriType type) {
+        this.uri = uri;
+        this.type = type;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public UriType getType() {
+        return type;
+    }
+
+    public void setType(UriType type) {
+        this.type = type;
+    }
+}
---
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/impl/mail/receive/util/UriType.java
@@ -0,0 +1,9 @@
+package won.bot.framework.eventbot.action.impl.mail.receive.util;
+
+/**
+ * Created by fsuda on 19.10.2016.
+ */
+public enum UriType {
+    NEED,
+    CONNECTION
+}
