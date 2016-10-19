63fc87140b794044f156fff98485cc0294ac9d08
quasarchimaere <sudafabian@gmail.com>
2016-10-19 12:16:29 +0200

    refactoring and listening to the new events and performing their respective actions

--- webofneeds/won-bot/src/main/java/won/bot/impl/Mail2WonBot.java
+++ webofneeds/won-bot/src/main/java/won/bot/impl/Mail2WonBot.java
@@ -3,12 +3,20 @@ package won.bot.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.MessageChannel;
 import won.bot.framework.bot.base.EventBot;
-import won.bot.framework.eventbot.action.impl.mail.Hint2MailParserAction;
-import won.bot.framework.eventbot.action.impl.mail.MailParserAction;
+import won.bot.framework.eventbot.EventListenerContext;
+import won.bot.framework.eventbot.action.impl.mail.receive.CreateNeedFromMailAction;
+import won.bot.framework.eventbot.action.impl.mail.receive.MailCommandAction;
+import won.bot.framework.eventbot.action.impl.mail.receive.MailParserAction;
+import won.bot.framework.eventbot.action.impl.mail.send.Connect2MailParserAction;
+import won.bot.framework.eventbot.action.impl.mail.send.Hint2MailParserAction;
+import won.bot.framework.eventbot.action.impl.mail.send.Message2MailAction;
+import won.bot.framework.eventbot.action.impl.wonmessage.CloseConnectionUriAction;
+import won.bot.framework.eventbot.action.impl.wonmessage.OpenConnectionUriAction;
 import won.bot.framework.eventbot.bus.EventBus;
-import won.bot.framework.eventbot.event.impl.mail.MailReceivedEvent;
+import won.bot.framework.eventbot.event.impl.mail.*;
 import won.bot.framework.eventbot.event.impl.wonmessage.ConnectFromOtherNeedEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.HintFromMatcherEvent;
+import won.bot.framework.eventbot.event.impl.wonmessage.MessageFromOtherNeedEvent;
 import won.bot.framework.eventbot.listener.impl.ActionOnEventListener;
 
 import javax.mail.internet.MimeMessage;
@@ -21,6 +29,8 @@ import javax.mail.internet.MimeMessage;
 public class Mail2WonBot extends EventBot{
     private static final String NAME_NEEDS = "mailNeeds";
     private static final String URIMAILRELATIONS_NAME = "uriMailRelations";
+    private static final String URIMIMEMESSAGERELATIONS_NAME = "uriMimeMessageRelations";
+    private static final String MAILIDURIRELATIONS_NAME = "mailIdUriRelations";
 
     @Autowired
     private MessageChannel receiveEmailChannel;
@@ -32,20 +42,64 @@ public class Mail2WonBot extends EventBot{
 
     @Override
     protected void initializeEventListeners() {
+        EventListenerContext ctx = getEventListenerContext();
         bus = getEventBus();
 
         bus.subscribe(MailReceivedEvent.class,
         new ActionOnEventListener(
-                getEventListenerContext(),
+                ctx,
                 "MailReceived",
-                new MailParserAction(getEventListenerContext(), NAME_NEEDS, URIMAILRELATIONS_NAME)
+                new MailParserAction(ctx)
         ));
 
         bus.subscribe(HintFromMatcherEvent.class,
         new ActionOnEventListener(
-                getEventListenerContext(),
+                ctx,
                 "HintReceived",
-                new Hint2MailParserAction(getEventListenerContext(), NAME_NEEDS, URIMAILRELATIONS_NAME, sendEmailChannel)
+                new Hint2MailParserAction(ctx, NAME_NEEDS, URIMAILRELATIONS_NAME, URIMIMEMESSAGERELATIONS_NAME, MAILIDURIRELATIONS_NAME, sendEmailChannel)
+        ));
+
+        bus.subscribe(ConnectFromOtherNeedEvent.class,
+        new ActionOnEventListener(
+                ctx,
+                "ConnectReceived",
+                new Connect2MailParserAction(ctx, NAME_NEEDS, URIMAILRELATIONS_NAME, URIMIMEMESSAGERELATIONS_NAME, MAILIDURIRELATIONS_NAME, sendEmailChannel)
+        ));
+
+        bus.subscribe(CreateNeedFromMailEvent.class,
+        new ActionOnEventListener(
+                ctx,
+                "CreateNeedFromMailEvent",
+                new CreateNeedFromMailAction(ctx, NAME_NEEDS, URIMAILRELATIONS_NAME, URIMIMEMESSAGERELATIONS_NAME)
+
+        ));
+        bus.subscribe(MailCommandEvent.class,
+        new ActionOnEventListener(
+                ctx,
+                "MailCommandEvent",
+                new MailCommandAction(ctx, MAILIDURIRELATIONS_NAME)
+        ));
+
+        bus.subscribe(CloseConnectionEvent.class,
+        new ActionOnEventListener(
+                ctx,
+                "CloseCommandEvent",
+                new CloseConnectionUriAction(ctx)
+        ));
+
+        bus.subscribe(OpenConnectionEvent.class,
+        new ActionOnEventListener(
+                ctx,
+                "OpenCommandEvent",
+                new OpenConnectionUriAction(ctx)
+        ));
+
+        //if the bot receives a text message - try to map the command of the text message to a DebugEvent
+        bus.subscribe(MessageFromOtherNeedEvent.class,
+        new ActionOnEventListener(
+                ctx,
+                "ReceivedTextMessage",
+                new Message2MailAction(ctx, MAILIDURIRELATIONS_NAME, sendEmailChannel)
         ));
     }
 
