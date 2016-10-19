b3057574059337bee316bf43d8da0da2d07e22f1
quasarchimaere <sudafabian@gmail.com>
2016-10-19 12:12:52 +0200

    added eventbot utils methods

--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/EventBotActionUtils.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/EventBotActionUtils.java
@@ -20,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import won.bot.framework.bot.BotContext;
 import won.bot.framework.eventbot.EventListenerContext;
+import won.bot.framework.eventbot.action.impl.mail.model.WonURI;
 import won.bot.framework.eventbot.event.Event;
 import won.bot.framework.eventbot.event.impl.wonmessage.FailureResponseEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.SuccessResponseEvent;
@@ -43,94 +44,98 @@ import java.util.HashMap;
  */
 public class EventBotActionUtils
 {
-  private static Logger logger = LoggerFactory.getLogger(EventBotActionUtils.class);
-  public static void rememberInListIfNamePresent(EventListenerContext ctx ,URI uri, String uriListName) {
-      if (uriListName != null && uriListName.trim().length() > 0){
-          ctx.getBotContext().appendToNamedNeedUriList(uri, uriListName);
-        logger.debug("remembering need in NamedNeedList {} ", uri);
-      } else {
-          ctx.getBotContext().rememberNeedUri(uri);
-        logger.debug("remembering need in List {} ", uri);
-      }
-  }
+    private static Logger logger = LoggerFactory.getLogger(EventBotActionUtils.class);
 
-  public static void rememberInNodeListIfNamePresent(EventListenerContext ctx, URI uri){
-    ctx.getBotContext().rememberNodeUri(uri);
-  }
-
-  public static void removeFromListIfNamePresent(EventListenerContext ctx ,URI uri, String uriListName) {
-    if (uriListName != null && uriListName.trim().length() > 0){
-      ctx.getBotContext().removeNeedUriFromNamedNeedUriList(uri, uriListName);
-      logger.debug("removing need from NamedNeedList {} ", uri);
-    } else {
-      ctx.getBotContext().removeNeedUri(uri);
-      logger.debug("removed need from bot context {} ", uri);
-    }
-  }
-
-  /**
-   * Creates a listener that waits for the response to the specified message. If a SuccessResponse is received,
-   * the successCallbck is executed, if a FailureResponse is received, the failureCallback is executed.
-   * @param outgoingMessage
-   * @param successCallback
-   * @param failureCallback
-   * @param context
-   * @return
-   */
- public static EventListener makeAndSubscribeResponseListener(final WonMessage outgoingMessage,
-   final EventListener successCallback, final EventListener failureCallback, EventListenerContext context) {
-
-    //create an event listener that processes the response to the wonMessage we're about to send
-    EventListener listener = new ActionOnEventListener(context,
-      new AcceptOnceFilter(OriginalMessageUriResponseEventFilter.forWonMessage(outgoingMessage)),
-      new BaseEventBotAction(context)
-      {
-        @Override
-        protected void doRun(final Event event) throws Exception {
-          if (event instanceof SuccessResponseEvent) {
-            successCallback.onEvent(event);
-          } else  if (event instanceof FailureResponseEvent){
-            failureCallback.onEvent(event);
-          }
+    public static void rememberInListIfNamePresent(EventListenerContext ctx ,URI uri, String uriListName) {
+        if (uriListName != null && uriListName.trim().length() > 0){
+            ctx.getBotContext().appendToNamedNeedUriList(uri, uriListName);
+            logger.debug("remembering need in NamedNeedList {} ", uri);
+        } else {
+            ctx.getBotContext().rememberNeedUri(uri);
+            logger.debug("remembering need in List {} ", uri);
         }
-      });
-   context.getEventBus().subscribe(SuccessResponseEvent.class, listener);
-   context.getEventBus().subscribe(FailureResponseEvent.class, listener);
-   return listener;
-  }
+    }
+
+    public static void rememberInNodeListIfNamePresent(EventListenerContext ctx, URI uri){
+        ctx.getBotContext().rememberNodeUri(uri);
+    }
+
+    public static void removeFromListIfNamePresent(EventListenerContext ctx ,URI uri, String uriListName) {
+        if (uriListName != null && uriListName.trim().length() > 0){
+            ctx.getBotContext().removeNeedUriFromNamedNeedUriList(uri, uriListName);
+            logger.debug("removing need from NamedNeedList {} ", uri);
+        } else {
+            ctx.getBotContext().removeNeedUri(uri);
+            logger.debug("removed need from bot context {} ", uri);
+        }
+    }
+
+    /**
+    * Creates a listener that waits for the response to the specified message. If a SuccessResponse is received,
+    * the successCallbck is executed, if a FailureResponse is received, the failureCallback is executed.
+    * @param outgoingMessage
+    * @param successCallback
+    * @param failureCallback
+    * @param context
+    * @return
+    */
+    public static EventListener makeAndSubscribeResponseListener(final WonMessage outgoingMessage,
+        final EventListener successCallback, final EventListener failureCallback, EventListenerContext context) {
+
+        //create an event listener that processes the response to the wonMessage we're about to send
+        EventListener listener = new ActionOnEventListener(context,
+            new AcceptOnceFilter(OriginalMessageUriResponseEventFilter.forWonMessage(outgoingMessage)),
+            new BaseEventBotAction(context)
+            {
+                @Override
+                protected void doRun(final Event event) throws Exception {
+                    if (event instanceof SuccessResponseEvent) {
+                        successCallback.onEvent(event);
+                    } else  if (event instanceof FailureResponseEvent){
+                        failureCallback.onEvent(event);
+                    }
+                }
+            }
+        );
+        context.getEventBus().subscribe(SuccessResponseEvent.class, listener);
+        context.getEventBus().subscribe(FailureResponseEvent.class, listener);
+        return listener;
+    }
 
 
-  /**
-   * Creates a listener that waits for the remote response to the specified message. If a SuccessResponse is received,
-   * the successCallbck is executed, if a FailureResponse is received, the failureCallback is executed.
-   * @param outgoingMessage
-   * @param successCallback
-   * @param failureCallback
-   * @param context
-   * @return
-   */
-  public static EventListener makeAndSubscribeRemoteResponseListener(final WonMessage outgoingMessage,
-      final EventListener successCallback, final EventListener failureCallback, EventListenerContext context) {
+    /**
+    * Creates a listener that waits for the remote response to the specified message. If a SuccessResponse is received,
+    * the successCallbck is executed, if a FailureResponse is received, the failureCallback is executed.
+    * @param outgoingMessage
+    * @param successCallback
+    * @param failureCallback
+    * @param context
+    * @return
+    */
+    public static EventListener makeAndSubscribeRemoteResponseListener(final WonMessage outgoingMessage,
+        final EventListener successCallback, final EventListener failureCallback, EventListenerContext context) {
 
-    //create an event listener that processes the remote response to the wonMessage we're about to send
-    EventListener listener = new ActionOnEventListener(context,
-     new AcceptOnceFilter(OriginalMessageUriRemoteResponseEventFilter.forWonMessage(outgoingMessage)),
-     new BaseEventBotAction(context)
-     {
-       @Override
-       protected void doRun(final Event event) throws Exception {
-         if (event instanceof SuccessResponseEvent) {
-           successCallback.onEvent(event);
-         } else  if (event instanceof FailureResponseEvent){
-           failureCallback.onEvent(event);
-         }
-       }
-     });
-    context.getEventBus().subscribe(SuccessResponseEvent.class, listener);
-    context.getEventBus().subscribe(FailureResponseEvent.class, listener);
-    return listener;
-  }
+        //create an event listener that processes the remote response to the wonMessage we're about to send
+        EventListener listener = new ActionOnEventListener(context,
+            new AcceptOnceFilter(OriginalMessageUriRemoteResponseEventFilter.forWonMessage(outgoingMessage)),
+            new BaseEventBotAction(context)
+            {
+                @Override
+                protected void doRun(final Event event) throws Exception {
+                    if (event instanceof SuccessResponseEvent) {
+                        successCallback.onEvent(event);
+                    } else  if (event instanceof FailureResponseEvent){
+                        failureCallback.onEvent(event);
+                    }
+                }
+            }
+        );
+        context.getEventBus().subscribe(SuccessResponseEvent.class, listener);
+        context.getEventBus().subscribe(FailureResponseEvent.class, listener);
+        return listener;
+    }
 
+    //Util Methods to Get/Remove/Add Uri -> Address Relation
     public static void addUriAddressRelation(EventListenerContext context, String mapName, URI needURI, MimeMessage sender) {
         String email = null;
 
@@ -170,5 +175,74 @@ public class EventBotActionUtils
             return ((HashMap<URI, String>) uriMap).get(uri);
         }
         return null;
+    }
+
+    //Util Methods to Get/Remove/Add Uri -> MimeMessage Relation
+    public static void removeUriMimeMessageRelation(EventListenerContext context, String mapName, URI needURI) {
+        BotContext botContext = context.getBotContext();
+        Object uriMap = botContext.get(mapName);
+
+        if(uriMap != null && uriMap instanceof HashMap){
+            ((HashMap<URI, MimeMessage>) uriMap).remove(needURI);
+        }
+    }
+
+    public static MimeMessage getMimeMessageForURI(EventListenerContext context, String mapName, URI uri) {
+        BotContext botContext = context.getBotContext();
+        Object uriMap = botContext.get(mapName);
+
+        if(uriMap != null && uriMap instanceof HashMap){
+            return ((HashMap<URI, MimeMessage>) uriMap).get(uri);
+        }
+        return null;
+    }
+
+    public static void addUriMimeMessageRelation(EventListenerContext context, String mapName, URI needURI, MimeMessage mimeMessage) {
+        BotContext botContext = context.getBotContext();
+        Object uriMap = botContext.get(mapName);
+
+        if(uriMap == null || !(uriMap instanceof HashMap)){
+            uriMap = new HashMap<URI, MimeMessage>();
+        }
+
+        ((HashMap<URI, MimeMessage>) uriMap).put(needURI, mimeMessage);
+        botContext.put(mapName, uriMap);
+    }
+
+    //Util Methods to Get/Remove/Add MailId -> URI Relation
+    public static void removeMailIdWonURIRelation(EventListenerContext context, String mapName, String mailId) {
+        BotContext botContext = context.getBotContext();
+        Object uriMap = botContext.get(mapName);
+
+        if(uriMap != null && uriMap instanceof HashMap){
+            ((HashMap<String, WonURI>) uriMap).remove(mailId);
+        }
+    }
+
+    public static WonURI getWonURIForMailId(EventListenerContext context, String mapName, String mailId) {
+        BotContext botContext = context.getBotContext();
+        Object uriMap = botContext.get(mapName);
+
+        if(uriMap != null && uriMap instanceof HashMap){
+            return ((HashMap<String, WonURI>) uriMap).get(mailId);
+        }
+        return null;
+    }
+
+    public static void addMailIdWonURIRelation(EventListenerContext context, String mapName, String mailId, WonURI uri) {
+        BotContext botContext = context.getBotContext();
+        Object uriMap = botContext.get(mapName);
+
+        if(uriMap == null || !(uriMap instanceof HashMap)){
+            uriMap = new HashMap<String, WonURI>();
+        }
+
+        ((HashMap<String, WonURI>) uriMap).put(mailId, uri);
+        botContext.put(mapName, uriMap);
+    }
+
+    public static HashMap<String, WonURI> getMailIdURIRelations(EventListenerContext context, String mapName) {
+        BotContext botContext = context.getBotContext();
+        return (HashMap<String, WonURI>) botContext.get(mapName);
     }
 }
