e035f5cc552df3eaaff0b52304b3af683d12e0e3
hfriedrich <heiko.friedrich@researchstudio.at>
2016-11-04 11:22:17 +0100

    remove unused mail code

--- webofneeds/won-owner-webapp/src/main/java/won/owner/web/WonOwnerMailSender.java
+++ webofneeds/won-owner-webapp/src/main/java/won/owner/web/WonOwnerMailSender.java
@@ -64,37 +64,6 @@ public class WonOwnerMailSender {
     this.wonMailSender = wonMailSender;
   }
 
-  @Deprecated
-  /**
-   * @deprecated as composes email with link to the old GUI
-   */
-  public void sendPrivateLink(String toEmail, String privateLink) {
-
-    String subject = "Your request posting";
-    privateLink = uriService.getOwnerProtocolOwnerURI() + "/#/private-link?id=" + privateLink;
-    String ownerAppLink = uriService.getOwnerProtocolOwnerURI().toString();
-
-    String text = "Dear user," +
-      "\n\n" +
-      "Thank you for posting your request. Your request private link is  " +
-      privateLink +
-      "\n\n\n" +
-      "Sincerely yours,\nOwner application team" +
-      "\n\n\n\n" +
-      "You have received this email because you have made a posing as not signed-in user at " +
-      ownerAppLink +
-      ". If you received this email in error please click on the link below." +
-      "\n" +
-      //TODO implement the link that should close the posting..."
-      "\n\n" +
-      "This is an automatic email, please do not reply.";
-
-    this.wonMailSender.sendTextMessage(toEmail, subject, text);
-
-  }
-
-
-
 
   public void sendConversationNotificationHtmlMessage(String toEmail, String localNeed, String
     remoteNeed, String localConnection, String textMsg) {
