d15b960f11040e1809460f962169329074ac43e4
Kevin Singer <ksinger@researchstudio.at>
2016-11-18 17:36:17 +0100

    slide-in animation.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/topnav.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/topnav.js
@@ -11,10 +11,11 @@ import { actionCreators }  from '../actions/actions';
 
 function genTopnavConf() {
     let template = `
-        <div class="slide-in" ng-show="self.connectionHasBeenLost">
+        <!-- <div class="slide-in" ng-show="self.connectionHasBeenLost">-->
+        <div class="slide-in" ng-class="{'slide-in--visible': self.connectionHasBeenLost}">
             Lost connection &ndash; make sure your internet-connection
             is working, then click &ldquo;reconnect&rdquo;.
-            <button ng-show="!self.reconnecting" ng-click="self.reconnect()" class="si__button red">
+            <button ng-show="self.connectionHasBeenLost && !self.reconnecting" ng-click="self.reconnect()" class="si__button red">
                 Reconnect
             </button>
 
