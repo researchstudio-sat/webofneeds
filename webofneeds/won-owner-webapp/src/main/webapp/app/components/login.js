3dd93332e58ca3bd7de616cb5f0d7c1f50578837
Kevin Singer <ksinger@researchstudio.at>
2016-12-21 15:20:37 +0100

    Auto-filling account-credentials working.
    It requires a hack though, that manually triggers
    an input-event when the login-mask becomes visible,
    as the auto-fill mechanism fails to do so in some
    browsers/plugins, which in turn leaves angular oblivious.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/login.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/login.js
@@ -3,7 +3,10 @@
  */
 ;
 import angular from 'angular';
-import { attach } from '../utils';
+import {
+    attach,
+    delay,
+} from '../utils';
 import { actionCreators }  from '../actions/actions';
 
 function genLoginConf() {
@@ -11,7 +14,8 @@ function genLoginConf() {
                         <span class="wl__button__caption">Sign in</span>
                         <img src="generated/icon-sprite.svg#ico16_arrow_up_hi" class="wl__button__carret">
                     </a>
-                    <div ng-form="loginForm">
+                    <!-- <div ng-form="loginForm">-->
+                    <form ng-submit="::self.login(self.email, self.password)" id="loginForm" class="loginForm">
                         <input
                             id="loginEmail"
                             placeholder="Email address"
@@ -24,12 +28,21 @@ function genLoginConf() {
                             {{self.loginError}}
                         </span>
                         <input
+                            id="loginPassword"
                             placeholder="Password"
                             ng-model="self.password"
                             type="password"
                             required
                             ng-keyup="self.loginReset() || ($event.keyCode == 13 && self.login(self.email, self.password))"/>
-                    </div>
+
+                        <!-- <input type="submit" value="LOGIN"/>-->
+                        <button
+                            class="won-button--filled lighterblue"
+                            ng-disabled="loginForm.$invalid">
+                            <!--ng-click="::self.login(self.email, self.password)">-->
+                                Sign in
+                        </button>
+                    </form>
                     <!-- TODO: Implement remember me and forgot password --><!--div class="wl__table">
                         <div class="wlt__left">
                             <input type="checkbox" ng-model="self.rememberMe" id="rememberMe"/><label for="rememberMe">Remember me</label>
@@ -38,20 +51,14 @@ function genLoginConf() {
                             <a href="#">Forgot Password?</a>
                         </div>
                     </div>-->
-                    <button
-                        class="won-button--filled lighterblue"
-                        ng-disabled="loginForm.$invalid"
-                        ng-click="::self.login(self.email, self.password)">
-                            Sign in
-                    </button>
-                    <span class="wl__register">
+                    <div class="wl__register">
                         No Account yet?
                         <a ui-sref="landingpage({focusSignup: true})">
                             Sign up
                         </a>
-                    </span>`;
+                    </div>`;
 
-    const serviceDependencies = ['$q', '$ngRedux', '$scope', /*'$routeParams' /*injections as strings here*/];
+    const serviceDependencies = ['$q', '$ngRedux', '$scope', '$element' /*'$routeParams' /*injections as strings here*/];
 
     class Controller {
         constructor(/* arguments <- serviceDependencies */){
@@ -70,6 +77,29 @@ function genLoginConf() {
 
             const disconnect = this.$ngRedux.connect(login, actionCreators)(this);
             this.$scope.$on('$destroy',disconnect);
+
+
+            this.autofillHack();
+
+            this.$scope.$watch(() => this.loginVisible, (newVis, oldVis) => {
+                console.log('lic4dbg loginVisible: ', oldVis, newVis);
+                //if(newVis && !oldVis) {
+                this.autofillHack();
+                //}
+            });
+        }
+
+        /**
+         * auto-fill hack. firefox doesn't fire an input event when auto-filling,
+         * so we do this here manually to make sure the ng-model updates itself.
+         */
+        autofillHack() {
+            return delay(0).then(() => {
+                this.$element.find('#loginEmail').trigger('input');
+                this.$element.find('#loginPassword').trigger('input');
+                delay(100).then(() => console.log("lic4dbg email: ", this.email));
+            });
+
         }
     }
     Controller.$inject = serviceDependencies;
