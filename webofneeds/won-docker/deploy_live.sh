4453416a332e2c175fc953ef2d023b9ade566c0d
hfriedrich <heiko.friedrich@researchstudio.at>
2017-01-20 16:18:05 +0100

    move gencert into docker-compose file

--- webofneeds/won-docker/deploy_live.sh
+++ webofneeds/won-docker/deploy_live.sh
@@ -16,23 +16,8 @@ rm won_certificate_passwd_file
 # for entries of alternative server names: matchat.org, www.matchat.org, satvm01.researchstudio.at
 scp $WORKSPACE/webofneeds/won-docker/image/gencert/openssl.conf won@satvm01:$base_folder/openssl.conf
 
-# owner server certificate generator
-# PLEASE NOTE that value of PASS should be the same used in your server.xml for SSLPassword on wonnode and owner,
-# and the same as activemq.broker.keystore.password used in your wonnode activemq spring configurations for broker
-# this image creates a keypair and a certificate based on it and stores the respective files as in the specified
-# folder (see parameter of option '-v'). If different certificates are to be created for different images
-# (e.g. owner and node), they should be put in different folders and these images should mount those different
-# folders where needed (e.g. for tomcat, that folder is '/usr/local/tomcat/conf/ssl/', as used e.g. in the run
-# command of the wonnode). Note that the filename of the certificate is also used in the tomcat config, (see
-# owner/ssl/server.xml) so be careful when changing it.
-docker -H satvm01:2375 rm gencert || echo 'No docker container found to remove with name: gencert'
-docker -H satvm01:2375 run --name=gencert -e CN="matchat.org" \
--e "PASS=file:/usr/local/certs/out/won_certificate_passwd_file" -e "OPENSSL_CONFIG_FILE=/usr/local/openssl.conf" \
--v $base_folder/won-server-certs:/usr/local/certs/out/ \
--v $base_folder/openssl.conf:/usr/local/openssl.conf webofneeds/gencert:live
-
 # copy the nginx.conf file to the proxy server
-rsync $WORKSPACE/webofneeds/won-docker/image/nginx/nginx.conf won@satvm01:$base_folder/nginx.conf
+scp $WORKSPACE/webofneeds/won-docker/image/nginx/nginx.conf won@satvm01:$base_folder/nginx.conf
 
 echo build the docker containers
 docker -H satvm01:2375 pull webofneeds/bigdata
