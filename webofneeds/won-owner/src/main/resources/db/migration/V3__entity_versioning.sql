a4dbc81550c9aef60b286d376fa79f81dcddd5d7
Florian Kleedorfer <florian.kleedorfer@researchstudio.at>
2017-03-29 13:56:38 +0200

    improved db migration script

--- webofneeds/won-owner/src/main/resources/db/migration/V3__entity_versioning.sql
+++ webofneeds/won-owner/src/main/resources/db/migration/V3__entity_versioning.sql
@@ -1,4 +1,7 @@
-ALTER TABLE event_container DROP COLUMN connection_id;
 ALTER TABLE connection ADD COLUMN event_container_id BIGINT NOT NULL;
-ALTER TABLE event_container DROP COLUMN need_id;
+UPDATE connection set event_container_id = (SELECT id FROM event_container WHERE connection_id = connection.id);
+ALTER TABLE event_container DROP COLUMN connection_id;
+
 ALTER TABLE need ADD COLUMN event_container_id BIGINT NOT NULL;
+UPDATE need set event_container_id = (SELECT id FROM event_container WHERE need_id = need.id);
+ALTER TABLE event_container DROP COLUMN need_id;
