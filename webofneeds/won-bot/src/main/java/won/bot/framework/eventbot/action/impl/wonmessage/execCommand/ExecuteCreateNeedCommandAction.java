71bfa14370ac1bc0487fb73c7c985146a18d0389
Florian Kleedorfer <florian.kleedorfer@researchstudio.at>
2017-04-05 08:51:49 +0200

    made more error-resistant
    publishing the FailureEvent before removing the URI from the list makes sure the
    event ist published even if the removal causes an error

--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/impl/wonmessage/execCommand/ExecuteCreateNeedCommandAction.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/impl/wonmessage/execCommand/ExecuteCreateNeedCommandAction.java
@@ -100,8 +100,8 @@ public class ExecuteCreateNeedCommandAction extends BaseEventBotAction {
             public void onEvent(Event event) throws Exception {
                 String textMessage = WonRdfUtils.MessageUtils.getTextMessage(((FailureResponseEvent) event).getFailureMessage());
                 logger.debug("need creation failed for need URI {}, original message URI {}: {}", new Object[]{needURI, ((FailureResponseEvent) event).getOriginalMessageURI(), textMessage});
-                EventBotActionUtils.removeFromList(getEventListenerContext(), needURI, createNeedCommandEvent.getUriListName());
                 getEventListenerContext().getEventBus().publish(new CreateNeedCommandFailureEvent(needURI, needUriBeforeCreation, createNeedCommandEvent, textMessage));
+                EventBotActionUtils.removeFromList(getEventListenerContext(), needURI, createNeedCommandEvent.getUriListName());
             }
         };
         EventBotActionUtils.makeAndSubscribeResponseListener(
