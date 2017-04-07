11f8496bf24f5d726368507c516f8ab88829bccb
hfriedrich <heiko.friedrich@researchstudio.at>
2017-04-07 19:24:40 +0200

    add normalization to need model wrapper

--- webofneeds/won-core/src/main/java/won/protocol/util/NeedModelWrapper.java
+++ webofneeds/won-core/src/main/java/won/protocol/util/NeedModelWrapper.java
@@ -1,9 +1,12 @@
 package won.protocol.util;
 
 import com.google.common.collect.HashBiMap;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.jena.graph.Node;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.rdf.model.impl.ResourceImpl;
+import org.apache.jena.rdf.model.impl.StatementImpl;
 import org.apache.jena.sparql.path.Path;
 import org.apache.jena.sparql.path.PathParser;
 import org.apache.jena.vocabulary.DCTerms;
@@ -19,9 +22,7 @@ import won.protocol.vocabulary.WON;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * This class wraps the need models (need and sysinfo graphs in a need dataset).
@@ -418,5 +419,156 @@ public class NeedModelWrapper {
 
         Node node = nodes.iterator().next().asNode();
         return RdfUtils.getNodeForPropertyPath(needModel, node, path);
+    }
+
+    private boolean isSplittableNode(RDFNode node) {
+        return node.isResource() &&
+                (node.isAnon() ||
+                        (   node.asResource().getURI().startsWith(getNeedUri()) &&
+                                (! node.asResource().getURI().equals(getNeedUri()))
+                        ));
+    }
+
+    /**
+     * Returns the RDFNodes reachable from the specified startNode in breadth-first order.
+     * @param startNode
+     * @param model
+     * @return
+     */
+    private List<RDFNode> breadthFirstOrder(RDFNode startNode, Model model){
+        return breadthFirstOrder(startNode, model, Collections.emptyList());
+    }
+
+    /**
+     * Returns the RDFNodes reachable from the specified startNode in breadth-first order.
+     * @param startNode
+     * @param model
+     * @param blacklist
+     * @return
+     */
+    private List<RDFNode> breadthFirstOrder(RDFNode startNode, Model model, List<RDFNode> blacklist){
+        if (!startNode.isResource()) return Collections.emptyList();
+        if (blacklist.contains(startNode)) return Collections.emptyList();
+        Resource startResource = startNode.asResource();
+        List<RDFNode> toVisit =  model.listObjectsOfProperty(startResource, null).toList();
+        List<RDFNode> result = new ArrayList<>();
+        result.add(0, startNode); //put start node first -> breadth first
+        List<RDFNode> newBlacklist = new ArrayList<>(blacklist.size()+1);
+        newBlacklist.add(startNode); //don't revisit
+        newBlacklist.addAll(blacklist);
+        for(RDFNode newStartNode: toVisit) {
+            //recurse
+            result.addAll(breadthFirstOrder(newStartNode, model, Collections.unmodifiableList(newBlacklist)));
+        }
+        return result;
+    }
+
+    /**
+     * Returns a copy of the model in which no node reachable from the need node has multiple incoming edges
+     * (unless the graph contains a circle, see below). This is achieved by making copies of all nodes that have multiple
+     * incoming edges, such that each copy and the original get on of the incoming edges. The outgoing
+     * edges of the original are replicated in the copies.
+     *
+     * Nodes are visited in breadth first order. Each node is only be split once.
+     * Nodes that were newly introduced by this algorithm are never split.
+     *
+     * In that special case that the graph contains a circle, the resulting graph still contains a circle, and
+     * possibly one or more nodes with more than one incoming edge.
+     *
+     * @return
+     */
+    public Model normalizeNeedModel() {
+        Model copy = RdfUtils.cloneModel(needModel);
+        Set<RDFNode> blacklist = new HashSet<>();
+        RDFNode needNode = copy.getResource(getNeedUri().toString());
+        List<RDFNode> nodesInBreadthFirstOrderFromNeed = breadthFirstOrder(needNode, copy);
+        List<RDFNode> nodesToVisit = new ArrayList<>();
+        nodesToVisit.addAll(nodesInBreadthFirstOrderFromNeed);
+        //now add all the other nodes that are objects
+        List<RDFNode> otherNodes = needModel.listObjects().toList();
+        otherNodes.removeAll(nodesInBreadthFirstOrderFromNeed);
+        nodesToVisit.addAll(otherNodes);
+        for(RDFNode toSplitIfNecessary: nodesToVisit) {
+            List<RDFNode> processedAndCreatedNodes = findAndSplitOneNode(copy,toSplitIfNecessary, blacklist);
+            blacklist.addAll(processedAndCreatedNodes);
+        }
+        return copy;
+    }
+
+    /**
+     * Finds a node to split and applies the binary split until it is
+     * unsplit (i.e. each copy and the remaining original has exactly one incoming edge.
+     *
+     * The split node and all new copies are returned and are not to be processed
+     * any further during the splitting algorithm.
+     *
+     * @return true if a node was split, false if there were none
+     */
+    private List<RDFNode> findAndSplitOneNode(Model model, RDFNode node, Set<RDFNode> blacklist) {
+
+        if (!blacklist.contains(node) && isSplittableNode(node)) {
+            StmtIterator stmtIt = model.listStatements(null, null, node);
+            List<Resource> subjectsOfInEdges = new ArrayList<>();
+            while (stmtIt.hasNext()) {
+                Statement stmt = stmtIt.nextStatement();
+                subjectsOfInEdges.add(stmt.getSubject());
+            }
+            if (subjectsOfInEdges.size() > 1) {
+                //more than 1 in-edges. split node
+                //remember the new nodes and the current node and pass them back as result
+                //so they can be added to the blacklist
+                List<RDFNode> newlyProcessed = new ArrayList<>();
+                for (Resource subjectToSplitOff : subjectsOfInEdges) {
+                    RDFNode newNode = binarySplitNode(node.asResource(), subjectToSplitOff, model);
+                    newlyProcessed.add(newNode);
+                }
+                newlyProcessed.add(node);
+                return newlyProcessed;
+            }
+            return Collections.emptyList();
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * Splits one copy off the specified node, connects it with the specified subject, copies all outgoing edges
+     * and returns the newly generated copy.
+     * @param node
+     * @param subjectToSplitOff
+     * @param model
+     * @return
+     */
+    private RDFNode binarySplitNode(Resource node, Resource subjectToSplitOff, Model model) {
+        //make copy
+        Resource copy = copyNode(node, model);
+        //re-point one edge from subjectToSplitOff to copy (doesn't matter if it has multiple edges to it or not)
+        StmtIterator it = model.listStatements(subjectToSplitOff, null, node);
+        Statement statement = it.nextStatement();
+        it.close();
+        Statement copiedStatement = new StatementImpl(statement.getSubject(), statement.getPredicate(), copy);
+        model.add(copiedStatement);
+        model.remove(statement);
+        List<Statement> statementList = new ArrayList<>();
+        //copy outgoing edges from node to copy
+        it = model.listStatements(node, null, (RDFNode)null);
+        while(it.hasNext()) {
+            statement = it.nextStatement();
+            copiedStatement = new StatementImpl(copy, statement.getPredicate(), statement.getObject());
+            statementList.add(copiedStatement);
+        }
+        model.add(statementList);
+        return copy;
+    }
+
+    private Resource copyNode(Resource node, Model model) {
+        if (node.isAnon()) return model.createResource();
+        int i = 0;
+        String uri = node.getURI() + RandomStringUtils.randomAlphanumeric(4);
+        String newUri = uri+"_"+ i;
+        while (model.containsResource(new ResourceImpl(newUri))){
+            i++;
+            newUri = uri+"_"+i;
+        }
+        return model.getResource(newUri);
     }
 }
