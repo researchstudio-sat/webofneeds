ef023d1ba97b9b38cf58b7564f84b1be6421722f
hfriedrich <heiko.friedrich@researchstudio.at>
2017-04-12 14:11:45 +0200

    update solr querying

--- webofneeds/won-matcher-solr/src/main/java/won/matcher/solr/query/factory/GeoDistFilterQueryFactory.java
+++ webofneeds/won-matcher-solr/src/main/java/won/matcher/solr/query/factory/GeoDistFilterQueryFactory.java
@@ -1,40 +1,31 @@
 package won.matcher.solr.query.factory;
 
-import org.apache.jena.query.Dataset;
-import won.matcher.solr.index.NeedIndexer;
-import won.protocol.model.NeedContentPropertyType;
-import won.protocol.util.DefaultNeedModelWrapper;
-
 /**
  * Created by hfriedrich on 22.08.2016.
  */
-public class GeoDistFilterQueryFactory extends NeedDatasetQueryFactory
-{
-  private Float latitude;
-  private Float longitude;
-  private double distance;
+public class GeoDistFilterQueryFactory extends SolrQueryFactory {
 
-  public GeoDistFilterQueryFactory(Dataset need, double distanceInKilometers) {
-    super(need);
+    private float latitude;
+    private float longitude;
+    private String solrLocationField;
+    private double distance;
 
-    DefaultNeedModelWrapper needModelWrapper = new DefaultNeedModelWrapper(need);
-    latitude = needModelWrapper.getLocationLatitude(NeedContentPropertyType.ALL);
-    longitude = needModelWrapper.getLocationLongitude(NeedContentPropertyType.ALL);
-    distance = distanceInKilometers;
-  }
+    public GeoDistFilterQueryFactory(String solrLocationField, float latitude, float longitude, double distanceInKilometers) {
 
-  @Override
-  protected String makeQueryString() {
-
-    if (longitude == null || latitude == null) {
-      return "";
+        this.solrLocationField = solrLocationField;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        distance = distanceInKilometers;
     }
 
-    // create a geographical distance filter with radius "distance"
-    StringBuilder sb = new StringBuilder();
-    sb.append("{!geofilt sfield=").append(NeedIndexer.SOLR_LOCATION_COORDINATES_FIELD).append(" pt=").append(latitude)
-      .append(",").append(longitude).append(" d=").append(distance).append("}");
-    return sb.toString();
-  }
+    @Override
+    protected String makeQueryString() {
+
+        // create a geographical distance filter with radius "distance" (in kilometers)
+        StringBuilder sb = new StringBuilder();
+        sb.append("{!geofilt sfield=").append(solrLocationField).append(" pt=")
+                .append(latitude).append(",").append(longitude).append(" d=").append(distance).append("}");
+        return sb.toString();
+    }
 
 }
