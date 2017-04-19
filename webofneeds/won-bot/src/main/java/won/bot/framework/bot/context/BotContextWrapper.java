7043bc77e9711952a61128fa5abfbdfad1446ded
quasarchimaere <sudafabian@gmail.com>
2017-04-19 12:38:49 +0200

    added helper method in botcontextwrapper

--- webofneeds/won-bot/src/main/java/won/bot/framework/bot/context/BotContextWrapper.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/bot/context/BotContextWrapper.java
@@ -1,6 +1,12 @@
 package won.bot.framework.bot.context;
 
+import won.bot.framework.eventbot.action.impl.needlifecycle.AbstractCreateNeedAction;
+
+import java.net.URI;
+
 public class BotContextWrapper {
+    public static final String KEY_NEED_REMOTE_NEED_ASSOCIATION = "need_remote_need";
+
     private String needCreateListName;
     private BotContext botContext;
 
@@ -15,5 +21,15 @@ public class BotContextWrapper {
 
     public BotContext getBotContext() {
         return botContext;
+    }
+
+    public URI getUriAssociation(URI uri){
+        return (URI) getBotContext().loadFromObjectMap(KEY_NEED_REMOTE_NEED_ASSOCIATION, uri.toString());
+    }
+
+    public void addUriAssociation(URI uri, URI uri2){
+        // save the mapping between the original and the reaction in to the context.
+        getBotContext().saveToObjectMap(KEY_NEED_REMOTE_NEED_ASSOCIATION, uri.toString(), uri2);
+        getBotContext().saveToObjectMap(KEY_NEED_REMOTE_NEED_ASSOCIATION, uri2.toString(), uri);
     }
 }
