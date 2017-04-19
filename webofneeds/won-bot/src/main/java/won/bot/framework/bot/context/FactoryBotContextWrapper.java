ec582b4d4617c199d14c0cff1de78c8861da6969
quasarchimaere <sudafabian@gmail.com>
2017-04-19 12:41:18 +0200

    added functionality

--- webofneeds/won-bot/src/main/java/won/bot/framework/bot/context/FactoryBotContextWrapper.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/bot/context/FactoryBotContextWrapper.java
@@ -1,15 +1,35 @@
 package won.bot.framework.bot.context;
 
 
+import java.net.URI;
+
 public class FactoryBotContextWrapper extends BotContextWrapper {
     private String factoryListName;
+    private String factoryInternalIdName;
 
-    public FactoryBotContextWrapper(BotContext botContext, String needCreateListName, String factoryListName) {
+    public FactoryBotContextWrapper(BotContext botContext, String needCreateListName, String factoryListName, String factoryInternalIdName) {
         super(botContext, needCreateListName);
         this.factoryListName = factoryListName;
+        this.factoryInternalIdName = factoryInternalIdName;
     }
 
     public String getFactoryListName() {
-        return null;
+        return factoryListName;
+    }
+
+    public String getFactoryInternalIdName() {
+        return factoryInternalIdName;
+    }
+
+    public boolean isFactoryNeed(URI uri) {
+        return getBotContext().isInNamedNeedUriList(uri, factoryListName);
+    }
+
+    public URI getURIFromInternal(URI uri) {
+        return (URI) getBotContext().loadFromObjectMap(factoryInternalIdName, uri.toString());
+    }
+
+    public void addInternalIdToUriReference(URI internalUri, URI uri){
+        getBotContext().saveToObjectMap(factoryInternalIdName, internalUri.toString(), uri);
     }
 }
