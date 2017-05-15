8aa75493ebf77af96c065bdf10f3ec2891649b55
Florian Kleedorfer <florian.kleedorfer@researchstudio.at>
2017-05-15 15:37:04 +0200

    removed unused method

--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/behaviour/BotBehaviour.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/behaviour/BotBehaviour.java
@@ -20,11 +20,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import won.bot.framework.eventbot.EventListenerContext;
 import won.bot.framework.eventbot.event.Event;
-import won.bot.framework.eventbot.filter.EventFilter;
 import won.bot.framework.eventbot.listener.EventListener;
-import won.bot.framework.eventbot.listener.impl.ActionOnEventListener;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class BotBehaviour {
@@ -120,13 +121,6 @@ public abstract class BotBehaviour {
         }
         activeListeners.clear();
         active.set(false);
-    }
-
-    /**
-     * Cleans up the coordination behaviours, to be used in case of an error in the behaviour (because it won't terminate correctly).
-     */
-    private final synchronized void cleanupCoordiationBehaviours(){
-        coordinationBehaviours.stream().forEach(b -> b.deactivate());
     }
 
     /**
