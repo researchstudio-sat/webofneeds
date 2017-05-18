fbe1566c62ec7c97786be2c14fa5827ddbaea294
Kevin Singer <e1028092@student.tuwien.ac.at>
2017-05-18 15:39:03 +0200

    Decreased intendation to fix syntax highlighting.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/feed-item.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/feed-item.js
@@ -24,88 +24,88 @@ import feedItemLineModule from './feed-item-line';
 const serviceDependencies = ['$scope', '$interval', '$ngRedux'];
 function genComponentConf() {
     let template = `
-            <div class="fi clickable" ui-sref="post({postUri: self.ownNeed.get('@id')})">
-                <won-square-image
-                    src="self.ownNeed.get('titleImg')"
-                    title="self.ownNeedContent.get('dc:title')"
-                    uri="self.ownNeed.get('@id')">
-                </won-square-image>
-                <div class="fi__description">
-                    <div class="fi__description__topline">
-                        <div class="fi__description__topline__title">
-                            {{ self.ownNeedContent.get('dc:title') }}
-                        </div>
-                        <div class="fi__description__topline__date">
-                            {{ self.createdOn }}
-                        </div>
+        <div class="fi clickable" ui-sref="post({postUri: self.ownNeed.get('@id')})">
+            <won-square-image
+                src="self.ownNeed.get('titleImg')"
+                title="self.ownNeedContent.get('dc:title')"
+                uri="self.ownNeed.get('@id')">
+            </won-square-image>
+            <div class="fi__description">
+                <div class="fi__description__topline">
+                    <div class="fi__description__topline__title">
+                        {{ self.ownNeedContent.get('dc:title') }}
                     </div>
-                    <div class="fi__description__subtitle">
-                    <!--
-                        <span class="fi__description__subtitle__group" ng-show="self.ownNeed.group">
-                            <img src="generated/icon-sprite.svg#ico36_group" class="fi__description__subtitle__group__icon">
-                            {{self.ownNeed.group}}
-                            <span class="fi__description__subtitle__group__dash"> &ndash; </span>
-                        </span>
-                        -->
-                        <span class="fi__description__subtitle__type">
-                            {{
-                                self.labels.type[
-                                    self.inferLegacyNeedType(self.ownNeed)
-                                ]
-                            }}
-                        </span>
+                    <div class="fi__description__topline__date">
+                        {{ self.createdOn }}
                     </div>
                 </div>
-            </div>
-            <div class="fmil">
-
-                <won-feed-item-line
-                    class="fmil__item clickable"
-                    ng-repeat="cnctUri in self.connectionUris track by $index"
-                    connection-uri="cnctUri"
-                    ng-show="$index < self.maxNrOfItemsShown"
-                    ui-sref="post({
-                        postUri: self.ownNeed.get('@id'),
-                        connectionUri: cnctUri,
-                        connectionType: cnct.getIn(['connection', 'hasConnectionState'])
-                    })">
-                </won-feed-item-line>
-
-                <div class="fmil__more clickable"
-                     ng-show="self.connectionUris.size === self.maxNrOfItemsShown + 1"
-                     ng-click="self.showMore()">
-                        1 more activity
-                </div>
-                <div class="fmil__more clickable"
-                     ng-show="self.connectionUris.size > self.maxNrOfItemsShown + 1"
-                     ng-click="self.showMore()">
-                        {{self.connectionUris.size - self.maxNrOfItemsShown}} more activities
+                <div class="fi__description__subtitle">
+                <!--
+                    <span class="fi__description__subtitle__group" ng-show="self.ownNeed.group">
+                        <img src="generated/icon-sprite.svg#ico36_group" class="fi__description__subtitle__group__icon">
+                        {{self.ownNeed.group}}
+                        <span class="fi__description__subtitle__group__dash"> &ndash; </span>
+                    </span>
+                    -->
+                    <span class="fi__description__subtitle__type">
+                        {{
+                            self.labels.type[
+                                self.inferLegacyNeedType(self.ownNeed)
+                            ]
+                        }}
+                    </span>
                 </div>
             </div>
+        </div>
+        <div class="fmil">
 
-            <div class="fi__footer" ng-show="self.unreadMatchesCount() || self.unreadRequestsCount()">
-                <div class="fi__footer__indicators">
-                    <a class="fi__footer__indicators__item clickable"
-                       ui-sref="postMatches({myUri: self.ownNeed.get('@id')})"
-                       ng-show="self.unreadMatchesCount()">
-                        <img src="generated/icon-sprite.svg#ico36_match" class="fi__footer__indicators__item__icon"/>
-                        <span class="fi__footer__indicators__item__caption">
-                           {{ self.unreadMatchesCount() }}
-                           Match{{self.unreadMatchesCount() > 1 ? 'es' : ''}}
-                        </span>
-                    </a>
-                    <a class="fi__footer__indicators__item clickable"
-                       ui-sref="postRequests({myUri: self.ownNeed.get('@id')})"
-                       ng-show="self.unreadRequestsCount()">
-                        <img src="generated/icon-sprite.svg#ico36_incoming" class="fi__footer__indicators__item__icon"/>
-                        <span class="fi__footer__indicators__item__caption">
-                            {{self.unreadRequestsCount()}}
-                            Incoming Request{{ self.unreadRequestsCount() > 1 ? 's' : ''}}
-                        </span>
-                    </a>
-                </div>
+            <won-feed-item-line
+                class="fmil__item clickable"
+                ng-repeat="cnctUri in self.connectionUris track by $index"
+                connection-uri="cnctUri"
+                ng-show="$index < self.maxNrOfItemsShown"
+                ui-sref="post({
+                    postUri: self.ownNeed.get('@id'),
+                    connectionUri: cnctUri,
+                    connectionType: cnct.getIn(['connection', 'hasConnectionState'])
+                })">
+            </won-feed-item-line>
+
+            <div class="fmil__more clickable"
+                 ng-show="self.connectionUris.size === self.maxNrOfItemsShown + 1"
+                 ng-click="self.showMore()">
+                    1 more activity
             </div>
-            `;
+            <div class="fmil__more clickable"
+                 ng-show="self.connectionUris.size > self.maxNrOfItemsShown + 1"
+                 ng-click="self.showMore()">
+                    {{self.connectionUris.size - self.maxNrOfItemsShown}} more activities
+            </div>
+        </div>
+
+        <div class="fi__footer" ng-show="self.unreadMatchesCount() || self.unreadRequestsCount()">
+            <div class="fi__footer__indicators">
+                <a class="fi__footer__indicators__item clickable"
+                   ui-sref="postMatches({myUri: self.ownNeed.get('@id')})"
+                   ng-show="self.unreadMatchesCount()">
+                    <img src="generated/icon-sprite.svg#ico36_match" class="fi__footer__indicators__item__icon"/>
+                    <span class="fi__footer__indicators__item__caption">
+                       {{ self.unreadMatchesCount() }}
+                       Match{{self.unreadMatchesCount() > 1 ? 'es' : ''}}
+                    </span>
+                </a>
+                <a class="fi__footer__indicators__item clickable"
+                   ui-sref="postRequests({myUri: self.ownNeed.get('@id')})"
+                   ng-show="self.unreadRequestsCount()">
+                    <img src="generated/icon-sprite.svg#ico36_incoming" class="fi__footer__indicators__item__icon"/>
+                    <span class="fi__footer__indicators__item__caption">
+                        {{self.unreadRequestsCount()}}
+                        Incoming Request{{ self.unreadRequestsCount() > 1 ? 's' : ''}}
+                    </span>
+                </a>
+            </div>
+        </div>
+    `;
     
     class Controller {
         constructor() {
