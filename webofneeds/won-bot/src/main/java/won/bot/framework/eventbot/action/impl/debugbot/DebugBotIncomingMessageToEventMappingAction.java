818fc69ca7ac67ecd3760b9f3ff973be6e4793f6
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2017-06-19 22:09:45 +0200

    added the 'validate' command
    which activates the ValidateConnectionDataBehaviour

--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/impl/debugbot/DebugBotIncomingMessageToEventMappingAction.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/impl/debugbot/DebugBotIncomingMessageToEventMappingAction.java
@@ -20,6 +20,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.rdf.model.Model;
 import won.bot.framework.eventbot.EventListenerContext;
 import won.bot.framework.eventbot.action.BaseEventBotAction;
+import won.bot.framework.eventbot.behaviour.ValidateConnectionDataBehaviour;
 import won.bot.framework.eventbot.bus.EventBus;
 import won.bot.framework.eventbot.event.BaseNeedAndConnectionSpecificEvent;
 import won.bot.framework.eventbot.event.ConnectionSpecificEvent;
@@ -29,11 +30,15 @@ import won.bot.framework.eventbot.event.impl.command.close.CloseCommandEvent;
 import won.bot.framework.eventbot.event.impl.command.connectionmessage.ConnectionMessageCommandEvent;
 import won.bot.framework.eventbot.event.impl.command.deactivate.DeactivateNeedCommandEvent;
 import won.bot.framework.eventbot.event.impl.debugbot.*;
+import won.bot.framework.eventbot.event.impl.validate.ValidateConnectionCommandEvent;
+import won.bot.framework.eventbot.event.impl.validate.ValidateConnectionCommandFailureEvent;
+import won.bot.framework.eventbot.event.impl.validate.ValidateConnectionCommandSuccessEvent;
 import won.bot.framework.eventbot.listener.EventListener;
 import won.protocol.message.WonMessage;
 import won.protocol.model.Connection;
 import won.protocol.util.WonRdfUtils;
 
+import java.time.Duration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -50,6 +55,7 @@ public class DebugBotIncomingMessageToEventMappingAction extends BaseEventBotAct
     Pattern PATTERN_CHATTY_ON = Pattern.compile("^chatty\\s+on$", Pattern.CASE_INSENSITIVE);
     Pattern PATTERN_CHATTY_OFF = Pattern.compile("^chatty\\s+off$", Pattern.CASE_INSENSITIVE);
     Pattern PATTERN_SEND_N = Pattern.compile("^send ([1-9])$", Pattern.CASE_INSENSITIVE);
+    Pattern PATTERN_VALIDATE = Pattern.compile("^validate$", Pattern.CASE_INSENSITIVE);
 
 
     public static final String[] USAGE_MESSAGES = {
@@ -61,6 +67,7 @@ public class DebugBotIncomingMessageToEventMappingAction extends BaseEventBotAct
             "    'deactivate':  deactivate remote need of the current connection",
             "    'chatty on|off': (do not) send chat messages spontaneously every now and then (default: on)",
             "    'send N':      send N messages, one per second. N must be an integer between 1 and 9",
+            "    'validate':    download the connection data and validate it",
             "    'usage':       display this message"
     };
 
@@ -141,6 +148,42 @@ public class DebugBotIncomingMessageToEventMappingAction extends BaseEventBotAct
                     int n = Integer.parseInt(nStr);
 
                     bus.publish(new SendNDebugCommandEvent(con, n));
+                }else if (PATTERN_VALIDATE.matcher(message).matches()) {
+                    //acknowledge the command
+                    Model messageModel = WonRdfUtils.MessageUtils.textMessage("Ok, I'm going to validate the data in our connection. This may take a while.");
+                    bus.publish(new ConnectionMessageCommandEvent(con, messageModel));
+                    //start validation behaviour
+                    ValidateConnectionCommandEvent command = new ValidateConnectionCommandEvent(con.getNeedURI(), con.getConnectionURI());
+                    ValidateConnectionDataBehaviour validateConnectionDataBehaviour = new ValidateConnectionDataBehaviour(ctx,command, Duration.ofSeconds(60));
+                    validateConnectionDataBehaviour.onResult(new BaseEventBotAction(ctx) {
+                        @Override
+                        protected void doRun(Event event, EventListener executingListener) throws Exception {
+                            if (event instanceof ValidateConnectionCommandSuccessEvent) {
+                                ValidateConnectionCommandSuccessEvent successEvent = (ValidateConnectionCommandSuccessEvent) event;
+                                Model messageModel = WonRdfUtils.MessageUtils
+                                        .textMessage(successEvent.getMessage());
+                                bus.publish(new ConnectionMessageCommandEvent(con, messageModel));
+                                if (!successEvent.isValid()) {
+                                    String message = successEvent.getMessage();
+                                    if (message == null || message.trim().length() == 0) {
+                                        message = "[no message available]";
+                                    }
+                                    messageModel = WonRdfUtils.MessageUtils
+                                            .textMessage("Problem report: " + message);
+                                    bus.publish(new ConnectionMessageCommandEvent(con, messageModel));
+                                }
+                            } else if (event instanceof ValidateConnectionCommandFailureEvent){
+                                String message = ((ValidateConnectionCommandFailureEvent)event).getMessage();
+                                if (message == null || message.trim().length() == 0) {
+                                    message = "[no message available]";
+                                }
+                                Model messageModel = WonRdfUtils.MessageUtils
+                                        .textMessage("Could not validate connection data. Problem: " + message);
+                                bus.publish(new ConnectionMessageCommandEvent(con, messageModel));
+                            }
+                        }
+                    });
+                    validateConnectionDataBehaviour.activate();
                 } else {
                     //default: answer with eliza.
                     bus.publish(new MessageToElizaEvent(con, message));
