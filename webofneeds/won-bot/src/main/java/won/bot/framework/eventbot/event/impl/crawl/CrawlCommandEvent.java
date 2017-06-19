2f918e43faf919d147e5dd09795ebe46bb8a2fdc
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2017-06-19 22:00:14 +0200

    new Crawl Commands/Actions
    used for the new 'validate' command in the debugbot

---
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/crawl/CrawlCommandEvent.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2012  Research Studios Austria Forschungsges.m.b.H.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package won.bot.framework.eventbot.event.impl.crawl;
+
+import org.apache.jena.sparql.path.Path;
+import won.bot.framework.eventbot.event.BaseNeedSpecificEvent;
+import won.bot.framework.eventbot.event.impl.cmd.CommandEvent;
+
+import java.net.URI;
+import java.util.List;
+
+/**
+ * Initiates the crawling of linked data. The WebID of the specified need is used.
+ */
+public class CrawlCommandEvent extends BaseNeedSpecificEvent implements CommandEvent {
+    private List<Path> propertyPaths;
+    private URI startURI;
+    private int getMaxRequest=1000;
+    private int maxDepth = 5;
+
+    public CrawlCommandEvent(URI needURI, URI startURI, List<Path> propertyPaths, int getMaxRequest, int maxDepth) {
+        super(needURI);
+        this.propertyPaths = propertyPaths;
+        this.startURI = startURI;
+        this.getMaxRequest = getMaxRequest;
+        this.maxDepth = maxDepth;
+    }
+
+    public URI getStartURI() {
+        return startURI;
+    }
+
+    public List<Path> getPropertyPaths() {
+        return propertyPaths;
+    }
+
+    public int getGetMaxRequest() {
+        return getMaxRequest;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+}
