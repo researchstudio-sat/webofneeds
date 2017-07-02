41971306d373dced90dabe079102c98bd8513012
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2017-07-02 12:39:50 +0200

    switched query back to use conditional

--- webofneeds/won-cryptography/src/main/java/won/cryptography/webid/WonDefaultAccessControlRules.java
+++ webofneeds/won-cryptography/src/main/java/won/cryptography/webid/WonDefaultAccessControlRules.java
@@ -3,7 +3,6 @@ package won.cryptography.webid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import won.protocol.model.MessageEventPlaceholder;
 import won.protocol.repository.MessageEventRepository;
 
 import java.net.URI;
@@ -36,8 +35,8 @@ public class WonDefaultAccessControlRules implements AccessControlRules
     }
     URI resourceUri = URI.create(resourceURI);
     URI webId = URI.create(firstWebId);
-    MessageEventPlaceholder messageEventPlaceholder = messageEventRepository.isReadPermittedForWebID( resourceUri, webId);
-    return messageEventPlaceholder != null;
+    boolean accessGranted = messageEventRepository.isReadPermittedForWebID( resourceUri, webId);
+    return accessGranted;
   }
 
 }
