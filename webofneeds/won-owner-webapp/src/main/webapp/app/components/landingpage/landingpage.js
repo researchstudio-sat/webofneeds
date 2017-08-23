d4cf1a7e36c3a451e48f2344a3b7c00c3c6a465b
Kevin Singer <e1028092@student.tuwien.ac.at>
2017-08-23 17:57:22 +0200

    @landing-page: Opening "about"-page in newtab to avoid losing draft.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/landingpage/landingpage.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/landingpage/landingpage.js
@@ -10,14 +10,14 @@ import { actionCreators }  from '../../actions/actions';
 
 import * as srefUtils from '../../sref-utils';
 
-const serviceDependencies = ['$ngRedux', '$scope', /*'$routeParams' /*injections as strings here*/];
+const serviceDependencies = ['$ngRedux', '$scope', '$state' /*'$routeParams' /*injections as strings here*/];
 
 class LandingpageController {
     constructor(/* arguments <- serviceDependencies */){
         attach(this, serviceDependencies, arguments);
         Object.assign(this, srefUtils); // bind srefUtils to scope
 
-        window.lp4dbg = this;
+        window.ldgp4dbg = this;
 
         const self = this;
 
@@ -25,6 +25,11 @@ class LandingpageController {
 
         const disconnect = this.$ngRedux.connect(select, actionCreators)(this);
         this.$scope.$on('$destroy',disconnect);
+    }
+
+    openAboutInNewTab() {
+        const url = this.$state? this.$state.href('about') : '#/about';
+        window.open(url);
     }
 }
 
