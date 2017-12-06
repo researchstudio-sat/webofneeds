cb204d4f400a7f9306e63eacf520c086bff7819d
hfriedrich <heiko.friedrich@researchstudio.at>
2017-12-06 14:42:13 +0100

    create data extraction tests

--- webofneeds/won-utils/won-utils-goals/src/test/java/won/utils/goals/blending/GraphBlendingTest.java
+++ webofneeds/won-utils/won-utils-goals/src/test/java/won/utils/goals/blending/GraphBlendingTest.java
@@ -3,7 +3,6 @@ package won.utils.goals.blending;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.junit.Assert;
@@ -92,19 +91,8 @@ public class GraphBlendingTest {
         Model actual = GraphBlending.blendSimple(m1, m2, "http://example.org/test#blended");
         actual.write(System.out, "TRIG");
 
-        Assert.assertFalse(containSameStatements(m1, m2));
-        Assert.assertTrue(containSameStatements(expected, actual));
-    }
-
-    private boolean containSameStatements(Model m1, Model m2) {
-
-        for (Statement stmt1 : m1.listStatements().toList()) {
-            if (!m2.contains(stmt1)) {
-                return false;
-            }
-        }
-
-        return (m1.listStatements().toList().size() == m2.listStatements().toList().size());
+        Assert.assertFalse(m1.isIsomorphicWith(m2));
+        Assert.assertTrue(expected.isIsomorphicWith(actual));
     }
 
     private Dataset loadDataset(String path) throws IOException {
--- webofneeds/won-utils/won-utils-goals/src/test/java/won/utils/goals/extraction/DataExtractionTest.java
+++ webofneeds/won-utils/won-utils-goals/src/test/java/won/utils/goals/extraction/DataExtractionTest.java
@@ -1,17 +1,86 @@
 package won.utils.goals.extraction;
 
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class DataExtractionTest {
 
     @Test
-    public void test() throws IOException {
+    public void playgroundExample() throws IOException {
         DataExtraction ex = new DataExtraction();
-        Dataset ds = null;
-        Dataset result = ex.extract(ds);
-        System.out.println(result);
+        Dataset ds = loadDataset("/won/utils/goals/extraction/playground-example.trig");
+        Model result = ex.extract(ds);
+
+        RDFDataMgr.write(System.out, result, Lang.TRIG);
+    }
+
+    @Test
+    public void additionalNodeNotCoveredByShape() throws IOException {
+        DataExtraction ex = new DataExtraction();
+        Dataset ds = loadDataset("/won/utils/goals/extraction/additional-node-not-covered-by-shape.trig");
+        Model result = ex.extract(ds);
+
+        RDFDataMgr.write(System.out, result, Lang.TRIG);
+    }
+
+    @Test
+    public void additionalNodeNotCoveredByShapeNoErrors() throws IOException {
+        DataExtraction ex = new DataExtraction();
+        Dataset ds = loadDataset("/won/utils/goals/extraction/additional-node-not-covered-by-shape-noerrors.trig");
+        Model result = ex.extract(ds);
+
+        RDFDataMgr.write(System.out, result, Lang.TRIG);
+    }
+
+    @Test
+    public void additionalNodeNotCoveredByShapePersonClosed() throws IOException {
+        DataExtraction ex = new DataExtraction();
+        Dataset ds = loadDataset("/won/utils/goals/extraction/additional-node-not-covered-by-shape-person-closed.trig");
+        Model result = ex.extract(ds);
+
+        RDFDataMgr.write(System.out, result, Lang.TRIG);
+    }
+
+    @Test
+    public void additionalNodeNotCoveredByShapePropoertyPath() throws IOException {
+        DataExtraction ex = new DataExtraction();
+        Dataset ds = loadDataset("/won/utils/goals/extraction/additional-node-not-covered-by-shape-property-path.trig");
+        Model result = ex.extract(ds);
+
+        RDFDataMgr.write(System.out, result, Lang.TRIG);
+    }
+
+    @Test
+    public void additionalNodeNotCoveredByShapePropoertyPathWithError() throws IOException {
+        DataExtraction ex = new DataExtraction();
+        Dataset ds = loadDataset("/won/utils/goals/extraction/additional-node-not-covered-by-shape-property-path-with-error.trig");
+        Model result = ex.extract(ds);
+
+        RDFDataMgr.write(System.out, result, Lang.TRIG);
+    }
+
+    private Dataset loadDataset(String path) throws IOException {
+
+        InputStream is = null;
+        Dataset dataset = null;
+        try {
+            is = getClass().getResourceAsStream(path);
+            dataset = DatasetFactory.create();
+            RDFDataMgr.read(dataset, is, RDFFormat.TRIG.getLang());
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+
+        return dataset;
     }
 }
