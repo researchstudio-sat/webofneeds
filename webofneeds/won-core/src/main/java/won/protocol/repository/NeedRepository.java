ea74ce85122b4aa53cd8bee8ed13a9d7e4a66958
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2018-01-10 10:25:29 +0100

    Add need deactivation of connected abandoned needs
    New config property defining an inactivity timeout after which needs are
    closed, even if they have active connections

--- webofneeds/won-core/src/main/java/won/protocol/repository/NeedRepository.java
+++ webofneeds/won-core/src/main/java/won/protocol/repository/NeedRepository.java
@@ -87,6 +87,26 @@ public interface NeedRepository extends WonRepository<Need> {
             "group by need " +
             "having max(mep.creationDate) > :startDate and max(mep.creationDate) < :endDate "
     )
+    Slice<Need> findNeedsInactiveBetweenAndNotConnected(@Param("startDate") Date start, @Param("endDate") Date end, Pageable pageable);
+    
+    /**
+     * Finds needs that have been inactive between start and end date
+     * @param start
+     * @param end
+     * @param pageable
+     * @return
+     */
+    @Query("select distinct need from Need need " +
+            "join Connection c on ( c.needURI = need.needURI ) join MessageEventPlaceholder mep on (mep.parentURI = need.needURI or mep.parentURI = c.connectionURI) " +
+            "where " +
+            "need.state = 'ACTIVE' " +
+            "and " +
+            "mep.messageType <> 'NEED_MESSAGE' " +
+            "and " +
+            "( mep.senderURI = c.connectionURI or mep.senderNeedURI = need.needURI)" +
+            "group by need " +
+            "having max(mep.creationDate) > :startDate and max(mep.creationDate) < :endDate "
+    )
     Slice<Need> findNeedsInactiveBetween(@Param("startDate") Date start, @Param("endDate") Date end, Pageable pageable);
 
     @Query("select distinct need from Need need " +
@@ -97,6 +117,19 @@ public interface NeedRepository extends WonRepository<Need> {
             "mep.messageType <> 'NEED_MESSAGE' " +
             "and " +
             " (select count(*) from Connection con where con.needURI = need.needURI and con.state = 'CONNECTED') = 0" +
+            "and " +
+            "( mep.senderURI = c.connectionURI or mep.senderNeedURI = need.needURI)" +
+            "group by need " +
+            "having max(mep.creationDate) < :sinceDate"
+    )
+    Slice<Need> findNeedsInactiveSinceAndNotConnected(@Param("sinceDate") Date since, Pageable pageable);
+    
+    @Query("select distinct need from Need need " +
+            "join Connection c on ( c.needURI = need.needURI ) join MessageEventPlaceholder mep on (mep.parentURI = need.needURI or mep.parentURI = c.connectionURI) " +
+            "where " +
+            "need.state = 'ACTIVE' " +
+            "and " +
+            "mep.messageType <> 'NEED_MESSAGE' " +
             "and " +
             "( mep.senderURI = c.connectionURI or mep.senderNeedURI = need.needURI)" +
             "group by need " +
