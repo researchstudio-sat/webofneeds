ebd0084ddd8f18dacd19c63bf1b177bd4c8f05b3
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2018-02-16 14:19:41 +0100

    Add WonConversationUtils

--- webofneeds/won-utils/won-utils-conversation/src/main/java/won/protocol/util/WonConversationUtils.java
+++ webofneeds/won-utils/won-utils-conversation/src/main/java/won/protocol/util/WonConversationUtils.java
@@ -2,8 +2,12 @@ package won.protocol.util;
 
 import java.net.URI;
 import java.util.List;
+import java.util.function.Function;
 
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 
 public class WonConversationUtils {
 	
@@ -13,16 +17,93 @@ public class WonConversationUtils {
 	 * @return
 	 */
 	public static List<URI> getAllMessageURIs(Dataset conversationDataset){
-		return WonConversationUtilsFunctionFactory.getAllMessagesFunction().apply(conversationDataset);
+		return WonConversationQueryBuilder
+				.getBuilderForMessageUris().oldestFirst().build().apply(conversationDataset);
 	}
-
-	/**
-	 * Returns the URI of the create message of the need that the specified connection belongs to.
-	 * @param connectionURI
-	 * @return
-	 */
-	public static URI getCreateMessageURI(URI connectionURI, Dataset conversationDataset) {
-		throw new UnsupportedOperationException("not yet implemented");
+	
+	private static <T> T getFirstOrNull(Dataset dataset, Function<Dataset, List<T>> function) {
+		//RDFDataMgr.write(System.err, dataset, Lang.TRIG);
+		List<T> results = function.apply(dataset);
+		if (results.size() > 0) return results.get(0);
+		return null;
+	}
+	
+	public static URI getLatestMessageOfNeed(Dataset conversationDataset, URI senderNeed){
+		return (URI) getFirstOrNull(conversationDataset,
+				WonConversationQueryBuilder.getBuilderForMessageUris()
+					.noResponses()
+					.newestFirst()
+					.limit(1)
+					.senderNeed(senderNeed)
+					.build());
+		/*
+		return (URI) getFirstOrNull(conversationDataset, WonLatestMessageQueryBuilder.getBuilderForMessageUris()
+				.senderNeed(senderNeed)
+				.build());
+				*/
+	}
+	
+	public static URI getLatestAcceptsMessageOfNeed(Dataset conversationDataset, URI senderNeed){
+		return (URI) getFirstOrNull(conversationDataset,
+				WonConversationQueryBuilder.getBuilderForMessageUris()
+					.noResponses()
+					.newestFirst()
+					.limit(1)
+					.senderNeed(senderNeed)
+					.isAcceptsMessage()
+					.build());
+	}
+	
+	public static URI getLatestAcceptsMessage(Dataset conversationDataset){
+		return (URI) getFirstOrNull(conversationDataset,
+				WonConversationQueryBuilder.getBuilderForMessageUris()
+					.noResponses()
+					.newestFirst()
+					.limit(1)
+					.isAcceptsMessage()
+					.build());
+	}
+	
+	public static URI getLatestRetractsMessageOfNeed(Dataset conversationDataset, URI senderNeed){
+		return (URI) getFirstOrNull(conversationDataset,
+				WonConversationQueryBuilder.getBuilderForMessageUris()
+				.noResponses()
+				.newestFirst()
+				.limit(1)
+				.senderNeed(senderNeed)
+				.isRetractsMessage()
+				.build());
+	}
+	
+	public static URI getLatestProposesMessageOfNeed(Dataset conversationDataset, URI senderNeed){
+		return (URI) getFirstOrNull(conversationDataset,
+				WonConversationQueryBuilder.getBuilderForMessageUris()
+				.noResponses()
+				.newestFirst()
+				.limit(1)
+				.senderNeed(senderNeed)
+				.isProposesMessage()
+				.build());
+	}
+	
+	public static URI getLatestProposesToCancelMessageOfNeed(Dataset conversationDataset, URI senderNeed){
+		return (URI) getFirstOrNull(conversationDataset,
+				WonConversationQueryBuilder.getBuilderForMessageUris()
+				.noResponses()
+				.newestFirst()
+				.limit(1)
+				.senderNeed(senderNeed)
+				.isProposesToCancelMessage()
+				.build());
+	}
+	
+	
+	
+	
+	public static String getTextMessage(Dataset conversationDataset, URI messageUri) {
+		return (String) getFirstOrNull(conversationDataset,
+				WonMessageQueryBuilder.getBuilder(x -> x.get("text").asLiteral().toString())
+				.messageUri(messageUri).build());
 	}
 	
 	
