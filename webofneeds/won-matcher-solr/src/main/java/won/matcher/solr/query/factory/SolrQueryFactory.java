71fd6a1cf7994642ec16c7a30e58f1392fd91ed6
hfriedrich <heiko.friedrich@researchstudio.at>
2018-02-28 14:32:14 +0100

    do not use dummy query parts for normal need queries to avoid moot matches

--- webofneeds/won-matcher-solr/src/main/java/won/matcher/solr/query/factory/SolrQueryFactory.java
+++ webofneeds/won-matcher-solr/src/main/java/won/matcher/solr/query/factory/SolrQueryFactory.java
@@ -3,23 +3,31 @@ package won.matcher.solr.query.factory;
 /**
  * Created by hfriedrich on 28.07.2016.
  */
-public abstract class SolrQueryFactory
-{
-  protected double boost = 1.0;
+public abstract class SolrQueryFactory {
+    protected double boost = 1.0;
 
-  public void setBoost(double boost) { this.boost = boost; }
-
-  public String toString() { return createQuery(); }
-
-  abstract protected String makeQueryString();
-
-  public String createQuery() {
-
-    if (boost != 1.0) {
-      return "(" + makeQueryString() + ")^" + boost;
+    public void setBoost(double boost) {
+        this.boost = boost;
     }
 
-    return makeQueryString();
-  }
+    public String toString() {
+        return createQuery();
+    }
+
+    abstract protected String makeQueryString();
+
+    public String createQuery() {
+
+        String queryString = makeQueryString();
+        if (queryString == null) {
+            return null;
+        }
+
+        if (boost != 1.0) {
+            return "(" + queryString + ")^" + boost;
+        }
+
+        return queryString;
+    }
 
 }
