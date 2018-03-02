30d7022cadca07494ec009f363dbb57e6f0ccb06
quasarchimaere <fabian.suda@researchstudio.at>
2018-03-02 10:39:20 +0100

    adds agreementUri to AgreementEvents

--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/analyzation/agreement/AgreementErrorEvent.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/analyzation/agreement/AgreementErrorEvent.java
@@ -4,11 +4,13 @@ import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.Model;
 import won.protocol.model.Connection;
 
+import java.net.URI;
+
 /**
  * Created by fsuda on 27.11.2017.
  */
 public class AgreementErrorEvent extends AgreementEvent {
-    public AgreementErrorEvent(Connection con, Model payload) {
-        super(con, payload);
+    public AgreementErrorEvent(Connection con, URI agreementUri, Model payload) {
+        super(con, agreementUri, payload);
     }
 }
--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/analyzation/agreement/AgreementEvent.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/analyzation/agreement/AgreementEvent.java
@@ -5,18 +5,26 @@ import org.apache.jena.rdf.model.Model;
 import won.bot.framework.eventbot.event.BaseNeedAndConnectionSpecificEvent;
 import won.protocol.model.Connection;
 
+import java.net.URI;
+
 /**
  * Created by fsuda on 27.11.2017.
  */
 public abstract class AgreementEvent extends BaseNeedAndConnectionSpecificEvent {
     private final Model payload;
+    private final URI agreementUri;
 
-    public AgreementEvent(Connection con, Model payload) {
+    public AgreementEvent(Connection con, URI agreementUri, Model payload) {
         super(con);
+        this.agreementUri = agreementUri;
         this.payload = payload;
     }
 
     public Model getPayload() {
         return payload;
+    }
+
+    public URI getAgreementUri() {
+        return agreementUri;
     }
 }
