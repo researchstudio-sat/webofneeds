01c1277f19515f0c3e8440e6a704480cf155e8d6
quasarchimaere <fabian.suda@researchstudio.at>
2018-03-12 10:25:57 +0100

    adds REJECT to the WonAgreement message vocab, (reject to reject a proposal, from either side)

--- webofneeds/won-core/src/main/java/won/protocol/vocabulary/WONAGR.java
+++ webofneeds/won-core/src/main/java/won/protocol/vocabulary/WONAGR.java
@@ -3,7 +3,6 @@ package won.protocol.vocabulary;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.Resource;
 
 public class WONAGR {
 	 public static final String BASE_URI = "http://purl.org/webofneeds/agreement#";
@@ -11,5 +10,6 @@ public class WONAGR {
 	 public static Property PROPOSES = m.createProperty(BASE_URI+"proposes");
 	 public static Property PROPOSES_TO_CANCEL = m.createProperty(BASE_URI+"proposesToCancel");
 	 public static Property ACCEPTS = m.createProperty(BASE_URI+"accepts");
+     public static Property REJECT = m.createProperty(BASE_URI+"reject");
     
 }
