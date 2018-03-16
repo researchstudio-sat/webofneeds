25bb20792fb8739340436c10f01ea313281126f2
quasarchimaere <fabian.suda@researchstudio.at>
2018-03-16 07:39:26 +0100

    add methods and lists to store a current state

--- webofneeds/won-bot/src/main/java/won/bot/framework/bot/context/FactoryBotContextWrapper.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/bot/context/FactoryBotContextWrapper.java
@@ -1,5 +1,6 @@
 package won.bot.framework.bot.context;
 
+import won.bot.framework.eventbot.action.impl.factory.model.Precondition;
 import won.bot.framework.eventbot.action.impl.factory.model.Proposal;
 
 import java.net.URI;
@@ -9,11 +10,13 @@ public class FactoryBotContextWrapper extends BotContextWrapper {
     private final String factoryListName = getBotName() + ":factoryList";
     private final String factoryInternalIdName = getBotName() + ":factoryInternalId";
     private final String factoryOfferToFactoryNeedMapName = getBotName() + ":factoryOfferToFactoryNeedMap";
-    private final String preconditionStateMapName = getBotName() + ":preconditionStateMap";
     private final String connectionToPreconditionListMapName = getBotName() + ":connectionToPreconditionListMap";
+    private final String connectionToProposalListMapName = getBotName() + ":connectionToProposalListMap";
     private final String preconditionToConnectionMapName = getBotName() + ":preconditionToConnectionMap";
-    private final String precondtionToProposalListMapName = getBotName() + ":precondtionToProposalListMap";
+    private final String preconditionToProposalListMapName = getBotName() + ":precondtionToProposalListMap";
+    private final String preconditionConversationStateMapName = getBotName() + ":preconditionConversationStateMap";
     private final String proposalToPreconditionListMapName = getBotName() + ":proposalToPreconditionListMap";
+    private final String proposalToConnectionMapName = getBotName() + ":proposalToConnectionMap";
 
     public FactoryBotContextWrapper(BotContext botContext, String botName) {
         super(botContext, botName);
@@ -47,8 +50,8 @@ public class FactoryBotContextWrapper extends BotContextWrapper {
      * @param preconditionURI to retrieve the state from
      * @return the saved state of the precondition, null if the state was never saved before (undeterminable)
      */
-    public Boolean getPreconditionState(String preconditionURI) {
-        return (Boolean) getBotContext().loadFromObjectMap(preconditionStateMapName, preconditionURI);
+    public Boolean getPreconditionConversationState(String preconditionURI) {
+        return (Boolean) getBotContext().loadFromObjectMap(preconditionConversationStateMapName, preconditionURI);
     }
 
     /**
@@ -56,8 +59,8 @@ public class FactoryBotContextWrapper extends BotContextWrapper {
      * @param preconditionURI to save the state of
      * @param state to save
      */
-    public void addPreconditionState(String preconditionURI, boolean state) {
-        getBotContext().saveToObjectMap(preconditionStateMapName, preconditionURI, state);
+    public void addPreconditionConversationState(String preconditionURI, boolean state) {
+        getBotContext().saveToObjectMap(preconditionConversationStateMapName, preconditionURI, state);
     }
 
     /**
@@ -79,7 +82,16 @@ public class FactoryBotContextWrapper extends BotContextWrapper {
      * @return List of all URI-Strings of preconditions save for the given connectionURI
      */
     public List<String> getPreconditionsForConnectionUri(URI connectionURI) {
-        return (List<String>)(List<?>) getBotContext().loadFromListMap(connectionToPreconditionListMapName, connectionURI.toString());
+        return getPreconditionsForConnectionUri(connectionURI.toString());
+    }
+
+    /**
+     * Returns a List of All saved Precondition URIS for the connection
+     * @param connectionURI string of the uri of the connection to retrieve the preconditionList of
+     * @return List of all URI-Strings of preconditions save for the given connectionURI
+     */
+    public List<String> getPreconditionsForConnectionUri(String connectionURI) {
+        return (List<String>)(List<?>) getBotContext().loadFromListMap(connectionToPreconditionListMapName, connectionURI);
     }
 
     /**
@@ -90,25 +102,78 @@ public class FactoryBotContextWrapper extends BotContextWrapper {
         return (URI) getBotContext().loadFromObjectMap(preconditionToConnectionMapName, preconditionURI);
     }
 
-    public void addPreconditionProposalRelation(String preconditionURI, Proposal proposal) {
-        getBotContext().addToListMap(precondtionToProposalListMapName, preconditionURI, proposal);
-        getBotContext().addToListMap(proposalToPreconditionListMapName, proposal.getUri().toString(), preconditionURI);
+    public void addPreconditionProposalRelation(Precondition precondition, Proposal proposal) {
+        getBotContext().addToListMap(preconditionToProposalListMapName, precondition.getUri(), proposal);
+        getBotContext().addToListMap(proposalToPreconditionListMapName, proposal.getUri().toString(), precondition);
+    }
+
+    public boolean hasPreconditionProposalRelation(String preconditionURI, String proposalURI) {
+        return getPreconditionsForProposalUri(proposalURI).contains(preconditionURI);
     }
 
     public List<Proposal> getProposalsForPreconditionUri(String preconditionURI){
-        return (List<Proposal>)(List<?>) getBotContext().loadFromListMap(precondtionToProposalListMapName, preconditionURI);
+        return (List<Proposal>)(List<?>) getBotContext().loadFromListMap(preconditionToProposalListMapName, preconditionURI);
+    }
+
+    public List<Proposal> getProposalsForConnectionUri(String connectionURI){
+        return (List<Proposal>)(List<?>) getBotContext().loadFromListMap(connectionToPreconditionListMapName, connectionURI);
     }
 
     /**
-     * Returns a List of All saved Precondition URIS for the connection
+     * Returns a List of All saved Precondition URIS for the proposal
      * @param proposalURI uri of the proposal to retrieve the preconditionList of
      * @return List of all URI-Strings of preconditions save for the given connectionURI
      */
-    public List<String> getPreconditionsForProposalUri(URI proposalURI) {
+    public List<String> getPreconditionsForProposalUri(String proposalURI) {
         return (List<String>)(List<?>) getBotContext().loadFromListMap(connectionToPreconditionListMapName, proposalURI.toString());
     }
 
-    public void removeAllConnectionData(URI connectionURI) {
+    /**
+     * Removes all the stored entries for a given connectionURI
+     * @param connectionURI the string of the connection URI that is to be removed from here
+     */
+    public void removeConnectionReferences(String connectionURI) {
         //TODO: REMOVE ALL UNUSED VALUES (TO BE DETERMINED)
+        getPreconditionsForConnectionUri(connectionURI).forEach(this::removePreconditionReferences);
+        getProposalsForConnectionUri(connectionURI).forEach(this::removeProposalReferences);
+
+        getBotContext().removeFromListMap(connectionToPreconditionListMapName, connectionURI);
+        getBotContext().removeFromListMap(connectionToProposalListMapName, connectionURI);
+    }
+
+
+    /**
+     * Removes All the stored entries in all Maps Lists or MapList for the given Proposal
+     * @param proposal to be removed
+     */
+    public void removeProposalReferences(Proposal proposal) {
+        removeProposalReferences(proposal.getUri());
+    }
+
+    /**
+     * Removes All the stored entries in all Maps Lists or MapList for the given ProposalURI
+     * @param proposalURI to be removed
+     */
+    public void removeProposalReferences(URI proposalURI) {
+        removeProposalReferences(proposalURI.toString());
+    }
+
+    /**
+     * Removes All the stored entries in all Maps Lists or MapList for the given ProposalURI
+     * @param proposalURI the string of the proposalURI to be removed
+     */
+    public void removeProposalReferences(String proposalURI) {
+        getBotContext().removeFromObjectMap(proposalToConnectionMapName, proposalURI);
+        getBotContext().removeLeavesFromListMap(connectionToProposalListMapName, proposalURI);
+        getBotContext().removeFromListMap(proposalToPreconditionListMapName, proposalURI);
+        getBotContext().removeLeavesFromListMap(preconditionToProposalListMapName, proposalURI);
+    }
+
+    public void removePreconditionReferences(String preconditionURI) {
+        getBotContext().removeFromObjectMap(preconditionConversationStateMapName, preconditionURI);
+        getBotContext().removeLeavesFromListMap(connectionToPreconditionListMapName, preconditionURI);
+        getBotContext().removeFromObjectMap(preconditionToConnectionMapName, preconditionURI);
+        getBotContext().removeFromObjectMap(preconditionToProposalListMapName, preconditionURI);
+        getBotContext().removeLeavesFromListMap(proposalToPreconditionListMapName, preconditionURI);
     }
 }
