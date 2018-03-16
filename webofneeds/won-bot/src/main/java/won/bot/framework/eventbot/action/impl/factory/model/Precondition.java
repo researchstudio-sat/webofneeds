b9529e2358ec9103dd37645c1be627d7935b4151
quasarchimaere <fabian.suda@researchstudio.at>
2018-03-16 07:35:12 +0100

    adds precondition pojo

---
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/action/impl/factory/model/Precondition.java
@@ -0,0 +1,44 @@
+package won.bot.framework.eventbot.action.impl.factory.model;
+
+import java.io.Serializable;
+import java.net.URI;
+
+/**
+ * Created by fsuda on 15.03.2018.
+ */
+public class Precondition implements Serializable{
+    private String uri;
+    private boolean met;
+
+    public Precondition(String uri, boolean met) {
+        this.uri = uri;
+        this.met = met;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public boolean isMet() {
+        return met;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Precondition precondition = (Precondition) o;
+
+        return uri.equals(precondition.uri);
+    }
+
+    @Override
+    public int hashCode() {
+        return uri.hashCode();
+    }
+}
