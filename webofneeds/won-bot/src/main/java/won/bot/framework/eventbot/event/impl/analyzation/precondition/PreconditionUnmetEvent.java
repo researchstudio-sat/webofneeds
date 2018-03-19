2fe262dcc352954f931ee465ff731c14ce69b496
quasarchimaere <fabian.suda@researchstudio.at>
2018-03-19 14:22:56 +0100

    adds preconditionUri to PreconditionEvent

--- webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/analyzation/precondition/PreconditionUnmetEvent.java
+++ webofneeds/won-bot/src/main/java/won/bot/framework/eventbot/event/impl/analyzation/precondition/PreconditionUnmetEvent.java
@@ -3,11 +3,13 @@ package won.bot.framework.eventbot.event.impl.analyzation.precondition;
 import won.protocol.model.Connection;
 import won.utils.goals.GoalInstantiationResult;
 
+import java.net.URI;
+
 /**
  * Created by fsuda on 27.11.2017.
  */
 public class PreconditionUnmetEvent extends PreconditionEvent {
-    public PreconditionUnmetEvent(Connection con, GoalInstantiationResult payload) {
-        super(con, payload);
+    public PreconditionUnmetEvent(Connection con, String preconditionUri, GoalInstantiationResult payload) {
+        super(con, preconditionUri, payload);
     }
 }
