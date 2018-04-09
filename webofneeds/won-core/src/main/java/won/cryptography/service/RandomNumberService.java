82ea4db3e714fb249a0090c84db7ee476e1f6537
quasarchimaere <fabian.suda@researchstudio.at>
2018-04-09 07:46:16 +0200

    refactors the random number generator so that the first symbol of a generated string is never a number (without the use of a loop)

--- webofneeds/won-core/src/main/java/won/cryptography/service/RandomNumberService.java
+++ webofneeds/won-core/src/main/java/won/cryptography/service/RandomNumberService.java
@@ -8,7 +8,9 @@ public interface RandomNumberService
 {
 
   /**
-   * generates a URI safe random string with the given length
+   * generates a URI safe random string with the given length that does not start with a number.
+   * We do this so that we generate URIs for which prefixing will always work with N3.js
+   * https://github.com/RubenVerborgh/N3.js/issues/121
    * @param length
    */
   public String generateRandomString(int length);
