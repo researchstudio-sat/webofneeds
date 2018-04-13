24c0181e01c3205623295adb5ea439265c196082
Kristina Weinberger <kristina.weinberger@researchstudio.at>
2018-04-13 16:19:20 +0200

    Stay in chats overview when sending a request

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/send-request.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/send-request.js
@@ -187,6 +187,9 @@ function genComponentConf() {
 
         sendRequest(message) {
             if(!this.connection || (this.ownNeed && this.ownNeed.get("isWhatsAround"))){
+
+                this.router__stateGoResetParams('overviewIncomingRequests');
+                
                 if(this.ownNeed && this.ownNeed.get("isWhatsAround")){
                     //Close the connection if there was a present connection for a whatsaround need
                     this.connections__close(this.connectionUri);
@@ -197,7 +200,6 @@ function genComponentConf() {
                 }
 
                 //this.router__stateGoCurrent({connectionUri: null, sendAdHocRequest: null});
-                this.router__stateGoResetParams('connections');
             }else{
                 this.needs__connect(
                 		this.ownNeed.get("uri"), 
