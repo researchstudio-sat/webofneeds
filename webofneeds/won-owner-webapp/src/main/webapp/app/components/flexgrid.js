e61b7af7fb55400cb617ad1352225ccabaf407d7
Kristina Weinberger <kristina.weinberger@researchstudio.at>
2018-04-17 16:12:46 +0200

    Fixes #1414

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/flexgrid.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/flexgrid.js
@@ -7,9 +7,22 @@ function genComponentConf() {
     let template = `
             <div class="flexgrid__item" ng-repeat="item in self.items">
                 <div class="fgi__block" ng-class="{'clickable' : item.detail !== undefined}" ng-click="self.openElement($index)">
-                    <img class="fgi__image" ng-src="{{item.imageSrc}}"/>
-                    <span class="fgi__text" ng-show="item.text2 === undefined && item.separatorText === undefined">{{item.text}}</span>
-                    <span class="fgi__text" ng-show="item.text2 !== undefined && item.separatorText !== undefined">{{item.text}}<won-labelled-hr label="::item.separatorText"></won-labelled-hr>{{item.text2}}</span>
+                    <img class="fgi__image" 
+                        ng-show="item.imageSrc !== undefined" 
+                        ng-src="{{item.imageSrc}}"/>
+                    <svg style="--local-primary:var(--won-primary-color);"
+                        class="fgi__image" 
+                        ng-show="item.svgSrc !== undefined">
+                            <use href="{{item.svgSrc}}"/>
+                    </svg>
+                    <span class="fgi__text" ng-show="item.text2 === undefined && item.separatorText === undefined">
+                        {{item.text}}
+                    </span>
+                    <span class="fgi__text" ng-show="item.text2 !== undefined && item.separatorText !== undefined">
+                        {{item.text}}
+                        <won-labelled-hr label="::item.separatorText"></won-labelled-hr>
+                        {{item.text2}}
+                    </span>
                     <svg style="--local-primary:var(--won-primary-color);"
                         class="fgi__arrow"
                         ng-show="item.detail !== undefined && $index === self.selectedIdx">
