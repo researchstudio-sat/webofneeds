e28967a645bd15d6a963ed6403f2138796eb10bf
quasarchimaere <fabian.suda@researchstudio.at>
2018-05-03 14:48:36 +0200

    made two context menu components (one for post and one for connection)

---
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/connection-context-dropdown.js
@@ -0,0 +1,128 @@
+/**
+ * Created by ksinger on 30.03.2017.
+ */
+
+
+
+import angular from 'angular';
+import ngAnimate from 'angular-animate';
+import { actionCreators }  from '../actions/actions.js';
+import won from '../won-es6.js';
+import {
+    attach,
+} from '../utils.js';
+import {
+    selectOpenConnectionUri,
+    selectNeedByConnectionUri,
+} from '../selectors.js';
+import {
+    connect2Redux,
+} from '../won-utils.js';
+
+const serviceDependencies = ['$scope', '$ngRedux', '$element'];
+function genComponentConf() {
+    let template = `
+            <svg class="cdd__icon__small clickable"
+                style="--local-primary:#var(--won-secondary-color);"
+                ng-show="!self.contextMenuOpen"
+                ng-click="self.contextMenuOpen = true">
+                    <use href="#ico16_arrow_down"></use>
+            </svg>
+            <div class="cdd__contextmenu contextmenu" ng-show="self.contextMenuOpen">
+                <div class="content" ng-click="self.contextMenuOpen = false">
+                    <div class="topline">
+                        <svg class="cdd__icon__small__contextmenu clickable"
+                            style="--local-primary:black;">
+                            <use href="#ico16_arrow_up"></use>
+                        </svg>
+                    </div>
+                    <!-- Buttons when connection is available -->
+                    <button
+                        class="won-button--outlined thin red"
+                        ng-if="!self.isSuggested"
+                        ng-click="self.goToPost(self.connection.get('remoteNeedUri'))">
+                        Show Post Details
+                    </button>
+                    <button
+                        ng-if="self.isConnected || self.isSuggested"
+                        class="won-button--filled red"
+                        ng-click="self.closeConnection()">
+                        Close Connection
+                    </button>
+                    <button
+                        ng-if="self.isSentRequest"
+                        class="won-button--filled red"
+                        ng-click="self.closeConnection()">
+                        Cancel Request
+                    </button>
+                </div>
+            </div>
+        `;
+
+    class Controller {
+        constructor() {
+            attach(this, serviceDependencies, arguments);
+
+            const self = this;
+            const selectFromState = (state) => {
+                const connectionUri = selectOpenConnectionUri(state);
+
+                const post = connectionUri && selectNeedByConnectionUri(state, connectionUri);
+                const connection = post && post.getIn(["connections", connectionUri]);
+                const connectionState = connection && connection.get('state');
+
+                return {
+                    connection,
+                    connectionUri,
+                    isConnected: connectionState === won.WON.Connected,
+                    isSentRequest: connectionState === won.WON.RequestSent,
+                    isReceivedRequest: connectionState === won.WON.RequestReceived,
+                    isSuggested: connectionState === won.WON.Suggested
+                }
+            };
+            connect2Redux(selectFromState, actionCreators, [], this);
+
+            this.$scope.$on('$destroy', () => {
+                angular.element(window.document).unbind('click');
+            });
+
+            angular.element(window.document).bind('click',
+                    event => {
+                    var clickedElement = event.target;
+                    //hide MainMenu if click was outside of the component and menu was open
+                    if(this.contextMenuOpen && !this.$element[0].contains(clickedElement)){
+                        this.contextMenuOpen = false;
+                        this.$scope.$apply();
+                    }
+                }
+            );
+        }
+
+        closeConnection(){
+            this.connections__close(this.connectionUri);
+            this.router__stateGoCurrent({connectionUri: null});
+        }
+
+        goToPost(postUri) {
+            this.router__stateGoCurrent({postUri: postUri});
+        }
+    }
+    Controller.$inject = serviceDependencies;
+
+    return {
+        restrict: 'E',
+        controller: Controller,
+        controllerAs: 'self',
+        bindToController: true, //scope-bindings -> ctrl
+        scope: {
+        },
+        template: template
+    }
+}
+
+export default angular.module('won.owner.components.connectionContextDropdown', [
+    ngAnimate,
+])
+    .directive('wonConnectionContextDropdown', genComponentConf)
+    .name;
+
---
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/post-context-dropdown.js
@@ -0,0 +1,122 @@
+/**
+ * Created by ksinger on 30.03.2017.
+ */
+
+
+
+import angular from 'angular';
+import ngAnimate from 'angular-animate';
+import { actionCreators }  from '../actions/actions.js';
+import won from '../won-es6.js';
+import {
+    attach,
+} from '../utils.js';
+import {
+    connect2Redux,
+} from '../won-utils.js';
+import {
+    selectOpenPostUri,
+} from '../selectors.js';
+
+const serviceDependencies = ['$scope', '$ngRedux', '$element'];
+function genComponentConf() {
+    let template = `
+            <svg class="cdd__icon__small clickable"
+                style="--local-primary:#var(--won-secondary-color);"
+                ng-show="!self.contextMenuOpen"
+                ng-click="self.contextMenuOpen = true">
+                    <use href="#ico16_arrow_down"></use>
+            </svg>
+            <div class="cdd__contextmenu contextmenu" ng-show="self.contextMenuOpen">
+                <div class="content" ng-click="self.contextMenuOpen = false">
+                    <div class="topline">
+                        <svg class="cdd__icon__small__contextmenu clickable"
+                            style="--local-primary:black;">
+                            <use href="#ico16_arrow_up"></use>
+                        </svg>
+                    </div>
+                    <!-- Buttons for post -->
+                    <button class="won-button--filled red"
+                        ng-if="self.isOwnPost && self.isInactive"
+                        ng-click="self.reOpenPost()">
+                        Reopen Post
+                    </button>
+                    <button class="won-button--filled red"
+                        ng-if="self.isOwnPost && self.isActive"
+                        ng-click="self.closePost()">
+                        Close Post
+                    </button>
+                </div>
+            </div>
+        `;
+
+    class Controller {
+        constructor() {
+            attach(this, serviceDependencies, arguments);
+
+            const self = this;
+            const selectFromState = (state) => {
+                const postUri = selectOpenPostUri(state);
+                const post = postUri && state.getIn(["needs", postUri]);
+                const postState = post && post.get("state");
+
+                return {
+                    isOwnPost: post && post.get("ownNeed"),
+                    isActive: postState === won.WON.ActiveCompacted,
+                    isInactive: postState === won.WON.InactiveCompacted,
+                    post,
+                    postUri,
+                }
+            };
+            connect2Redux(selectFromState, actionCreators, [], this);
+
+            this.$scope.$on('$destroy', () => {
+                angular.element(window.document).unbind('click');
+            });
+
+            angular.element(window.document).bind('click',
+                    event => {
+                    var clickedElement = event.target;
+                    //hide MainMenu if click was outside of the component and menu was open
+                    if(this.contextMenuOpen && !this.$element[0].contains(clickedElement)){
+                        this.contextMenuOpen = false;
+                        this.$scope.$apply();
+                    }
+                }
+            );
+        }
+
+        closePost() {
+            if(this.isOwnPost){
+                console.log("CLOSING THE POST: "+this.post.get('uri'));
+                this.needs__close(this.post.get('uri'));
+                this.router__stateGoCurrent({postUri : null})
+            }
+        }
+
+        reOpenPost() {
+            if(this.isOwnPost){
+                console.log("RE-OPENING THE POST: "+this.post.get('uri'));
+                this.needs__reopen(this.post.get('uri'));
+            }
+        }
+    }
+    Controller.$inject = serviceDependencies;
+
+    return {
+        restrict: 'E',
+        controller: Controller,
+        controllerAs: 'self',
+        bindToController: true, //scope-bindings -> ctrl
+        scope: {
+        },
+        template: template
+    }
+}
+
+export default angular.module('won.owner.components.postContextDropdown', [
+    ngAnimate,
+])
+    .directive('wonPostContextDropdown', genComponentConf)
+    .name;
+
