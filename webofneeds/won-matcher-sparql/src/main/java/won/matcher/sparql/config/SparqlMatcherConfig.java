b18d7a3b260bc94e0d72fad8448b1e29f22f53dc
Gabriel Pickl <gabriel.pickl@researchstudio.at>
2018-05-22 18:07:05 +0200

    Removed unused code and added simple sparql query.

--- webofneeds/won-matcher-sparql/src/main/java/won/matcher/sparql/config/SparqlMatcherConfig.java
+++ webofneeds/won-matcher-sparql/src/main/java/won/matcher/sparql/config/SparqlMatcherConfig.java
@@ -13,88 +13,10 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("file:${WON_CONFIG_DIR}/matcher-sparql.properties")
 public class SparqlMatcherConfig
 {
-  @Value("${matcher.solr.uri.solr.server}")
-  private String solrServerUri;
-
-  @Value("${matcher.solr.core}")
-  private String solrCore;
-
-  @Value("${matcher.solr.test.core}")
-  private String solrTestCore;
-
-  @Value("${matcher.solr.uri.solr.server.public}")
-  private String solrServerPublicUri;
-
-  @Value("${matcher.solr.query.maxHints}")
-  private int maxHints;
-
-  @Value("${matcher.solr.query.maxHintsForCounterparts}")
-  private int maxHintsForCounterparts;
-
-  @Value("${matcher.solr.index.commit}")
-  private boolean commitIndexedNeedImmediately;
-
-  @Value("${matcher.solr.query.score.threshold}")
-  private float scoreThreshold;
-
-  @Value("${matcher.solr.query.cutAfterIthElbowInScore}")
-  private int cutAfterIthElbowInScore;
-
-  @Value("${matcher.solr.query.score.normalizationFactor}")
-  private float scoreNormalizationFactor;
-
-  public float getScoreThreshold() {
-    return scoreThreshold;
-  }
-
-  public String getSolrServerUri() {
-    return solrServerUri;
-  }
-
-  public int getMaxHints() {
-    return maxHints;
-  }
-
-    public int getMaxHintsForCounterparts() {
-        return maxHintsForCounterparts;
-    }
-
-  public boolean isCommitIndexedNeedImmediately() {
-    return commitIndexedNeedImmediately;
-  }
-
-  public String getSolrServerPublicUri() {
-    return solrServerPublicUri;
-  }
-
-  public int getCutAfterIthElbowInScore() {
-    return cutAfterIthElbowInScore;
-  }
-
-  public float getScoreNormalizationFactor() {
-    return scoreNormalizationFactor;
-  }
-
-  public String getSolrCore() {
-    return solrCore;
-  }
-
-  public String getSolrTestCore() {
-    return solrTestCore;
-  }
-
-  public String getSolrEndpointUri(boolean useTestCore) {
-    String server = getSolrServerUri();
-    if (!server.endsWith("/")) {
-      server += "/";
-    }
-
-    String core = server + (useTestCore ? getSolrTestCore() : getSolrCore());
-    if (!core.endsWith("/")) {
-      core += "/";
-    }
-
-    return core;
-  }
-
+	 @Value("${matcher.sparql.uri.sparql.endpoint}")
+	 private String sparqlEndpoint;
+	 
+	 public String getSparqlEndpoint() {
+		 return sparqlEndpoint;
+	 }
 }
