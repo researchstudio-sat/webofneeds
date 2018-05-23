b2155d1a3aa15d1c731019df58abf899b7f4452b
Kevin Singer <kevin.singer@researchstudio.at>
2018-05-23 18:32:18 +0200

    Can't deep-load needs yet. Removed param that was causing an error.

--- webofneeds/won-owner-webapp/src/main/webapp/app/won-message-utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/won-message-utils.js
@@ -594,7 +594,7 @@ function fetchAllAccessibleAndRelevantData(
 
 function fetchOwnNeedAndDispatch(needUri, curriedDispatch = () => undefined) {
   const needP = won
-    .ensureLoaded(needUri, { requesterWebId: needUri, deep: true }) //ensure loaded does net seem to be necessary as it is called within getNeed also the requesterWebId is not necessary for need requests
+    .ensureLoaded(needUri, { requesterWebId: needUri }) //ensure loaded does net seem to be necessary as it is called within getNeed also the requesterWebId is not necessary for need requests
     .then(() => won.getNeed(needUri));
   needP.then(need =>
     curriedDispatch(wellFormedPayload({ ownNeeds: { [needUri]: need } }))
