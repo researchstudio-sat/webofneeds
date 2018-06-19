a12d38f088af740a4ddebc30a1d5cb67773f95a5
maxstolze <maximilian.stolze@researchstudio.at>
2018-06-19 15:46:56 +0200

    NoHintforCounterpart for seeks part
    Issue #1977

--- webofneeds/won-owner-webapp/src/main/webapp/app/service/need-builder.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/service/need-builder.js
@@ -274,7 +274,13 @@ import { getIn } from "../utils.js";
     const matchingContext = args.is
       ? args.is.matchingContext
       : args.seeks.matchingContext;
-
+    const noHintForCounterpart = isWhatsAround
+      ? true
+      : isWhatsNew
+        ? true
+        : args.seeks
+          ? true
+          : false;
     let isContentUri, seeksContentUri;
     if (isWhatsAround) {
       isContentUri = won.WON.contentNodeBlankUri.whatsAround;
@@ -302,7 +308,7 @@ import { getIn } from "../utils.js";
 
           isWhatsAround ? "won:WhatsAround" : undefined,
           isWhatsNew ? "won:WhatsNew" : undefined,
-          isWhatsAround || isWhatsNew ? "won:NoHintForCounterpart" : undefined,
+          noHintForCounterpart ? "won:NoHintForCounterpart" : undefined,
 
           noHints ? "won:NoHintForMe" : undefined,
           noHints ? "won:NoHintForCounterpart" : undefined,
