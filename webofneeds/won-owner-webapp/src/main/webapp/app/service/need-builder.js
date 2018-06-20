0597f2c3bef3a7f844c5a5c64e3571b62fcf8686
Kristina Weinberger <kristina.weinberger@researchstudio.at>
2018-06-20 13:51:42 +0200

    Changes how person details are parsed into RDF

--- webofneeds/won-owner-webapp/src/main/webapp/app/service/need-builder.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/service/need-builder.js
@@ -145,32 +145,29 @@ import { getIn } from "../utils.js";
       "won:hasAttachment": hasAttachmentUrls(isOrSeeksData)
         ? isOrSeeksData.attachmentUris.map(uri => ({ "@id": uri }))
         : undefined,
-      "won:hasPerson": !isOrSeeksData.person
-        ? undefined
-        : {
-            "@type": "foaf:Person",
-            "foaf:title": getIn(isOrSeeksData, ["person", "title"])
-              ? getIn(isOrSeeksData, ["person", "title"])
-              : undefined,
-            "foaf:name": getIn(isOrSeeksData, ["person", "name"])
-              ? getIn(isOrSeeksData, ["person", "name"])
-              : undefined,
-            "s:worksFor": getIn(isOrSeeksData, ["person", "company"])
-              ? {
-                  "@type": "s:Organization",
-                  "s:name": getIn(isOrSeeksData, ["person", "company"]),
-                }
-              : undefined,
-            "s:jobTitle": getIn(isOrSeeksData, ["person", "position"])
-              ? getIn(isOrSeeksData, ["person", "position"])
-              : undefined,
-            "s:knowsAbout": getIn(isOrSeeksData, ["person", "skills"])
-              ? getIn(isOrSeeksData, ["person", "skills"]).toJS()
-              : undefined,
-            "dc:description": getIn(isOrSeeksData, ["person", "bio"])
-              ? getIn(isOrSeeksData, ["person", "bio"])
-              : undefined,
-          },
+      "won:hasPerson": isOrSeeksData.person,
+
+      "foaf:title": getIn(isOrSeeksData, ["person", "title"])
+        ? getIn(isOrSeeksData, ["person", "title"])
+        : undefined,
+      "foaf:name": getIn(isOrSeeksData, ["person", "name"])
+        ? getIn(isOrSeeksData, ["person", "name"])
+        : undefined,
+      "s:worksFor": getIn(isOrSeeksData, ["person", "company"])
+        ? {
+            "@type": "s:Organization",
+            "s:name": getIn(isOrSeeksData, ["person", "company"]),
+          }
+        : undefined,
+      "s:jobTitle": getIn(isOrSeeksData, ["person", "position"])
+        ? getIn(isOrSeeksData, ["person", "position"])
+        : undefined,
+      "s:knowsAbout": getIn(isOrSeeksData, ["person", "skills"])
+        ? getIn(isOrSeeksData, ["person", "skills"]).toJS()
+        : undefined,
+      // "dc:description": getIn(isOrSeeksData, ["person", "bio"])
+      //   ? getIn(isOrSeeksData, ["person", "bio"])
+      //   : undefined,
       "won:hasLocation": !isOrSeeksData.location
         ? undefined
         : {
