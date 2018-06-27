0c54cf336778d539ea4792f65343886c65d2abbd
quasarchimaere <fabian.suda@researchstudio.at>
2018-06-27 08:31:20 +0200

    add new labels for messageTypes

--- webofneeds/won-owner-webapp/src/main/webapp/app/service/won.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/service/won.js
@@ -257,6 +257,7 @@ won.WONMSG.closeNeedSentMessageCompacted =
   won.WONMSG.prefix + ":DeactivateSentMessage";
 won.WONMSG.hintMessage = won.WONMSG.baseUri + "HintMessage";
 won.WONMSG.hintMessageCompacted = won.WONMSG.prefix + ":HintMessage";
+won.WONMSG.hintFeedbackMessage = won.WONMSG.baseUri + "HintFeedbackMessage";
 won.WONMSG.hintFeedbackMessageCompacted =
   won.WONMSG.prefix + ":HintFeedbackMessage";
 won.WONMSG.connectMessage = won.WONMSG.baseUri + "ConnectMessage";
