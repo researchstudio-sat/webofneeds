e427488ee2f07e38bc733664c150378577c17211
quasarchimaere <fabian.suda@researchstudio.at>
2018-07-24 14:18:40 +0200

    fixed #2199 (show close instead of backarrow on desktop view for usecase creation)

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/create-post.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/create-post.js
@@ -43,8 +43,12 @@ function genComponentConf() {
             <a class="cp__header__back clickable"
                 ng-click="self.router__stateGoCurrent({useCase: undefined})">
                 <svg style="--local-primary:var(--won-primary-color);"
-                    class="cp__header__back__icon">
+                    class="cp__header__back__icon show-in-responsive">
                     <use xlink:href="#ico36_backarrow" href="#ico36_backarrow"></use>
+                </svg>
+                <svg style="--local-primary:var(--won-primary-color);"
+                    class="cp__header__back__icon hide-in-responsive">
+                    <use xlink:href="#ico36_close" href="#ico36_close"></use>
                 </svg>
             </a>
             <svg class="cp__header__icon"
