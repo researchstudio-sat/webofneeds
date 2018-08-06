039295d33f36140f185f8d846a213dbfda299a3d
quasarchimaere <fabian.suda@researchstudio.at>
2018-08-06 12:48:12 +0200

    implemented first working file/image parseToRDF and parseFromRDF (without using attachments, as they do not work with our current detail retrival architecture )

--- webofneeds/won-owner-webapp/src/main/webapp/config/detail-definitions.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/detail-definitions.js
@@ -787,116 +787,160 @@ export const details = {
       return undefined;
     },
   },
-  //files: {
-  //  identifier: "files",
-  //  label: "Files",
-  //  icon: "#ico36_media_circle",
-  //  placeholder: "",
-  //  accepts: "*/*",
-  //  component: "won-file-picker",
-  //  viewerComponent: "won-file-viewer",
-  //  parseToRDF: function({ value }) {
-  //    //TODO: IMPL THIS METHOD
-  //    if (!value) {
-  //      return { "won:hasFile": undefined };
-  //    }
-  //    return { "won:hasFile": value };
-  //  },
-  //  parseFromRDF: function(jsonLDImm) {
-  //    //TODO: IMPL THIS METHOD
-  //    const files =
-  //      jsonLDImm &&
-  //      (jsonLDImm.get("won:hasFile") ||
-  //        jsonLDImm.get("http://purl.org/webofneeds/model#hasFile"));
-  //    console.log("files", files);
-  //    return undefined;
-  //
-  //    /*if (!tags) {
-  //      return undefined;
-  //    } else if (is("String", tags)) {
-  //      return Immutable.fromJS([tags]);
-  //    } else if (is("Array", tags)) {
-  //      return Immutable.fromJS(tags);
-  //    } else if (Immutable.List.isList(tags)) {
-  //      return tags; // id; it is already in the format we want
-  //    } else {
-  //      console.error(
-  //        "Found unexpected format of tags (should be Array, " +
-  //          "Immutable.List, or a single tag as string): " +
-  //          JSON.stringify(tags)
-  //      );
-  //      return undefined;
-  //    }*/
-  //  },
-  //  generateHumanReadable: function({ value, includeLabel }) {
-  //    if (value) {
-  //      let humanReadable = "";
-  //      if (value.length > 1) {
-  //        humanReadable = value.length + " Files";
-  //      } else {
-  //        humanReadable = value[0].name;
-  //      }
-  //
-  //      return includeLabel ? this.label + ": " + humanReadable : humanReadable;
-  //    }
-  //    return undefined;
-  //  },
-  //},
-  //images: {
-  //  identifier: "images",
-  //  label: "Images",
-  //  icon: "#ico36_media_circle",
-  //  placeholder: "",
-  //  accepts: "image/*",
-  //  component: "won-file-picker",
-  //  viewerComponent: "won-file-viewer",
-  //  parseToRDF: function({ value }) {
-  //    //TODO: IMPL THIS METHOD
-  //    if (!value) {
-  //      return { "won:hasImage": undefined };
-  //    }
-  //    return { "won:hasImage": value };
-  //  },
-  //  parseFromRDF: function(jsonLDImm) {
-  //    //TODO: IMPL THIS METHOD
-  //    const images =
-  //      jsonLDImm &&
-  //      (jsonLDImm.get("won:hasImage") ||
-  //        jsonLDImm.get("http://purl.org/webofneeds/model#hasImage"));
-  //    console.log("images", images);
-  //    return undefined;
-  //
-  //    /*if (!tags) {
-  //      return undefined;
-  //    } else if (is("String", tags)) {
-  //      return Immutable.fromJS([tags]);
-  //    } else if (is("Array", tags)) {
-  //      return Immutable.fromJS(tags);
-  //    } else if (Immutable.List.isList(tags)) {
-  //      return tags; // id; it is already in the format we want
-  //    } else {
-  //      console.error(
-  //        "Found unexpected format of tags (should be Array, " +
-  //          "Immutable.List, or a single tag as string): " +
-  //          JSON.stringify(tags)
-  //      );
-  //      return undefined;
-  //    }*/
-  //  },
-  //  generateHumanReadable: function({ value, includeLabel }) {
-  //    if (value) {
-  //      let humanReadable = "";
-  //      if (value.length > 1) {
-  //        humanReadable = value.length + " Images";
-  //      } else {
-  //        humanReadable = value[0].name;
-  //      }
-  //
-  //      return includeLabel ? this.label + ": " + humanReadable : humanReadable;
-  //    }
-  //    return undefined;
-  //  },
-  //},
+  files: {
+    identifier: "files",
+    label: "Files",
+    icon: "#ico36_media_circle",
+    placeholder: "",
+    accepts: "*/*",
+    component: "won-file-picker",
+    viewerComponent: "won-file-viewer",
+    parseToRDF: function({ value }) {
+      if (!value) {
+        return { "won:hasFile": undefined };
+      }
+      let payload = [];
+      value.forEach(file => {
+        //TODO: SAVE CORRECT RDF THIS METHOD
+        if (file.name && file.type && file.data) {
+          let f = {
+            "@type": "s:FileObject",
+            "s:name": file.name,
+            "s:type": file.type,
+            "s:data": file.data,
+          };
+
+          payload.push(f);
+        }
+      });
+      if (payload.length > 0) {
+        return { "won:hasFile": payload };
+      }
+      return { "won:hasFile": undefined };
+    },
+    parseFromRDF: function(jsonLDImm) {
+      const files = jsonLDImm && jsonLDImm.get("won:hasFile");
+      let parsedFiles = [];
+
+      if (Immutable.List.isList(files)) {
+        files &&
+          files.forEach(file => {
+            //TODO: RETRIEVE FROM CORRECT RDF THIS METHOD
+            let f = {
+              name: get(file, "s:name"),
+              type: get(file, "s:type"),
+              data: get(file, "s:data"),
+            };
+            if (f.name && f.type && f.data) {
+              parsedFiles.push(f);
+            }
+          });
+      } else {
+        let f = {
+          name: get(files, "s:name"),
+          type: get(files, "s:type"),
+          data: get(files, "s:data"),
+        };
+        if (f.name && f.type && f.data) {
+          return Immutable.fromJS([f]);
+        }
+      }
+      if (parsedFiles.length > 0) {
+        return Immutable.fromJS(parsedFiles);
+      }
+      return undefined;
+    },
+    generateHumanReadable: function({ value, includeLabel }) {
+      if (value) {
+        let humanReadable = "";
+        if (value.length > 1) {
+          humanReadable = value.length + " Files";
+        } else {
+          humanReadable = value[0].name;
+        }
+
+        return includeLabel ? this.label + ": " + humanReadable : humanReadable;
+      }
+      return undefined;
+    },
+  },
+  images: {
+    identifier: "images",
+    label: "Images",
+    icon: "#ico36_media_circle",
+    placeholder: "",
+    accepts: "image/*",
+    component: "won-file-picker",
+    viewerComponent: "won-file-viewer",
+    parseToRDF: function({ value }) {
+      if (!value) {
+        return { "won:hasImage": undefined };
+      }
+      let payload = [];
+      value.forEach(image => {
+        //TODO: SAVE CORRECT RDF THIS METHOD
+        if (image.name && image.type && image.data) {
+          let img = {
+            "@type": "s:ImageObject",
+            "s:name": image.name,
+            "s:type": image.type,
+            "s:data": image.data,
+          };
+
+          payload.push(img);
+        }
+      });
+      if (payload.length > 0) {
+        return { "won:hasImage": payload };
+      }
+      return { "won:hasImage": undefined };
+    },
+    parseFromRDF: function(jsonLDImm) {
+      const images = jsonLDImm && jsonLDImm.get("won:hasImage");
+      let imgs = [];
+
+      if (Immutable.List.isList(images)) {
+        images &&
+          images.forEach(image => {
+            //TODO: RETRIEVE FROM CORRECT RDF THIS METHOD
+            let img = {
+              name: get(image, "s:name"),
+              type: get(image, "s:type"),
+              data: get(image, "s:data"),
+            };
+            if (img.name && img.type && img.data) {
+              imgs.push(img);
+            }
+          });
+      } else {
+        let img = {
+          name: get(images, "s:name"),
+          type: get(images, "s:type"),
+          data: get(images, "s:data"),
+        };
+        if (img.name && img.type && img.data) {
+          return Immutable.fromJS([img]);
+        }
+      }
+      if (imgs.length > 0) {
+        return Immutable.fromJS(imgs);
+      }
+      return undefined;
+    },
+    generateHumanReadable: function({ value, includeLabel }) {
+      if (value) {
+        let humanReadable = "";
+        if (value.length > 1) {
+          humanReadable = value.length + " Images";
+        } else {
+          humanReadable = value[0].name;
+        }
+
+        return includeLabel ? this.label + ": " + humanReadable : humanReadable;
+      }
+      return undefined;
+    },
+  },
   ttl: {
     identifier: "ttl",
     label: "Turtle (TTL)",
