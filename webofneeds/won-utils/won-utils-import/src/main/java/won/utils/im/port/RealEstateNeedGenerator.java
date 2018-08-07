567092a984c576f4a1eb9fce903161c48023b2eb
Kristina Weinberger <kristina.weinberger@researchstudio.at>
2018-08-07 16:19:33 +0200

    Added hardcoded content for locations and amenities.

--- webofneeds/won-utils/won-utils-import/src/main/java/won/utils/im/port/RealEstateNeedGenerator.java
+++ webofneeds/won-utils/won-utils-import/src/main/java/won/utils/im/port/RealEstateNeedGenerator.java
@@ -1,5 +1,7 @@
 package won.utils.im.port;
 
+import java.util.HashMap;
+
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.DC;
 import org.apache.jena.vocabulary.RDF;
@@ -35,14 +37,122 @@ public class RealEstateNeedGenerator {
     static Property schema_unitCode = model.createProperty("http://schema.org/unitCode");
     static Property schema_value = model.createProperty("http://schema.org/value");
 
+    private static HashMap<String, String> loc1 = new HashMap();
+    private static HashMap<String, String> loc2 = new HashMap();
+    private static HashMap<String, String> loc3 = new HashMap();
+    private static HashMap<String, String> loc4 = new HashMap();
+    private static HashMap<String, String> loc5 = new HashMap();
+    private static HashMap<String, String> loc6 = new HashMap();
+    private static HashMap<String, String> loc7 = new HashMap();
+    private static HashMap<String, String> loc8 = new HashMap();
+    private static HashMap<String, String> loc9 = new HashMap();
+    private static HashMap<String, String> loc0 = new HashMap();
+
+    private static HashMap<String, String>[] locations = new HashMap[10];
+    private static String[] amenities = { "Balcony", "Parkingspace", "Garden", "Bathtub", "furnished",
+            "Parquetflooring", "Elevator", "Cellar", "Pool", "Sauna", "accessible" };
+
     public static void main(String[] args) {
+        loc0.put("nwlat", "48.385349");
+        loc0.put("nwlng", "16.821063");
+        loc0.put("selat", "48.309745");
+        loc0.put("selng", "16.729174");
+        loc0.put("lat", "48.288651");
+        loc0.put("lng", "16.705195");
+        loc0.put("name", "Gemeinde Weikendorf, Bezirk Gänserndorf, Lower Austria, 2253, Austria");
+        locations[0] = loc0;
+
+        // initialize locations
+        loc1.put("nwlat", "48.213814");
+        loc1.put("nwlng", "16.340870");
+        loc1.put("selat", "48.236309");
+        loc1.put("selng", "16.370149");
+        loc1.put("lat", "48.225073");
+        loc1.put("lng", "16.358398");
+        loc1.put("name", "Alsergrund, Vienna, 1090, Austria");
+        locations[1] = loc1;
+
+        loc2.put("nwlat", "48.145908");
+        loc2.put("nwlng", "14.126198");
+        loc2.put("selat", "48.465908");
+        loc2.put("selng", "14.446198");
+        loc2.put("lat", "48.305908");
+        loc2.put("lng", "14.286198");
+        loc2.put("name", "Linz, Upper Austria, 4010, Austria");
+        locations[2] = loc2;
+
+        loc3.put("nwlat", "46.910256");
+        loc3.put("nwlng", "15.278572");
+        loc3.put("selat", "47.230256");
+        loc3.put("selng", "15.598572");
+        loc3.put("lat", "47.070256");
+        loc3.put("lng", "15.438572");
+        loc3.put("name", "Graz, Styria, 8011, Austria");
+        locations[3] = loc3;
+
+        loc4.put("nwlat", "47.638135");
+        loc4.put("nwlng", "12.886481");
+        loc4.put("selat", "47.958135");
+        loc4.put("selng", "13.206481");
+        loc4.put("lat", "47.798135");
+        loc4.put("lng", "13.046481");
+        loc4.put("name", "Salzburg, 5020, Austria");
+        locations[4] = loc4;
+
+        loc5.put("nwlat", "48.164398");
+        loc5.put("nwlng", "15.582912");
+        loc5.put("selat", "48.244399");
+        loc5.put("selng", "15.662912");
+        loc5.put("lat", "48.204399");
+        loc5.put("lng", "15.622912");
+        loc5.put("name", "St. Pölten, Lower Austria, 3102, Austria");
+        locations[5] = loc5;
+
+        loc6.put("nwlat", "47.480016");
+        loc6.put("nwlng", "9.654882");
+        loc6.put("selat", "47.534581");
+        loc6.put("selng", "9.807672");
+        loc6.put("lat", "47.502578");
+        loc6.put("lng", "9.747292");
+        loc6.put("name", "Bregenz, Vorarlberg, Austria");
+        locations[6] = loc6;
+
+        loc7.put("nwlat", "46.782816");
+        loc7.put("nwlng", "14.467960");
+        loc7.put("selat", "46.462816");
+        loc7.put("selng", "14.147960");
+        loc7.put("lat", "46.622816");
+        loc7.put("lng", "14.307960");
+        loc7.put("name", "Klagenfurt, Klagenfurt am Wörthersee, Carinthia, 9020, Austria");
+        locations[7] = loc7;
+
+        loc8.put("nwlat", "47.425430");
+        loc8.put("nwlng", "11.552769");
+        loc8.put("selat", "47.105430");
+        loc8.put("selng", "11.232769");
+        loc8.put("lat", "47.265430");
+        loc8.put("lng", "11.392769");
+        loc8.put("name", "Innsbruck, Tyrol, 6020, Austria");
+        locations[8] = loc8;
+
+        loc9.put("nwlat", "48.145711");
+        loc9.put("nwlng", "16.560306");
+        loc9.put("selat", "47.951363");
+        loc9.put("selng", "16.253757");
+        loc9.put("lat", "47.875098");
+        loc9.put("lng", "15.866162");
+        loc9.put("name", "Bezirk Baden, Lower Austria, Austria");
+        locations[9] = loc9;
+
         generateNeeds();
     }
 
     private static void generateNeeds() {
         for (int i = 0; i < 9; i++) {
             // TODO: unique URI
-            String needURI = "https://localhost:8443/won/resource/event/m3tuwsuahplc#need";
+            // String needURI =
+            // "https://localhost:8443/won/resource/event/m3tuwsuahplc#need";
+            String needURI = "https://localhost:8443/won/resource/event/" + "real_estate_sample_" + i + "#need";
 
             model = ModelFactory.createDefaultModel();
 
@@ -126,7 +236,7 @@ public class RealEstateNeedGenerator {
     }
 
     private static Resource addAmenities(Resource resource) {
-    	Resource amenityResource = model.createResource();
+        Resource amenityResource = model.createResource();
         Resource schema_LocationFeatureSpecification = model
                 .createResource("http://schema.org/LocationFeatureSpecification");
 
@@ -140,10 +250,10 @@ public class RealEstateNeedGenerator {
     }
 
     private static Resource addFloorSize(Resource resource) {
-    	Resource floorSizeResource = model.createResource();
-    	Resource schema_QuantitativeValue = model.createResource("http://schema.org/QuantitativeValue");
+        Resource floorSizeResource = model.createResource();
+        Resource schema_QuantitativeValue = model.createResource("http://schema.org/QuantitativeValue");
 
-    	resource.addProperty(schema_floorSize, floorSizeResource);
+        resource.addProperty(schema_floorSize, floorSizeResource);
         floorSizeResource.addProperty(RDF.type, schema_QuantitativeValue);
         floorSizeResource.addProperty(schema_unitCode, "MTK");
         floorSizeResource.addProperty(schema_value, "111");
@@ -151,12 +261,11 @@ public class RealEstateNeedGenerator {
     }
 
     private static Resource addNumberOfRooms(Resource resource) {
-    	resource.addProperty(schema_numberOfRooms, "5");
+        resource.addProperty(schema_numberOfRooms, "5");
         return resource;
     }
 
     private static Resource addPriceSpecification(Resource resource) {
-    	Resource schema_QuantitativeValue = model.createResource("http://schema.org/QuantitativeValue");
         Resource schema_CompoundPriceSpecification = model
                 .createResource("http://schema.org/CompoundPriceSpecification");
         Resource priceSpecificationResource = model.createResource();
