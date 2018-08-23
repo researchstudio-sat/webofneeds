dab39be22aad27edc676ec13dcaadea8e4364ec9
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2018-08-23 12:17:34 +0200

    ignore exception in FailResponder

--- webofneeds/won-node/src/main/java/won/node/camel/route/fixed/WonMessageRoutes.java
+++ webofneeds/won-node/src/main/java/won/node/camel/route/fixed/WonMessageRoutes.java
@@ -73,6 +73,7 @@ public class WonMessageRoutes extends RouteBuilder
         //sends the failure response, using the current transaction
         from("direct:sendFailureResponse")
                 .routeId("direct:sendFailureResponse")
+                .onException(Exception.class).handled(true).end()
                 .transacted("PROPAGATION_REQUIRES_NEW")
                 .to("bean:parentLocker")
                 .to("bean:failResponder");
