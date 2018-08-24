69beb7f13e5af0eb1af1d77eba641d0460f101d0
Kevin Singer <kevin.singer@researchstudio.at>
2018-08-24 15:22:46 +0200

    s/https/http/ @ shacl prefix definition.

--- webofneeds/won-owner-webapp/src/main/webapp/app/service/won.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/service/won.js
@@ -642,7 +642,7 @@ won.defaultContext = {
   cert: "http://www.w3.org/ns/auth/cert#",
   woncrypt: "http://purl.org/webofneeds/woncrypt#",
   s: "http://schema.org/",
-  sh: "https://www.w3.org/ns/shacl#",
+  sh: "http://www.w3.org/ns/shacl#",
   foaf: "http://xmlns.com/foaf/0.1/",
   "msg:hasMessageType": {
     "@id": "http://purl.org/webofneeds/message#hasMessageType",
