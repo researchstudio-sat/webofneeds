ebcd2d69ca0da60c0d30eb1720794d99959f1941
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2018-08-27 17:46:17 +0200

    Change node so it drops hints when it gets too many

--- webofneeds/won-node/src/main/java/won/node/camel/processor/fixed/HintMessageProcessor.java
+++ webofneeds/won-node/src/main/java/won/node/camel/processor/fixed/HintMessageProcessor.java
@@ -1,8 +1,14 @@
 package won.node.camel.processor.fixed;
 
+import java.net.URI;
+import java.util.Collection;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import won.node.camel.processor.AbstractCamelProcessor;
 import won.node.camel.processor.annotation.FixedMessageProcessor;
 import won.protocol.message.WonMessage;
@@ -10,13 +16,11 @@ import won.protocol.message.processor.camel.WonCamelConstants;
 import won.protocol.model.Connection;
 import won.protocol.model.ConnectionEventType;
 import won.protocol.model.ConnectionState;
+import won.protocol.repository.ConnectionRepository;
 import won.protocol.util.RdfUtils;
 import won.protocol.util.WonRdfUtils;
 import won.protocol.vocabulary.WON;
 import won.protocol.vocabulary.WONMSG;
-
-import java.net.URI;
-import java.util.Collection;
 
 /**
  * User: syim
@@ -26,6 +30,11 @@ import java.util.Collection;
 @FixedMessageProcessor(direction = WONMSG.TYPE_FROM_EXTERNAL_STRING, messageType = WONMSG.TYPE_HINT_STRING)
 public class HintMessageProcessor extends AbstractCamelProcessor {
 
+    @Autowired 
+    private ConnectionRepository connectionRepository;
+    
+    @Value("${ignore.hints.suggested.connection.count.max}")
+    private Long maxSuggestedConnectionCount = 100L;
 
     public void process(Exchange exchange) throws Exception {
         Message message = exchange.getIn();
@@ -34,10 +43,15 @@ public class HintMessageProcessor extends AbstractCamelProcessor {
         logger.debug("STORING message with id {}", wonMessage.getMessageURI());
 
         URI needURIFromWonMessage = wonMessage.getReceiverNeedURI();
+        if (isTooManyHints(needURIFromWonMessage)) {
+          return;
+        }
+        
         URI wonNodeFromWonMessage = wonMessage.getReceiverNodeURI();
         URI otherNeedURIFromWonMessage = URI.create(RdfUtils.findOnePropertyFromResource(
                 wonMessage.getMessageContent(), wonMessage.getMessageURI(),
                 WON.HAS_MATCH_COUNTERPART).asResource().getURI());
+        
         double wmScore = RdfUtils.findOnePropertyFromResource(
                 wonMessage.getMessageContent(), wonMessage.getMessageURI(),
                 WON.HAS_MATCH_SCORE).asLiteral().getDouble();
@@ -68,6 +82,11 @@ public class HintMessageProcessor extends AbstractCamelProcessor {
         //build message to send to owner, put in header
         //set the receiver to the newly generated connection uri
         wonMessage.addMessageProperty(WONMSG.RECEIVER_PROPERTY, con.getConnectionURI());
+    }
+
+    private boolean isTooManyHints(URI needURIFromWonMessage) {
+      long hintCount = connectionRepository.countByNeedURIAndState(needURIFromWonMessage, ConnectionState.SUGGESTED);
+      return (hintCount > maxSuggestedConnectionCount );
     }
 
 
