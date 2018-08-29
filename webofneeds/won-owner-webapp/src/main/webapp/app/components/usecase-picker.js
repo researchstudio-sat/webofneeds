9aacb6da003e3caf5a88b9b4101d7df9b414b1a1
Kristina Weinberger <kristina.weinberger@researchstudio.at>
2018-08-29 15:17:42 +0200

    Merged usecase-picker and usecase-picker-content

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/usecase-picker.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/usecase-picker.js
@@ -3,14 +3,14 @@
  */
 import angular from "angular";
 import ngAnimate from "angular-animate";
+import labelledHrModule from "./labelled-hr.js";
 
 import "ng-redux";
 import { attach, getIn } from "../utils.js";
 import { actionCreators } from "../actions/actions.js";
 import { connect2Redux } from "../won-utils.js";
 import { selectIsConnected } from "../selectors.js";
-import usecasePickerContentModule from "./usecase-picker-content.js";
-import usecaseGroupModule from "./usecase-group.js";
+import { useCaseGroups } from "useCaseDefinitions";
 
 import "style/_usecase-picker.scss";
 
@@ -22,9 +22,10 @@ const serviceDependencies = [
 
 function genComponentConf() {
   const template = `
-        <div class="ucp__header">
+        <!-- HEADER -->
+        <div class="ucp__header" ng-if="self.useCaseGroup === 'all'">
             <a class="ucp__header__back clickable"
-                ng-click="self.router__stateGoCurrent({showUseCases: undefined, useCase: undefined, useCaseGroup: undefined})">
+                ng-click="self.router__stateGoCurrent({useCase: undefined, useCaseGroup: undefined})">
                 <svg style="--local-primary:var(--won-primary-color);"
                     class="ucp__header__back__icon">
                     <use xlink:href="#ico36_close" href="#ico36_close"></use>
@@ -32,16 +33,61 @@ function genComponentConf() {
             </a>
             <span class="ucp__header__title">What do you have or want?</span>
         </div>
-        <won-usecase-picker-content ng-if="!self.useCaseGroup">
-        </won-usecase-picker-content>
-        <won-usecase-group ng-if="!!self.useCaseGroup">
-        </won-usecase-group>
+
+        <!-- WHAT'S AROUND -->
+        <button class="ucp__createx won-pending-publishing won-button--filled red"
+                ng-if="self.pendingPublishing"
+                ng-disabled="self.pendingPublishing">
+            <span>Finding out what's going on&hellip;</span>
+        </button>
+        <div class="ucp__createx">
+            <button class="won-button--filled red ucp__createx__button"
+                    ng-if="!self.pendingPublishing"
+                    ng-click="self.createWhatsAround()"
+                    ng-disabled="self.pendingPublishing">
+                <svg class="won-button-icon" style="--local-primary:white;">
+                    <use xlink:href="#ico36_location_current" href="#ico36_location_current"></use>
+                </svg>
+                <span>What's in your Area?</span>
+            </button>
+            <button class="won-button--filled red ucp__createx__button"
+                    ng-if="!self.pendingPublishing"
+                    ng-click="self.createWhatsNew()"
+                    ng-disabled="self.pendingPublishing">
+                <span>What's new?</span>
+            </button>
+        </div>
+
+        <div class="ucp__main">
+        <won-labelled-hr label="::'Or'" class="ucp__main__labelledhr"></won-labelled-hr>
+
+        <!-- TODO: SEARCH FIELD -->
+        <!-- TODO: SEARCH RESULTS -->
+        <!-- USE CASE GROUPS - TODO: only show while not searching --> 
+        <div class="ucp__main__usecase-group"
+          ng-repeat="useCaseGroup in self.useCaseGroups"
+          ng-if="self.showUseCaseGroupHeaders && self.displayableUseCaseGroup(useCaseGroup)"
+          ng-click="self.startFrom(useCaseGroup)">
+              <svg class="ucp__main__usecase-group__icon"
+                ng-if="!!useCaseGroup.icon">
+                <use xlink:href="{{ useCaseGroup.icon }}" href="{{ useCaseGroup.icon }}"></use>
+              </svg>
+              <div class="ucp__main__usecase-group__label"
+                ng-if="!!useCaseGroup.label">
+                  {{ useCaseGroup.label }}
+              </div>
+        </div>
+        <!-- TODO: USE CASES WITHOUT GROUPS -->
+        </div>
     `;
 
   class Controller {
     constructor(/* arguments <- serviceDependencies */) {
       attach(this, serviceDependencies, arguments);
       window.ucp4dbg = this;
+
+      this.useCaseGroups = useCaseGroups;
+      this.showUseCaseGroupHeaders = this.showUseCaseGroups();
 
       const selectFromState = state => {
         const useCaseGroup = getIn(state, [
@@ -52,12 +98,90 @@ function genComponentConf() {
 
         return {
           useCaseGroup,
+          pendingPublishing: state.get("creatingWhatsX"),
           connectionHasBeenLost: !selectIsConnected(state),
         };
       };
 
       // Using actionCreators like this means that every action defined there is available in the template.
       connect2Redux(selectFromState, actionCreators, [], this);
+    }
+
+    createWhatsAround() {
+      if (!this.pendingPublishing) {
+        this.needs__whatsAround();
+      }
+    }
+
+    createWhatsNew() {
+      if (!this.pendingPublishing) {
+        this.needs__whatsNew();
+      }
+    }
+
+    startFrom(selectedUseCaseGroup) {
+      const selectedGroupIdentifier =
+        selectedUseCaseGroup && selectedUseCaseGroup.identifier;
+
+      if (selectedGroupIdentifier) {
+        this.router__stateGoCurrent({
+          useCaseGroup: encodeURIComponent(selectedGroupIdentifier),
+        });
+      } else {
+        console.log(
+          "No identifier found for given usecase group, ",
+          selectedUseCaseGroup
+        );
+      }
+    }
+
+    /**
+     * Only display the headers of the useCaseGroups if there are multiple displayable useCaseGroups
+     * @returns {boolean}
+     */
+    showUseCaseGroups() {
+      let countDisplayedUseCaseGroups = 0;
+
+      for (const key in this.useCaseGroups) {
+        if (
+          this.displayableUseCaseGroup(this.useCaseGroups[key]) &&
+          ++countDisplayedUseCaseGroups > 1
+        ) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    /**
+     * return if the given useCaseGroup is displayable or not
+     * needs to have at least one displayable UseCase
+     * @param useCase
+     * @returns {*}
+     */
+    displayableUseCaseGroup(useCaseGroup) {
+      const useCaseGroupValid =
+        useCaseGroup &&
+        (useCaseGroup.label || useCaseGroup.icon) &&
+        useCaseGroup.useCases;
+
+      if (useCaseGroupValid) {
+        for (const key in useCaseGroup.useCases) {
+          if (this.displayableUseCase(useCaseGroup.useCases[key])) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    /**
+     * return if the given useCase is displayable or not
+     * @param useCase
+     * @returns {*}
+     */
+    displayableUseCase(useCase) {
+      return useCase && useCase.identifier && (useCase.label || useCase.icon);
     }
   }
 
@@ -77,9 +201,5 @@ function genComponentConf() {
 
 export default //.controller('CreateNeedController', [...serviceDependencies, CreateNeedController])
 angular
-  .module("won.owner.components.usecasePicker", [
-    ngAnimate,
-    usecasePickerContentModule,
-    usecaseGroupModule,
-  ])
+  .module("won.owner.components.usecasePicker", [ngAnimate, labelledHrModule])
   .directive("wonUsecasePicker", genComponentConf).name;
