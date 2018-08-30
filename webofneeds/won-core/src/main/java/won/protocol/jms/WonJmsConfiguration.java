d7813d7724ea1de71149211aab64da4629db6045
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2018-08-30 12:25:30 +0200

    Change activemq settings
    Trying to prevent clogging the system with old unseen messages

--- webofneeds/won-core/src/main/java/won/protocol/jms/WonJmsConfiguration.java
+++ webofneeds/won-core/src/main/java/won/protocol/jms/WonJmsConfiguration.java
@@ -1,8 +1,10 @@
 package won.protocol.jms;
 
-import org.apache.camel.component.jms.JmsConfiguration;
-
 import javax.jms.ConnectionFactory;
+
+import org.apache.camel.component.jms.JmsConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * User: LEIH-NB
@@ -10,14 +12,23 @@ import javax.jms.ConnectionFactory;
  */
 public class WonJmsConfiguration extends JmsConfiguration
 {
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  
   public WonJmsConfiguration(ConnectionFactory connectionFactory){
     super(connectionFactory);
     configureDefaultJmsConfiguration();
+    
   }
   public void configureDefaultJmsConfiguration(){
     setTimeToLive(0);
     setDisableTimeToLive(true);
     setRequestTimeout(0);
+    setAsyncConsumer(true);
+    setDeliveryPersistent(false);
+    setDisableReplyTo(true);
+    setTimeToLive(5000);
+    setTransacted(false);
+    logger.info("default jms configuration setup done");
   }
 
   public void configureJmsConfigurationForQueues(){
