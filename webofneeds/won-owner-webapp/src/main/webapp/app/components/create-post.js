792de4c9fd1f81ae985a63d9468c10445c97e099
maxstolze <maximilian.stolze@researchstudio.at>
2018-08-30 10:42:53 +0200

    Rebase and remove unused new instruement-picker/viewer

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/create-post.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/create-post.js
@@ -30,11 +30,7 @@ import selectPickerModule from "./details/picker/select-picker.js";
 import rangePickerModule from "./details/picker/range-picker.js";
 import filePickerModule from "./details/picker/file-picker.js";
 import workflowPickerModule from "./details/picker/workflow-picker.js";
-<<<<<<< Upstream, based on origin/master
 import petrinetPickerModule from "./details/picker/petrinet-picker.js";
-=======
-import instrumentPickerModule from "./details/picker/instrument-picker.js";
->>>>>>> eda9505 Implement musician use case
 
 import "style/_create-post.scss";
 import "style/_responsiveness-utils.scss";
@@ -410,11 +406,7 @@ angular
     rangePickerModule,
     filePickerModule,
     workflowPickerModule,
-<<<<<<< Upstream, based on origin/master
     petrinetPickerModule,
-=======
-    instrumentPickerModule,
->>>>>>> eda9505 Implement musician use case
     ngAnimate,
   ])
   .directive("wonCreatePost", genComponentConf).name;
