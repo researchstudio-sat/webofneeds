a38708a7d37cfc7a1243b3063e298fd5560d0a8a
Kevin Singer <kevin.singer@researchstudio.at>
2018-09-28 13:20:24 +0200

    Moved viewer and picker imports to their own main module.

---
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/details/viewer/viewers.js
@@ -0,0 +1,36 @@
+import personViewerModule from "./person-viewer.js";
+import descriptionViewerModule from "./description-viewer.js";
+import locationViewerModule from "./location-viewer.js";
+import tagsViewerModule from "./tags-viewer.js";
+import travelActionViewerModule from "./travel-action-viewer.js";
+import titleViewerModule from "./title-viewer.js";
+import numberViewerModule from "./number-viewer.js";
+import priceViewerModule from "./price-viewer.js";
+import datetimeViewerModule from "./datetime-viewer.js";
+import dropdownViewerModule from "./dropdown-viewer.js";
+import selectViewerModule from "./select-viewer.js";
+import rangeViewerModule from "./range-viewer.js";
+import fileViewerModule from "./file-viewer.js";
+import workflowViewerModule from "./workflow-viewer.js";
+import petrinetViewerModule from "./petrinet-viewer.js";
+
+/**
+ * module names for angular's module system
+ */
+export default [
+  personViewerModule,
+  descriptionViewerModule,
+  locationViewerModule,
+  tagsViewerModule,
+  travelActionViewerModule,
+  titleViewerModule,
+  numberViewerModule,
+  priceViewerModule,
+  datetimeViewerModule,
+  dropdownViewerModule,
+  selectViewerModule,
+  rangeViewerModule,
+  fileViewerModule,
+  workflowViewerModule,
+  petrinetViewerModule,
+];
