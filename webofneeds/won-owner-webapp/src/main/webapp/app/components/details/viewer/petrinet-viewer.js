f803d70137cbe1f2eff85796e7db8eae209e68b1
quasarchimaere <fabian.suda@researchstudio.at>
2018-10-18 17:00:34 +0200

    Updates petrinet-viewer styling

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/details/viewer/petrinet-viewer.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/details/viewer/petrinet-viewer.js
@@ -14,21 +14,21 @@ function genComponentConf() {
           <span class="petrinetv__header__label" ng-if="self.detail.label">{{self.detail.label}}</span>
         </div>
         <div class="petrinetv__content">
-          <a class="petrinetv__content__download" ng-show="self.content"
-            ng-href="data:{{self.content.get('type')}};base64,{{self.content.get('data')}}"
-            download="{{ self.content.get('name') }}">
-            <div class="petrinetv__content__download__label clickable">
-              {{ self.content.get('name') }}
-            </div>
-            <svg class="petrinetv__content__download__typeicon">
-              <use xlink:href="#ico36_uc_transport_demand" href="#ico36_uc_transport_demand"></use>
-            </svg>
-          </a>
           <won-petrinet-state
             class="petrinetv__content__state"
             ng-if="self.content && self.content.get('processURI')"
             process-uri="self.content.get('processURI')">
           </won-petrinet-state>
+          <a class="petrinetv__content__download" ng-show="self.content"
+            ng-href="data:{{self.content.get('type')}};base64,{{self.content.get('data')}}"
+            download="{{ self.content.get('name') }}">
+            <svg class="petrinetv__content__download__typeicon">
+              <use xlink:href="#ico36_uc_transport_demand" href="#ico36_uc_transport_demand"></use>
+            </svg>
+            <div class="petrinetv__content__download__label clickable">
+              Download '{{ self.content.get('name') }}'
+            </div>
+          </a>
         </div>
       </div>
     `;
