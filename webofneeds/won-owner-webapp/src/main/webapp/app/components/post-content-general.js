d58ba35ad243d215abf6479047fc60f75233859d
Gabriel Pickl <gabriel.pickl@researchstudio.at>
2018-10-23 14:28:08 +0200

    Made slightly more adjustable reputation.

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/post-content-general.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/post-content-general.js
@@ -24,7 +24,7 @@ function genComponentConf() {
             </div>
             <div class="pcg__columns__left__item__value">
               {{ self.persona.getIn(['jsonld', 's:name']) }}
-              (Reputation: <span style="color: var(--won-primary-color)">★★★★☆</span>)
+              (Reputation: <span style="color: var(--won-primary-color)">{{self.ratingStars(3)}}</span>)
             </div>
           </div>
           <div class="pcg__columns__left__item" ng-if="self.friendlyTimestamp">
@@ -96,6 +96,13 @@ function genComponentConf() {
         };
       };
       connect2Redux(selectFromState, actionCreators, ["self.postUri"], this);
+    }
+
+    ratingStars(rating) {
+      if (rating > 5 || rating < 0) {
+        return "☆☆☆☆☆";
+      }
+      return "★".repeat(rating) + "☆".repeat(5 - rating);
     }
   }
 
