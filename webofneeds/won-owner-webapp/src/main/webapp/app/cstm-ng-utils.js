e9efc645e11e4eab9b7cff38e8a7ee66e585f91b
quasarchimaere <fabian.suda@researchstudio.at>
2018-11-05 13:45:08 +0100

    Removes unused function "broadcastEvent"

--- webofneeds/won-owner-webapp/src/main/webapp/app/cstm-ng-utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/cstm-ng-utils.js
@@ -6,24 +6,6 @@
 import angular from "angular";
 
 /**
- * Expects an object with a $scope and/or $element variable (e.g. a controller
- * with those two bound two it) and publishes an event to both of these, yielding
- * a solution that's both angular-1.X- as well as standard-js-conform.
- * @param scopeAndElem
- * @param eventName
- * @param eventData
- */
-export function broadcastEvent(scopeAndElem, eventName, eventData) {
-  const payload = { detail: eventData };
-  if (scopeAndElem.$scope) {
-    //scopeAndElem.$scope.$broadcast(eventName, eventData);
-    scopeAndElem.$scope.$broadcast(eventName, payload);
-
-    console.log("broadcasting");
-  }
-}
-
-/**
  * Registers an input listener on the ngElement. The
  * callback will be invoked if the user has stopped
  * typing for `doneTypingInterval` milliseconds.
