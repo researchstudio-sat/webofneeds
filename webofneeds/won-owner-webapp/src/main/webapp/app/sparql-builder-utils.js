1dd646fa3cff3db65a11d45251f84494d9d7f3e6
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2018-11-08 11:15:02 +0100

    Bugfix for the whatsnew query.

--- webofneeds/won-owner-webapp/src/main/webapp/app/sparql-builder-utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/sparql-builder-utils.js
@@ -636,14 +636,14 @@ export function generateWhatsNewQuery() {
   return `PREFIX won: <http://purl.org/webofneeds/model#>
         PREFIX s: <http://schema.org/>
         PREFIX dct: <http://purl.org/dc/terms/>
-        SELECT DISTINCT ?result ?score
-            BIND ((YEAR(?created) - 1970) * 315360000
+        SELECT DISTINCT ?result ?score WHERE {
+          BIND ((YEAR(?created) - 1970) * 315360000
                + MONTH(?created) * 26280000
                + DAY(?created) * 86400
                + HOURS(?created) * 3600
                + MINUTES(?created) * 60
                + SECONDS(?created)
-                as ?score) WHERE {
+                as ?score)
           ?result <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> won:Need.
           ?result won:isInState  won:Active .
           ?result dct:created ?created.
