3104eb58e5ada81608a0d8075e4a16c088554ca5
quasarchimaere <sudafabian@gmail.com>
2018-11-26 16:08:32 +0100

    Reformat User.java class

--- webofneeds/won-owner/src/main/java/won/owner/model/User.java
+++ webofneeds/won-owner/src/main/java/won/owner/model/User.java
@@ -39,225 +39,226 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 @Entity
 @Table(
-		name = "wonuser", //don't use 'user' - see above
-		uniqueConstraints = @UniqueConstraint(columnNames = {"username"})
+        name = "wonuser", //don't use 'user' - see above
+        uniqueConstraints = @UniqueConstraint(columnNames = {"username"})
 )
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class User implements UserDetails, Persistable<Long>{
+public class User implements UserDetails, Persistable<Long> {
 
-	@Id
-	@GeneratedValue
-	@Column(name = "id")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
 
-  @Column(name = "username")
-	private String username;
+    @Column(name = "username")
+    private String username;
 
-  @Column(name = "password")
-	private String password;
+    @Column(name = "password")
+    private String password;
 
 
-  @OneToMany(fetch = FetchType.EAGER)
-  @OrderBy("creationDate desc")
-  @JoinTable(name = "wonuser_userneed", joinColumns = {@JoinColumn(name = "wonuser_id")})
-  private List<UserNeed> userNeeds;
+    @OneToMany(fetch = FetchType.EAGER)
+    @OrderBy("creationDate desc")
+    @JoinTable(name = "wonuser_userneed", joinColumns = {@JoinColumn(name = "wonuser_id")})
+    private List<UserNeed> userNeeds;
 
-  @Column(name = "role")
-  private String role;
+    @Column(name = "role")
+    private String role;
 
-  @Column(name = "email")
-  private String email;
-  
-  @JoinColumn(name = "keystore_id")
-  @OneToOne(cascade = CascadeType.ALL,
-  fetch = FetchType.LAZY, optional = false)
-  private KeystoreHolder keystoreHolder;
+    @Column(name = "email")
+    private String email;
 
-  @JoinColumn(name = "keystore_password_id")
-  @OneToOne(cascade = CascadeType.ALL,
-  fetch = FetchType.LAZY, optional = false)
-  private KeystorePasswordHolder keystorePasswordHolder;
-  
-  //TODO: eager is dangerous here, but we need it as the User object is kept in the http session which outlives the
-  //hibernate session. However, this wastes space and may lead to memory issues during high usage. Fix it.
-  @ElementCollection( fetch = FetchType.EAGER)
-  private Set<URI> draftURIs;
+    @JoinColumn(name = "keystore_id")
+    @OneToOne(cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY, optional = false)
+    private KeystoreHolder keystoreHolder;
 
-	@Transient
-	private Collection<SimpleGrantedAuthority> authorities;
+    @JoinColumn(name = "keystore_password_id")
+    @OneToOne(cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY, optional = false)
+    private KeystorePasswordHolder keystorePasswordHolder;
 
-	public User() {
-	}
+    //TODO: eager is dangerous here, but we need it as the User object is kept in the http session which outlives the
+    //hibernate session. However, this wastes space and may lead to memory issues during high usage. Fix it.
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<URI> draftURIs;
 
-	public User(final String username, final String password) {
-		this.username = username;
-		this.password = password;
-    this.role ="ROLE_ACCOUNT";
-    this.email = this.username;
-	}
+    @Transient
+    private Collection<SimpleGrantedAuthority> authorities;
 
-  public User(final String username, final String password, String role) {
-    this.username = username;
-    this.password = password;
-    if (role == null) {
-    	this.role = "ROLE_ACCOUNT";
-    } else {
-    	this.role = role;
+    public User() {
     }
-  }
 
-  @Override
-  public boolean isNew() {
-	  return this.id == null;
-  }
-  
-	@Override
-	public String toString() {
-		return "User{" +
-				"id=" + id +
-				", username='" + username + '\'' +
-				", password='" + password + '\'' +
-        ", role='" + role + '\'' +
-				'}';
-	}
+    public User(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+        this.role = "ROLE_ACCOUNT";
+        this.email = this.username;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public User(final String username, final String password, String role) {
+        this.username = username;
+        this.password = password;
+        if (role == null) {
+            this.role = "ROLE_ACCOUNT";
+        } else {
+            this.role = role;
+        }
+    }
 
-	public void setId(final Long id) {
-		this.id = id;
-	}
+    @Override
+    public boolean isNew() {
+        return this.id == null;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    public void setId(final Long id) {
+        this.id = id;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-    authorities = new ArrayList<SimpleGrantedAuthority>(1);
-    authorities.add(new SimpleGrantedAuthority(role));
-		return authorities;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	@Override
-	public String getPassword() {
-		return password;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-	@Override
-	public String getUsername() {
-		return username;
-	}
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
-  public void setUsername(final String username) {
-    this.username = username;
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        authorities = new ArrayList<SimpleGrantedAuthority>(1);
+        authorities.add(new SimpleGrantedAuthority(role));
+        return authorities;
+    }
 
-  public void setPassword(final String password) {
-    this.password = password;
-  }
-  
-  public KeystoreHolder getKeystoreHolder() {
-	return keystoreHolder;
-  }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-  public void setKeystoreHolder(KeystoreHolder keystoreHolder) {
-	this.keystoreHolder = keystoreHolder;
-  }
-  
-  public void setKeystorePasswordHolder(KeystorePasswordHolder keystorePassword) {
-	this.keystorePasswordHolder = keystorePassword;
-  }
-  
-  public KeystorePasswordHolder getKeystorePasswordHolder() {
-	return keystorePasswordHolder;
-  }
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public KeystoreHolder getKeystoreHolder() {
+        return keystoreHolder;
+    }
+
+    public void setKeystoreHolder(KeystoreHolder keystoreHolder) {
+        this.keystoreHolder = keystoreHolder;
+    }
+
+    public void setKeystorePasswordHolder(KeystorePasswordHolder keystorePassword) {
+        this.keystorePasswordHolder = keystorePassword;
+    }
+
+    public KeystorePasswordHolder getKeystorePasswordHolder() {
+        return keystorePasswordHolder;
+    }
   
   /*
-	public List<Need> getNeeds() {
+    public List<Need> getNeeds() {
 		return needs;
 	}          */
 
-  public void addNeedUri(UserNeed userNeed){
-    this.userNeeds.add(userNeed);
-  }
+    public void addNeedUri(UserNeed userNeed) {
+        this.userNeeds.add(userNeed);
+    }
 
-  public List<UserNeed> getUserNeeds() {
-    return userNeeds;
-  }
+    public List<UserNeed> getUserNeeds() {
+        return userNeeds;
+    }
 
-  public void setUserNeeds(final List<UserNeed> userNeeds) {
-    this.userNeeds = userNeeds;
-  }
+    public void setUserNeeds(final List<UserNeed> userNeeds) {
+        this.userNeeds = userNeeds;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(final String email) {
-    this.email = email;
-  }
+    public void setEmail(final String email) {
+        this.email = email;
+    }
 
-  public String getRole() {
-    return role;
-  }
+    public String getRole() {
+        return role;
+    }
 
-  public void setRole(final String role) {
-    this.role = role;
-  }
+    public void setRole(final String role) {
+        this.role = role;
+    }
 
-  public Set<URI> getDraftURIs(){
-    return draftURIs;
-  }
-	/*public void setNeeds(final List<Need> needs) {
-		this.needs = needs;
-	}     */
-  public void setDrafts(final Set<URI> draftURIs) {
-    this.draftURIs = draftURIs;
-  }
+    public Set<URI> getDraftURIs() {
+        return draftURIs;
+    }
 
-	@Override
-	public boolean equals(final Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+    /*public void setNeeds(final List<Need> needs) {
+        this.needs = needs;
+    }     */
+    public void setDrafts(final Set<URI> draftURIs) {
+        this.draftURIs = draftURIs;
+    }
 
-		final User user = (User) o;
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-		if (id != null ? !id.equals(user.id) : user.id != null) return false;
-		if (userNeeds != null ? !userNeeds.equals(user.userNeeds) : user.userNeeds != null) return false;
-		if (password != null ? !password.equals(user.password) : user.password != null) return false;
-		if (username != null ? !username.equals(user.username) : user.username != null) return false;
-    if (role != null ? !role.equals(user.role) : user.role != null) return false;
-    if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        final User user = (User) o;
 
-		return true;
-	}
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (userNeeds != null ? !userNeeds.equals(user.userNeeds) : user.userNeeds != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+        if (role != null ? !role.equals(user.role) : user.role != null) return false;
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
 
-	@Override
-	public int hashCode() {
-		int result = id != null ? id.hashCode() : 0;
-		result = 31 * result + (username != null ? username.hashCode() : 0);
-		result = 31 * result + (password != null ? password.hashCode() : 0);
-		result = 31 * result + (userNeeds != null ? userNeeds.hashCode() : 0);
-    result = 31 * result + (role != null ? role.hashCode() : 0);
-    result = 31 * result + (email != null ? email.hashCode() : 0);
-		return result;
-	}
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (userNeeds != null ? userNeeds.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
