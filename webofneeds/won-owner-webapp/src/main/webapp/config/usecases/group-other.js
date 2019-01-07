9b2a600014e90b77cb00329a93b237885bc74eb9
quasarchimaere <sudafabian@gmail.com>
2019-01-07 11:32:48 +0100

    Merge GroupChat branch

--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-other.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-other.js
@@ -1,6 +1,10 @@
+import won from "../../app/service/won.js";
+
 import { complain } from "./uc-complain.js";
 import { handleComplaint } from "./uc-handle-complaint.js";
 import { customUseCase } from "./uc-custom.js";
+
+import { details, emptyDraft } from "../detail-definitions.js";
 
 export const otherGroup = {
   identifier: "othergroup",
@@ -10,5 +14,15 @@ export const otherGroup = {
     complain: complain,
     handleComplaint: handleComplaint,
     customUseCase: customUseCase,
+    groupChat: {
+      identifier: "groupChat",
+      label: "New Groupchat Post",
+      draft: {
+        ...emptyDraft,
+        facet: { "@id": "#groupFacet", "@type": won.WON.GroupFacet },
+      },
+      details: details,
+      seeksDetails: details,
+    },
   },
 };
