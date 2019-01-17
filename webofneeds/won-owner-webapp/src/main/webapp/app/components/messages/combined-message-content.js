f641c652819ad41349e30ab5d5f876b6dd23cb80
quasarchimaere <sudafabian@gmail.com>
2019-01-17 13:56:52 +0100

    Fixes #2642 personaName shown at wrong side

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/messages/combined-message-content.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/messages/combined-message-content.js
@@ -92,14 +92,15 @@ function genComponentConf() {
 
         /*Extract persona name from message:
 
-          either within the need of the originatorUri-need (in group-chat-messages)
-          or
-          within the remoteNeedUri-need of the connection (for 1:1 chats)
-        */
+         either within the need of the originatorUri-need (in group-chat-messages)
+         or
+         within the remoteNeedUri-need of the connection (for 1:1 chats)
+         */
         const relevantNeedUri =
-          !get(message, "outgoingMessage") && this.groupChatMessage
+          !get(message, "outgoingMessage") &&
+          (this.groupChatMessage
             ? get(message, "originatorUri")
-            : get(connection, "remoteNeedUri");
+            : get(connection, "remoteNeedUri"));
         const relevantPersonaUri =
           relevantNeedUri && getIn(allNeeds, [relevantNeedUri, "heldBy"]);
         const personaName =
