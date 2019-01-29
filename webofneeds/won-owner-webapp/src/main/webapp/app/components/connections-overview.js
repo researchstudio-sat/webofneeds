1826539f86c1cba9b8c4c888a0b117a69e997f92
quasarchimaere <sudafabian@gmail.com>
2019-01-29 14:59:47 +0100

    Enhances performance of receivedMessagesReadPresent performance

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/connections-overview.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/connections-overview.js
@@ -300,8 +300,9 @@ function genComponentConf() {
             const receivedMessages = messages.filter(
               msg => !msg.get("outgoingMessage")
             );
-            const receivedMessagesReadPresent =
-              receivedMessages.filter(msg => !msg.get("unread")).size > 0;
+            const receivedMessagesReadPresent = receivedMessages.find(
+              msg => !msg.get("unread")
+            );
 
             if (!receivedMessagesReadPresent) {
               this.connections__showMoreMessages(conn.get("uri"), MESSAGECOUNT);
