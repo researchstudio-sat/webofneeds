6625d89c3dd6304a4995b9b96c0fc6a949faee15
quasarchimaere <sudafabian@gmail.com>
2019-01-31 14:02:04 +0100

    Adds loading and failedToLoad parts for view-template

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/create-post.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/create-post.js
@@ -8,13 +8,12 @@ import "ng-redux";
 import labelledHrModule from "./labelled-hr.js";
 import createIsseeksModule from "./create-isseeks.js";
 import publishButtonModule from "./publish-button.js";
-import { get, attach, delay } from "../utils.js";
+import { get, attach, delay, getIn } from "../utils.js";
 import { actionCreators } from "../actions/actions.js";
 import { connect2Redux } from "../won-utils.js";
-import {
-  selectIsConnected,
-  getUseCaseFromRoute,
-} from "../selectors/general-selectors.js";
+import * as generalSelectors from "../selectors/general-selectors.js";
+import * as needUtils from "../need-utils.js";
+import * as processSelectors from "../selectors/process-selectors.js";
 
 import { useCases } from "useCaseDefinitions";
 
@@ -31,7 +30,7 @@ function genComponentConf() {
   const template = `
         <div class="cp__header">
             <a class="cp__header__back clickable"
-                ng-click="self.router__stateGoCurrent({useCase: undefined})">
+                ng-click="self.router__back()">
                 <svg style="--local-primary:var(--won-primary-color);"
                     class="cp__header__back__icon show-in-responsive">
                     <use xlink:href="#ico36_backarrow" href="#ico36_backarrow"></use>
@@ -47,17 +46,39 @@ function genComponentConf() {
                 style="--local-primary:var(--won-primary-color);">
                     <use xlink:href="{{self.useCase['icon']}}" href="{{self.useCase['icon']}}"></use>
             </svg>
-            <span class="cp__header__title">{{self.useCase.label}}</span>
+            <span class="cp__header__title" ng-if="!self.isCreateFromNeed">{{self.useCase.label}}</span>
+            <span class="cp__header__title" ng-if="self.isCreateFromNeed">Duplicate from '{{self.useCase.label}}'</span>
         </div>
         <div class="cp__content">
-
+            <div class="cp__content__loading" ng-if="!self.showCreateInput && self.isFromNeedLoading">
+                <svg class="cp__content__loading__spinner hspinner">
+                    <use xlink:href="#ico_loading_anim" href="#ico_loading_anim"></use>
+                </svg>
+                <span class="cp__content__loading__label">
+                    Loading...
+                </span>
+            </div>
+            <div class="cp__content__failed" ng-if="!self.showCreateInput && self.hasFromNeedFailedToLoad">
+                <svg class="cp__content__failed__icon">
+                    <use xlink:href="#ico16_indicator_error" href="#ico16_indicator_error"></use>
+                </svg>
+                <span class="cp__content__failed__label">
+                    Failed To Load - Need might have been deleted
+                </span>
+                <div class="cp__content__failed__actions">
+                    <button class="cp__content__failed__actions__button red won-button--outlined thin"
+                        ng-click="self.needs__fetchUnloadedNeed(self.fromNeedUri)()">
+                        Try Reload
+                    </button>
+                </div>
+            </div>
             <!-- ADD TITLE AND DETAILS -->
             <div class="cp__content__branchheader"
-              ng-if="self.useCase.details">
+              ng-if="self.showCreateInput && self.useCase.details">
               Your offer or self description
             </div>
             <won-create-isseeks 
-                ng-if="self.useCase.details"
+                ng-if="self.showCreateInput && self.useCase.details"
                 is-or-seeks="::'Description'"
                 detail-list="self.useCase.details"
                 initial-draft="self.useCase.draft.content"
@@ -65,11 +86,11 @@ function genComponentConf() {
                 on-scroll="::self.scrollIntoView(element)">
             </won-create-isseeks>
             <div class="cp__content__branchheader"
-              ng-if="self.useCase.seeksDetails">
+              ng-if="self.showCreateInput && self.useCase.seeksDetails">
               Looking For
             </div>
             <won-create-isseeks 
-                ng-if="self.useCase.seeksDetails" 
+                ng-if="self.showCreateInput && self.useCase.seeksDetails"
                 is-or-seeks="::'Search'" 
                 detail-list="self.useCase.seeksDetails"
                 initial-draft="self.useCase.draft.seeks"
@@ -79,7 +100,7 @@ function genComponentConf() {
         </div>
         <div class="cp__footer" >
             <won-labelled-hr label="::'done?'" class="cp__footer__labelledhr"></won-labelled-hr>
-            <won-publish-button on-publish="self.publish(persona)" is-valid="self.isValid()"></won-publish-button>
+            <won-publish-button on-publish="self.publish(persona)" is-valid="self.isValid()" ng-if="self.showCreateInput"></won-publish-button>
         </div>
     `;
 
@@ -98,22 +119,119 @@ function genComponentConf() {
       this.isNew = true;
 
       const selectFromState = state => {
-        const useCaseString = getUseCaseFromRoute(state);
+        const fromNeedUri = generalSelectors.getFromNeedUriFromRoute(state);
+        const mode = generalSelectors.getModeFromRoute(state);
+        let fromNeed;
+
+        let useCaseString;
+        let useCase;
+
+        const isCreateFromNeed = !!(fromNeedUri && mode === "DUPLICATE");
+
+        let isFromNeedLoading = false;
+        let isFromNeedToLoad = false;
+        let hasFromNeedFailedToLoad = false;
+
+        if (isCreateFromNeed) {
+          (isFromNeedLoading = processSelectors.isNeedLoading(
+            state,
+            fromNeedUri
+          )),
+            (isFromNeedToLoad = processSelectors.isNeedToLoad(
+              state,
+              fromNeedUri
+            )),
+            (hasFromNeedFailedToLoad = processSelectors.hasNeedFailedToLoad(
+              state,
+              fromNeedUri
+            )),
+            (fromNeed =
+              !isFromNeedLoading &&
+              !isFromNeedToLoad &&
+              !hasFromNeedFailedToLoad &&
+              getIn(state, ["needs", fromNeedUri]));
+
+          if (fromNeed) {
+            //TODO: DETERMINE THE CORRECT USECASE
+            useCaseString = "customUseCase";
+            useCase = selectUseCaseFrom(useCaseString, useCases);
+
+            const fromNeedContent = get(fromNeed, "content");
+            const fromNeedSeeks = get(fromNeed, "seeks");
+            const facetsReset = needUtils.getFacetsWithKeysReset(fromNeed);
+            const defaultFacetReset = needUtils.getDefaultFacetWithKeyReset(
+              fromNeed
+            );
+            const seeksFacetsReset = needUtils.getSeeksFacetsWithKeysReset(
+              fromNeed
+            );
+            const seeksDefaultFacetReset = needUtils.getSeeksDefaultFacetWithKeyReset(
+              fromNeed
+            );
+
+            if (fromNeedContent) {
+              useCase.draft.content = fromNeedContent.toJS();
+            }
+            if (fromNeedSeeks) {
+              useCase.draft.seeks = fromNeedSeeks.toJS();
+            }
+
+            if (facetsReset) {
+              useCase.draft.content.facets = facetsReset.toJS();
+            }
+            if (defaultFacetReset) {
+              useCase.draft.content.defaultFacet = defaultFacetReset.toJS();
+            }
+
+            if (seeksFacetsReset) {
+              useCase.draft.seeks.facets = seeksFacetsReset.toJS();
+            }
+            if (seeksDefaultFacetReset) {
+              useCase.draft.seeks.defaultFacet = seeksDefaultFacetReset.toJS();
+            }
+          }
+        } else {
+          useCaseString = generalSelectors.getUseCaseFromRoute(state);
+          useCase = selectUseCaseFrom(useCaseString, useCases);
+        }
 
         return {
           processingPublish: state.getIn(["process", "processingPublish"]),
-          connectionHasBeenLost: !selectIsConnected(state),
+          connectionHasBeenLost: !generalSelectors.selectIsConnected(state),
           useCaseString,
-          useCase: selectUseCaseFrom(useCaseString, useCases),
+          useCase,
+          fromNeed,
+          fromNeedUri,
+          isCreateFromNeed,
+          isFromNeedLoading,
+          isFromNeedToLoad,
+          hasFromNeedFailedToLoad,
+          showCreateInput:
+            useCase &&
+            !(
+              isCreateFromNeed &&
+              (isFromNeedLoading || hasFromNeedFailedToLoad || isFromNeedToLoad)
+            ),
         };
       };
-
-      delay(0).then(() => {
-        this.loadInitialDraft();
-      });
-
       // Using actionCreators like this means that every action defined there is available in the template.
       connect2Redux(selectFromState, actionCreators, [], this);
+
+      this.$scope.$watch(
+        () => this.isFromNeedToLoad,
+        () => delay(0).then(() => this.ensureFromNeedIsLoaded())
+      );
+
+      this.$scope.$watch(
+        () => this.showCreateInput,
+        () => delay(0).then(() => this.loadInitialDraft())
+      );
+    }
+
+    ensureFromNeedIsLoaded() {
+      if (this.isFromNeedToLoad) {
+        this.needs__fetchUnloadedNeed(this.fromNeedUri);
+      }
     }
 
     onResize() {
@@ -169,12 +287,7 @@ function genComponentConf() {
     }
 
     loadInitialDraft() {
-      // just to be sure this is loaded already
-      if (!this.useCase) {
-        selectUseCaseFrom(this.useCaseString, useCases);
-      }
-
-      if (this.useCase && this.useCase.draft) {
+      if (this.showCreateInput && this.useCase.draft) {
         // deep clone of draft
         this.draftObject = JSON.parse(JSON.stringify(this.useCase.draft));
       }
