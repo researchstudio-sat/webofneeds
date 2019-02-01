61aefb8562a2906e5a2241cbdef0d2d63c71f8b9
quasarchimaere <quasarchimaere@users.noreply.github.com>
2019-02-01 10:08:47 +0100

    fixes #2717
    fi

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/connections/connections.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/connections/connections.js
@@ -148,6 +148,7 @@ class ConnectionsController {
         showNeedOverlay: !!viewNeedUri,
         viewNeedUri,
         hideListSideInResponsive:
+          !showCreateFromPost ||
           !hasOwnedNeeds ||
           showGroupPostAdministration ||
           selectedConnection ||
