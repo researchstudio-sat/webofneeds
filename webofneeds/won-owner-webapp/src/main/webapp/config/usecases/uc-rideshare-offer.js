01c6afb045e96f5e514299e12c756e97bdbaeb87
quasarchimaere <sudafabian@gmail.com>
2019-02-06 14:54:51 +0100

    Adapt query for uc-rent-rideshare-offer

--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/uc-rideshare-offer.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/uc-rideshare-offer.js
@@ -6,7 +6,6 @@ import { findLatestIntervallEndInJsonLdOrNowAndAddMillis } from "../../app/won-u
 import won from "../../app/won-es6.js";
 import { getIn, isValidDate } from "../../app/utils.js";
 import {
-  filterInVicinity,
   filterAboutTime,
   concatenateFilters,
   sparqlQuery,
@@ -41,55 +40,166 @@ export const rideShareOffer = {
     const toLocation = getIn(draft, ["content", "travelAction", "toLocation"]);
     const fromTime = getIn(draft, ["content", "fromDatetime"]);
 
-    const baseFilter = {
-      prefixes: {
-        won: won.defaultContext["won"],
-        s: won.defaultContext["s"],
-      },
-      operations: [
-        `${resultName} a won:Need.`,
-        `${resultName} won:seeks ?seeks.`,
-        fromLocation && `?seeks won:travelAction/s:fromLocation ?fromLocation.`,
-        toLocation && `?seeks won:travelAction/s:toLocation ?toLocation.`,
-        isValidDate(fromTime) && `?seeks s:validFrom ?starttime.`,
-      ],
-    };
+    let filter;
+    if (
+      fromLocation &&
+      fromLocation.lat &&
+      fromLocation.lng &&
+      toLocation &&
+      toLocation.lat &&
+      toLocation.lng
+    ) {
+      const baseFilter = {
+        prefixes: {
+          won: won.defaultContext["won"],
+          s: won.defaultContext["s"],
+          geo: "http://www.bigdata.com/rdf/geospatial#",
+          xsd: "http://www.w3.org/2001/XMLSchema#",
+        },
+        operations: [
+          `${resultName} a won:Need.`,
+          `${resultName} won:seeks ?seeks.`,
+          isValidDate(fromTime) && `?seeks s:validFrom ?starttime.`,
+          `?seeks won:travelAction/s:fromLocation ?fromLocation.`,
+          `?seeks won:travelAction/s:toLocation ?toLocation.`,
+          "?fromLocation s:geo ?fromLocation_geo.",
+          "?fromLocation_geo s:latitude ?fromLocation_lat;",
+          "s:longitude ?fromLocation_lon;",
+          `bind (abs(xsd:decimal(?fromLocation_lat) - ${
+            fromLocation.lat
+          }) as ?fromLatDiffRaw)`,
+          `bind (abs(xsd:decimal(?fromLocation_lon) - ${
+            fromLocation.lng
+          }) as ?fromLonDiff)`,
+          "bind (if ( ?fromLatDiffRaw > 180, 360 - ?fromLatDiffRaw, ?fromLatDiffRaw ) as ?fromLatDiff)",
+          "bind ( ?fromLatDiff * ?fromLatDiff + ?fromLonDiff * ?fromLonDiff as ?fromLocation_geoDistanceScore)",
+          "?toLocation s:geo ?toLocation_geo.",
+          "?toLocation_geo s:latitude ?toLocation_lat;",
+          "s:longitude ?toLocation_lon;",
+          `bind (abs(xsd:decimal(?toLocation_lat) - ${
+            toLocation.lat
+          }) as ?toLatDiffRaw)`,
+          `bind (abs(xsd:decimal(?toLocation_lon) - ${
+            toLocation.lng
+          }) as ?toLonDiff)`,
+          "bind (if ( ?toLatDiffRaw > 180, 360 - ?toLatDiffRaw, ?toLatDiffRaw ) as ?toLatDiff)",
+          "bind ( ?toLatDiff * ?toLatDiff + ?toLonDiff * ?toLonDiff as ?toLocation_geoDistanceScore)",
+          "bind (?toLocation_geoDistanceScore + ?fromLocation_geoDistanceScore as ?distScore)",
+        ],
+      };
 
-    const fromLocationFilter = filterInVicinity(
-      "?fromLocation",
-      fromLocation,
-      /*radius=*/ 5
-    );
-    const toLocationFilter = filterInVicinity(
-      "?toLocation",
-      toLocation,
-      /*radius=*/ 5
-    );
+      const timeFilter = filterAboutTime(
+        "?starttime",
+        fromTime,
+        12 /* hours before and after*/
+      );
 
-    const timeFilter = filterAboutTime(
-      "?starttime",
-      fromTime,
-      12 /* hours before and after*/
-    );
+      filter = concatenateFilters([baseFilter, timeFilter]);
+    } else if (fromLocation && fromLocation.lat && fromLocation.lng) {
+      const baseFilter = {
+        prefixes: {
+          won: won.defaultContext["won"],
+          s: won.defaultContext["s"],
+          geo: "http://www.bigdata.com/rdf/geospatial#",
+          xsd: "http://www.w3.org/2001/XMLSchema#",
+        },
+        operations: [
+          `${resultName} a won:Need.`,
+          `${resultName} won:seeks ?seeks.`,
+          isValidDate(fromTime) && `?seeks s:validFrom ?starttime.`,
+          `?seeks won:travelAction/s:fromLocation ?fromLocation.`,
+          "?fromLocation s:geo ?fromLocation_geo.",
+          "?fromLocation_geo s:latitude ?fromLocation_lat;",
+          "s:longitude ?fromLocation_lon;",
+          `bind (abs(xsd:decimal(?fromLocation_lat) - ${
+            fromLocation.lat
+          }) as ?fromLatDiffRaw)`,
+          `bind (abs(xsd:decimal(?fromLocation_lon) - ${
+            fromLocation.lng
+          }) as ?fromLonDiff)`,
+          "bind (if ( ?fromLatDiffRaw > 180, 360 - ?fromLatDiffRaw, ?fromLatDiffRaw ) as ?fromLatDiff)",
+          "bind ( ?fromLatDiff * ?fromLatDiff + ?fromLonDiff * ?fromLonDiff as ?fromLocation_geoDistanceScore)",
+          "bind (?fromLocation_geoDistanceScore as ?distScore)",
+        ],
+      };
 
-    const concatenatedFilter = concatenateFilters([
-      baseFilter,
-      fromLocationFilter,
-      toLocationFilter,
-      timeFilter,
-    ]);
+      const timeFilter = filterAboutTime(
+        "?starttime",
+        fromTime,
+        12 /* hours before and after*/
+      );
 
-    return sparqlQuery({
-      prefixes: concatenatedFilter.prefixes,
+      filter = concatenateFilters([baseFilter, timeFilter]);
+    } else if (toLocation && toLocation.lat && toLocation.lng) {
+      const baseFilter = {
+        prefixes: {
+          won: won.defaultContext["won"],
+          s: won.defaultContext["s"],
+          geo: "http://www.bigdata.com/rdf/geospatial#",
+          xsd: "http://www.w3.org/2001/XMLSchema#",
+        },
+        operations: [
+          `${resultName} a won:Need.`,
+          `${resultName} won:seeks ?seeks.`,
+          isValidDate(fromTime) && `?seeks s:validFrom ?starttime.`,
+          `?seeks won:travelAction/s:toLocation ?toLocation.`,
+          "?toLocation s:geo ?toLocation_geo.",
+          "?toLocation_geo s:latitude ?toLocation_lat;",
+          "s:longitude ?toLocation_lon;",
+          `bind (abs(xsd:decimal(?toLocation_lat) - ${
+            toLocation.lat
+          }) as ?toLatDiffRaw)`,
+          `bind (abs(xsd:decimal(?toLocation_lon) - ${
+            toLocation.lng
+          }) as ?toLonDiff)`,
+          "bind (if ( ?toLatDiffRaw > 180, 360 - ?toLatDiffRaw, ?toLatDiffRaw ) as ?toLatDiff)",
+          "bind ( ?toLatDiff * ?toLatDiff + ?toLonDiff * ?toLonDiff as ?toLocation_geoDistanceScore)",
+          "bind (?toLocation_geoDistanceScore as ?distScore)",
+        ],
+      };
+
+      const timeFilter = filterAboutTime(
+        "?starttime",
+        fromTime,
+        12 /* hours before and after*/
+      );
+
+      filter = concatenateFilters([baseFilter, timeFilter]);
+    } else {
+      const baseFilter = {
+        prefixes: {
+          won: won.defaultContext["won"],
+          s: won.defaultContext["s"],
+        },
+        operations: [
+          `${resultName} a won:Need.`,
+          `${resultName} won:seeks ?seeks.`,
+          isValidDate(fromTime) && `?seeks s:validFrom ?starttime.`,
+        ],
+      };
+
+      const timeFilter = filterAboutTime(
+        "?starttime",
+        fromTime,
+        12 /* hours before and after*/
+      );
+
+      filter = concatenateFilters([baseFilter, timeFilter]);
+    }
+
+    const generatedQuery = sparqlQuery({
+      prefixes: filter.prefixes,
       distinct: true,
       variables: [resultName],
-      where: concatenatedFilter.operations,
+      where: filter.operations,
       orderBy: [
         {
           order: "ASC",
-          variable: "?fromLocation_geoDistance",
+          variable: "?distScore",
         },
       ],
     });
+
+    return generatedQuery;
   },
 };
