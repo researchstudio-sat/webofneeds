1993ee2aad0b3fc14b7f592a83bd7e331447cd47
quasarchimaere <sudafabian@gmail.com>
2019-02-11 16:05:22 +0100

    Fixes #2650

--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-artists.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-artists.js
@@ -1,6 +1,8 @@
 import { musicianSearch } from "./uc-musician-search.js";
 import { bandSearch } from "./uc-band-search.js";
 import { serviceOffer } from "./uc-service-offer.js";
+import { rehearsalRoomOffer } from "./uc-rehearsal-room-offer.js";
+import { rehearsalRoomSearch } from "./uc-rehearsal-room-search.js";
 
 export const artistGroup = {
   identifier: "artistgroup",
@@ -10,5 +12,7 @@ export const artistGroup = {
     bandSearch: bandSearch,
     musicianSearch: musicianSearch,
     serviceOffer: serviceOffer,
+    rehearsalRoomSearch: rehearsalRoomSearch,
+    rehearsalRoomOffer: rehearsalRoomOffer,
   },
 };
