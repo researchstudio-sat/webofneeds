d85873177f314461960dfa8862dac38cdc7ce381
quasarchimaere <sudafabian@gmail.com>
2019-02-11 08:15:42 +0100

    Removes commented import

--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-cycling.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-cycling.js
@@ -1,6 +1,5 @@
 import { cyclingInterest } from "./uc-cycling-interest.js";
 import { cyclingPlan } from "./uc-cycling-plan.js";
-//import { cyclingPlan } from "./uc-cycling-plan.js";
 
 export const cyclingGroup = {
   identifier: "cyclingGroup",
