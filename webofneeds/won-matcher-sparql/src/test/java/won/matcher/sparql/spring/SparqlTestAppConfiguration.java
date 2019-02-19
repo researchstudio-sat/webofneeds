5657d857a5d884a95c1dcb5f301d845e88ca36cf
Kristina Weinberger <kristina.weinberger@researchstudio.at>
2019-02-19 19:01:34 +0100

    Removed unused config files

--- webofneeds/won-matcher-sparql/src/test/java/won/matcher/sparql/spring/SparqlTestAppConfiguration.java
+++ webofneeds/won-matcher-sparql/src/test/java/won/matcher/sparql/spring/SparqlTestAppConfiguration.java
@@ -12,9 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 @ImportResource({"classpath:spring/component/sparqlMatcherEvaluation.xml",
                  "classpath:spring/component/sparqlMatcherEvaluation.xml"})
 @PropertySource({"file:${WON_CONFIG_DIR}/matcher-sparql.properties",
-                 "file:${WON_CONFIG_DIR}/cluster-node.properties",
-                 "file:${WON_CONFIG_DIR}/mail-dir-bot.properties",
-                 "file:${WON_CONFIG_DIR}/need-dir-bot.properties"})
+                 "file:${WON_CONFIG_DIR}/cluster-node.properties"})
 @ComponentScan({"won.matcher.service.common.config", "won.matcher.service.common.service.http",
                 "won.matcher.sparql.config", "won.matcher.sparql.index", "won.matcher.sparql.hints",
                 "won.matcher.sparql.evaluation", "won.matcher.sparql.query"})
