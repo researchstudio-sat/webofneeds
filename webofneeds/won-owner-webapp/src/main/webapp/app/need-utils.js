07a23e2cc16b7bfba5c0f6b04354e7c519293bdb
quasarchimaere <sudafabian@gmail.com>
2019-03-28 15:50:40 +0100

    Adds utility function to determine if a need isEditable or not

--- webofneeds/won-owner-webapp/src/main/webapp/app/need-utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/need-utils.js
@@ -89,6 +89,29 @@ export function isUsableAsTemplate(need) {
 }
 
 /**
+ * This checks if the need is allowed to be edited,
+ * it is only allowed if the need exists, and if it is OWNED and not one of the following:
+ * - DirectResponseNeed
+ * - WhatsAroundNeed
+ * - WhatsNewNeed
+ * - SearchNeed
+ * @param need
+ * @returns {*|boolean}
+ */
+export function isEditable(need) {
+  return (
+    need &&
+    isOwned(need) &&
+    !(
+      isDirectResponseNeed(need) ||
+      isWhatsAroundNeed(need) ||
+      isWhatsNewNeed(need) ||
+      isSearchNeed(need)
+    )
+  );
+}
+
+/**
  * Determines if a given need is a DirectResponse-Need
  * @param need
  * @returns {*|boolean}
