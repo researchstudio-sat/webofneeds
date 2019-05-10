fb57f60477e16408a7211ae2a12236e99974f236
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2019-05-10 16:34:31 +0200

    Continue work on ontology documentation generation

--- webofneeds/won-vocab/src/main/resources/utils/generate-docs/widoco.sh
+++ webofneeds/won-vocab/src/main/resources/utils/generate-docs/widoco.sh
@@ -23,47 +23,76 @@ then
 	only_ont=$2
 fi
 
-output_base=${script_path}/../../../../../../doc
-cd "${output_base}"
-ontology_root=${script_path}/../../
+output_base=${script_path}/../../../../../pub
+ontology_root=${script_path}/../../ontology
 echo "(re)generating ontology documentation in: ${output_base}"
-mkdir -p ${output_base}
-mkdir -p ${output_base}/ext
-onts=(won-core won-message won-agreement won-modification)
-rewrite_base="https://raw.githubusercontent.com/researchstudio-sat/webofneeds/master/webofneeds/won-vocab/src/main/resources/ontology/"
+onts=(core message agreement modification)
+rewrite_base="https://researchstudio-sat.github.io/webofneeds/ontologies"
 ext_onts=(buddy chat group hold review schema)
 
 
-for ont in ${onts[@]} 
-do
+function generate_for_ontology() {
+	ont=$1
+	is_ext=$2
 	if [[ ! -z ${only_ont} && ${only_ont} != ${ont} ]]
 	then
 		# the user specified only_ont and it's not the current one. skip.
 		echo "Only generating docs for ontology ${only_ont}. Skipping ${ont} (main)"
-		continue
+		return
 	fi
-	output_path="${ont}"
-	mkdir -p "${output_path}"	
+	if [[ ${is_ext} = true ]]
+	then
+		output_path="ext/${ont}"
+		ont_src_dir="${ontology_root}/ext"
+		ont_file_stem="won-ext-${ont}"
+	else 
+		output_path="${ont}"
+		ont_src_dir="${ontology_root}"
+		ont_file_stem="won-${ont}"
+	fi
+	ont_widoco_dir="${ont_src_dir}/${ont_file_stem}-widoco"
+	ont_file_name="${ont_file_stem}.ttl"
+	ont_file="${ont_src_dir}/${ont_file_name}"
+	config_file="${ont_widoco_dir}/widoco.conf"
+	if [[ -r ${config_file} ]]
+	then
+		config_file_opt="-confFile ${config_file}"
+	fi
+	mkdir -p "${output_path}"
 	echo "generating documentation for ${ont} in ${output_path}" 
-	java -jar ${widoco_jar} -ontFile "${ontology_root}/${ont}.ttl" -outFolder "${output_path}" \
-		-oops -rewriteAll -htaccess -webVowl \
-		-licensius -rewriteBase "${rewrite_base}" 
-	java -jar ${widoco_jar} -ontFile "${ontology_root}/${ont}.ttl" -outFolder "${output_path}" -crossRef -rewriteAll
+	java -jar ${widoco_jar} -ontFile "${ont_file}" -outFolder "${output_path}" \
+		-oops -rewriteAll -htaccess -rewriteBase '' -webVowl \
+		-licensius ${config_file_opt}
+	java -jar ${widoco_jar} -ontFile "${ont_file}" -outFolder "${output_path}" -crossRef -rewriteAll
+	cp "${output_path}/index-en.html" "${output_path}/index.html"
+	for section_html_file in ${output_path}/sections/*.html
+	do
+		section_html_filename=${section_html_file##*/}
+		section_md_filename=${section_html_filename%.html}.md
+		section_md_file=${ont_widoco_dir}/${section_md_filename}
+		if [[ -r ${section_md_file} ]]
+		then
+			echo -e "replacing template text in ${section_html_filename} with content of ${section_md_file}"
+			perl -i -0pe "s@(?<=<span class=\"markdown\">)[^>]+(?=</span>)@\`cat ${section_md_file}\`@e" ${section_html_file}
+		fi
+	done
+	if [[ -d ${ont_widoco_dir}/img ]]
+	then
+		echo "copying images folder"
+		cp -a ${ont_widoco_dir}/img ${output_path}
+	fi
+}
+
+mkdir -p ${output_base}
+mkdir -p ${output_base}/ext
+cd "${output_base}"
+
+for ont in ${onts[@]} 
+do
+	generate_for_ontology ${ont} false
 done
 
 for ont in ${ext_onts[@]} 
 do
-	if [[ ! -z ${only_ont} && ${only_ont} != ${ont} ]]
-	then
-		# the user specified only_ont and it's not the current one. skip.
-		echo "Only generating docs for ontology ${only_ont}. Skipping ${ont} (ext)"
-		continue
-	fi
-	output_path="ext/won-ext-${ont}"
-	mkdir -p "${output_path}"	
-	echo "generating documentation for ${ont} in ${output_path}" 
-	java -jar ${widoco_jar} -ontFile "${ontology_root}/ext/won-ext-${ont}.ttl" -outFolder "${output_path}" \
-		-oops -rewriteAll -htaccess -webVowl \
-		-licensius  -doNotDisplaySerializations -rewriteBase "${rewrite_base}" 
-	java -jar ${widoco_jar} -ontFile "${ontology_root}/ext/won-ext-${ont}.ttl" -outFolder "${output_path}" -crossRef -rewriteAll
+	generate_for_ontology ${ont} true
 done
