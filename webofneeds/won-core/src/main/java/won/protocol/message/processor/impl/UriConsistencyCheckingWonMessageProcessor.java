015b09c4326221a27482e6c5a67f7cf0a8b98fa1
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2019-05-19 16:10:06 +0200

    Fix uri check

--- webofneeds/won-core/src/main/java/won/protocol/message/processor/impl/UriConsistencyCheckingWonMessageProcessor.java
+++ webofneeds/won-core/src/main/java/won/protocol/message/processor/impl/UriConsistencyCheckingWonMessageProcessor.java
@@ -17,8 +17,9 @@ import won.protocol.service.WonNodeInformationService;
 import won.protocol.util.WonRdfUtils;
 
 /**
- * Check if the event, graph or atom uri is well-formed according the node's
- * domain and its path conventions User: ypanchenko Date: 23.04.2015
+ * Checks if the event, graph or atom uri is well-formed according the node's
+ * domain and its path conventions. Used on incoming messages. User: ypanchenko
+ * Date: 23.04.2015
  */
 public class UriConsistencyCheckingWonMessageProcessor implements WonMessageProcessor {
     @Autowired
@@ -27,7 +28,7 @@ public class UriConsistencyCheckingWonMessageProcessor implements WonMessageProc
 
     @Override
     public WonMessage process(final WonMessage message) throws UriAlreadyInUseException {
-        // extract info about own, sender and receiver nodes:
+        // extract info about local, remote and receiver nodes:
         URI senderNode = message.getSenderNodeURI();
         URI recipientNode = message.getRecipientNodeURI();
         WonNodeInfo senderNodeInfo = null;
@@ -40,43 +41,47 @@ public class UriConsistencyCheckingWonMessageProcessor implements WonMessageProc
         if (recipientNode != null) {
             recipientNodeInfo = wonNodeInformationService.getWonNodeInformation(recipientNode);
         }
-        WonNodeInfo ownNodeInfo = null;
+        WonNodeInfo localNodeInfo = null;
+        WonNodeInfo remoteNodeInfo = null;
         URI msgUri = message.getMessageURI();
         if (msgUri.getScheme().equals(senderNode.getScheme())
                         && msgUri.getAuthority().equals(senderNode.getAuthority())) {
-            ownNodeInfo = senderNodeInfo;
+            localNodeInfo = senderNodeInfo;
+            remoteNodeInfo = recipientNodeInfo;
         } else if (msgUri.getScheme().equals(recipientNode.getScheme())
                         && msgUri.getAuthority().equals(recipientNode.getAuthority())) {
-            ownNodeInfo = recipientNodeInfo;
+            localNodeInfo = recipientNodeInfo;
+            remoteNodeInfo = senderNodeInfo;
         }
-        URI ownNode = URI.create(ownNodeInfo.getWonNodeURI());
+        URI localNode = URI.create(localNodeInfo.getWonNodeURI());
         // do checks for consistency between these nodes and message direction, as well
         // as atoms,
         // events and connection uris:
-        // my node should be either receiver or sender node
-        checkHasMyNode(message, ownNode);
+        // local node should be either receiver or sender node
+        checkHasNode(message, localNode);
         // Any message URI used must conform to a URI pattern specified by the
         // respective publishing service:
-        // Check that event URI corresponds to my pattern
-        checkLocalEventURI(message, ownNodeInfo);
+        // Check that event URI corresponds to local pattern
+        checkLocalEventURI(message, localNodeInfo);
         // Check that remote URI, if any, correspond to ?senderNode's event pattern
-        checkRemoteEventURI(message, senderNodeInfo);
-        // Check that atom URI for create_atom message corresponds to my pattern
-        checkCreateMsgAtomURI(message, ownNodeInfo);
+        checkRemoteEventURI(message, remoteNodeInfo);
+        // Check that atom URI for create_atom message corresponds to local pattern
+        checkCreateMsgAtomURI(message, localNodeInfo);
         // Specified sender-recipientAtom/Connection must conform to
         // sender-recipientNode
         // URI pattern
         checkSenders(senderNodeInfo, message);
         checkReceivers(recipientNodeInfo, message);
-        // Check that my node is sender or receiver node URI, depending on the message
+        // Check that local node is sender or receiver node URI, depending on the
+        // message
         // direction
-        checkDirection(message, ownNode);
+        checkDirection(message, localNode);
         return message;
     }
 
-    private void checkHasMyNode(final WonMessage message, URI ownNode) {
-        if (!ownNode.equals(message.getSenderNodeURI()) && !ownNode.equals(message.getRecipientNodeURI())) {
-            throw new UriNodePathException("neither sender nor receiver is " + ownNode);
+    private void checkHasNode(final WonMessage message, URI localNode) {
+        if (!localNode.equals(message.getSenderNodeURI()) && !localNode.equals(message.getRecipientNodeURI())) {
+            throw new UriNodePathException("neither sender nor receiver is " + localNode);
         }
     }
 
@@ -88,46 +93,45 @@ public class UriConsistencyCheckingWonMessageProcessor implements WonMessageProc
         checkNodeConformance(senderNodeInfo, message.getSenderAtomURI(), message.getSenderURI(), null);
     }
 
-    private void checkDirection(final WonMessage message, final URI ownNode) {
+    private void checkDirection(final WonMessage message, final URI localNode) {
         WonMessageDirection direction = message.getEnvelopeType();
         URI recipientNode = message.getRecipientNodeURI();
         URI senderNode = message.getSenderNodeURI();
-        URI node;
         switch (direction) {
             case FROM_EXTERNAL:
-                // my node should be a receiver node
-                if (!ownNode.equals(recipientNode)) {
-                    throw new UriNodePathException(recipientNode + " is expected to be " + ownNode);
+                // local node should be a receiver node
+                if (!localNode.equals(recipientNode)) {
+                    throw new UriNodePathException(recipientNode + " is expected to be " + localNode);
                 }
                 break;
             case FROM_OWNER:
-                // my node should be a sender node
-                if (!ownNode.equals(senderNode)) {
-                    throw new UriNodePathException(senderNode + " is expected to be " + ownNode);
+                // local node should be a sender node
+                if (!localNode.equals(senderNode)) {
+                    throw new UriNodePathException(senderNode + " is expected to be " + localNode);
                 }
                 break;
             case FROM_SYSTEM:
-                // my node should be a sender node
-                if (!ownNode.equals(senderNode)) {
-                    throw new UriNodePathException(senderNode + " is expected to be " + ownNode);
+                // local node should be a sender node
+                if (!localNode.equals(senderNode)) {
+                    throw new UriNodePathException(senderNode + " is expected to be " + localNode);
                 }
                 break;
         }
     }
 
-    private void checkLocalEventURI(final WonMessage message, WonNodeInfo ownNodeInfo) {
-        checkNodeConformance(ownNodeInfo, null, null, message.getMessageURI());
+    private void checkLocalEventURI(final WonMessage message, WonNodeInfo localNodeInfo) {
+        checkNodeConformance(localNodeInfo, null, null, message.getMessageURI());
     }
 
-    private void checkRemoteEventURI(final WonMessage message, final WonNodeInfo senderNodeInfo) {
-        checkNodeConformance(senderNodeInfo, null, null, message.getCorrespondingRemoteMessageURI());
+    private void checkRemoteEventURI(final WonMessage message, final WonNodeInfo remoteNodeInfo) {
+        checkNodeConformance(remoteNodeInfo, null, null, message.getCorrespondingRemoteMessageURI());
     }
 
-    private void checkCreateMsgAtomURI(final WonMessage message, final WonNodeInfo ownNodeInfo) {
+    private void checkCreateMsgAtomURI(final WonMessage message, final WonNodeInfo localNodeInfo) {
         // check only for create message
         if (message.getMessageType() == WonMessageType.CREATE_ATOM) {
             URI atomURI = WonRdfUtils.AtomUtils.getAtomURI(message.getCompleteDataset());
-            checkNodeConformance(ownNodeInfo, atomURI, null, null);
+            checkNodeConformance(localNodeInfo, atomURI, null, null);
         }
         return;
     }
