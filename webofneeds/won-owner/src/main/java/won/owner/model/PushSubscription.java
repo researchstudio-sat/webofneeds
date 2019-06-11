9ad4438e957e107e7b7962a687b55e43f2ad77ca
quasarchimaere <sudafabian@gmail.com>
2019-06-11 16:33:44 +0200

    Fixes formatting

--- webofneeds/won-owner/src/main/java/won/owner/model/PushSubscription.java
+++ webofneeds/won-owner/src/main/java/won/owner/model/PushSubscription.java
@@ -31,8 +31,10 @@ public class PushSubscription {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         PushSubscription that = (PushSubscription) o;
         return endpoint.equals(that.endpoint);
     }
