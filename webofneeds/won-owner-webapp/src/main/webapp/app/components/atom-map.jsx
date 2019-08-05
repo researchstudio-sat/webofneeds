15e3bed61aedaa550c3dd1cde254839fdfd22507
quasarchimaere <sudafabian@gmail.com>
2019-08-05 11:50:51 +0200

    Reformat code

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/atom-map.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/atom-map.jsx
@@ -12,7 +12,6 @@ const currentLocationIcon = Leaflet.divIcon({
 });
 
 export default class WonAtomMap extends React.Component {
-
   render() {
     // TODO Impl Center and zoom to all markers (work with bounds: <Map ref="map" bounds={bounds} className="atom-map__mapmount" boundsOptions={{padding: [50, 50]}} zoom={zoom}>)
     // TODO Impl MarkerClusters see -> https://yuzhva.github.io/react-leaflet-markercluster/
@@ -21,62 +20,86 @@ export default class WonAtomMap extends React.Component {
     const currentLocation = this.props && this.props.currentLocation;
     const disableControls = !!(this.props && this.props.disableControls);
     const zoom = 13;
-    if(locations && locations.length > 0) {
-      const currentLocationTupel = currentLocation && [currentLocation.get("lat"), currentLocation.get("lng")];
-      const firstLocationTupel = locations[0] && [locations[0].get("lat"), locations[0].get("lng")];
+    if (locations && locations.length > 0) {
+      const currentLocationTupel = currentLocation && [
+        currentLocation.get("lat"),
+        currentLocation.get("lng"),
+      ];
+      const firstLocationTupel = locations[0] && [
+        locations[0].get("lat"),
+        locations[0].get("lng"),
+      ];
 
       const locationMarkers = locations.map((location, index) => {
-          const lat = location && location.get("lat");
-          const lng = location && location.get("lng");
-          if (lat != undefined && lng != undefined) {
-            const locationTupel = [lat, lng];
-            //We should use key here, but i do not know if we can find a unique key from the provided properties :-/
-            return <Marker key={locationTupel.toString() + "/" + index} position={locationTupel} />;
-          }
+        const lat = location && location.get("lat");
+        const lng = location && location.get("lng");
+        if (lat != undefined && lng != undefined) {
+          const locationTupel = [lat, lng];
+          //We should use key here, but i do not know if we can find a unique key from the provided properties :-/
+          return (
+            <Marker
+              key={locationTupel.toString() + "/" + index}
+              position={locationTupel}
+            />
+          );
+        }
       });
 
-      const currentLocationMarker = currentLocationTupel
-        ? <Marker position={currentLocationTupel} icon={currentLocationIcon}/>
-        : undefined;
+      const currentLocationMarker = currentLocationTupel ? (
+        <Marker position={currentLocationTupel} icon={currentLocationIcon} />
+      ) : (
+        undefined
+      );
 
       return (
-          <VisibilitySensor
-            partialVisibility={true}
-          >
-            {
-              ({isVisible}) => {
-                if(isVisible) {
-                  return (
-                    <Map ref="map" center={currentLocationTupel ? currentLocationTupel : firstLocationTupel} className="atom-map__mapmount" zoom={zoom} zoomControl={!disableControls}>
-                      <TileLayer
-                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://www.matchat.org/tile/{z}/{x}/{y}.png"
-                      />
-                      {locationMarkers}
-                      {currentLocationMarker}
-                    </Map>
-                  );
-                } else {
-                  return (
-                    <div className="atom-map__mapmount atom-map__mapmount--loading">
-                      <svg className="won-atom-map__spinner hspinner"><use xlinkHref="#ico_loading_anim" href="#ico_loading_anim"/></svg>
-                    </div>
-                  );
-                }
-              }
+        <VisibilitySensor partialVisibility={true}>
+          {({ isVisible }) => {
+            if (isVisible) {
+              return (
+                <Map
+                  ref="map"
+                  center={
+                    currentLocationTupel
+                      ? currentLocationTupel
+                      : firstLocationTupel
+                  }
+                  className="atom-map__mapmount"
+                  zoom={zoom}
+                  zoomControl={!disableControls}
+                >
+                  <TileLayer
+                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="https://www.matchat.org/tile/{z}/{x}/{y}.png"
+                  />
+                  {locationMarkers}
+                  {currentLocationMarker}
+                </Map>
+              );
+            } else {
+              return (
+                <div className="atom-map__mapmount atom-map__mapmount--loading">
+                  <svg className="won-atom-map__spinner hspinner">
+                    <use
+                      xlinkHref="#ico_loading_anim"
+                      href="#ico_loading_anim"
+                    />
+                  </svg>
+                </div>
+              );
             }
-          </VisibilitySensor>
+          }}
+        </VisibilitySensor>
       );
     }
     console.debug("render with no location(s)");
-    return <div/>;
+    return <div />;
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
     console.debug("componentDidUpdate");
-    var map = this.refs.map;
-    if(this.refs.map) {
-      var leafletElement = this.refs.map.leafletElement;
+    let map = this.refs.map;
+    if (this.refs.map) {
+      let leafletElement = this.refs.map.leafletElement;
       leafletElement && leafletElement.invalidateSize();
     }
   }
