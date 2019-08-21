e1907bd7eb817e76118a75c68fcd60d6f284b2fa
quasarchimaere <sudafabian@gmail.com>
2019-08-21 14:43:27 +0200

    Refactor pokemongympicker to react

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/details/picker/pokemon-gym-picker.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/details/picker/pokemon-gym-picker.jsx
@@ -6,11 +6,40 @@ import PropTypes from "prop-types";
 export default class PokemonGymPicker extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = this.initialValue || {};
   }
 
   render() {
-    return <pokemon-gym-picker>TODO: IMPL</pokemon-gym-picker>;
+    return (
+      <pokemon-gym-picker>
+        <label htmlFor="pgp__ex" className="pgp__label">
+          Gym Ex:
+        </label>
+        <input
+          type="checkbox"
+          id="pgp__ex"
+          className="pgp__ex"
+          value={this.state.ex}
+          onChange={this.updateEx.bind(this)}
+        />
+      </pokemon-gym-picker>
+    );
+  }
+
+  updateEx(event) {
+    this.setState({ ex: event.target.checked }, this.update.bind(this));
+  }
+  /**
+   * Checks validity and uses callback method
+   */
+  update() {
+    if (this.props.detail.isValid(this.state)) {
+      this.props.onUpdate({
+        value: this.state,
+      });
+    } else {
+      this.props.onUpdate({ value: undefined });
+    }
   }
 }
 PokemonGymPicker.propTypes = {
