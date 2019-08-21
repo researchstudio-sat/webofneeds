9e4abf67397aaa759d52d6f26b19315b0aa2d181
quasarchimaere <sudafabian@gmail.com>
2019-08-21 13:42:26 +0200

    Refactor datetime-picker

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/details/picker/datetime-picker.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/details/picker/datetime-picker.jsx
@@ -2,15 +2,49 @@ import React from "react";
 
 import "~/style/_datetimepicker.scss";
 import PropTypes from "prop-types";
+import DateTimePicker from "react-datetime-picker";
+import { isValidDate } from "../../../utils.js";
 
 export default class WonDatetimePicker extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      date: props.initialValue,
+    };
   }
 
   render() {
-    return <won-datetime-picker>TODO: IMPL</won-datetime-picker>;
+    return (
+      <won-datetime-picker>
+        <button
+          className="datetimep__button won-button--filled red"
+          onClick={this.currentDatetime.bind(this)}
+        >
+          Now
+        </button>
+        <DateTimePicker
+          onChange={this.onChange.bind(this)}
+          value={this.state.date}
+        />
+      </won-datetime-picker>
+    );
+  }
+
+  onChange(date) {
+    this.setState({ date: date }, this.update.bind(this));
+    console.debug("Date: ", date);
+  }
+
+  currentDatetime() {
+    this.setState({ date: new Date() }, this.update.bind(this));
+  }
+
+  update() {
+    if (isValidDate(this.state.date)) {
+      this.props.onUpdate({ value: this.state.date });
+    } else {
+      this.props.onUpdate({ value: undefined });
+    }
   }
 }
 WonDatetimePicker.propTypes = {
