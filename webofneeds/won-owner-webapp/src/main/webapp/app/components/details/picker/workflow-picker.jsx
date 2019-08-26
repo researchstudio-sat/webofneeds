bee91254e0a6fe7853af7d11b9420ac2e9ca6d27
quasarchimaere <sudafabian@gmail.com>
2019-08-26 12:51:53 +0200

    Implemented workflowpicker/viewer removed ability to view workflows (this is really not that important anyway) and removed unneeded dependencies

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/details/picker/workflow-picker.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/details/picker/workflow-picker.jsx
@@ -2,19 +2,66 @@ import React from "react";
 
 import "~/style/_workflowpicker.scss";
 import PropTypes from "prop-types";
+import WonFileDropzone from "../../file-dropzone";
 
 export default class WonWorkflowPicker extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      addedWorkflow: props.initialValue,
+    };
+    this.update = this.update.bind(this);
+    this.updateWorkflow = this.updateWorkflow.bind(this);
+    this.removeWorkflow = this.removeWorkflow.bind(this);
   }
 
   render() {
-    return <won-workflow-picker>TODO: IMPL</won-workflow-picker>;
+    return (
+      <won-workflow-picker>
+        {this.state.addedWorkflow ? (
+          <div className="workflowp__preview">
+            <div className="workflowp__preview__label clickable">
+              {this.state.addedWorkflow.name}
+            </div>
+            <svg
+              className="workflowp__preview__remove"
+              onClick={this.removeWorkflow}
+            >
+              <use xlinkHref="#ico36_close" href="#ico36_close" />
+            </svg>
+            <svg className="workflowp__preview__typeicon">
+              <use
+                xlinkHref="#ico36_uc_transport_demand"
+                href="#ico36_uc_transport_demand"
+              />
+            </svg>
+          </div>
+        ) : (
+          <WonFileDropzone
+            onFilePicked={this.updateWorkflow}
+            accepts={this.props.detail.accepts}
+            multiSelect={false}
+          />
+        )}
+      </won-workflow-picker>
+    );
+  }
+
+  updateWorkflow({ file }) {
+    this.setState({ addedWorkflow: file }, () => {
+      this.update();
+    });
+  }
+
+  removeWorkflow() {
+    this.setState({ addedWorkflow: undefined }, this.update);
+  }
+  update() {
+    this.props.onUpdate({ value: this.state.addedWorkflow });
   }
 }
 WonWorkflowPicker.propTypes = {
-  initialValue: PropTypes.string,
+  initialValue: PropTypes.object,
   detail: PropTypes.object,
   onUpdate: PropTypes.func.isRequired,
 };
