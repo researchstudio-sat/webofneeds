b10200b74e2aadadebff810b564ec23b01a19511
quasarchimaere <fabian.suda@researchstudio.at>
2019-10-25 21:07:32 +0200

    All Atoms with BuddySocket available in add-buddy

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/add-buddy.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/add-buddy.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { get } from "../utils.js";
+import * as atomUtils from "../redux/utils/atom-utils.js";
+import * as generalSelectors from "../redux/selectors/general-selectors.js";
+import WonAtomHeader from "../components/atom-header.jsx";
 import PropTypes from "prop-types";
 
 import "~/style/_add-buddy.scss";
 
-// TODO: Action Label in desktopView
+const mapStateToProps = (state, ownProps) => {
+  const ownedAtomsWithBuddySocket = generalSelectors.getOwnedAtomsWithBuddySocket(
+    state
+  );
+
+  return {
+    ownedAtomsWithBuddySocketArray:
+      ownedAtomsWithBuddySocket &&
+      ownedAtomsWithBuddySocket
+        .filter(atom => atomUtils.isActive(atom))
+        .filter(atom => get(atom, "uri") !== ownProps.atomUri)
+        .toArray(),
+    atomUri: ownProps.atomUri,
+  };
+};
+
 // TODO: Change Icon maybe PersonIcon with a Plus
 // TODO: Immediate Action if only one Persona is owned by the User -> Open Modal Dialog to ask
 // TODO: Display Possible Personas and Personas that are already Buddies or have pending requests
-export default class WonAddBuddy extends React.Component {
+class WonAddBuddy extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,6 +37,22 @@ export default class WonAddBuddy extends React.Component {
   }
 
   render() {
+    let buddySelectionElement =
+      this.props.ownedAtomsWithBuddySocketArray &&
+      this.props.ownedAtomsWithBuddySocketArray.map(atom => {
+        return (
+          <div
+            className="add-buddy__addbuddymenu__content__selection__buddy"
+            key={get(atom, "uri")}
+          >
+            <WonAtomHeader atomUri={get(atom, "uri")} hideTimestamp={true} />
+            <svg className="add-buddy__addbuddymenu__content__selection__buddy__status">
+              <use xlinkHref="#ico36_plus_circle" href="#ico36_plus_circle" />
+            </svg>
+          </div>
+        );
+      });
+
     const dropdownElement = this.state.contextMenuOpen && (
       <div className="add-buddy__addbuddymenu">
         <div className="add-buddy__addbuddymenu__content">
@@ -35,7 +70,7 @@ export default class WonAddBuddy extends React.Component {
             </div>
           </div>
           <div className="add-buddy__addbuddymenu__content__selection">
-            TODO Buddy Selection
+            {buddySelectionElement}
           </div>
         </div>
       </div>
@@ -80,4 +115,7 @@ export default class WonAddBuddy extends React.Component {
 WonAddBuddy.propTypes = {
   atomUri: PropTypes.string.isRequired,
   className: PropTypes.string,
+  ownedAtomsWithBuddySocketArray: PropTypes.arrayOf(PropTypes.object),
 };
+
+export default connect(mapStateToProps)(WonAddBuddy);
