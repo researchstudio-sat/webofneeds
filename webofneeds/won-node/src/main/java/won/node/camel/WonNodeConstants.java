71b3b191f8d205cf5655d9b1f26736eee9366768
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2019-11-06 21:15:49 +0100

    Add node constants

---
+++ webofneeds/won-node/src/main/java/won/node/camel/WonNodeConstants.java
@@ -0,0 +1,9 @@
+package won.node.camel;
+
+public class WonNodeConstants {
+    public static final String FROM_OWNER_QUEUENAME = "OwnerProtocol.in";
+    public static final String FROM_NODE_QUEUENAME = "AtomProtocol.in";
+    public static final String FROM_MATCHER_QUEUENAME = "MatcherProtocol.in";
+    public static final String TO_OWNER_QUEUENAME = "OwnerProtocolOut";
+    public static final String TO_MATCHER_TOPIC = "MatcherProtocol.Out.Atom";
+}
