cc9dc569a6f3bc7faac8b66728399112cae885fc
quasarchimaere <sudafabian@gmail.com>
2019-11-26 08:46:06 +0100

    Fixes dropping of index -> drop index does not need a UNIQUE keyword Drops same name index on owner webapp too -> assumption, it is also probably necessary to check all the indices to begin with, as some of the unique ones seem rather faulty now

--- webofneeds/won-node/src/main/resources/db/migration/V9_0__message_refactoring.sql
+++ webofneeds/won-node/src/main/resources/db/migration/V9_0__message_refactoring.sql
@@ -1,7 +1,7 @@
 ALTER TABLE atom ALTER COLUMN state SET NOT NULL;
 
 -- DatasetHolder is no longer unique for MessageEvents 
-DROP UNIQUE INDEX IDX_ME_UNIQUE_DATASETHOLDER_ID;
+DROP INDEX IDX_ME_UNIQUE_DATASETHOLDER_ID;
 
 /* TODO
  *  [x] change constraint from MessageEvent @UniqueConstraint(name = "IDX_ME_UNIQUE_MESSAGE_URI", columnNames = { "messageURI", "parentURI" }),
---
+++ webofneeds/won-owner/src/main/resources/db/migration/V13_0__message_refactoring.sql
@@ -0,0 +1,2 @@
+-- DatasetHolder is no longer unique for MessageEvents
+DROP INDEX IDX_ME_UNIQUE_DATASETHOLDER_ID;
