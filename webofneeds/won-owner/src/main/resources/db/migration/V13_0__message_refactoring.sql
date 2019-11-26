d3389822dbfd5e3e5d1606f7ed2779ce993c2561
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2019-11-26 15:06:42 +0100

    Add missing field for owner db

--- webofneeds/won-owner/src/main/resources/db/migration/V13_0__message_refactoring.sql
+++ webofneeds/won-owner/src/main/resources/db/migration/V13_0__message_refactoring.sql
@@ -1,2 +1,4 @@
 -- DatasetHolder is no longer unique for MessageEvents
 DROP INDEX IF EXISTS IDX_ME_UNIQUE_DATASETHOLDER_ID;
+-- add the field for the serialized set of unconfirmed message uris
+ALTER TABLE message_container ADD COLUMN unconfirmed bytea NOT NULL;
