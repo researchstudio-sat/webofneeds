c25ffc0a45f8129ae2ae542eb2694e9823c06a9b
quasarchimaere <sudafabian@gmail.com>
2019-11-26 09:01:34 +0100

    Adds IF EXISTS to index dropping

--- webofneeds/won-node/src/main/resources/db/migration/V9_0__message_refactoring.sql
+++ webofneeds/won-node/src/main/resources/db/migration/V9_0__message_refactoring.sql
@@ -1,26 +1,26 @@
 ALTER TABLE atom ALTER COLUMN state SET NOT NULL;
 
 -- DatasetHolder is no longer unique for MessageEvents 
-DROP INDEX IDX_ME_UNIQUE_DATASETHOLDER_ID;
+DROP INDEX IF EXISTS IDX_ME_UNIQUE_DATASETHOLDER_ID;
 
 /* TODO
  *  [x] change constraint from MessageEvent @UniqueConstraint(name = "IDX_ME_UNIQUE_MESSAGE_URI", columnNames = { "messageURI", "parentURI" }),
  *  [ ] manyToOne rel message->datasetholder
  */
 -- message_event is now only unique with respect to messageuri and parenturi
-DROP INDEX  IDX_ME_UNIQUE_MESSAGE_URI;
+DROP INDEX IF EXISTS IDX_ME_UNIQUE_MESSAGE_URI;
 CREATE UNIQUE INDEX IDX_ME_UNIQUE_MESSAGE_URI_PER_PARENT ON message_event (messageuri, parenturi);
 
 -- drop correspondingremotemessageuri
 ALTER TABLE message_event DROP column correspondingremotemessageuri;
-DROP INDEX IDX_ME_UNIQUE_CORREXPONDING_REMOTE_MESSAGE_URI;
-DROP INDEX IDX_ME_INNERMOST_MESSAGE_URI_RECIPIENT_ATOM_URI;
+DROP INDEX IF EXISTS IDX_ME_UNIQUE_CORREXPONDING_REMOTE_MESSAGE_URI;
+DROP INDEX IF EXISTS IDX_ME_INNERMOST_MESSAGE_URI_RECIPIENT_ATOM_URI;
 CREATE INDEX IDX_ME_INNERMOST_MESSAGE_URI_RECIPIENT_ATOM_URI on message_event (messageuri, recipientatomuri, innermostmessageuri);
 
 -- drop innermostmessageuri
 ALTER TABLE message_event DROP column innermostmessageuri;
-DROP INDEX IDX_ME_INNERMOST_MESSAGE_URI_RECIPIENT_ATOM_URI;
-CREATE INDEX IDX_ME_RECIPIENT_ATOM_URI on message_event(messageuri, recipientatomuri) 
+DROP INDEX IF EXISTS IDX_ME_INNERMOST_MESSAGE_URI_RECIPIENT_ATOM_URI;
+CREATE INDEX IDX_ME_RECIPIENT_ATOM_URI on message_event(messageuri, recipientatomuri);
 
 -- add special fields for response information (respondingToURI, responseContainerURI), so we can make sure we 
 -- store only one response from a container
