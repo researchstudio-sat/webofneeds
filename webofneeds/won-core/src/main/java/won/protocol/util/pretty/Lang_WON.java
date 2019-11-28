7ae3e461bf9ba4ae5a37153c84ffcaf51bc8d5dd
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2019-11-28 21:24:48 +0100

    Add init method

--- webofneeds/won-core/src/main/java/won/protocol/util/pretty/Lang_WON.java
+++ webofneeds/won-core/src/main/java/won/protocol/util/pretty/Lang_WON.java
@@ -3,8 +3,17 @@ package won.protocol.util.pretty;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.riot.RDFFormatVariant;
+import org.apache.jena.riot.RDFWriterRegistry;
 
 public class Lang_WON {
+    static {
+        RDFWriterRegistry.register(Lang_WON.TRIG_WON_CONVERSATION,
+                        new ConversationDatasetWriterFactory());
+    }
     public static final RDFFormat TRIG_WON_CONVERSATION = new RDFFormat(Lang.TRIG,
                     new RDFFormatVariant("TriG_WoN_Conversation"));
+
+    public static void init() {
+        // just give the client the chance to trigger the static initializer
+    }
 }
