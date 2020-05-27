3bf3695beefbffb6adc08cd57813feb902bf1cba
quasarchimaere <sudafabian@gmail.com>
2020-05-27 15:03:22 +0200

    Changes some viewer components to functions

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/details/viewer/title-viewer.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/details/viewer/title-viewer.jsx
@@ -3,28 +3,26 @@ import React from "react";
 import "~/style/_title-viewer.scss";
 import PropTypes from "prop-types";
 
-export default class WonTitleViewer extends React.Component {
-  render() {
-    const icon = this.props.detail.icon && (
-      <svg className="titlev__header__icon">
-        <use xlinkHref={this.props.detail.icon} href={this.props.detail.icon} />
-      </svg>
-    );
+export default function WonTitleViewer({ content, detail, className }) {
+  const icon = detail.icon && (
+    <svg className="titlev__header__icon">
+      <use xlinkHref={detail.icon} href={detail.icon} />
+    </svg>
+  );
 
-    const label = this.props.detail.icon && (
-      <span className="titlev__header__label">{this.props.detail.label}</span>
-    );
+  const label = detail.icon && (
+    <span className="titlev__header__label">{detail.label}</span>
+  );
 
-    return (
-      <won-title-viewer class={this.props.className}>
-        <div className="titlev__header">
-          {icon}
-          {label}
-        </div>
-        <div className="titlev__content">{this.props.content}</div>
-      </won-title-viewer>
-    );
-  }
+  return (
+    <won-title-viewer class={className}>
+      <div className="titlev__header">
+        {icon}
+        {label}
+      </div>
+      <div className="titlev__content">{content}</div>
+    </won-title-viewer>
+  );
 }
 WonTitleViewer.propTypes = {
   detail: PropTypes.object,
