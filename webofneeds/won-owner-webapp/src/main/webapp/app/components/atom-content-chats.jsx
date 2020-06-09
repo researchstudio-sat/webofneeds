5eaf64c818218fc6e8f82709ae2ca0859dad2638
quasarchimaere <sudafabian@gmail.com>
2020-06-09 13:44:32 +0200

    Adds reactions to chats-view

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/atom-content-chats.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/atom-content-chats.jsx
@@ -2,22 +2,28 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
+import vocab from "../service/vocab.js";
 import {
   get,
   generateLink,
   sortByDate,
   filterConnectionsBySearchValue,
+  filterAtomsBySearchValue,
 } from "../utils.js";
 import * as atomUtils from "../redux/utils/atom-utils.js";
 import * as connectionUtils from "../redux/utils/connection-utils.js";
+import * as generalSelectors from "../redux/selectors/general-selectors.js";
+import * as accountUtils from "../redux/utils/account-utils.js";
+import * as wonLabelUtils from "../won-label-utils.js";
+
 import WonConnectionSelectionItem from "./connection-selection-item.jsx";
 import WonTitlePicker from "./details/picker/title-picker.jsx";
+import WonSocketAddAtom from "./socket-add-atom.jsx";
 
 import ico16_arrow_down from "~/images/won-icons/ico16_arrow_down.svg";
+import ico36_plus from "~/images/won-icons/ico36_plus.svg";
 
 import "~/style/_atom-content-chats.scss";
-import * as generalSelectors from "../redux/selectors/general-selectors";
-import * as accountUtils from "../redux/utils/account-utils";
 
 export default function AtomContentChats({ atom }) {
   const history = useHistory();
@@ -29,6 +35,7 @@ export default function AtomContentChats({ atom }) {
   const [showSuggestions, toggleSuggestions] = useState(false);
   const [showClosed, toggleClosed] = useState(false);
   const [searchText, setSearchText] = useState({ value: "" });
+  const [showAddPicker, toggleAddPicker] = useState(false);
 
   const storedAtoms = useSelector(generalSelectors.getAtoms);
   const ownedConnectionsToSocketUri = useSelector(
@@ -37,6 +44,13 @@ export default function AtomContentChats({ atom }) {
       generalSelectors.getAllOwnedConnectionsWithTargetSocketUri(chatSocketUri)(
         state
       )
+  );
+
+  const filteredStoredAtoms = filterAtomsBySearchValue(storedAtoms, searchText);
+
+  const reactions = atomUtils.getReactions(
+    atom,
+    vocab.CHAT.ChatSocketCompacted
   );
 
   // If the atom is not owned, we will show Our ChatConnections to this atom instead
@@ -114,6 +128,31 @@ export default function AtomContentChats({ atom }) {
     );
   }
 
+  function generateAddItem() {
+    if (reactions) {
+      const onClick = () => {
+        toggleAddPicker(!showAddPicker);
+      };
+
+      return (
+        <div className="socketadd clickable" onClick={onClick}>
+          <svg className="socketadd__icon" title="Create a new post">
+            <use xlinkHref={ico36_plus} href={ico36_plus} />
+          </svg>
+          <span className="socketadd__label">
+            {isAtomOwned
+              ? `Add ${wonLabelUtils.getSocketItemLabels(reactions.keys())}`
+              : `Connect ${wonLabelUtils.getSocketItemLabels(
+                  reactions.keys()
+                )}`}
+          </span>
+        </div>
+      );
+    } else {
+      return undefined;
+    }
+  }
+
   return (
     <won-atom-content-chats>
       <div className="acc__search">
@@ -123,22 +162,33 @@ export default function AtomContentChats({ atom }) {
           detail={{ placeholder: "Filter Chats" }}
         />
       </div>
-      {activeChatConnections.size > 0 ? (
-        <div className="acc__segment">
-          <div className="acc__segment__content borderTop">
-            {generateConnectionItems(activeChatConnections)}
-          </div>
-        </div>
-      ) : (
-        <div className="acc__segment">
-          <div className="acc__segment__content">
-            <div className="acc__empty">
-              {searchText.value.trim().length > 0 ? "No Results" : "No Chats"}
+      {!showAddPicker ? (
+        activeChatConnections.size > 0 || reactions ? (
+          <div className="acc__segment">
+            <div className="acc__segment__content borderTop">
+              {generateConnectionItems(activeChatConnections)}
+              {generateAddItem()}
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="acc__segment">
+            <div className="acc__segment__content">
+              <div className="acc__empty">
+                {searchText.value.trim().length > 0
+                  ? `No ${wonLabelUtils.getSocketItemsLabel(
+                      vocab.CHAT.ChatSocketCompacted
+                    )} found for '${searchText.value.trim()}'`
+                  : `No ${wonLabelUtils.getSocketItemsLabel(
+                      vocab.CHAT.ChatSocketCompacted
+                    )}`}
+              </div>
+            </div>
+          </div>
+        )
+      ) : (
+        undefined
       )}
-      {suggestedChatConnections.size > 0 ? (
+      {!showAddPicker && suggestedChatConnections.size > 0 ? (
         <div className="acc__segment">
           {generateHeaderItem(
             "Suggestions",
@@ -157,7 +207,7 @@ export default function AtomContentChats({ atom }) {
       ) : (
         undefined
       )}
-      {closedChatConnections.size > 0 ? (
+      {!showAddPicker && closedChatConnections.size > 0 ? (
         <div className="acc__segment">
           {generateHeaderItem(
             "Closed",
@@ -173,6 +223,18 @@ export default function AtomContentChats({ atom }) {
             undefined
           )}
         </div>
+      ) : (
+        undefined
+      )}
+      {showAddPicker ? (
+        <WonSocketAddAtom
+          addToAtom={atom}
+          storedAtoms={filteredStoredAtoms}
+          addToSocketType={vocab.CHAT.ChatSocketCompacted}
+          reactions={reactions}
+          accountState={accountState}
+          onClose={() => toggleAddPicker(!showAddPicker)}
+        />
       ) : (
         undefined
       )}
