b88c9dad4c4ab7ff05f98a06f5f274a28ee14e9f
quasarchimaere <sudafabian@gmail.com>
2020-07-14 12:31:49 +0200

    - Adapt afterparty/breakfast/sightseeing social usecases to plans/interests

--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-interests.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-interests.js
@@ -1,7 +1,10 @@
+import { breakfastInterest, breakfastPlan } from "./uc-breakfast.js";
+import { afterpartyInterest, afterpartyPlan } from "./uc-afterparty.js";
 import { cyclingInterest, cyclingPlan } from "./uc-cycling.js";
 import { lunchInterest, lunchPlan } from "./uc-lunch.js";
 import { genericInterest, genericPlan } from "./uc-interest.js";
 import { pokemonInterest, pokemonGoRaid } from "./uc-pokemon.js";
+import { sightseeingInterest, sightseeingPlan } from "./uc-sightseeing.js";
 
 import ico36_detail_interests from "../../images/won-icons/ico36_detail_interests.svg";
 import ico36_detail_datetime from "~/images/won-icons/ico36_detail_datetime.svg";
@@ -11,6 +14,9 @@ export const interestsGroup = {
   label: "Interests",
   icon: ico36_detail_interests,
   subItems: {
+    sightseeingInterest: sightseeingInterest,
+    afterpartyInterest: afterpartyInterest,
+    breakfastInterest: breakfastInterest,
     lunchInterest: lunchInterest,
     cyclingInterest: cyclingInterest,
     pokemonInterest: pokemonInterest,
@@ -23,6 +29,9 @@ export const plansGroup = {
   label: "Plans",
   icon: ico36_detail_datetime,
   subItems: {
+    sightseeingPlan: sightseeingPlan,
+    afterpartyPlan: afterpartyPlan,
+    breakfastPlan: breakfastPlan,
     lunchPlan: lunchPlan,
     cyclingPlan: cyclingPlan,
     pokemonGoRaid: pokemonGoRaid,
--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-social.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-social.js
@@ -1,6 +1,3 @@
-import { getBreakfast } from "./uc-get-breakfast";
-import { afterparty } from "./uc-afterparty";
-import { sightseeing } from "./uc-sightseeing";
 import { getToKnow } from "./uc-get-to-know.js";
 import { complain } from "./uc-complain";
 import { handleComplaint } from "./uc-handle-complaint";
@@ -12,11 +9,8 @@ export const socialGroup = {
   label: "Social",
   icon: ico36_uc_find_people,
   subItems: {
-    getBreakfast: getBreakfast,
-    afterparty: afterparty,
     complain: complain,
     getToKnow: getToKnow,
-    sightseeing: sightseeing,
     handleComplaint: handleComplaint,
   },
 };
