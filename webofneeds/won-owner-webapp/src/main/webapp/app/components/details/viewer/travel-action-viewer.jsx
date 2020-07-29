569a598890ad549b1245ecb507fdc6fb4c565f09
quasarchimaere <sudafabian@gmail.com>
2020-07-29 13:19:42 +0200

    Refactor viewers to function components

--- webofneeds/won-owner-webapp/src/main/webapp/app/components/details/viewer/travel-action-viewer.jsx
+++ webofneeds/won-owner-webapp/src/main/webapp/app/components/details/viewer/travel-action-viewer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "~/style/_travel-action-viewer.scss";
 import ico_filter_map from "~/images/won-icons/ico-filter_map.svg";
@@ -7,102 +7,87 @@ import PropTypes from "prop-types";
 import { get } from "../../../utils.js";
 import WonAtomMap from "../../atom-map";
 
-export default class WonTravelActionViewer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locationExpanded: false,
-    };
-  }
+export default function WonTravelActionViewer({ detail, content, className }) {
+  const [locationExpanded, toggleLocationExpanded] = useState(false);
 
-  toggleLocation() {
-    this.setState({ locationExpanded: !this.state.locationExpanded });
-  }
+  const icon = detail.icon && (
+    <svg className="rv__header__icon">
+      <use xlinkHref={detail.icon} href={detail.icon} />
+    </svg>
+  );
 
-  render() {
-    const icon = this.props.detail.icon && (
-      <svg className="rv__header__icon">
-        <use xlinkHref={this.props.detail.icon} href={this.props.detail.icon} />
-      </svg>
-    );
+  const label = detail.icon && (
+    <span className="rv__header__label">{detail.label}</span>
+  );
 
-    const label = this.props.detail.icon && (
-      <span className="rv__header__label">{this.props.detail.label}</span>
-    );
-
-    const fromAddress = get(this.props.content, "fromAddress");
-    const toAddress = get(this.props.content, "toAddress");
-    const addressElement =
-      fromAddress || toAddress ? (
-        <div
-          className="rv__content__text clickable"
-          onClick={() => this.toggleLocation()}
+  const fromAddress = get(content, "fromAddress");
+  const toAddress = get(content, "toAddress");
+  const addressElement =
+    fromAddress || toAddress ? (
+      <div
+        className="rv__content__text clickable"
+        onClick={() => toggleLocationExpanded(!locationExpanded)}
+      >
+        <div>
+          {fromAddress ? (
+            <span>
+              <strong>From: </strong>
+              {fromAddress}
+            </span>
+          ) : (
+            undefined
+          )}
+          <br />
+          {toAddress ? (
+            <span>
+              <strong>To: </strong>
+              {toAddress}
+            </span>
+          ) : (
+            undefined
+          )}
+        </div>
+        <svg className="rv__content__text__carret">
+          <use xlinkHref={ico_filter_map} href={ico_filter_map} />
+        </svg>
+        <svg
+          className={
+            "rv__content__text__carret " +
+            (locationExpanded
+              ? " rv__content__text__carret--expanded "
+              : " rv__content__text__carret--collapsed ")
+          }
         >
-          <div>
-            {fromAddress ? (
-              <span>
-                <strong>From: </strong>
-                {fromAddress}
-              </span>
-            ) : (
-              undefined
-            )}
-            <br />
-            {toAddress ? (
-              <span>
-                <strong>To: </strong>
-                {toAddress}
-              </span>
-            ) : (
-              undefined
-            )}
-          </div>
-          <svg className="rv__content__text__carret">
-            <use xlinkHref={ico_filter_map} href={ico_filter_map} />
-          </svg>
-          <svg
-            className={
-              "rv__content__text__carret " +
-              (this.state.locationExpanded
-                ? " rv__content__text__carret--expanded "
-                : " rv__content__text__carret--collapsed ")
-            }
-          >
-            <use xlinkHref={ico16_arrow_down} href={ico16_arrow_down} />
-          </svg>
-        </div>
-      ) : (
-        undefined
-      );
-
-    const map =
-      this.props.content &&
-      this.state.locationExpanded &&
-      (get(this.props.content, "fromLocation") ||
-        get(this.props.content, "toLocation")) ? (
-        <WonAtomMap
-          locations={[
-            get(this.props.content, "fromLocation"),
-            get(this.props.content, "toLocation"),
-          ]}
-        />
-      ) : (
-        undefined
-      );
-
-    return (
-      <won-travel-action-viewer class={this.props.className}>
-        <div className="rv__header">
-          {icon}
-          {label}
-        </div>
-        <div className="rv__content">
-          {addressElement}
-          {map}
-        </div>
-      </won-travel-action-viewer>
+          <use xlinkHref={ico16_arrow_down} href={ico16_arrow_down} />
+        </svg>
+      </div>
+    ) : (
+      undefined
     );
-  }
+
+  const map =
+    content &&
+    locationExpanded &&
+    (get(content, "fromLocation") || get(content, "toLocation")) ? (
+      <WonAtomMap
+        locations={[get(content, "fromLocation"), get(content, "toLocation")]}
+      />
+    ) : (
+      undefined
+    );
+
+  return (
+    <won-travel-action-viewer class={className}>
+      <div className="rv__header">
+        {icon}
+        {label}
+      </div>
+      <div className="rv__content">
+        {addressElement}
+        {map}
+      </div>
+    </won-travel-action-viewer>
+  );
 }
 WonTravelActionViewer.propTypes = {
   detail: PropTypes.object,
