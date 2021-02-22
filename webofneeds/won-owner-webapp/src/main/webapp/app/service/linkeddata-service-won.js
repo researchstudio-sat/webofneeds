18b5dcbbbb7a24eb05a7df8e934a22ec300a0b2a
quasarchimaere <fabian.suda@researchstudio.at>
2021-02-22 12:14:50 +0100

    Move most method processing to ld-worker

--- webofneeds/won-owner-webapp/src/main/webapp/app/service/linkeddata-service-won.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/service/linkeddata-service-won.js
@@ -17,11 +17,10 @@
 /**
  * Created by fkleedorfer on 05.09.2014.
  */
-import { is, getIn, extractAtomUriBySocketUri } from "../utils.js";
+import { is } from "../utils.js";
 
 import jsonld from "jsonld/dist/jsonld.min.js";
 import won from "./won.js";
-import vocab from "./vocab.js";
 
 import worker from "workerize-loader?[name].[contenthash:8]!../../ld-worker.js";
 
@@ -34,52 +33,7 @@ import worker from "workerize-loader?[name].[contenthash:8]!../../ld-worker.js";
   won.fetchAtom = (atomUri, requestCredentials) => {
     const ldWorker = worker();
 
-    return ldWorker
-      .fetchJsonLdDataset(atomUri, requestCredentials)
-      .then(jsonLdData =>
-        Promise.all([
-          jsonld.frame(jsonLdData, {
-            "@id": atomUri, // start the framing from this uri. Otherwise will generate all possible nesting-variants.
-            "@context": vocab.defaultContext,
-            "@embed": "@always",
-          }),
-          jsonld.frame(jsonLdData, {
-            "@type": vocab.AUTH.AuthorizationCompacted,
-            "@context": vocab.defaultContext,
-            "@embed": "@always",
-          }),
-        ])
-      )
-      .then(([atomJsonLd, authsJsonLd]) => {
-        // usually the atom-data will be in a single object in the '@graph' array.
-        // We can flatten this and still have valid json-ld
-        const flattenedAtomJsonLd = getIn(atomJsonLd, ["@graph", 0])
-          ? getIn(atomJsonLd, ["@graph", 0])
-          : atomJsonLd;
-        flattenedAtomJsonLd["@context"] = atomJsonLd["@context"]; // keep context
-        if (
-          !flattenedAtomJsonLd ||
-          getIn(flattenedAtomJsonLd, ["@graph", "length"]) === 0
-        ) {
-          console.error(
-            "Received empty graph ",
-            atomJsonLd,
-            " for atom ",
-            atomUri
-          );
-          return {
-            atom: { "@context": flattenedAtomJsonLd["@context"] },
-            auth: authsJsonLd,
-          };
-        }
-        return { atom: flattenedAtomJsonLd, auth: authsJsonLd };
-      })
-      .catch(e => {
-        const msg = "Failed to get atom " + atomUri + ".";
-        e.message += msg;
-        console.error(e.message);
-        throw e;
-      });
+    return ldWorker.Atom(atomUri, requestCredentials);
   };
 
   won.validateEnvelopeDataForAtom = atomUri => {
@@ -119,55 +73,10 @@ import worker from "workerize-loader?[name].[contenthash:8]!../../ld-worker.js";
     requestCredentials
   ) => {
     const ldWorker = worker();
-    return ldWorker
-      .fetchJsonLdDataset(connectionContainerUri, requestCredentials)
-      .then(jsonLdData =>
-        jsonld.frame(jsonLdData, {
-          "@id": connectionContainerUri,
-          "@context": vocab.defaultContext,
-          "@embed": "@always",
-        })
-      )
-      .then(
-        connectionContainer =>
-          connectionContainer && connectionContainer["@id"]
-            ? jsonld.frame(connectionContainer, {
-                "@id": connectionContainer["@id"],
-                "@embed": "@always",
-              })
-            : undefined
-      )
-      .then(connectionContainerFramed => {
-        if (connectionContainerFramed) {
-          const parseJsonLdConnection = jsonLdConnection => ({
-            uri: jsonLdConnection["@id"],
-            type: jsonLdConnection["@type"],
-            modified:
-              jsonLdConnection["http://purl.org/dc/terms/modified"]["@value"],
-            socket: jsonLdConnection[vocab.WON.socket]["@id"],
-            connectionState: jsonLdConnection[vocab.WON.connectionState]["@id"],
-            sourceAtom: jsonLdConnection[vocab.WON.sourceAtom]["@id"],
-            targetAtom: jsonLdConnection[vocab.WON.targetAtom]["@id"],
-            targetSocket: jsonLdConnection[vocab.WON.targetSocket]["@id"],
-            hasEvents: [],
-          });
-          if (is("Array", connectionContainerFramed[vocab.RDFS.member])) {
-            return connectionContainerFramed[vocab.RDFS.member].map(
-              parseJsonLdConnection
-            );
-          } else if (connectionContainerFramed[vocab.RDFS.member]) {
-            return [
-              parseJsonLdConnection(
-                connectionContainerFramed[vocab.RDFS.member]
-              ),
-            ];
-          } else {
-            return [];
-          }
-        } else {
-          return [];
-        }
-      });
+    return ldWorker.fetchConnectionUrisWithStateByAtomUri(
+      connectionContainerUri,
+      requestCredentials
+    );
   };
 
   /**
@@ -190,50 +99,7 @@ import worker from "workerize-loader?[name].[contenthash:8]!../../ld-worker.js";
     }
 
     const ldWorker = worker();
-    return ldWorker
-      .fetchJsonLdDataset(connectionUri, fetchParams)
-      .then(jsonLdData =>
-        jsonld.frame(jsonLdData, {
-          "@id": connectionUri,
-          "@context": vocab.defaultContext,
-          "@embed": "@always",
-        })
-      )
-      .then(jsonLdConnection => jsonld.expand(jsonLdConnection))
-      .then(jsonLdConnection => {
-        const connectionContentGraph = jsonLdConnection[0];
-        return {
-          uri: connectionContentGraph["@id"],
-          type: connectionContentGraph["@type"][0],
-          modified:
-            connectionContentGraph["http://purl.org/dc/terms/modified"][0][
-              "@value"
-            ],
-          messageContainer:
-            connectionContentGraph[vocab.WON.messageContainer][0]["@id"],
-          socket: connectionContentGraph[vocab.WON.socket][0]["@id"],
-          wonNode: connectionContentGraph[vocab.WON.wonNode][0]["@id"],
-          connectionState:
-            connectionContentGraph[vocab.WON.connectionState][0]["@id"],
-          sourceAtom: connectionContentGraph[vocab.WON.sourceAtom][0]["@id"],
-          targetAtom: connectionContentGraph[vocab.WON.targetAtom][0]["@id"],
-          targetSocket:
-            connectionContentGraph[vocab.WON.targetSocket][0]["@id"],
-          previousConnectionState:
-            connectionContentGraph[vocab.WON.previousConnectionState] &&
-            connectionContentGraph[vocab.WON.previousConnectionState][0]["@id"],
-          targetConnection:
-            connectionContentGraph[vocab.WON.targetConnection] &&
-            connectionContentGraph[vocab.WON.targetConnection][0]["@id"],
-          hasEvents: [],
-        };
-      })
-      .catch(e => {
-        const msg = "Failed to get connection " + connectionUri + ".";
-        e.message += msg;
-        console.error(e.message);
-        throw e;
-      });
+    return ldWorker.fetchConnection(connectionUri, fetchParams);
   };
 
   /**
@@ -260,21 +126,14 @@ import worker from "workerize-loader?[name].[contenthash:8]!../../ld-worker.js";
       );
     }
 
-    const connectionContainerPromise = messageContainerUri
-      ? Promise.resolve(messageContainerUri)
-      : won
-          .fetchConnection(connectionUri)
-          .then(connection => connection.messageContainer);
-
-    return connectionContainerPromise
-      .then(messageContainerUri => {
-        const ldWorker = worker();
-        return ldWorker.fetchJsonLdDataset(
-          messageContainerUri,
-          fetchParams,
-          true
-        );
-      })
+    const ldWorker = worker();
+    //TODO: MOVE THE SUBSEQUENT PROMISES TO ld-worker once we figured out how to parse a message within a worker with wonMessageFromJsonLd
+    return ldWorker
+      .fetchMessagesOfConnection(
+        connectionUri,
+        messageContainerUri,
+        fetchParams
+      )
       .then(responseObject =>
         jsonld.expand(responseObject.jsonLdData).then(jsonLdData => {
           const messages = {};
@@ -352,23 +211,12 @@ import worker from "workerize-loader?[name].[contenthash:8]!../../ld-worker.js";
       );
     }
 
-    fetchParams.socket = senderSocketUri;
-    fetchParams.targetSocket = targetSocketUri;
-
     const ldWorker = worker();
-    return ldWorker
-      .fetchJsonLdDataset(
-        extractAtomUriBySocketUri(senderSocketUri) + "/c",
-        fetchParams
-      )
-      .then(jsonLdData =>
-        jsonld.frame(jsonLdData, {
-          "@type": vocab.WON.Connection,
-          "@context": vocab.defaultContext,
-          "@embed": "@always",
-        })
-      )
-      .then(jsonResp => jsonResp && jsonResp["@id"]);
+    return ldWorker.fetchConnectionUrisBySocket(
+      senderSocketUri,
+      targetSocketUri,
+      fetchParams
+    );
   };
 
   /**
@@ -397,30 +245,11 @@ import worker from "workerize-loader?[name].[contenthash:8]!../../ld-worker.js";
       );
     }
 
-    fetchParams.socket = senderSocketUri;
-    fetchParams.targetSocket = targetSocketUri;
-
     const ldWorker = worker();
-    return (
-      ldWorker
-        .fetchJsonLdDataset(
-          extractAtomUriBySocketUri(senderSocketUri) + "/c",
-          fetchParams
-        )
-        .then(jsonLdData =>
-          jsonld.frame(jsonLdData, {
-            "@type": vocab.WON.Connection,
-            "@context": vocab.defaultContext,
-            "@embed": "@always",
-          })
-        )
-        //add the eventUris
-        .then(jsonResp => jsonResp && jsonResp["@id"])
-        .then(connUri =>
-          won.fetchConnection(connUri, {
-            requesterWebId: fetchParams.requesterWebId,
-          })
-        )
+    return ldWorker.fetchConnectionBySocket(
+      senderSocketUri,
+      targetSocketUri,
+      fetchParams
     );
   };
 })();
