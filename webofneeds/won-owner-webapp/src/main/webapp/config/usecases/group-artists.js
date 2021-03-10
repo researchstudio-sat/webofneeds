11172df48d09b755964aa20dfa832fd955ffb705
maxstolze <maximilian.stolze@researchstudio.at>
2021-03-10 14:45:41 +0100

    Own UC for GroupChat to reuse

--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-artists.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/group-artists.js
@@ -1,6 +1,5 @@
 import { musicianSearch } from "./uc-musician-search.js";
 import { bandSearch } from "./uc-band-search.js";
-import { serviceOffer } from "./uc-service-offer.js";
 import { rehearsalRoomOffer } from "./uc-rehearsal-room-offer.js";
 import { rehearsalRoomSearch } from "./uc-rehearsal-room-search.js";
 
@@ -13,7 +12,6 @@ export const artistGroup = {
   subItems: {
     rehearsalRoomOffer: rehearsalRoomOffer,
     bandSearch: bandSearch,
-    serviceOffer: serviceOffer,
     rehearsalRoomSearch: rehearsalRoomSearch,
     musicianSearch: musicianSearch,
   },
