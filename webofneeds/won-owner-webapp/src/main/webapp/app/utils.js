65246fd75ffd90728c0e518591f057d0356c303f
quasarchimaere <sudafabian@gmail.com>
2021-03-22 08:16:45 +0100

    Fixes #3367 jwttoken parsing

--- webofneeds/won-owner-webapp/src/main/webapp/app/utils.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/utils.js
@@ -665,16 +665,19 @@ export function parseWorkerError(error) {
 }
 
 export function parseJwtToken(token) {
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  const jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function(c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
+  if (token) {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function(c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  }
 
-  return JSON.parse(jsonPayload);
+  return undefined;
 }
