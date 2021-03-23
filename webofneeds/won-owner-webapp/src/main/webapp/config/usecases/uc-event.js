feada6907b68ac2188d64edd2adf99598911d261
quasarchimaere <sudafabian@gmail.com>
2021-03-23 13:56:27 +0100

    Adds project usecase to organizer reaction

--- webofneeds/won-owner-webapp/src/main/webapp/config/usecases/uc-event.js
+++ webofneeds/won-owner-webapp/src/main/webapp/config/usecases/uc-event.js
@@ -50,7 +50,7 @@ export const event = {
     },
     [vocab.WXSCHEMA.EventInverseSocketCompacted]: {
       [vocab.WXSCHEMA.EventSocketCompacted]: {
-        useCaseIdentifier: ["organization", "persona"],
+        useCaseIdentifier: ["organization", "persona", "project"],
       },
     },
     [vocab.WXSCHEMA.AssociatedArticleSocketCompacted]: {
