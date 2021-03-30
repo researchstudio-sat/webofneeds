fa6834ff176ec688018ac104725919835793594b
maxstolze <maximilian.stolze@researchstudio.at>
2021-03-30 10:00:01 +0200

    React ServiceWorker [wip]

--- webofneeds/won-owner-webapp/src/main/webapp/app/app.js
+++ webofneeds/won-owner-webapp/src/main/webapp/app/app.js
@@ -53,7 +53,8 @@ import Immutable from "immutable";
 import { getQueryParams, generateLink } from "./utils.js";
 import * as accountUtils from "./redux/utils/account-utils.js";
 import * as processUtils from "./redux/utils/process-utils.js";
-import { runPushAgent } from "./push-agent";
+//import { runPushAgent } from "./push-agent";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import ico_loading_anim from "~/images/won-icons/ico_loading_anim.svg";
 import WonModalDialog from "~/app/components/modal-dialog";
 import * as viewSelectors from "~/app/redux/selectors/view-selectors";
@@ -90,7 +91,8 @@ runMessagingAgent(store);
 
 if (enableNotifications) {
   console.debug("runPushAgent");
-  runPushAgent(store); // TODO: runPushAgent used to get $ngRedux and ngRedux had a connect method attached -> not sure if that can be applied to the given store though
+  serviceWorkerRegistration.register();
+  //runPushAgent(store); // TODO: runPushAgent used to get $ngRedux and ngRedux had a connect method attached -> not sure if that can be applied to the given store though
 }
 
 // Initiate the initial load
